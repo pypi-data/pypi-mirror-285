dea84abdab180a384153dab4effece1859235e3b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-16 04:03:53 +0000

    Add package lython-basic/1.0.2/lython_basic-1.0.2.tar.gz

---
+++ packages/lython-basic/lython_basic-1.0.2.tar.gz/lython_basic-1.0.2/lython_basic/schema.sql
@@ -0,0 +1,19 @@
+DROP TABLE IF EXISTS user;
+DROP TABLE IF EXISTS ChatResponses;
+
+CREATE TABLE user (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    username TEXT UNIQUE NOT NULL,
+    password TEXT NOT NULL
+);
+
+CREATE TABLE ChatResponses (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    user_id INT NOT NULL,
+    input_prompt TEXT NOT NULL,
+    response_text TEXT NOT NULL,
+    model_used VARCHAR(50),
+    temperature DECIMAL,
+    max_tokens INT,
+    FOREIGN KEY (user_id) REFERENCES user(id)
+);
