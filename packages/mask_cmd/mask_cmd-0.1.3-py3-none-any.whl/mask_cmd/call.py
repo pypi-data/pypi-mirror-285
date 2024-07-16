4c0dc1fa61e0c5d23ad70824a7d02ef3912636a2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-16 16:06:45 +0000

    Add package mask_cmd/0.1.3/mask_cmd-0.1.3-py3-none-any.whl

---
+++ packages/mask_cmd/mask_cmd-0.1.3-py3-none-any.whl/mask_cmd/call.py
@@ -0,0 +1,13 @@
+from mask_plus.plus import plus
+from mask_divide.divide import divide
+from mask_times.times import multiply
+
+
+def add():
+    plus()
+
+def cut():
+    divide()
+
+def duplicate():
+    multiply()
