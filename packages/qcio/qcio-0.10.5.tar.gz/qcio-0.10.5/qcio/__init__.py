13868636f70864bd110730729f55180bf3b3a580
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-16 14:03:45 +0000

    Add package qcio/0.10.5/qcio-0.10.5.tar.gz

---
+++ packages/qcio/qcio-0.10.5.tar.gz/qcio-0.10.5/qcio/__init__.py
@@ -0,0 +1,29 @@
+# https://github.com/python-poetry/poetry/pull/2366#issuecomment-652418094
+from importlib import metadata
+
+from .models import *  # noqa: F403
+from .utils import json_dumps
+
+__version__ = metadata.version(__name__)
+
+__all__ = [  # noqa: F405
+    # Core Models
+    "CalcType",
+    "Model",
+    "Provenance",
+    "Structure",
+    "Molecule",
+    "Files",
+    "FileInput",
+    "ProgramInput",
+    "ProgramOutput",
+    "DualProgramInput",
+    "NoResults",
+    "SinglePointResults",
+    "OptimizationResults",
+    "SinglePointOutput",
+    "Wavefunction",
+    "ProgramArgs",
+    "ProgramArgsSub",
+    "json_dumps",
+]
