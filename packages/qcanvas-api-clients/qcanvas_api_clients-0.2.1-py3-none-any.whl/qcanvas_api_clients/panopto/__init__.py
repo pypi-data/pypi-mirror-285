5b9a59bd603aa0c6e1c685aa747846e5e545aafa
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-16 14:03:56 +0000

    Add package qcanvas-api-clients/0.2.1/qcanvas_api_clients-0.2.1-py3-none-any.whl

---
+++ packages/qcanvas-api-clients/qcanvas_api_clients-0.2.1-py3-none-any.whl/qcanvas_api_clients/panopto/__init__.py
@@ -0,0 +1,15 @@
+from .panopto_client import PanoptoClient
+from .panopto_client_config import PanoptoClientConfig
+from .panopto_types import (
+    AlternativeSession,
+    AlternativeParent,
+    Parent,
+    Session,
+    FolderInfo,
+    SessionList,
+    DeliveryInfo,
+    Delivery,
+    Stream,
+    PodcastStream,
+    Variant,
+)
