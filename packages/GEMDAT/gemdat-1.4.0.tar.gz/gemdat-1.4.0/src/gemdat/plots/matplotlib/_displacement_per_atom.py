a73558e0b936ebabc4e98f8f5f960d9acdde37c6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-16 16:06:28 +0000

    Add package GEMDAT/1.4.0/gemdat-1.4.0.tar.gz

---
+++ packages/GEMDAT/gemdat-1.4.0.tar.gz/gemdat-1.4.0/src/gemdat/plots/matplotlib/_displacement_per_atom.py
@@ -0,0 +1,31 @@
+from __future__ import annotations
+
+from typing import TYPE_CHECKING
+
+import matplotlib.pyplot as plt
+
+if TYPE_CHECKING:
+    from gemdat.trajectory import Trajectory
+
+
+def displacement_per_atom(*, trajectory: Trajectory) -> plt.Figure:
+    """Plot displacement per atom.
+
+    Parameters
+    ----------
+    trajectory : Trajectory
+        Input trajectory, i.e. for the diffusing atom
+
+    Returns
+    -------
+    fig : matplotlib.figure.Figure
+        Output figure
+    """
+    fig, ax = plt.subplots()
+
+    for distances in trajectory.distances_from_base_position():
+        ax.plot(distances, lw=0.3)
+
+    ax.set(title='Displacement per site', xlabel='Time step', ylabel='Displacement (Å)')
+
+    return fig
