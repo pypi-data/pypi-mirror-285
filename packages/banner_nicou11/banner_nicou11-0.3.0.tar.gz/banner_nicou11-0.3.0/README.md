3072203dac3027f7324a36a9ebf5155918eae361
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-16 14:04:04 +0000

    Add package banner_nicou11/0.3.0/banner_nicou11-0.3.0.tar.gz

---
+++ packages/banner_nicou11/banner_nicou11-0.3.0.tar.gz/banner_nicou11-0.3.0/README.md
@@ -0,0 +1,11 @@
+# banner_nicou11
+
+## **Usage**
+
+In Python type:
+```
+$ show-banner
+```
+
+## **License**
+https://github.com/Nicou11/pyfiglet
---
+++ packages/banner_nicou11/banner_nicou11-0.3.0.tar.gz/banner_nicou11-0.3.0/src/banner_nicou11/banner.py
@@ -0,0 +1,6 @@
+from pyfiglet import Figlet
+
+def show():
+    from pyfiglet import Figlet
+    f = Figlet(font='slant')
+    print(f.renderText('Nicou11'))
