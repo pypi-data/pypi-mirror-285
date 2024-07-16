6b70795cf744e538dc2b5e0ed8f61bca03d874e0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-16 14:04:04 +0000

    Add package ErrorHook/2.2.4/errorhook-2.2.4.tar.gz

---
+++ packages/ErrorHook/errorhook-2.2.4.tar.gz/errorhook-2.2.4/setup.py
@@ -0,0 +1,24 @@
+import setuptools
+
+with open("README.md", "r", encoding='utf-8') as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="ErrorHook",
+    version="2.2.4",
+    author="cc1287",
+    author_email="bilibili_cc1287@126.com",
+    description="Hook your Error.",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://gitee.com/cc1287/error-hook",
+    packages=setuptools.find_packages(),
+    package_data={'ErrorHook': ['*.file']},
+    install_requires=['datetime'],
+    classifiers=(
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: Microsoft :: Windows :: Windows 10",
+        "Operating System :: Microsoft :: Windows :: Windows 11",
+    ),
+)
