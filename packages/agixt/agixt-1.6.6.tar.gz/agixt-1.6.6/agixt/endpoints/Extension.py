c8619a7afbc232628fd85e6df3fa2086ac0f90ad
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-16 14:03:16 +0000

    Add package agixt/1.6.6/agixt-1.6.6.tar.gz

---
+++ packages/agixt/agixt-1.6.6.tar.gz/agixt-1.6.6/agixt/endpoints/Extension.py
@@ -0,0 +1,73 @@
+from fastapi import APIRouter, HTTPException, Depends, Header
+from Extensions import Extensions
+from ApiClient import Agent, Conversations, verify_api_key, get_api_client, is_admin
+from Models import CommandExecution
+
+
+app = APIRouter()
+
+
+@app.get(
+    "/api/extensions/settings",
+    tags=["Extensions"],
+    dependencies=[Depends(verify_api_key)],
+)
+async def get_extension_settings(user=Depends(verify_api_key)):
+    try:
+        return {"extension_settings": Extensions().get_extension_settings()}
+    except Exception:
+        raise HTTPException(status_code=400, detail="Unable to retrieve settings.")
+
+
+@app.get(
+    "/api/extensions/{command_name}/args",
+    tags=["Extensions"],
+    dependencies=[Depends(verify_api_key)],
+)
+async def get_command_args(command_name: str, user=Depends(verify_api_key)):
+    return {"command_args": Extensions().get_command_args(command_name=command_name)}
+
+
+@app.get("/api/extensions", tags=["Extensions"], dependencies=[Depends(verify_api_key)])
+async def get_extensions(user=Depends(verify_api_key)):
+    extensions = Extensions().get_extensions()
+    return {"extensions": extensions}
+
+
+@app.post(
+    "/api/agent/{agent_name}/command",
+    tags=["Extensions"],
+    dependencies=[Depends(verify_api_key)],
+)
+async def run_command(
+    agent_name: str,
+    command: CommandExecution,
+    user=Depends(verify_api_key),
+    authorization: str = Header(None),
+):
+    if is_admin(email=user, api_key=authorization) != True:
+        raise HTTPException(status_code=403, detail="Access Denied")
+    ApiClient = get_api_client(authorization=authorization)
+    agent_config = Agent(
+        agent_name=agent_name, user=user, ApiClient=ApiClient
+    ).get_agent_config()
+    command_output = await Extensions(
+        agent_name=agent_name,
+        agent_config=agent_config,
+        conversation_name=command.conversation_name,
+        ApiClient=ApiClient,
+        api_key=authorization,
+        user=user,
+    ).execute_command(
+        command_name=command.command_name, command_args=command.command_args
+    )
+    if (
+        command.conversation_name != ""
+        and command.conversation_name != None
+        and command_output != None
+    ):
+        c = Conversations(conversation_name=command.conversation_name, user=user)
+        c.log_interaction(role=agent_name, message=command_output)
+    return {
+        "response": command_output,
+    }
