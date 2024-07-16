de2dbc7222d5cb07466c7fb7f2978380bf967b6f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-16 16:06:02 +0000

    Add package seven-shop-studio/1.0.24/seven_shop_studio-1.0.24.tar.gz

---
+++ packages/seven-shop-studio/seven_shop_studio-1.0.24.tar.gz/seven_shop_studio-1.0.24/seven_shop_studio/models/db_models/cart/cart_model_ex.py
@@ -0,0 +1,15 @@
+from seven_shop_studio.models.db_models.cart.cart_model import *
+
+
+class CartModelEx(CartModel):
+    def __init__(self, db_connect_key='db_shopping_center', sub_table=None, db_transaction=None, context=None):
+        super().__init__(db_connect_key, sub_table, db_transaction, context)
+    
+    def get_cart_list(self, shop_id, begin_time, end_time):
+        """
+        :description: 获取时间段内购物车列表
+        :last_editors: KangWenBin
+        """
+        sql = "SELECT a.user_code,a.buy_count,a.add_time FROM cart_tb a JOIN goods_tb b ON a.goods_id = b.id WHERE b.shop_id = %s and a.add_time >= %s and a.add_time < %s"
+        params = (shop_id, begin_time, end_time)
+        return self.db.fetch_all_rows(sql, params)
