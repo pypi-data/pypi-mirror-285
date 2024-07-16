2cacef61fc9658c450978ba9772992b1aba6cbd5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-16 17:03:54 +0000

    Add package indian-repair/1.1/indian-repair-1.1.tar.gz

---
+++ packages/indian-repair/indian-repair-1.1.tar.gz/indian-repair-1.1/README.md
@@ -0,0 +1,17 @@
+# indian-repair
+
+Crazy Tool that can fix your network, storage, lag or any effin issue that you have no idea about. Just run indian-repair and see the magic.
+
+## Installation
+
+You can install the package using pip:
+
+```sh
+pip install indian-repair
+```
+
+## Usage
+
+```sh
+indian-repair
+```
---
+++ packages/indian-repair/indian-repair-1.1.tar.gz/indian-repair-1.1/setup.py
@@ -0,0 +1,27 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="indian-repair",
+    version="1.1",
+    packages=find_packages(),
+    install_requires=[
+        'click',  
+    ],
+    entry_points={
+        'console_scripts': [
+            'indian-repair=indian_repair.cli:cli',
+        ],
+    },
+    author="Pradeep Kumar N",
+    author_email="prajeet67@gmail.com",
+    description="Crazy Tool that can fix your network, storage, lag or any effin issue that you have no idea about. Just run indian-repair and see the magic.",
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
+    url="https://github.com/sickuritywizard/indian-repair",
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+)
