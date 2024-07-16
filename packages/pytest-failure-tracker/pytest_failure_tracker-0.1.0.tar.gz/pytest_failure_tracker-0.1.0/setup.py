fd4aeac20cc0d6a5b741deb0234838dd0614a660
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-16 16:06:42 +0000

    Add package pytest-failure-tracker/0.1.0/pytest_failure_tracker-0.1.0.tar.gz

---
+++ packages/pytest-failure-tracker/pytest_failure_tracker-0.1.0.tar.gz/pytest_failure_tracker-0.1.0/setup.py
@@ -0,0 +1,25 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="pytest-failure-tracker",
+    version="0.1.0",
+    author="Krystian Safjan",
+    author_email="ksafjan@gmail.com",
+    description="A pytest plugin for tracking test failures over multiple runs",
+    long_description=open("README.md").read(),
+    long_description_content_type="text/markdown",
+    url="https://github.com/izikeros/pytest-failure-tracker",
+    package_dir={"": "src"},
+    packages=find_packages(where="src"),
+    install_requires=["pytest>=6.0.0"],
+    classifiers=[
+        "Framework :: Pytest",
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    entry_points={
+        "pytest11": ["failures = pytest_failure_tracker.plugin"],
+    },
+    python_requires=">=3.6",
+)
