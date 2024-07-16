d78637c0ac8a6dcccf83a1a9919b088dfe704191
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-16 16:06:00 +0000

    Add package IntelliPy/0.10.4/IntelliPy-0.10.4.tar.gz

---
+++ packages/IntelliPy/IntelliPy-0.10.4.tar.gz/IntelliPy-0.10.4/setup.py
@@ -0,0 +1,24 @@
+# setup.py
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="IntelliPy",
+    version="0.10.4",
+    author="Nicolas Ruffini",
+    author_email="n.ruffini@gmx.de",
+    description="Automatic IntelliCage data analysis using Python",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/NiRuff/IntelliPy",
+    packages=["intellipy"],
+    install_requires=["numpy>=2.0.0", "pandas>=2.2.2", "xlsxwriter>=3.2.0", "matplotlib>=3.9.1", "seaborn>=0.13.2"],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.9',
+)
