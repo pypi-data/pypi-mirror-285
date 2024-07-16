a615f2926438e3df6c317bbaf7bdd7206fdbfb19
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-16 16:05:38 +0000

    Add package cachebox/3.4.0/cachebox-3.4.0-pp38-pypy38_pp73-musllinux_1_2_x86_64.whl

---
+++ packages/cachebox/cachebox-3.4.0-pp38-pypy38_pp73-musllinux_1_2_x86_64.whl/cachebox/__init__.py
@@ -0,0 +1,35 @@
+"""
+The fastest caching library written in Rust.
+
+Example::
+
+    from cachebox import TTLCache
+    import time
+
+    cache = TTLCache(1000, ttl=2)
+    cache[0] = 1
+    time.sleep(2)
+    cache.get(0, None) # None
+"""
+
+from ._cachebox import (
+    BaseCacheImpl as BaseCacheImpl,
+    Cache as Cache,
+    FIFOCache as FIFOCache,
+    LFUCache as LFUCache,
+    RRCache as RRCache,
+    LRUCache as LRUCache,
+    TTLCache as TTLCache,
+    VTTLCache as VTTLCache,
+    __version__ as __version__,
+    __author__ as __author__,
+    version_info as version_info,
+)
+
+from .utils import (
+    cached as cached,
+    cachedmethod as cachedmethod,
+    make_hash_key as make_hash_key,
+    make_typed_key as make_typed_key,
+    items_in_order as items_in_order,
+)
