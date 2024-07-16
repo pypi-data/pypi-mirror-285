618725ef259674032037be927b404caec828a560
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-16 03:03:24 +0000

    Add package streaminghub-pydfds/0.1.36/streaminghub_pydfds-0.1.36.tar.gz

---
+++ packages/streaminghub-pydfds/streaminghub_pydfds-0.1.36.tar.gz/streaminghub_pydfds-0.1.36/src/streaminghub_pydfds/util.py
@@ -0,0 +1,19 @@
+import json
+from pathlib import Path
+
+from .typing import Config
+
+
+def load_config() -> Config:
+    config_path = Path("~/.streaminghubrc").expanduser().resolve()
+    assert config_path.exists() and config_path.is_file()
+    with open(config_path) as f:
+        data = json.load(f)
+    config = Config(**data)
+    return config
+
+
+def write_config(config: Config):
+    config_path = Path("~/.streaminghubrc").expanduser().resolve()
+    with open(config_path) as f:
+        json.dump(config.model_dump(), f)
