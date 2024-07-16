79a454c367b85ec62a3de3ad281b39eed7164f75
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-16 15:04:14 +0000

    Add package graftm/0.15.1/graftm-0.15.1.tar.gz

---
+++ packages/graftm/graftm-0.15.1.tar.gz/graftm-0.15.1/setup.py
@@ -0,0 +1,32 @@
+from setuptools import setup, find_packages
+
+with open('README.md') as readme_file:
+    readme = readme_file.read()
+
+exec(open('graftm/version.py').read()) # loads __version__
+
+setup(name='graftm',
+      version=__version__,
+      author='Joel Boyd, Ben Woodcroft',
+      description='GraftM is a pipeline used for identifying and classifying marker gene reads from metagenomic datasets',
+      long_description=readme,
+      description_content_type="text/markdown",
+      long_description_content_type="text/markdown",
+      license='GPL3+',
+      keywords="",
+      packages=find_packages(exclude='docs'),
+      install_requires=('biopython >=1.64',
+                        'biom-format >=2.1.4',
+                        'extern >=0.0.4',
+                        'taxtastic >=0.5.4',
+                        'bird_tool_utils >= 0.2.17',
+                        'DendroPy >= 4.1.0',
+                        'pyyaml', # Possibly these three not needed, maybe taxtastic now fixed.
+                        'fastalite',
+                        'jinja2'),
+      url='http://geronimp.github.io/graftM',
+      scripts=['bin/graftM'],
+      data_files=[
+          ('share', ['share/18S.hmm']),
+      ],
+)
