61e869e2d88ca76ec12a1f5c5a5e5576f42acd0a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-16 15:05:06 +0000

    Add package Stsocialicon/0.0.2/Stsocialicon-0.0.2.tar.gz

---
+++ packages/Stsocialicon/Stsocialicon-0.0.2.tar.gz/Stsocialicon-0.0.2/setup.py
@@ -0,0 +1,19 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='Stsocialicon',
+    version='0.0.2',
+    packages=find_packages(),
+    author='Sumit Kumar Singh',
+    author_email='sumitsingh9441@gmail.com',
+    description='A package for generating social media icons with customizable colors and sizes.',
+    keywords = "streamlit social media links",
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
+    url='https://github.com/RAJPUTRoCkStAr/Stsocialicon',
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+    ],
+)
