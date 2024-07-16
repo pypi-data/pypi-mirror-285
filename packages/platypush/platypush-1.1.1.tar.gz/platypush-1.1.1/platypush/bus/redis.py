14b8ee0ba0948a817e1ba00286d356588f0e33af
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-16 17:04:08 +0000

    Add package platypush/1.1.1/platypush-1.1.1.tar.gz

---
+++ packages/platypush/platypush-1.1.1.tar.gz/platypush-1.1.1/platypush/bus/redis.py
@@ -0,0 +1,79 @@
+import logging
+import threading
+
+from platypush.bus import Bus
+from platypush.message import Message
+
+logger = logging.getLogger('platypush:bus:redis')
+
+
+class RedisBus(Bus):
+    """
+    Overrides the in-process in-memory local bus with a Redis bus
+    """
+
+    DEFAULT_REDIS_QUEUE: str = 'platypush/bus'
+
+    def __init__(self, *args, on_message=None, redis_queue=None, **kwargs):
+        from platypush.utils import get_redis
+
+        super().__init__(on_message=on_message)
+        self.redis = get_redis(*args, **kwargs)
+        self.redis_args = kwargs
+        self.redis_queue = redis_queue or self.DEFAULT_REDIS_QUEUE
+        self.on_message = on_message
+        self.thread_id = threading.get_ident()
+        self._pubsub = None
+        self._pubsub_lock = threading.RLock()
+
+    @property
+    def pubsub(self):
+        with self._pubsub_lock:
+            if not self._pubsub:
+                self._pubsub = self.redis.pubsub()
+            return self._pubsub
+
+    def poll(self):
+        """
+        Polls the Redis queue for new messages
+        """
+        with self.pubsub as pubsub:
+            pubsub.subscribe(self.redis_queue)
+            try:
+                for msg in pubsub.listen():
+                    if msg.get('type') != 'message':
+                        continue
+
+                    if self.should_stop():
+                        break
+
+                    try:
+                        data = msg.get('data', b'').decode('utf-8')
+                        parsed_msg = Message.build(data)
+                        if parsed_msg and self.on_message:
+                            self.on_message(parsed_msg)
+                    except Exception as e:
+                        logger.exception(e)
+            finally:
+                pubsub.unsubscribe(self.redis_queue)
+
+    def post(self, msg):
+        """
+        Sends a message to the Redis queue
+        """
+        from redis import exceptions
+
+        try:
+            self.redis.publish(self.redis_queue, str(msg))
+        except exceptions.ConnectionError as e:
+            if not self.should_stop():
+                # Raise the exception only if the bus it not supposed to be
+                # stopped
+                raise e
+
+    def stop(self):
+        super().stop()
+        self.redis.close()
+
+
+# vim:sw=4:ts=4:et:
---
+++ packages/platypush/platypush-1.1.1.tar.gz/platypush-1.1.1/setup.py
@@ -0,0 +1,129 @@
+#!/usr/bin/env python
+
+import json
+import os
+
+from setuptools import setup, find_packages
+
+
+def path(fname=''):
+    return os.path.abspath(os.path.join(os.path.dirname(__file__), fname))
+
+
+def readfile(fname):
+    with open(path(fname)) as f:
+        return f.read()
+
+
+def pkg_files(dir):
+    paths = []
+    for p, _, files in os.walk(dir):
+        for file in files:
+            paths.append(os.path.join('..', p, file))
+    return paths
+
+
+def scan_manifests():
+    for root, _, files in os.walk('platypush'):
+        for file in files:
+            if file == 'manifest.json':
+                yield os.path.join(root, file)
+
+
+def parse_deps(deps):
+    ret = []
+    for dep in deps:
+        if dep.startswith('git+'):
+            continue  # Don't include git dependencies in the setup.py, or Twine will complain
+
+        ret.append(dep)
+
+    return ret
+
+
+def parse_manifest(manifest_file):
+    with open(manifest_file) as f:
+        manifest = json.load(f).get('manifest')
+        if not manifest:
+            return None, None
+
+        name = '.'.join(manifest['package'].split('.')[2:])
+        return name, parse_deps(manifest.get('install', {}).get('pip', []))
+
+
+def parse_manifests():
+    ret = {}
+    for manifest_file in scan_manifests():
+        name, deps = parse_manifest(manifest_file)
+        if deps:
+            ret[name] = deps
+
+    return ret
+
+
+plugins = pkg_files('platypush/plugins')
+backend = pkg_files('platypush/backend')
+
+setup(
+    name="platypush",
+    version="1.1.1",
+    author="Fabio Manganiello",
+    author_email="fabio@manganiello.tech",
+    description="Platypush service",
+    license="MIT",
+    python_requires='>= 3.6',
+    keywords="home-automation automation iot mqtt websockets redis dashboard notifications",
+    url="https://platypush.tech",
+    packages=find_packages(exclude=['tests']),
+    include_package_data=True,
+    package_data={
+        'platypush': [
+            'migrations/alembic.ini',
+            'migrations/alembic/*',
+            'migrations/alembic/**/*',
+            'install/**',
+            'install/scripts/*',
+            'install/scripts/**/*',
+            'install/requirements/*',
+            'install/docker/*',
+            'components.json.gz',
+        ],
+    },
+    entry_points={
+        'console_scripts': [
+            'platypush=platypush:main',
+            'platydock=platypush.platydock:main',
+            'platyvenv=platypush.platyvenv:main',
+        ],
+    },
+    long_description=readfile('README.md'),
+    long_description_content_type='text/markdown',
+    classifiers=[
+        "Topic :: Utilities",
+        "License :: OSI Approved :: MIT License",
+        "Development Status :: 4 - Beta",
+    ],
+    install_requires=[
+        'alembic',
+        'croniter',
+        'docutils',
+        'flask',
+        'frozendict',
+        'marshmallow',
+        'marshmallow_dataclass',
+        'psutil',
+        'python-dateutil',
+        'python-magic',
+        'pyyaml',
+        'redis',
+        'requests',
+        'rsa',
+        'sqlalchemy',
+        'tornado',
+        'websocket-client',
+        'websockets',
+        'wheel',
+        'zeroconf>=0.27.0',
+    ],
+    extras_require=parse_manifests(),
+)
