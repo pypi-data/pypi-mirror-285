8b97cfcefdb7a1918e4c776a1457f77c15fdb16a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-16 15:03:42 +0000

    Add package smartfarm-iot/0.0.14/smartfarm_iot-0.0.14.tar.gz

---
+++ packages/smartfarm-iot/smartfarm_iot-0.0.14.tar.gz/smartfarm_iot-0.0.14/setup.py
@@ -0,0 +1,32 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='smartfarm_iot',
+    version='0.0.14',
+    packages=find_packages(include=['smartfarm_iot', 'smartfarm_iot.*',
+                                    'config', 'config.*', 'run.sh']),
+    install_requires=[
+        'paho-mqtt',
+        'pyModbusTCP'
+    ],
+    package_data={
+        'smartfarm_iot': ['config/*.json', 'config/*.csv'],
+    },
+    entry_points={
+        'console_scripts': [
+            'smartfarm_iot=smartfarm_iot.main:main',
+        ],
+    },
+    author='kimyuri',
+    author_email='rikim94@naver.com',
+    description='A package for Smart Farm IoT data collection and publishing using Modbus TCP and MQTT',
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
+    url='http://www.doctor-ag.com/',
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+    ],
+    python_requires='>=3.6',
+)
