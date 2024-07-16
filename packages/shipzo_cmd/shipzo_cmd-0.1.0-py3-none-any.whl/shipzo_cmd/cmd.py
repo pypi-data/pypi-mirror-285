54eaf7cafb2fdd6049df54b1e6a328742990e1c7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-16 16:05:44 +0000

    Add package shipzo_cmd/0.1.0/shipzo_cmd-0.1.0-py3-none-any.whl

---
+++ packages/shipzo_cmd/shipzo_cmd-0.1.0-py3-none-any.whl/shipzo_cmd/cmd.py
@@ -0,0 +1,8 @@
+from shipzo_plus.shipzo_plus import sum
+from shipzo_mult.shipzo_mult import mult
+from shipzo_divide.shipzo_divide import divide
+
+def call():
+    sum()
+    mult():
+    divide():
