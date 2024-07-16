3679e1a2d5a890247efaa5933cefe2a368941924
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-16 04:03:34 +0000

    Add package lmax-python-sdk/4.0.9/lmax_python_sdk-4.0.9-py2.py3-none-any.whl

---
+++ packages/lmax-python-sdk/lmax_python_sdk-4.0.9-py2.py3-none-any.whl/lmax_python_sdk/__init__.py
@@ -0,0 +1,20 @@
+"""
+
+Unofficial LMAX Python SDK
+
+This is a Python SDK for interacting with the LMAX trading platform. It provides a set of modules to interact with different aspects of the platform. Since the OpenAPI generator is messy and I needed to interact with LMAX rather implemented a very lightweight and simple SDK.
+"""
+
+from . import account
+from . import broker_market_data
+from . import public_data
+from . import client
+from . import ws_client
+from .validation import (
+    ClientBaseURLType,
+    TransactionCategory,
+    OrderType,
+    SideType,
+    TimeInForceType,
+    TriggerMethodType,
+)
