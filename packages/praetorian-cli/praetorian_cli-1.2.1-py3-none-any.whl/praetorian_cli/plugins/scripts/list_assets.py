e4b7c27e5a0c6ccc464afb1d3948062a82bc7892
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-16 15:05:04 +0000

    Add package praetorian-cli/1.2.1/praetorian_cli-1.2.1-py3-none-any.whl

---
+++ packages/praetorian-cli/praetorian_cli-1.2.1-py3-none-any.whl/praetorian_cli/plugins/scripts/list_assets.py
@@ -0,0 +1,47 @@
+"""
+This script retrieves the assets originating from a given asset. It retrieves
+the asset from the CLI. In turn, it execute a search using the my() function
+of the Chariot SDK to find all entities related to the asset. Finally, it
+filters for the attributes entities of the 'seed' class, which store the
+information of the assets originating from the given asset.
+
+Example usage:
+
+  praetorian chariot get asset '#asset#praetorian.com' --plugin list_assets
+
+"""
+import json
+
+
+def process(controller, cmd, cli_kwargs, output):
+    # Verify the upstream CLI command is compatible with the script
+    if cmd['product'] != 'chariot' or cmd['action'] != 'get' or cmd['type'] != 'asset':
+        print("This script works with the 'get asset' command only.")
+        return
+
+    # Validate CLI output
+    if not output:
+        print("The CLI output is empty.")
+        return
+
+    asset_details = json.loads(output)
+
+    if not asset_details['seed']:
+        print("The asset is not a root asset; the seed field is False.")
+        return
+
+    # Extract the asset name
+    asset = asset_details['name']
+
+    # Search for asset name
+    result = controller.my(dict(key=asset), pages=100)
+
+    # Filter out attributes of the class 'seed'. Then, we compose asset keys from the attribute keys
+    if 'attributes' in result:
+        print(f"The following assets originate from {asset}:")
+        for hit in result['attributes']:
+            if hit['name'] == asset and hit['class'] == 'seed':
+                fragments = hit['key'].split("#")
+                print(f'#asset#{fragments[3]}#{fragments[4]}')
+    else:
+        print(f'No asset originate from {asset}')
