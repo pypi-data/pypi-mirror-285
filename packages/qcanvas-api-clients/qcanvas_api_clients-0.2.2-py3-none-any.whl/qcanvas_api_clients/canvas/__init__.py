e11d7ebc99859b53fa485460fb2c1965cafd4d6c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-16 14:03:49 +0000

    Add package qcanvas-api-clients/0.2.2/qcanvas_api_clients-0.2.2-py3-none-any.whl

---
+++ packages/qcanvas-api-clients/qcanvas_api_clients-0.2.2-py3-none-any.whl/qcanvas_api_clients/canvas/__init__.py
@@ -0,0 +1,3 @@
+from .canvas_client import CanvasClient
+from .canvas_client_config import CanvasClientConfig
+from .canvas_types import LegacyPage, RemoteFile, DiscussionTopicHeader as Announcement
