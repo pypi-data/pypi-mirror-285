865514630e31d21e9c8a348e36741903e37b3b76
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-16 16:05:41 +0000

    Add package donzo_cmd/0.1.0/donzo_cmd-0.1.0.tar.gz

---
+++ packages/donzo_cmd/donzo_cmd-0.1.0.tar.gz/donzo_cmd-0.1.0/src/donzo_cmd/cmd.py
@@ -0,0 +1,14 @@
+from donzo_plus.calcul import plus
+from donzo_plus.calcul import multiple
+from donzo_plus.calcul import divide
+
+
+def call_plus(a,b):
+    plus(a,b)   
+
+def call_multiple(a,b):
+    multiple(a,b)
+
+def call_divide(a,b):
+    divide(a,b)
+             
