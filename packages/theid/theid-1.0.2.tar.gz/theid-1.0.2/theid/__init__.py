8be02fc425376816683c192fec64c880739be23b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-16 16:05:41 +0000

    Add package theid/1.0.2/theid-1.0.2.tar.gz

---
+++ packages/theid/theid-1.0.2.tar.gz/theid-1.0.2/theid/__init__.py
@@ -0,0 +1,22 @@
+import requests as __requests
+from ._auth import _token
+from ._auth import _credentials
+from ._auth import _request_headers
+from ._utils import _get_private_ip
+from ._utils import _system_id
+
+_access_key, _secret_key, _email = _credentials()
+_public_ip = __requests.get('https://devnull.cn/ip').json()['origin']
+_private_ip = _get_private_ip()
+_system_id = _system_id()
+
+def token():
+    try:
+        return _token(_email, _access_key, _secret_key)
+    except:
+        return {
+            'rc': 205,
+            'msg': 'unhealthy service.'
+        }
+def authorization_headers():
+    return _request_headers(token=token())
