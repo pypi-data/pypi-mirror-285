d6aa280c9b56f4987f9735de5319acff7d9c1ff6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-16 03:02:50 +0000

    Add package clipp/1.3.1/clipp-1.3.1-py3-none-any.whl

---
+++ packages/clipp/clipp-1.3.1-py3-none-any.whl/clipp/__init__.py
@@ -0,0 +1,11 @@
+"""
+A POSIX-compliant, CLI parser library for building CLI interfaces, designed
+to be flexible, intelligent, and uncompromisingly simple. Clipp aims to make
+code more reusable and easily scalable, without compromising performance.
+"""
+from clipp.core import Command, fill_paragraph, OptionGroup
+
+__author__ = "Ben Ohling"
+__copyright__ = f"Copyright (C) 2024, {__author__}"
+__version__ = "1.3.1"
+__all__ = ["Command", "fill_paragraph", "OptionGroup"]
