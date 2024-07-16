22008d7a6e8606d48a8e7eaadade7c909b5f81c1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-16 15:04:29 +0000

    Add package crocodile/14.0/crocodile-14.0.tar.gz

---
+++ packages/crocodile/crocodile-14.0.tar.gz/crocodile-14.0/myresources/crocodile/__init__.py
@@ -0,0 +1,15 @@
+
+"""Info
+"""
+
+__version__ = "14.0"
+__author__ = "Alex Al-Saffar"
+__date__ = "2024-07-17"
+docs = r"https://crocodile.readthedocs.io/en/latest/"
+git = r"https://github.com/thisismygitrepo/crocodile"
+pypi = r"https://pypi.org/project/crocodile"
+release_notes = """
+
+Revamped deep learning
+
+"""
---
+++ packages/crocodile/crocodile-14.0.tar.gz/crocodile-14.0/setup.py
@@ -0,0 +1,106 @@
+
+"""
+PKG installer
+
+"""
+
+from setuptools import setup
+from myresources.crocodile import __version__
+# import setuptools
+
+
+with open("README.md", "r", encoding="utf-8") as file:
+    long_desc = file.read()  # to help adding README to PyPi website not only Github
+
+# get python modules:
+# P("./myresources").search("*.py", r=True).apply(lambda x: x.split(at="myresources", sep=-1)[1].as_posix().replace("/", ".")[:-3]).filter(lambda x: "__init__" not in x).list
+
+setup(
+    name='crocodile',
+    version=__version__,
+    packages=['crocodile'],
+    # packages=setuptools.find_packages(where="myresources"),
+    package_dir={'': 'myresources'},
+    # py_modules=setuptools.find_packages(where="myresources") + ['crocodile.msc.ascii_art'],
+    py_modules=['crocodile.core',
+                'crocodile.croshell',
+                'crocodile.database',
+                'crocodile.deeplearning',
+                'crocodile.deeplearning_template',
+                'crocodile.deeplearning_df',
+                'crocodile.deeplearning_torch',
+                'crocodile.environment',
+                'crocodile.file_management',
+                'crocodile.matplotlib_management',
+                'crocodile.meta',
+                'crocodile.plotly_management',
+                'crocodile.run',
+                'crocodile.toolbox',
+                'crocodile.cluster.data_transfer',
+                'crocodile.cluster.distribute',
+                'crocodile.cluster.meta_handling',
+                'crocodile.cluster.remote_machine',
+                'crocodile.cluster.script_execution',
+                'crocodile.cluster.script_notify_upon_completion',
+                'crocodile.cluster.trial_file',
+                'crocodile.comms.helper_funcs',
+                'crocodile.comms.notification',
+                'crocodile.comms.onedrive',
+                'crocodile.msc.ascii_art',
+                'crocodile.msc.dl_template',
+                'crocodile.msc.numerical',
+                'crocodile.msc.odds'
+                ],
+    url='https://github.com/thisismygitrepo/crocodile',
+    project_urls={
+        "Bug Tracker": "https://github.com/thisismygitrepo/crocodile/issues",
+    },
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires=">=3.10",  # 3.9
+    license='Apache 2.0',
+    author='Alex Al-Saffar',
+    author_email='programmer@usa.com',
+    description='Deep Learning Framework & Workload Management For On-premise Personal Machines.',
+    long_description=long_desc,
+    long_description_content_type="text/markdown",
+    package_data={'crocodile': ['art/*']},
+    # include_package_data=True,
+    install_requires=[
+        # CORE:
+        "numpy",  # number crunching
+        "pandas",  # number crunching
+        "tabulate",  # pandas optional for pretty printing of tables
+        "matplotlib",  # viz
+
+        # Accessories
+        "psutil",  # monitor processes.
+        "joblib",  # multitasking
+        "ipython",  # interactive python
+        "fire",  # for automatic CLI interface
+        "tqdm",  # for progress bar
+        "dill",  # extends pickle
+        "cryptography",  # for encoding
+        "paramiko",  # for SSH
+        "requests",  # interacting with web
+        "colorlog",  # for colored logging
+
+    ],
+
+    extras_require={
+            'full': ['sqlalchemy', 'tensorflow',
+                    #  'torch',
+                     'scikit-learn', 'dash', 'dash_daq', 'dash_bootstrap_components',
+                     'click',
+                     'types-requests', 'types-paramiko', 'types-tqdm',
+                     'setuptools', 'wheel', 'twine']
+                },
+
+)
+
+with open("./myresources/crocodile/art/happy_croco", "r", encoding="utf-8") as file:
+    croco = file.read()  # search ascii art or characters art.
+print(croco)
