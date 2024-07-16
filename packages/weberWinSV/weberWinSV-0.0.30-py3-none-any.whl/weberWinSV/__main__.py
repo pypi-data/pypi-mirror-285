22191f006cba239cea3088e318095a68d2d96da1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-16 14:03:51 +0000

    Add package weberWinSV/0.0.30/weberWinSV-0.0.30-py3-none-any.whl

---
+++ packages/weberWinSV/weberWinSV-0.0.30-py3-none-any.whl/weberWinSV/__main__.py
@@ -0,0 +1,36 @@
+﻿# weberWinSV/__main__.py
+# python -m weberWinSV 入口程序
+
+import sys
+import os
+from .CWinSupervisor import StartCWinSupervisor
+
+
+def _startWinSV(sSettingPyFN, sGroupKeyName, sDictVarName):
+    sWorkDir = os.getcwd()
+    sFullFN = os.path.join(sWorkDir, sSettingPyFN)
+    if not os.path.exists(sFullFN):
+        print(f"_startWinSV.sFullFN={sFullFN}=NotExist!")
+        return False
+    # sPythonFileName = os.path.basename(sSettingPyFullFN)
+    # print(f'_startWinSV.sPythonFileName={sPythonFileName}=')
+    StartCWinSupervisor(sFullFN, sSettingPyFN, sDictVarName, sGroupKeyName)
+    return True
+
+
+if __name__ == "__main__":
+    # 调用包内的函数或执行其他初始化代码
+    sSettingPyFN = 'winsv4Run.py'
+    sGroupKeyName = 'GroupId4Run'
+    sDictVarName = 'gDictConfigByGroupId'
+    if len(sys.argv) >= 2:
+        sSettingPyFN = sys.argv[1]
+        if len(sys.argv) >= 3:
+            sGroupKeyName = sys.argv[2]
+            if len(sys.argv) >= 4:
+                sDictVarName = sys.argv[3]
+    if not _startWinSV(sSettingPyFN, sGroupKeyName, sDictVarName):
+        print("\nChange the directory to the directory where winsv4Run.py is located.\n\n")
+        print("python -m weberWinSV winsv4Run.py")
+
+
