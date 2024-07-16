cf55bac316fa4195d3229f81be1ff84e90a32a25
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-16 03:02:51 +0000

    Add package pyamplipi/0.4.10/pyamplipi-0.4.10.tar.gz

---
+++ packages/pyamplipi/pyamplipi-0.4.10.tar.gz/pyamplipi-0.4.10/pyamplipi/amplipi.py
@@ -0,0 +1,227 @@
+from typing import List, Optional, Dict, Any, Tuple
+
+from aiohttp import ClientSession
+
+from pyamplipi.client import Client
+from pyamplipi.models import Group, Stream, SourceUpdate, MultiZoneUpdate, ZoneUpdate, \
+    GroupUpdate, StreamUpdate, Announcement, Status, Config, Info, Source, Zone, Preset, \
+    PresetUpdate, PlayMedia
+
+
+json_ser_kwargs: Dict[str, Any] = dict(exclude_unset=True)
+
+# play_media functionality added in 0.4.1
+MIN_MEDIA_PLAYER_VERSION = (0, 4, 1)
+
+
+class AmpliPi:
+    def __init__(
+            self,
+            endpoint: str,
+            timeout: int = 10,
+            http_session: Optional[ClientSession] = None,
+            verify_ssl: bool = False,
+            disable_insecure_warning: bool = True,
+    ):
+        self._client = Client(
+            endpoint,
+            timeout,
+            http_session,
+            verify_ssl,
+            disable_insecure_warning,
+        )
+        self.version: Optional[Tuple[int]] = None
+
+    # -- status calls
+    async def get_status(self) -> Status:
+        response = await self._client.get('')
+        return Status.parse_obj(response)
+
+    async def load_config(self, config: Config) -> Status:
+        response = await self._client.post('load', config.json(**json_ser_kwargs))
+        return Status.parse_obj(response)
+
+    async def factory_reset(self) -> Status:
+        response = await self._client.post('factory_reset')
+        return Status.parse_obj(response)
+
+    async def system_reset(self) -> Status:
+        response = await self._client.post('reset')
+        return Status.parse_obj(response)
+
+    async def system_reboot(self) -> Status:
+        response = await self._client.post('reboot')
+        return Status.parse_obj(response)
+
+    async def system_shutdown(self) -> Status:
+        response = await self._client.post('shutdown')
+        return Status.parse_obj(response)
+
+    async def get_info(self) -> Info:
+        response = await self._client.get('info')
+        return Info.parse_obj(response)
+
+    async def get_version(self):
+        if self.version is not None:
+            return self.version
+
+        info = await self.get_info()
+        version_raw = info.version
+        version_nums = []
+        tmp = 0
+        for ch in version_raw:
+            if ch.isdigit():
+                tmp = (tmp*10) + int(ch)
+            else:
+                version_nums.append(tmp)
+                tmp = 0
+        version_nums.append(tmp)
+        self.version = tuple(version_nums[:3])
+        return self.version
+
+    # -- source calls
+    async def get_sources(self) -> List[Source]:
+        response = await self._client.get('sources')
+        return [Source.parse_obj(source) for source in response['sources']]
+
+    async def get_source(self, source_id: int) -> Source:
+        response = await self._client.get(f'sources/{source_id}')
+        return Source.parse_obj(response)
+
+    async def set_source(self, source_id: int, source_update: SourceUpdate) -> Status:
+        response = await self._client.patch(f'sources/{source_id}', source_update.json(**json_ser_kwargs))
+        return Status.parse_obj(response)
+
+    async def get_source_img(self, source_id: int, height: int, outfile: Optional[str] = None) -> None:
+        await self._client.get(f'sources/{source_id}/image/{height}', expect_json=False, outfile=outfile)
+        return None
+
+    # -- zone calls
+    async def get_zone(self, zone_id: int) -> Zone:
+        response = await self._client.get(f'zones/{zone_id}')
+        return Zone.parse_obj(response)
+
+    async def get_zones(self) -> List[Zone]:
+        response = await self._client.get('zones')
+        return [Zone.parse_obj(zone) for zone in response['zones']]
+
+    async def set_zones(self, zone_update: MultiZoneUpdate) -> Status:
+        response = await self._client.patch('zones', zone_update.json(**json_ser_kwargs))
+        return Status.parse_obj(response)
+
+    async def set_zone(self, zone_id: int, zone_update: ZoneUpdate) -> Status:
+        response = await self._client.patch(f'zones/{zone_id}',
+                                            zone_update.json(**json_ser_kwargs))
+        return Status.parse_obj(response)
+
+    # -- group calls
+    async def create_group(self, new_group: Group) -> Group:
+        response = await self._client.post('group', new_group.json(**json_ser_kwargs))
+        return Group.parse_obj(response)
+
+    async def get_groups(self) -> List[Group]:
+        response = await self._client.get('groups')
+        return [Group.parse_obj(group) for group in response['groups']]
+
+    async def get_group(self, group_id) -> Group:
+        response = await self._client.get(f'groups/{group_id}')
+        return Group.parse_obj(response)
+
+    async def delete_group(self, group_id):
+        response = await self._client.delete(f'groups/{group_id}')
+        return Status.parse_obj(response)
+
+    async def set_group(self, group_id, update: GroupUpdate) -> Status:
+        response = await self._client.patch(f'groups/{group_id}', update.json(**json_ser_kwargs))
+        return Status.parse_obj(response)
+
+    # -- stream calls
+    async def get_streams(self) -> List[Stream]:
+        response = await self._client.get('streams')
+        return [Stream.parse_obj(stream) for stream in response['streams']]
+
+    async def get_stream(self, stream_id: int) -> Stream:
+        response = await self._client.get(f'streams/{stream_id}')
+        return Stream.parse_obj(response)
+
+    async def play_stream(self, stream_id: int) -> Status:
+        response = await self._client.post(f'streams/{stream_id}/play')
+        return Status.parse_obj(response)
+
+    async def pause_stream(self, stream_id: int) -> Status:
+        response = await self._client.post(f'streams/{stream_id}/pause')
+        return Status.parse_obj(response)
+
+    async def previous_stream(self, stream_id: int) -> Status:
+        response = await self._client.post(f'streams/{stream_id}/prev')
+        return Status.parse_obj(response)
+
+    async def next_stream(self, stream_id: int) -> Status:
+        response = await self._client.post(f'streams/{stream_id}/next')
+        return Status.parse_obj(response)
+
+    async def stop_stream(self, stream_id: int) -> Status:
+        response = await self._client.post(f'streams/{stream_id}/stop')
+        return Status.parse_obj(response)
+
+    async def station_change_stream(self, stream_id: int, station: int) -> Status:
+        response = await self._client.post(f'streams/{stream_id}/station={station}')
+        return Status.parse_obj(response)
+
+    async def create_stream(self, new_stream: Stream) -> Status:
+        response = await self._client.post('stream', new_stream.json(**json_ser_kwargs))
+        return Status.parse_obj(response)
+
+    async def delete_stream(self, stream_id: int) -> Status:
+        response = await self._client.delete(f'streams/{stream_id}')
+        return Status.parse_obj(response)
+
+    async def set_stream(self, stream_id: int, update: StreamUpdate) -> Status:
+        response = await self._client.post(f'streams/{stream_id}', update.json(**json_ser_kwargs))
+        return Status.parse_obj(response)
+
+    # -- preset calls
+    async def get_presets(self) -> List[Preset]:
+        response = await self._client.get('presets')
+        return [Preset.parse_obj(preset) for preset in response['presets']]
+
+    async def get_preset(self, preset_id: int) -> Preset:
+        response = await self._client.get(f'presets/{preset_id}')
+        return Preset.parse_obj(response)
+
+    async def set_preset(self, preset_id: int, update: PresetUpdate) -> Status:
+        response = await self._client.patch(f'presets/{preset_id}', update.json(**json_ser_kwargs))
+        return Status.parse_obj(response)
+
+    async def create_preset(self, new_preset: Preset) -> Preset:
+        response = await self._client.post('preset', new_preset.json(**json_ser_kwargs))
+        return Preset.parse_obj(response)
+
+    async def delete_preset(self, preset_id: int) -> Status:
+        response = await self._client.delete(f'presets/{preset_id}')
+        return Status.parse_obj(response)
+
+    async def load_preset(self, preset_id: int) -> Status:
+        response = await self._client.post(f'presets/{preset_id}/load')
+        return Status.parse_obj(response)
+
+    # -- anounce call
+    async def announce(self, announcement: Announcement, timeout: int = None) -> Status:
+        response = await self._client.post('announce', announcement.json(**json_ser_kwargs), timeout=timeout)
+        return Status.parse_obj(response)
+
+    # -- play media call
+    async def play_media(self, media: PlayMedia) -> Status:
+        version = await self.get_version()
+        if version < MIN_MEDIA_PLAYER_VERSION:  # TOO OLD, USE ANNOUNCEMENT
+            announce = Announcement(media=media.media,
+                                    source_id=media.source_id)
+            response = await self._client.post('announce', announce.json(**json_ser_kwargs))
+            return Status.parse_obj(response)
+        else:
+            response = await self._client.post('play', media.json(**json_ser_kwargs))
+            return Status.parse_obj(response)
+
+    # -- client control
+    async def close(self):
+        await self._client.close()
---
+++ packages/pyamplipi/pyamplipi-0.4.10.tar.gz/pyamplipi-0.4.10/pyamplipi/models.py
@@ -0,0 +1,216 @@
+"""AmpliPi Data Models - Extracted from the amplipi repo.
+"""
+
+from enum import Enum
+from typing import List, Optional, Union
+
+from pydantic import BaseModel
+
+
+class SourceInfo(BaseModel):
+    name: str
+    state: str  # paused, playing, stopped, unknown, loading ???
+    artist: Optional[str]
+    track: Optional[str]
+    album: Optional[str]
+    station: Optional[str]  # name of radio station
+    img_url: Optional[str]
+    supported_cmds: List[str] = []
+
+
+class Source(BaseModel):
+    """ An audio source """
+    id: Optional[int]
+    name: str
+    input: str
+    info: Optional[SourceInfo]  # Additional info about the current audio playing from the stream (generated during
+    # playback')
+
+
+class SourceUpdate(BaseModel):
+    """ Partial reconfiguration of an audio Source """
+    name: Optional[str]
+    input: Optional[str]  # 'None', 'local', 'stream=ID'
+
+
+class SourceUpdateWithId(SourceUpdate):
+    """ Partial reconfiguration of a specific audio Source """
+    id: int
+
+
+class Zone(BaseModel):
+    """ Audio output to a stereo pair of speakers, typically belonging to a room """
+    id: Optional[int]
+    name: str
+    source_id: int
+    mute: bool
+    vol: int
+    vol_f: float
+    vol_min: int
+    vol_max: int
+    disabled: bool
+
+
+class ZoneUpdate(BaseModel):
+    """ Reconfiguration of a Zone """
+    name: Optional[str]
+    source_id: Optional[int]
+    mute: Optional[bool]
+    vol: Optional[int]
+    vol_f: Optional[float]
+    vol_min: Optional[int]
+    vol_max: Optional[int]
+    disabled: Optional[bool]
+
+
+class ZoneUpdateWithId(ZoneUpdate):
+    """ Reconfiguration of a specific Zone """
+    id: int
+
+
+class MultiZoneUpdate(BaseModel):
+    """ Reconfiguration of multiple zones specified by zone_ids and group_ids """
+    zones: Optional[List[int]]
+    groups: Optional[List[int]]
+    update: ZoneUpdate
+
+
+class Group(BaseModel):
+    """ A group of zones that can share the same audio input and be controlled as a group ie. Upstairs. Volume, mute,
+    and source_id fields are aggregates of the member zones."""
+    id: Optional[int]
+    name: str
+    source_id: Optional[int]
+    zones: List[int]
+    mute: Optional[bool]
+    vol_delta: Optional[int]
+    vol_f: Optional[float]
+
+
+class GroupUpdate(BaseModel):
+    """ Reconfiguration of a Group """
+    name: Optional[str]
+    source_id: Optional[int]
+    zones: Optional[List[int]]
+    mute: Optional[bool]
+    vol_delta: Optional[int]
+    vol_f: Optional[float]
+
+
+class GroupUpdateWithId(GroupUpdate):
+    """ Reconfiguration of a specific Group """
+    id: int
+
+
+class Stream(BaseModel):
+    """ Digital stream such as Pandora, AirPlay or Spotify """
+    id: Optional[int]
+    name: str
+    type: str
+    user: Optional[str]
+    password: Optional[str]
+    station: Optional[str]
+    url: Optional[str]
+    logo: Optional[str]
+    freq: Optional[str]
+    client_id: Optional[str]
+    token: Optional[str]
+
+
+class StreamUpdate(BaseModel):
+    """ Reconfiguration of a Stream """
+    name: str
+    user: Optional[str]
+    password: Optional[str]
+    station: Optional[str]
+    url: Optional[str]
+    logo: Optional[str]
+    freq: Optional[str]
+
+
+class StreamCommand(str, Enum):
+    PLAY = 'play'
+    PAUSE = 'pause'
+    NEXT = 'next'
+    PREV = 'prev'
+    STOP = 'stop'
+    LOVE = 'love'
+    BAN = 'ban'
+    SHELVE = 'shelve'
+
+
+class PresetState(BaseModel):
+    """ A set of partial configuration changes to make to sources, zones, and groups """
+    sources: Optional[List[SourceUpdateWithId]]
+    zones: Optional[List[ZoneUpdateWithId]]
+    groups: Optional[List[GroupUpdateWithId]]
+
+
+class Command(BaseModel):
+    """ A command to execute on a stream """
+    stream_id: int
+    cmd: str
+
+
+class Preset(BaseModel):
+    id: Optional[int]
+    name: str
+    state: Optional[PresetState]
+    commands: Optional[List[Command]]
+    last_used: Union[int, None] = None
+
+
+class PresetUpdate(BaseModel):
+    name: Optional[str]
+    state: Optional[PresetState]
+    commands: Optional[List[Command]]
+
+
+class Announcement(BaseModel):
+    media: str
+    vol: Optional[int]
+    vol_f: Optional[float]
+    source_id: Optional[int]
+    zones: Optional[List[int]]
+    groups: Optional[List[int]]
+
+class PlayMedia(BaseModel):
+    media: str
+    vol: Optional[int]
+    vol_f: Optional[float]
+    source_id: Optional[int]
+
+
+class FirmwareInfo(BaseModel):
+    version: Optional[str]
+    git_hash: Optional[str]
+    git_dirty: Optional[bool]
+
+
+class Info(BaseModel):
+    config_file: str = 'Uknown'
+    version: str = 'Unknown'
+    mock_ctrl: bool = False
+    mock_streams: bool = False
+    online: Optional[bool] = True
+    latest_release: Optional[str]
+    fw: Optional[List[FirmwareInfo]]
+
+
+class Config(BaseModel):
+    sources: List[Source] = []
+    zones: List[Zone] = []
+    groups: List[Group] = []
+    streams: List[Stream] = []
+    presets: List[Preset] = []
+
+
+class Status(Config):
+    info: Optional[Info]
+
+
+class AppSettings(BaseModel):
+    mock_ctrl: bool = True
+    mock_streams: bool = True
+    config_file: str = 'house.json'
+    delay_saves: bool = True
