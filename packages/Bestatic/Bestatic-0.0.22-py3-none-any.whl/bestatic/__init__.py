edfef01dd296f95f6ca7bc15198df39ddbbcb1ca
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-16 15:04:17 +0000

    Add package Bestatic/0.0.22/Bestatic-0.0.22-py3-none-any.whl

---
+++ packages/Bestatic/Bestatic-0.0.22-py3-none-any.whl/bestatic/__init__.py
@@ -0,0 +1,8 @@
+# Author: Tatsat Banerjee
+# Package Name: Bestatic
+
+__version__ = "0.0.22"
+__all__ = ['bestatic', 'generator', 'httpserver', 'quickstart', 'bestaticSitemap', 'newcontent', '__version__']
+
+
+
