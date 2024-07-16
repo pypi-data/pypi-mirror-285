61b30e0211af19eaacf239f0d5ba3141346bc0e4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-16 15:04:55 +0000

    Add package banner_monk/0.3.0/banner_monk-0.3.0.tar.gz

---
+++ packages/banner_monk/banner_monk-0.3.0.tar.gz/banner_monk-0.3.0/src/banner_monk/banner_monk.py
@@ -0,0 +1,40 @@
+from pyfiglet import Figlet
+
+def show():
+    f = Figlet(font='slant')
+    print(f.renderText("it's ya boiiiiii"))
+
+def pic():
+    p = """
+
+                                      '$&
+                                      @R$k
+                                    '$!!!M&
+                                    @?~~~!$k
+                                  '9!!~ ~~!MX
+                                  @X~~   `~!$k
+                                 9!!      ~~!$X
+                                dR!~       `~!$>
+                               XR!~         `~!$k:
+                              tR!~        ::~~!!MMXXHHHH!!<:.
+                             <$!xxiXX!!!~~~~~~~!!MMMMMMMMMMMMMMMXXXXx::
+                        .:X@N$$$RMMX!!!!~~~~~~~~!!MMMM@@MMMMMMMMMMMMXMSMMtHHHX!
+                  :xiM#"~  <$!~  `~~~!~~~~~~~~~~~!!MX!!!!!??#RR888MMMMMMMMMMMHH
+           ..XH@!~`       X$!~                 '~~~!MX!!!!!!!!!!!!?MMR@@$MMMMMM
+     :xiM#"~             <$!~                     '~!MM??MMX!!!!!!!!!!!!!!??#R$
+XH@M!``                 :$!!                        ~!R:   `~!MM!XH!!!!!!!!!!!!
+                       <$!~                         `!!M:        `~"??HHX!!!!!!
+                      :$!~                           `!!R:              `!!MMMH
+                     '$!~                             `~!8:                   ~
+                    :$!!                               ~!!N:
+                   '$!~                                 `!!N:
+                  .$!!~                                  ~~!&>
+                 '@!!~                                    ~!MN
+                .$!!~                                     ~~!M&>
+               :@$MHHHHHHHHH!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!H$N
+                 PINK FLOYD:  DARK SIDE OF THE MOON  BY CHAT 95
+
+
+"""
+
+print(p)
