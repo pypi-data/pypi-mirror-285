13fc7fad7ffdc091d79af8dc92642a2d6458e392
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-16 03:03:01 +0000

    Add package loopsim/0.6.1/loopsim-0.6.1-py3-none-any.whl

---
+++ packages/loopsim/loopsim-0.6.1-py3-none-any.whl/loopsim/analyze.py
@@ -0,0 +1,70 @@
+"""loop analysis and empirical distribution"""
+# Invariant: we are assuming that the loop files passed in are all valid
+
+
+import click
+import numpy as np
+import pandas as pd
+
+from . import common
+
+
+@click.command()
+@click.argument("loop_in_file", type=click.Path(exists=True, file_okay=True, dir_okay=False, readable=True))
+@click.argument("loop_out_file", type=click.Path(exists=False, file_okay=True, dir_okay=False, writable=True))
+@click.argument("intervals_file", type=click.Path(exists=True, file_okay=True, dir_okay=False, readable=True))
+def analyze(loop_in_file, loop_out_file, intervals_file):
+    """Perform analysis on a single loop file
+
+    Output the inputted loop file with an extra column.
+    Each row of the extra column will have the indices in the reference file that the loop on that row overlaps with."""
+
+    # Print params
+    print(f"Input loop file: {loop_in_file}")
+    print(f"Output loop file: {loop_out_file}")
+    print(f"Intervals file: {intervals_file}")
+    print(f"Delimiter for output: '{common.delimiter}'")
+
+    # Get intervals
+    intervals = pd.read_table(intervals_file, header=None, delimiter=common.detect_delimiter(intervals_file))
+
+    # Do analysis
+    ratios = []
+    loop_out = analyze_loop_file(loop_in_file, intervals, ratios)
+
+    # Output analysis
+    loop_out.to_csv(loop_out_file, header=None, index=None, sep=common.delimiter)
+    print(f"Outputted analyzed loop file to {loop_out_file}")
+    print(f"Ratio of overlapping intervals out of the total number of loops was: {ratios[0]}")
+
+
+def analyze_loop_file(loop_in_file, intervals, ratios):
+    """ratios isn't used here but is used for batch analysis"""
+    loop_in = pd.read_table(loop_in_file, header=None, delimiter=common.detect_delimiter(loop_in_file))
+
+    loop_in[0] = loop_in[0].astype("unicode")
+    intervals[0] = intervals[0].astype("unicode")
+
+    loop_in[6] = loop_in.apply(is_overlapping_hic, axis=1, args=(intervals,))
+
+    ratios.append(len(loop_in.loc[loop_in[6].notnull()]) / len(loop_in))
+
+    return loop_in
+
+
+def ranges_overlap(x_start, x_end, y_start, y_end):
+    """do ranges overlap? (boundaries being the same counts as overlapping)"""
+    return x_start <= y_end and y_start <= x_end
+
+
+def is_overlapping_hic(loop, intervals):
+    """check if loop has overlaps with an interval of interest (return indices)"""
+    intervals_chr = intervals.loc[intervals[0] == loop[0]]
+    loop_start = loop[1]
+    loop_end = loop[5]
+    overlaps = intervals_chr.apply(
+        lambda row, start, end: ranges_overlap(row[1], row[2], start, end), args=(loop_start, loop_end), axis=1
+    )
+    if len(overlaps[overlaps].index.values):
+        return overlaps[overlaps].index.values
+    return np.NaN
---
+++ packages/loopsim/loopsim-0.6.1-py3-none-any.whl/loopsim/visualize.py
@@ -0,0 +1,82 @@
+"""Plot ratio distribution"""
+
+import click
+import pandas as pd
+import seaborn as sb
+from scipy.stats import norm, normaltest
+
+from . import common
+
+
+@click.command()
+@click.argument(
+    "distribution_file",
+    type=click.Path(exists=True, file_okay=True, dir_okay=False, readable=True),
+)
+@click.argument(
+    "plot_file",
+    type=click.Path(exists=False, file_okay=True, dir_okay=False, writable=True),
+)
+@click.option(
+    "--other",
+    type=float,
+    help="if this is passed, a Z-test will be conducted on this value against the mean of the ratio distribution",
+)
+def visualize(distribution_file, plot_file, other):
+    """Get visualization and stats from distribution of ratios
+
+    Must pass distribution of (simulated) data.
+    Optionally, can pass a data point for statistical comparison with --other flag
+    """
+
+    # Print params
+    print(f"Obtaining overlapping ratios from: {distribution_file}.")
+
+    # Get data
+    dist = pd.read_table(
+        distribution_file,
+        header=None,
+        delimiter=common.detect_delimiter(distribution_file),
+    )
+
+    # Create distribution plot
+    ax = sb.histplot(data=dist[0], label="Simulated", kde=True)
+    ax.set(
+        title=f"Distribution of Overlapping Ratios by Frequency (N = {len(dist)})",
+        xlabel="Overlapping Ratio",
+        ylabel="Frequency",
+    )
+
+    if other:
+        ax.axvline(other, color="orange", label="Experimental")
+
+    legend_handles, _ = ax.get_legend_handles_labels()
+    ax.legend(handles=legend_handles)
+    sb.move_legend(ax, "upper left", bbox_to_anchor=(1, 1))
+
+    # Export to jpg
+    ax.get_figure().savefig(plot_file, bbox_inches="tight", dpi=300)
+    print(f"Exported plot to {plot_file}")
+
+    # Summary stats
+    print("\nSummary stats:")
+    print(f"Distribution mean: {dist[0].mean()}")
+    print(f"Distribution std: {dist[0].std()}")
+    print(f"Distribution min: {dist[0].min()}")
+    print(f"Distribution median: {dist[0].median()}")
+    print(f"Distribution max: {dist[0].max()}")
+
+    # Statistical tests (if comparison value is passed)
+    if other:
+        print("\nCalculating p-value based on empirical distribution:")
+        num_larger = len(dist.loc[dist[0] > other])
+        print(f"p-value: {num_larger / len(dist):.20f}")
+
+        print("\nCalculating p-value based on normal distribution:")
+        z_stat = (other - dist[0].mean()) / dist[0].std()
+        print(f"p-value: {1 - norm.cdf(z_stat):.20f}")
+
+    # Normality test
+    print("\nD'Agostino-Pearson normality test:")
+    _, p = normaltest(dist[0])
+    print(f"p-value: {p:.20f}")
