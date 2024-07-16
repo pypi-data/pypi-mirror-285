ac23bc5697acf6d9d12d82d5afaa518ef8ba7f49
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-16 04:03:46 +0000

    Add package ppim-migrator/0.0.7/ppim_migrator-0.0.7-py3-none-any.whl

---
+++ packages/ppim-migrator/ppim_migrator-0.0.7-py3-none-any.whl/ppim-migrator/__main__.py
@@ -0,0 +1,29 @@
+import click
+from .migrator import Migrator
+
+
+@click.group()
+def cli():
+    pass
+
+
+@cli.command()
+@click.argument("uid")
+def migrate_album(uid):
+    migrator = Migrator()
+    migrator.migrate_album(uid=uid)
+
+
+@cli.command()
+def migrate_favorites():
+    migrator = Migrator()
+    migrator.migrate_favorites()
+
+@cli.command()
+@click.option('--count', default=1000, help='Number of albums to migrate.')
+def migrate_all_albums(count):
+    migrator = Migrator()
+    migrator.migrate_all_albums(count)
+
+if __name__ == "__main__":
+    cli()
