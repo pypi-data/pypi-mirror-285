1e1e8ec70d77c34150ce57865162d90548326091
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-16 16:06:46 +0000

    Add package theid/1.0.5/theid-1.0.5.tar.gz

---
+++ packages/theid/theid-1.0.5.tar.gz/theid-1.0.5/theid/__init__.py
@@ -0,0 +1,32 @@
+import requests as __requests
+from ._auth import _token
+from ._auth import _credentials
+from ._auth import _request_headers
+from ._utils import _public_ip
+from ._utils import _private_ip
+from ._utils import _system_id
+
+_access_key, _secret_key, _email = _credentials()
+
+def token():
+    try:
+        return _token(_email, _access_key, _secret_key)
+    except:
+        return {
+            'rc': 205,
+            'msg': 'unhealthy service.'
+        }
+def authorization_headers():
+    return _request_headers(token=token())
+
+def system_id():
+    return _system_id()
+
+def email():
+    return _email
+
+def private_ip():
+    return _private_ip()
+
+def public_ip():
+    return _public_ip()
---
+++ packages/theid/theid-1.0.5.tar.gz/theid-1.0.5/theid/_utils.py
@@ -0,0 +1,21 @@
+import socket
+import hashlib
+import requests as __requests
+
+def _private_ip():
+    s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    s.connect(('8.8.8.8', 80))
+    private_ip_string = s.getsockname()[0]
+    s.close()
+    return private_ip_string
+
+def _public_ip():
+    return __requests.get('https://devnull.cn/ip').json()['origin']
+
+def _system_id():
+    try:
+        with open('/etc/machine-id', 'r') as f:
+            rr_string = hashlib.md5(f.read().encode()).hexdigest()
+    except:
+        rr_string = hashlib.md5(socket.gethostname().encode()).hexdigest()
+    return rr_string
