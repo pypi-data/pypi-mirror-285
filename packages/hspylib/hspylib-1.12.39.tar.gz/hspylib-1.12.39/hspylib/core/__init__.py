13f04f66d7c4c9a3a3cd22e28e33e580bd6abdbc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-16 03:03:01 +0000

    Add package hspylib/1.12.39/hspylib-1.12.39.tar.gz

---
+++ packages/hspylib/hspylib-1.12.39.tar.gz/hspylib-1.12.39/hspylib/core/__init__.py
@@ -0,0 +1,21 @@
+# _*_ coding: utf-8 _*_
+#
+# hspylib v1.12.39
+#
+# Package: main.hspylib.core
+"""Package initialization."""
+
+__all__ = [
+    'collection_filter', 
+    'config', 
+    'constants', 
+    'decorator', 
+    'enums', 
+    'exception', 
+    'metaclass', 
+    'namespace', 
+    'preconditions', 
+    'tools', 
+    'zoned_datetime'
+]
+__version__ = '1.12.39'
---
+++ packages/hspylib/hspylib-1.12.39.tar.gz/hspylib-1.12.39/hspylib/modules/eventbus/__init__.py
@@ -0,0 +1,12 @@
+# _*_ coding: utf-8 _*_
+#
+# hspylib v1.12.39
+#
+# Package: main.hspylib.modules.eventbus
+"""Package initialization."""
+
+__all__ = [
+    'event', 
+    'eventbus'
+]
+__version__ = '1.12.39'
