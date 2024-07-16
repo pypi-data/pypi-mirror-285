0588426cd2beb8b39325fea9d915c8a2fd3e201b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-16 15:05:16 +0000

    Add package optunahub/0.1.0b0/optunahub-0.1.0b0-py3-none-any.whl

---
+++ packages/optunahub/optunahub-0.1.0b0-py3-none-any.whl/optunahub/__init__.py
@@ -0,0 +1,6 @@
+from optunahub.hub import load_local_module
+from optunahub.hub import load_module
+from optunahub.version import __version__
+
+
+__all__ = ["load_module", "load_local_module", "__version__"]
