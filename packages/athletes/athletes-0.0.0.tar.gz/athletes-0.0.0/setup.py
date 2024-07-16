61b5423b0a496ff6f812e47cf7e2e634e41952f6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-16 03:02:59 +0000

    Add package athletes/0.0.0/athletes-0.0.0.tar.gz

---
+++ packages/athletes/athletes-0.0.0.tar.gz/athletes-0.0.0/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(
+    name="athletes",
+    description="",
+    long_description="",
+    long_description_content_type="text/markdown",
+    version="0.0.0",
+)
