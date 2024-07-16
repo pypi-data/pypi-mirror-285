c6c7a288fe9b9bd9e1d115d494ed651a20f3b0cf
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-16 14:03:41 +0000

    Add package ofxstatement-latvian/1.6.0/ofxstatement_latvian-1.6.0.tar.gz

---
+++ packages/ofxstatement-latvian/ofxstatement_latvian-1.6.0.tar.gz/ofxstatement_latvian-1.6.0/README.rst
@@ -0,0 +1,26 @@
+This is a plugin for `ofxstatement`_ to parse and convert proprietary bank statements of certain banks in Latvia to `OFX`_ files.
+
+Statements from these banks are currently supported:
+
+* `Swedbank`_ - CSV, FIDAVISTA xml files
+* `DNB`_ - FIDAVISTA xml files
+* `Citadele`_ - FIDAVISTA v1.2 xml files
+* `SEB`_ - CSV files
+
+.. _ofxstatement: https://github.com/kedder/ofxstatement
+.. _OFX: http://en.wikipedia.org/wiki/Open_Financial_Exchange
+.. _Swedbank: https://www.swedbank.lv/
+.. _DNB: https://www.dnb.lv/
+.. _Citadele: http://www.citadele.lv/
+.. _SEB: http://www.seb.lv/
+
+
+## Development
+To run locally and edit the code, do the following:
+```
+python3 -m venv venv
+source venv/bin/activate
+pip install -e ".[dev]"
+```
+
+Also for imports to work, temporary rename __init__.py inside of ofxstatement folder to something else.
---
+++ packages/ofxstatement-latvian/ofxstatement_latvian-1.6.0.tar.gz/ofxstatement_latvian-1.6.0/setup.py
@@ -0,0 +1,49 @@
+#!/usr/bin/python3
+"""Setup
+
+#Note: To publish new version: `./setup.py sdist upload`
+"""
+from setuptools import find_packages
+from distutils.core import setup
+
+version = "1.6.0"
+
+with open("README.rst") as f:
+    long_description = f.read()
+
+setup(
+    name="ofxstatement-latvian",
+    version=version,
+    author="Gints Murans",
+    author_email="gm@gm.lv",
+    url="https://github.com/gintsmurans/ofxstatement-latvian",
+    description=("Statement parsers for banks operating in Latvia"),
+    long_description=long_description,
+    license="GPLv3",
+    keywords=["ofx", "ofxstatement", "banking", "statement", "latvia"],
+    classifiers=[
+        "Development Status :: 5 - Production/Stable",
+        "Programming Language :: Python :: 3",
+        "Natural Language :: English",
+        "Topic :: Office/Business :: Financial :: Accounting",
+        "Topic :: Utilities",
+        "Environment :: Console",
+        "Operating System :: OS Independent",
+        "License :: OSI Approved :: GNU General Public License v3 (GPLv3)",
+    ],
+    packages=find_packages("src"),
+    package_dir={"": "src"},
+    namespace_packages=["ofxstatement", "ofxstatement.plugins"],
+    entry_points={
+        "ofxstatement": [
+            "swedbankLV = ofxstatement.plugins.swedbankLV:SwedbankLVPlugin",
+            "swedbankLVFV = ofxstatement.plugins.swedbankLVFiDAViSta:SwedbankLVFiDAViStaPlugin",
+            "dnbLV = ofxstatement.plugins.dnbLV:DnbLVPlugin",
+            "citadeleLV = ofxstatement.plugins.citadeleLV:CitadeleLVPlugin",
+            "sebLV = ofxstatement.plugins.sebLV:SebLVPlugin",
+        ]
+    },
+    install_requires=["ofxstatement"],
+    include_package_data=True,
+    zip_safe=True,
+)
