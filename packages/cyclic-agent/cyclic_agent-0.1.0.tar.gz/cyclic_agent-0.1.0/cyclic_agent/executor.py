011262862cf91bd129acbed8696a40c07b65d7a8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-16 16:07:22 +0000

    Add package cyclic-agent/0.1.0/cyclic_agent-0.1.0.tar.gz

---
+++ packages/cyclic-agent/cyclic_agent-0.1.0.tar.gz/cyclic_agent-0.1.0/cyclic_agent/executor.py
@@ -0,0 +1,47 @@
+import threading
+import time
+
+from cyclic_agent.state import State
+
+
+class CyclicExecutor:
+    def __init__(self, default_time_interval: float):
+        self.running = False
+        self.lock = threading.Lock()
+        self.default_time_interval = default_time_interval
+        self.killed = False
+        self.thread = None
+
+    def start(self, initial_state: State) -> None:
+        if not self.running:
+            self.running = True
+            self.thread = threading.Thread(target=self._main_loop, args=(initial_state,))
+            self.thread.start()
+
+    def pause(self):
+        with self.lock:
+            self.running = False
+
+    def resume(self):
+        with self.lock:
+            self.running = True
+
+    def kill(self):
+        with self.lock:
+            self.killed = True
+            self.running = False
+        if self.thread:
+            self.thread.join()
+
+    def _main_loop(self, state: State) -> None:
+        while True:
+            if self.killed:
+                return
+
+            if self.running:
+                state = state.next()
+                time.sleep(self.default_time_interval)
+
+
+class Timer:
+    ...
