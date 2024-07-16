2de1b4468bb56981db8de863294d53b10206a112
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-16 14:03:57 +0000

    Add package onnxscript/0.1.0.dev20240716/onnxscript-0.1.0.dev20240716-py3-none-any.whl

---
+++ packages/onnxscript/onnxscript-0.1.0.dev20240716-py3-none-any.whl/onnxscript/function_libs/torch_lib/ops/__init__.py
@@ -0,0 +1,15 @@
+# Copyright (c) Microsoft Corporation.
+# Licensed under the MIT License.
+__all__ = [
+    "core",
+    "fft",
+    "linalg",
+    "nested",
+    "nn",
+    "prims",
+    "sparse",
+    "special",
+    "vision",
+]
+
+from . import core, fft, linalg, nested, nn, prims, sparse, special, vision
