98a643c86e469bad2b9ccc483903dbac3bdff8ab
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-16 14:03:40 +0000

    Add package memwin/1.3.0/memwin-1.3.0.tar.gz

---
+++ packages/memwin/memwin-1.3.0.tar.gz/memwin-1.3.0/memwin/xprocess.py
@@ -0,0 +1,32 @@
+from .structs import *
+
+
+class XProcess:
+    def __init__(self, hwnd: int):
+        self.hwnd = hwnd
+        self.pid = 0
+        self.h_process = 0
+        
+    def get_pid(self) -> int:
+        '''
+        获取进程ID
+        '''
+        if self.pid:
+            return self.pid
+        process_id = wintypes.DWORD()
+        user32.GetWindowThreadProcessId(self.hwnd, ctypes.byref(process_id))
+        self.pid = process_id.value
+        return self.pid
+        
+    def get_h_process(self) -> int:
+        '''
+        获取进程句柄
+        '''
+        if self.h_process:
+            return self.h_process
+        self.pid = self.get_pid()
+        self.h_process = kernel32.OpenProcess(PROCESS_ALL_ACCESS, False, self.pid)
+        return self.h_process
+
+
+    
