25950f3d0865e025f181613643f7339f05cc956f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-16 16:07:27 +0000

    Add package autosubmitconfigparser/1.0.69/autosubmitconfigparser-1.0.69.tar.gz

---
+++ packages/autosubmitconfigparser/autosubmitconfigparser-1.0.69.tar.gz/autosubmitconfigparser-1.0.69/autosubmitconfigparser/config/yamlparser.py
@@ -0,0 +1,14 @@
+from ruamel.yaml import YAML
+
+class YAMLParserFactory:
+    def __init__(self):
+        pass
+
+    def create_parser(self):
+        return YAMLParser()
+
+class YAMLParser(YAML):
+
+    def __init__(self):
+        self.data = []
+        super(YAMLParser, self).__init__(typ="safe")
