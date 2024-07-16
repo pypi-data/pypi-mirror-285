c4b904885251e11af590a7a893685411b8c48dbe
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-16 17:03:36 +0000

    Add package Front11_cmd/0.1.2/front11_cmd-0.1.2.tar.gz

---
+++ packages/Front11_cmd/front11_cmd-0.1.2.tar.gz/front11_cmd-0.1.2/src/front11_cmd/front11_cmd.py
@@ -0,0 +1,14 @@
+from front11_plus import add
+from front11_div import divide
+from front11_multi import multiply
+
+def add(x, y):
+    return x + y
+
+def multiply(x, y):
+    return x * y
+
+def divide(x, y):
+    if y == 0:
+        raise ValueError("Division by zero is not allowed")
+    return x / y
