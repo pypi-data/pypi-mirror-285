9d7ade8461cf8a5b844f4c285018a21c18a7903e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-16 16:05:53 +0000

    Add package orm-study/0.0.3/orm_study-0.0.3-py3-none-any.whl

---
+++ packages/orm-study/orm_study-0.0.3-py3-none-any.whl/orm_study/quiz/constants.py
@@ -0,0 +1,26 @@
+CHAPTER = {
+    "1": "변수",
+    "2": "자료형",
+    "3": "연산자",
+}
+
+CHAPTER_CHOICE = f"""
+[white bold]
+파이썬 문제 풀기
+[green bold]
+챕터 1: {CHAPTER["1"]}
+챕터 2: {CHAPTER["2"]}
+챕터 3: {CHAPTER["3"]}
+"""
+
+TYPE_CHOICE = """
+[green bold]
+1. 객관식
+[blue bold]
+2. 주관식
+[red bold]
+3. 혼합
+"""
+
+MULTIPLE = "multiple_choice.txt"
+SUBJECTIVE = "subjective.txt"
