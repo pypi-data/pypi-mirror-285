763431ea7c7bfba182461e5ef682ad47dd6db441
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-16 17:04:10 +0000

    Add package taskly/0.0.0/taskly-0.0.0-py3-none-any.whl

---
+++ packages/taskly/taskly-0.0.0-py3-none-any.whl/todos/main.py
@@ -0,0 +1,20 @@
+from rich import print
+from rich.layout import Layout
+from rich.panel import Panel
+
+from _utils import *
+from todos import app
+
+
+def main():
+    clear_terminal()
+    cool_text("TASKLY")
+    """main function"""
+    while True:
+        done = app()
+        if done == 0:
+            break
+
+
+if __name__ == "__main__":
+    main()
