0d086f99cc7b764e180231c3e8c3b70e90357e1a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-16 14:03:44 +0000

    Add package python-wizard/0.1.4rc2/python_wizard-0.1.4rc2-py3-none-any.whl

---
+++ packages/python-wizard/python_wizard-0.1.4rc2-py3-none-any.whl/python_wizard/pw_dict.py
@@ -0,0 +1,21 @@
+from typing import List, Literal, Union, Any, Tuple,Dict
+
+def filter_dict(myDict:Dict,select_key):
+    # should be in my lib
+    ans = {key: value for key, value in myDict.items() if key in select_key}
+    return ans
+
+def filter_dict(myDict,select_key):
+    # imported from "C:\Users\Heng2020\OneDrive\Python NLP\NLP 02_Conjugation\Conju_PT.py"
+    # should be in my lib
+    ans = {key: value for key, value in myDict.items() if key in select_key}
+    return ans
+
+
+def reorder_dict(input_dict, new_order):
+    # imported from "C:\Users\Heng2020\OneDrive\Python NLP\NLP 02_Conjugation\Conju_PT.py"
+    from collections import OrderedDict
+    return OrderedDict((key, input_dict[key]) for key in new_order)
+
+__all__ = [name for name, obj in globals().items() 
+           if inspect.isfunction(obj) and not name.startswith('_')]
