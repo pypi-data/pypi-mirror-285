2179c0877c60d4af725221e2a36c02c8f0b0990a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-16 03:03:24 +0000

    Add package great-expectations-cloud/20240715.0.dev1/great_expectations_cloud-20240715.0.dev1.tar.gz

---
+++ packages/great-expectations-cloud/great_expectations_cloud-20240715.0.dev1.tar.gz/great_expectations_cloud-20240715.0.dev1/great_expectations_cloud/agent/actions/agent_action.py
@@ -0,0 +1,30 @@
+from __future__ import annotations
+
+import datetime
+from abc import abstractmethod
+from typing import TYPE_CHECKING, Generic, Optional, Sequence, TypeVar
+
+from pydantic.v1 import BaseModel
+
+from great_expectations_cloud.agent.models import CreatedResource, Event
+
+if TYPE_CHECKING:
+    from great_expectations.data_context import CloudDataContext
+
+
+class ActionResult(BaseModel):
+    id: str
+    type: str
+    created_resources: Sequence[CreatedResource]
+    job_duration: Optional[datetime.timedelta] = None  # noqa: UP007  # Python 3.8 doesn't support `X | Y` for type annotation
+
+
+_EventT = TypeVar("_EventT", bound=Event)
+
+
+class AgentAction(Generic[_EventT]):
+    def __init__(self, context: CloudDataContext):
+        self._context = context
+
+    @abstractmethod
+    def run(self, event: _EventT, id: str) -> ActionResult: ...
