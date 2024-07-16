a28a02aad1079545afbed54a77e157e7850fad72
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-16 03:02:55 +0000

    Add package amitraj/0.0.1/amitraj-0.0.1.tar.gz

---
+++ packages/amitraj/amitraj-0.0.1.tar.gz/amitraj-0.0.1/setup.py
@@ -0,0 +1,24 @@
+from setuptools import setup, find_packages
+ 
+classifiers = [
+  'Development Status :: 5 - Production/Stable',
+  'Intended Audience :: Education',
+  'Operating System :: Microsoft :: Windows :: Windows 10',
+  'License :: OSI Approved :: MIT License',
+  'Programming Language :: Python :: 3'
+]
+ 
+setup(
+  name='amitraj',
+  version='0.0.1',
+  description='List of programs',
+  long_description=open('README.txt').read() + '\n\n' + open('CHANGELOG.txt').read(),
+  url='',  
+  author='Riyansh Prem',
+  author_email='riyansh969@gmail.com',
+  license='MIT', 
+  classifiers=classifiers,
+  keywords='pro', 
+  packages=find_packages(),
+  install_requires=[''] 
+)
