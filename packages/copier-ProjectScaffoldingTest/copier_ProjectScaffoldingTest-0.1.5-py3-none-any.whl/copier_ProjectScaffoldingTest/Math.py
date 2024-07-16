c960b377a1ee887b7af8c2a520ffe2baf46aece3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-16 03:02:58 +0000

    Add package copier-ProjectScaffoldingTest/0.1.5/copier_ProjectScaffoldingTest-0.1.5-py3-none-any.whl

---
+++ packages/copier-ProjectScaffoldingTest/copier_ProjectScaffoldingTest-0.1.5-py3-none-any.whl/copier_ProjectScaffoldingTest/Math.py
@@ -0,0 +1,23 @@
+"""Basic math functions. This file illustrates how to create a python package that contains functions that can be invoked by other python code."""
+
+# Change to force build (1)
+
+
+# ----------------------------------------------------------------------
+def Add(x, y):
+    return x + y
+
+
+# ----------------------------------------------------------------------
+def Sub(x, y):
+    return x - y
+
+
+# ----------------------------------------------------------------------
+def Mult(x, y):
+    return x * y
+
+
+# ----------------------------------------------------------------------
+def Div(x, y):
+    return x / y
