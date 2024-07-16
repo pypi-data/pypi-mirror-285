33f9d090c2c8a1d5d8b59279d2e330c21a3729bc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-16 14:03:52 +0000

    Add package artbox/0.8.0/artbox-0.8.0.tar.gz

---
+++ packages/artbox/artbox-0.8.0.tar.gz/artbox-0.8.0/docs/index.md
@@ -0,0 +1,177 @@
+# ArtBox
+
+ArtBox is a tool set for handling multimedia files.
+
+- Documentation: https://ggpedia.games
+- License: BSD-3 Clause
+
+## Features
+
+TBD
+
+# Setup
+
+ArtBox uses some dependencies that maybe would not work well in your machine. In
+order to have everything well installed, create a conda/mamba environment and
+install `artbox` there.
+
+```bash
+$ mamba create --name artbox "python>=3.8.1,<3.12" pygobject pip
+$ conda activate artbox
+$ pip install artbox
+```
+
+## Examples
+
+For the following examples, create the a temporary folder for artbox:
+
+```bash
+$ mkdir /tmp/artbox
+```
+
+### Convert text to audio
+
+By default, the `artbox speech` uses
+[`edge-tts`](https://pypi.org/project/edge-tts/) engine, but if you can also
+specify [`gtts`](https://github.com/pndurette/gTTS) with the flag
+`--engine gtts`.
+
+```bash
+$ echo "Are you ready to join Link and Zelda in fighting off this unprecedented threat to Hyrule?" > /tmp/artbox/text.md
+$ artbox speech text-to-speech \
+    --title artbox \
+    --input-path /tmp/artbox/text.md \
+    --output-path /tmp/artbox/speech.mp3 \
+    --engine edge-tts
+```
+
+If you need to generate the audio for different language, you can use the flag
+`--lang`:
+
+```bash
+$ echo "Bom dia, mundo!" > /tmp/artbox/text.md
+$ artbox speech text-to-speech \
+    --title artbox \
+    --input-path /tmp/artbox/text.md \
+    --output-path /tmp/artbox/speech.mp3 \
+    --lang pt
+```
+
+If you are using `edge-tts` engine (the default one), you can also specify the
+locale for that language, for example:
+
+```bash
+$ echo "Are you ready to join Link and Zelda in fighting off this unprecedented threat to Hyrule?" > /tmp/artbox/text.md
+$ artbox speech text-to-speech \
+    --title artbox \
+    --input-path /tmp/artbox/text.md \
+    --output-path /tmp/artbox/speech.mp3 \
+    --engine edge-tts \
+    --lang en-IN
+```
+
+Additionally, if you are using edge-tts, you can specify `--rate`, `--volume`,
+and `--pitch`, for example:
+
+```bash
+$ echo "Do you want some coffee?" > /tmp/artbox/text.md
+$ artbox speech text-to-speech \
+    --title artbox \
+    --input-path /tmp/artbox/text.md \
+    --output-path /tmp/artbox/speech.mp3 \
+    --engine edge-tts \
+    --lang en \
+    --rate +10% \
+    --volume -10% \
+    --pitch -5Hz
+```
+
+### Download a youtube video
+
+If you want to download videos from the youtube, you can use the following
+command:
+
+```bash
+$ artbox youtube download \
+    --url https://www.youtube.com/watch?v=zw47_q9wbBE \
+    --output-path /tmp/artbox/
+```
+
+The command above downloads using a random resolution. If you want a specific
+resolution, use the flat `--resolution`:
+
+```bash
+$ artbox youtube download \
+    --url https://www.youtube.com/watch?v=zw47_q9wbBE \
+    --output-path /tmp/artbox/ \
+    --resolution 360p
+```
+
+### Create a song based on the musical notes
+
+```bash
+# json format
+echo '["E", "D#", "E", "D#", "E", "B", "D", "C", "A"]' > /tmp/artbox/notes.txt
+$ artbox sound notes-to-audio \
+  --input-path /tmp/artbox/notes.txt \
+  --output-path /tmp/artbox/music.mp3 \
+  --duration 2
+```
+
+### Remove the audio from a video
+
+First, download the youtube video `https://www.youtube.com/watch?v=zw47_q9wbBE`
+as explained before.
+
+Next, run the following command:
+
+```bash
+$ artbox video remove-audio \
+  --input-path "/tmp/artbox/The Legend of Zelda Breath of the Wild - Nintendo Switch Presentation 2017 Trailer.mp4" \
+  --output-path /tmp/artbox/botw.mp4
+```
+
+### Extract the audio from a video
+
+First, download the youtube video `https://www.youtube.com/watch?v=zw47_q9wbBE`
+as explained before.
+
+Next, run the following command:
+
+```bash
+$ artbox video extract-audio \
+  --input-path "/tmp/artbox/The Legend of Zelda Breath of the Wild - Nintendo Switch Presentation 2017 Trailer.mp4" \
+  --output-path /tmp/artbox/botw-audio.mp3
+```
+
+### Combine audio and video files
+
+First, execute the previous steps:
+
+- Download a youtube video
+- Remove the audio from a video
+- Extract the audio from a video
+
+Next, run the following command:
+
+```bash
+$ artbox video combine-video-and-audio \
+  --video-path /tmp/artbox/botw.mp4 \
+  --audio-path /tmp/artbox/botw-audio.mp3 \
+  --output-path /tmp/artbox/botw-combined.mp4
+```
+
+## Additional dependencies
+
+If you want to use Python to play your audio files, you can install `playsound`:
+
+```bash
+$ pip wheel --use-pep517 "playsound (==1.3.0)"
+```
+
+## Troubleshoot
+
+After installing with `poetry install`:
+
+- Patch `pytube` (ref: https://github.com/pytube/pytube/issues/1773):
+  `sed -i 's/(r"^$\\w+\\W")/(r"^\\w+\\W")/' $CONDA_PREFIX/lib/python3.*/site-packages/pytube/cipher.py`
---
+++ packages/artbox/artbox-0.8.0.tar.gz/artbox-0.8.0/src/artbox/cli.py
@@ -0,0 +1,403 @@
+"""Cli functions to define the arguments and to call Makim."""
+
+import typer
+
+from typing_extensions import Annotated
+
+from artbox import __version__
+from artbox.sounds import Sound
+from artbox.speech import SpeechFromText, SpeechToText
+from artbox.videos import Video, Youtube
+
+app = typer.Typer(
+    name="Artbox",
+    help="A set of tools for handling multimedia files.",
+    epilog=(
+        "If you have any problem, open an issue at: "
+        "https://github.com/ggpedia/artbox"
+    ),
+)
+
+app_sound = typer.Typer(
+    name="sound",
+    help="Audio processing commands for Artbox.",
+    short_help="Audio processing commands.",
+)
+app_video = typer.Typer(
+    name="video",
+    help="Video processing commands for Artbox.",
+    short_help="Video processing commands.",
+)
+app_speech = typer.Typer(
+    name="speech",
+    help="Speech processing commands for Artbox.",
+    short_help="Speech processing commands.",
+)
+app_youtube = typer.Typer(
+    name="youtube",
+    help="YouTube processing commands for Artbox.",
+    short_help="YouTube processing commands.",
+)
+
+app.add_typer(app_sound, name="sound")
+app.add_typer(app_video, name="video")
+app.add_typer(app_speech, name="speech")
+app.add_typer(app_youtube, name="youtube")
+
+
+@app.callback(invoke_without_command=True)
+def main(
+    ctx: typer.Context,
+    version: bool = typer.Option(
+        None,
+        "--version",
+        "-v",
+        is_flag=True,
+        help="Show the version and exit.",
+    ),
+) -> None:
+    """Process commands for specific flags; otherwise, show the help menu."""
+    if version:
+        typer.echo(f"Version: {__version__}")
+        raise typer.Exit()
+
+    if ctx.invoked_subcommand is None:
+        typer.echo(ctx.get_help())
+        raise typer.Exit(0)
+
+
+@app_speech.command("from-text")
+def speech_from_text(
+    title: Annotated[
+        str, typer.Option("--title", help="Specify the name of the audio file")
+    ] = "artbox",
+    input_path: Annotated[
+        str,
+        typer.Option(
+            "--input-path", help="Specify the path of the text file (txt)"
+        ),
+    ] = "",
+    output_path: Annotated[
+        str,
+        typer.Option(
+            "--output-path", help="Specify the path to store the audio file"
+        ),
+    ] = "",
+    engine: Annotated[
+        str,
+        typer.Option(
+            "--engine",
+            help="Choose the text-to-speech engine (Options: edge-tts, gtts)",
+        ),
+    ] = "edge-tts",
+    lang: Annotated[
+        str,
+        typer.Option(
+            "--lang", help="Choose the language for audio generation"
+        ),
+    ] = "en",
+    rate: Annotated[
+        str,
+        typer.Option("--rate", help="Decrease/Increase the rate level"),
+    ] = "+0%",
+    volume: Annotated[
+        str,
+        typer.Option("--volume", help="Decrease/Increase the volume level"),
+    ] = "+0%",
+    pitch: Annotated[
+        str,
+        typer.Option("--pitch", help="Decrease/Increase the pitch level"),
+    ] = "+0Hz",
+) -> None:
+    """Convert text to speech."""
+    args_dict = {
+        "title": title,
+        "input-path": input_path,
+        "output-path": output_path,
+        "engine": engine,
+        "lang": lang,
+        "rate": rate,
+        "volume": volume,
+        "pitch": pitch,
+    }
+
+    runner = SpeechFromText(args_dict)
+    runner.convert()
+
+
+@app_speech.command("to-text")
+def speech_to_text(
+    input_path: Annotated[
+        str,
+        typer.Option(
+            "--input-path",
+            help="Specify the path of the audio file (mp3 or wav)",
+        ),
+    ] = "",
+    output_path: Annotated[
+        str,
+        typer.Option(
+            "--output-path", help="Specify the path to store the text file"
+        ),
+    ] = "",
+    engine: Annotated[
+        str,
+        typer.Option(
+            "--engine",
+            help="Choose the text-to-speech engine (Options: google)",
+        ),
+    ] = "google",
+    lang: Annotated[
+        str,
+        typer.Option(
+            "--lang", help="Choose the language for audio generation"
+        ),
+    ] = "en",
+) -> None:
+    """Convert text to speech."""
+    args_dict = {
+        "input-path": input_path,
+        "output-path": output_path,
+        "engine": engine,
+        "lang": lang,
+    }
+
+    runner = SpeechToText(args_dict)
+    runner.convert()
+
+
+@app_sound.command("notes-to-audio")
+def sound_notes_to_audio(
+    input_path: Annotated[
+        str,
+        typer.Option(
+            "--input-path", help="Specify the path of the input file"
+        ),
+    ] = "",
+    output_path: Annotated[
+        str,
+        typer.Option(
+            "--output-path", help="Specify the path to store the audio file"
+        ),
+    ] = "",
+    duration: Annotated[
+        str,
+        typer.Option("--duration", help="Specify the duration of the audio"),
+    ] = "",
+) -> None:
+    """Convert notes to audio."""
+    args_dict = {
+        "input-path": input_path,
+        "output-path": output_path,
+        "duration": duration,
+    }
+
+    runner = Sound(args_dict)
+    runner.notes_to_audio()
+
+
+@app_sound.command("spectrogram")
+def sound_spectrogram(
+    input_path: Annotated[
+        str,
+        typer.Option(
+            "--input-path", help="Specify the path of the input file"
+        ),
+    ] = "",
+    output_path: Annotated[
+        str,
+        typer.Option(
+            "--output-path", help="Specify the path to store the audio file"
+        ),
+    ] = "",
+) -> None:
+    """Generate a spectrogram from an MP3 file and saves it as an image."""
+    args_dict = {
+        "input-path": input_path,
+        "output-path": output_path,
+    }
+
+    runner = Sound(args_dict)
+    runner.spectrogram()
+
+
+@app_video.command("remove-audio")
+def video_remove_audio(
+    input_path: Annotated[
+        str,
+        typer.Option(
+            "--input-path", help="Specify the path of the input video file"
+        ),
+    ] = "",
+    output_path: Annotated[
+        str,
+        typer.Option(
+            "--output-path", help="Specify the path to store the video file"
+        ),
+    ] = "",
+) -> None:
+    """Remove audio from video file."""
+    args_dict = {
+        "input-path": input_path,
+        "output-path": output_path,
+    }
+
+    runner = Video(args_dict)
+    runner.remove_audio()
+
+
+@app_video.command("extract-audio")
+def video_extract_audio(
+    input_path: Annotated[
+        str,
+        typer.Option(
+            "--input-path", help="Specify the path of the input video file"
+        ),
+    ] = "",
+    output_path: Annotated[
+        str,
+        typer.Option(
+            "--output-path",
+            help="Specify the path to store the extracted audio file",
+        ),
+    ] = "",
+) -> None:
+    """Extract audio from video file."""
+    args_dict = {
+        "input-path": input_path,
+        "output-path": output_path,
+    }
+
+    runner = Video(args_dict)
+    runner.extract_audio()
+
+
+@app_video.command("get-metadata")
+def video_get_metadata(
+    input_path: Annotated[
+        str,
+        typer.Option(
+            "--input-path", help="Specify the path of the input video file"
+        ),
+    ] = "",
+    output_path: Annotated[
+        str,
+        typer.Option(
+            "--output-path",
+            help="Specify the path to store the extracted audio file",
+        ),
+    ] = "",
+) -> None:
+    """Get the metadata from a video (mp4)."""
+    args_dict = {
+        "input-path": input_path,
+        "output-path": output_path,
+    }
+
+    runner = Video(args_dict)
+    runner.get_metadata()
+
+
+@app_video.command("combine-video-and-audio")
+def video_combine_audio_and_video(
+    video_path: Annotated[
+        str,
+        typer.Option(
+            "--video-path", help="Specify the path of the video file"
+        ),
+    ] = "",
+    audio_path: Annotated[
+        str,
+        typer.Option(
+            "--audio-path", help="Specify the path of the audio file"
+        ),
+    ] = "",
+    output_path: Annotated[
+        str,
+        typer.Option(
+            "--output-path",
+            help="Specify the path to store the combined video file",
+        ),
+    ] = "",
+) -> None:
+    """Combine audio and video files."""
+    args_dict = {
+        "video-path": video_path,
+        "audio-path": audio_path,
+        "output-path": output_path,
+    }
+
+    runner = Video(args_dict)
+    runner.combine_video_and_audio()
+
+
+@app_youtube.command("download")
+def youtube_download(
+    url: Annotated[
+        str,
+        typer.Option(
+            "--url", help="Specify the URL of the YouTube video to download"
+        ),
+    ] = "",
+    output_path: Annotated[
+        str,
+        typer.Option(
+            "--output-path",
+            help="Specify the path to store the downloaded video file",
+        ),
+    ] = "",
+    resolution: Annotated[
+        str,
+        typer.Option(
+            "--resolution", help="Set the quality of the downloaded video"
+        ),
+    ] = "",
+) -> None:
+    """Download youtube video."""
+    args_dict = {
+        "url": url,
+        "output-path": output_path,
+        "resolution": resolution,
+    }
+
+    runner = Youtube(args_dict)
+    runner.download()
+
+
+@app_youtube.command("cc")
+def youtube_cc(
+    url: Annotated[
+        str,
+        typer.Option(
+            "--url", help="Specify the URL of the YouTube video to download"
+        ),
+    ] = "",
+    output_path: Annotated[
+        str,
+        typer.Option(
+            "--output-path",
+            help=(
+                "Specify the path to store the downloaded video file "
+                "(.srt, .txt)"
+            ),
+        ),
+    ] = "/tmp/cc.txt",
+    lang: Annotated[
+        str,
+        typer.Option("--lang", help="Set the CC language to be downloaded"),
+    ] = "en",
+    format: Annotated[
+        str,
+        typer.Option("--format", help="Set the CC format (srt, text)"),
+    ] = "text",
+) -> None:
+    """Download youtube video CC."""
+    args_dict = {
+        "url": url,
+        "output-path": output_path,
+        "lang": lang,
+        "format": format,
+    }
+
+    runner = Youtube(args_dict)
+    runner.download_captions()
