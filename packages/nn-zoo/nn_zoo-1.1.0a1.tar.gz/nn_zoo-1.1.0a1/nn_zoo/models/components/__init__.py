e25f87d64ab1ab77d95d23a34f6975aabe8b9a2c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-16 04:04:03 +0000

    Add package nn-zoo/1.1.0a1/nn_zoo-1.1.0a1.tar.gz

---
+++ packages/nn-zoo/nn_zoo-1.1.0a1.tar.gz/nn_zoo-1.1.0a1/nn_zoo/models/components/__init__.py
@@ -0,0 +1,25 @@
+from .attention import SelfAttention
+from .residual import ResidualBasicBlock, ResidualBottleNeckBlock, ResidualStack
+from .vq import VectorQuantizer
+from .depthwise import (
+    DepthwiseConv1d,
+    DepthwiseConv2d,
+    DepthwiseConv3d,
+    DepthwiseSeparableConv1d,
+    DepthwiseSeparableConv2d,
+    DepthwiseSeparableConv3d,
+)
+
+__all__ = [
+    "DepthwiseConv1d",
+    "DepthwiseConv2d",
+    "DepthwiseConv3d",
+    "DepthwiseSeparableConv1d",
+    "DepthwiseSeparableConv2d",
+    "DepthwiseSeparableConv3d",
+    "SelfAttention",
+    "ResidualBasicBlock",
+    "ResidualBottleNeckBlock",
+    "ResidualStack",
+    "VectorQuantizer",
+]
