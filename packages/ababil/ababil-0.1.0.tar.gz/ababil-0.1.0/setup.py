3514bd03104e0c81d9f1e9e2cccf7241ce67e895
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-16 14:03:57 +0000

    Add package ababil/0.1.0/ababil-0.1.0.tar.gz

---
+++ packages/ababil/ababil-0.1.0.tar.gz/ababil-0.1.0/setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup, find_packages
+ 
+# Setting up
+setup(
+    name="ababil",
+    version='0.1.0',
+    author="Ababil Hossain",
+    author_email="Ababilhossain35@gmail.com",
+    description="",
+    long_description_content_type="",
+ 
+    packages=find_packages(),
+    install_requires=[],
+    keywords=[],
+    classifiers=[]
+)
