5e7c3e1acc2a8bd335c0bc53e3f2c3338988c2c0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-16 16:07:21 +0000

    Add package cyclic-agent/0.1.0/cyclic_agent-0.1.0-py3-none-any.whl

---
+++ packages/cyclic-agent/cyclic_agent-0.1.0-py3-none-any.whl/cyclic_agent/state.py
@@ -0,0 +1,26 @@
+from __future__ import annotations
+from abc import abstractmethod
+from pydantic import BaseModel
+
+
+class State[SigT](BaseModel):
+    @abstractmethod
+    def next(self, signal: SigT | None = None) -> State[SigT]:
+        """Transition to the next state."""
+        raise NotImplementedError
+
+
+# def state(fn: Callable[[A], S]):
+#     sig = inspect.signature(fn)
+#
+#     def wrapper(*args, **kwargs) -> State[A, S]:
+#         bound_args = sig.bind(*args, **kwargs)
+#         bound_args.apply_defaults()
+#         if bound_args.arguments:
+#             state_input = next(iter(bound_args.arguments.values()))
+#         else:
+#             raise ValueError("Function requires at least one argument, none provided")
+#         a: State[A, S] = State(state_input, fn)
+#         return a
+#
+#     return wrapper
