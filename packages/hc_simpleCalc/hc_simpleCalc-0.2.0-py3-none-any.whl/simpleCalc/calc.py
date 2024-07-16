ca353613d3392cf881d54c860b293bb656252557
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-16 16:05:40 +0000

    Add package hc_simpleCalc/0.2.0/hc_simpleCalc-0.2.0-py3-none-any.whl

---
+++ packages/hc_simpleCalc/hc_simpleCalc-0.2.0-py3-none-any.whl/simpleCalc/calc.py
@@ -0,0 +1,10 @@
+# src/simpleCalc/calc.py
+
+def add:
+	print("더하기")
+
+def div:
+	print("나누기")
+
+def mul:
+	print("곱하기")
---
+++ packages/hc_simpleCalc/hc_simpleCalc-0.2.0-py3-none-any.whl/simplecalc/calc.py
@@ -0,0 +1,10 @@
+# calc.py
+
+def addition():
+	print("더하기")
+
+def division():
+	print("나누기")
+
+def multiply():
+	print("곱하기")
