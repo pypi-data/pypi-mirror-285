61c0552c7afb66067897d83dd4d282975f1959d6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-16 03:02:50 +0000

    Add package sageworks/0.6.16/sageworks-0.6.16.tar.gz

---
+++ packages/sageworks/sageworks-0.6.16.tar.gz/sageworks-0.6.16/setup.py
@@ -0,0 +1,46 @@
+#!/usr/bin/env python
+"""Setup.py for SageWorks: Sagemaker Workbench"""
+
+from setuptools import setup, find_namespace_packages
+
+# Readme
+with open("README.md", "r") as f:
+    readme = f.read()
+
+# Requirements
+with open("requirements.txt", "r") as f:
+    install_requires = f.read().strip().split("\n")
+
+setup(
+    name="sageworks",
+    description="SageWorks: A Python WorkBench for creating and deploying AWS SageMaker Models",
+    long_description=readme,
+    long_description_content_type="text/markdown",
+    author="SuperCowPowers LLC",
+    author_email="support@supercowpowers.com",
+    url="https://github.com/SuperCowPowers/sageworks",
+    package_dir={"": "src"},
+    packages=find_namespace_packages(where="src"),
+    include_package_data=True,
+    package_data={
+        "sageworks": [
+            "resources/signature_verify_pub.pem",
+            "resources/open_source_api.key",
+            "core/transforms/features_to_model/light_xgb_model/xgb_model.template",
+            "core/transforms/features_to_model/light_xgb_model/requirements.txt",
+        ]
+    },
+    install_requires=install_requires,
+    license="MIT",
+    keywords="SageMaker, Machine Learning, AWS, Python, Utilities",
+    classifiers=[
+        "Development Status :: 4 - Beta",
+        "License :: OSI Approved :: MIT License",
+        "Programming Language :: Python :: 3.10",
+        "Programming Language :: Python :: 3.11",
+        "Programming Language :: Python :: 3.12",
+        "Topic :: Scientific/Engineering",
+    ],
+    setup_requires=["setuptools_scm"],
+    entry_points={"console_scripts": "sageworks = sageworks.repl.sageworks_shell:launch_shell"},
+)
---
+++ packages/sageworks/sageworks-0.6.16.tar.gz/sageworks-0.6.16/tests/create_test_artifacts/delete_test_artifacts.py
@@ -0,0 +1,142 @@
+"""This Script Deletes the SageWorks Artifacts used for the tests"""
+
+import time
+from sageworks.api.data_source import DataSource
+from sageworks.api.feature_set import FeatureSet
+from sageworks.api.model import Model
+from sageworks.api.endpoint import Endpoint
+from sageworks.aws_service_broker.aws_service_broker import AWSServiceBroker
+
+
+if __name__ == "__main__":
+    # This forces a refresh on all the data we get from the AWs Broker
+    AWSServiceBroker().get_all_metadata(force_refresh=True)
+
+    # Delete the test_data Artifacts
+    ds = DataSource("test_data")
+    if ds.exists():
+        print("Deleting test_data...")
+        ds.delete()
+    ds = DataSource("abc")
+    if ds.exists():
+        print("Deleting abc...")
+        ds.delete()
+    ds = DataSource("abc_2")
+    if ds.exists():
+        print("Deleting abc_2...")
+        ds.delete()
+    fs = FeatureSet("test_features")
+    if fs.exists():
+        print("Deleting test_features...")
+        fs.delete()
+    m = Model("test-model")
+    if m.exists():
+        print("Deleting test-model model...")
+        m.delete()
+    end = Endpoint("test-end")
+    if end.exists():
+        print("Deleting test-end endpoint...")
+        end.delete()
+    end = Endpoint("abc")
+    if end.exists():
+        print("Deleting abc endpoint...")
+        end.delete()
+    end = Endpoint("abc-2")
+    if end.exists():
+        print("Deleting abc-2 endpoint...")
+        end.delete()
+
+    # Delete the abalone_data Artifacts
+    ds = DataSource("abalone_data")
+    if ds.exists():
+        print("Deleting abalone_data...")
+        ds.delete()
+    ds = DataSource("abalone_data_copy")
+    if ds.exists():
+        print("Deleting abalone_data_copy...")
+        ds.delete()
+    fs = FeatureSet("abalone_features")
+    if fs.exists():
+        print("Deleting abalone_features...")
+        fs.delete()
+    m = Model("abalone-regression")
+    if m.exists():
+        print("Deleting abalone-regression model...")
+        m.delete()
+    m = Model("abalone-regression-full")
+    if m.exists():
+        print("Deleting abalone-regression-full model...")
+        m.delete()
+    end = Endpoint("abalone-regression-end")
+    if end.exists():
+        print("Deleting abalone-regression-end...")
+        end.delete()
+    end = Endpoint("abalone-regression-end-rt")
+    if end.exists():
+        print("Deleting abalone-regression-end-rt...")
+        end.delete()
+
+    # Delete abalone_classification Artifacts
+    fs = FeatureSet("abalone_classification")
+    if fs.exists():
+        print("Deleting abalone_classification...")
+        fs.delete()
+    m = Model("abalone-classification")
+    if m.exists():
+        print("Deleting abalone-classification model...")
+        m.delete()
+    end = Endpoint("abalone-classification-end")
+    if end.exists():
+        print("Deleting abalone-classification-end endpoint...")
+        end.delete()
+
+    # Delete the wine_data Artifacts
+    ds = DataSource("wine_data")
+    if ds.exists():
+        print("Deleting wine_data...")
+        ds.delete()
+    fs = FeatureSet("wine_features")
+    if fs.exists():
+        print("Deleting wine_features...")
+        fs.delete()
+    m = Model("wine-classification")
+    if m.exists():
+        print("Deleting wine-classification model...")
+        m.delete()
+    end = Endpoint("wine-classification-end")
+    if end.exists():
+        print("Deleting wine-classification-end endpoint...")
+        end.delete()
+
+    # AQSol Artifacts
+    ds = DataSource("aqsol_data")
+    if ds.exists():
+        print("Deleting aqsol_data...")
+        ds.delete()
+    fs = FeatureSet("aqsol_features")
+    if fs.exists():
+        print("Deleting aqsol_features...")
+        fs.delete()
+    m = Model("aqsol-regression")
+    if m.exists():
+        print("Deleting aqsol-regression model...")
+        m.delete()
+    end = Endpoint("aqsol-regression-end")
+    if end.exists():
+        print("Deleting aqsol-regression-end endpoint...")
+        end.delete()
+    fs = FeatureSet("aqsol_mol_descriptors")
+    if fs.exists():
+        print("Deleting aqsol_mol_descriptors...")
+        fs.delete()
+    m = Model("aqsol-descriptors-regression")
+    if m.exists():
+        print("Deleting aqsol-mol-regression model...")
+        m.delete()
+    end = Endpoint("aqsol-mol-regression-end")
+    if end.exists():
+        print("Deleting aqsol-mol-regression-end endpoint...")
+        end.delete()
+
+    time.sleep(5)
+    print("All test artifacts should now be deleted!")
