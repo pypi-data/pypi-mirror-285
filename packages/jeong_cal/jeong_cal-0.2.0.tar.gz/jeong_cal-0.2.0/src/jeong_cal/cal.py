257a0c41b61283d8dc873639505ce466205b83f8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-16 16:05:41 +0000

    Add package jeong_cal/0.2.0/jeong_cal-0.2.0.tar.gz

---
+++ packages/jeong_cal/jeong_cal-0.2.0.tar.gz/jeong_cal-0.2.0/src/jeong_cal/cal.py
@@ -0,0 +1,14 @@
+from haram_sum.sum import sum
+from jeong_mul.mul import mul
+from haram_sub.sub import sub
+def cl_sum():
+    print(sum())
+
+
+def cl_sub():
+    print(sub())
+
+
+def cl_mul():
+    print(mul())
+
