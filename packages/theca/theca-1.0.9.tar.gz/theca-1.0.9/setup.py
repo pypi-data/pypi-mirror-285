9cc97a35cdee895cd0f87a6c0e47232c7fa242ef
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-16 17:03:54 +0000

    Add package theca/1.0.9/theca-1.0.9.tar.gz

---
+++ packages/theca/theca-1.0.9.tar.gz/theca-1.0.9/setup.py
@@ -0,0 +1,17 @@
+from setuptools import setup
+
+setup(
+    name='theca',
+    version='1.0.9',
+    author='Xizhen Du',
+    author_email='xizhendu@gmail.com',
+    url='https://github.com/xizhendu/theca-sdk-python',
+    long_description=open('README.md', 'r').read(),
+    long_description_content_type='text/markdown',
+    description='Simple Python client library for TheCA.cn',
+    # packages=['thedns'],
+    install_requires=[
+        "requests",
+        "theid"
+    ]
+)
