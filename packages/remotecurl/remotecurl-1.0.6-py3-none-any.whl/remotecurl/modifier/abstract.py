c98b9a39f46f64b554cd609de7c39c79a9355243
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-16 16:04:59 +0000

    Add package remotecurl/1.0.6/remotecurl-1.0.6-py3-none-any.whl

---
+++ packages/remotecurl/remotecurl-1.0.6-py3-none-any.whl/remotecurl/modifier/abstract.py
@@ -0,0 +1,40 @@
+"""This file contains an abstract modifier class"""
+
+
+from typing import Optional
+from remotecurl.common.util import check_args, get_absolute_url
+
+
+class Modifier:
+    
+    path: str
+    url: str
+    allow_url_rules: list[str]
+    deny_url_rules: list[str]
+    encoding: str
+    
+    def __init__(
+        self, url: str, path: str, encoding: Optional[str] = None,
+        allow_url_rules: list[str] = ["^(.*)$"], deny_url_rules: list[str] = []
+    ) -> None:
+        """Initialize a modifier"""
+        self.url = url
+        self.path = path
+        self.encoding = encoding
+        self.allow_url_rules = allow_url_rules
+        self.deny_url_rules = deny_url_rules
+
+    def _modify_link(self, relative_url: str) -> None:
+        """Modify given link"""
+        if relative_url == "#":
+            return relative_url        
+        else:
+            abs_url = get_absolute_url(self.url, relative_url)
+            if check_args(abs_url, self.allow_url_rules, self.deny_url_rules):
+                return self.path + abs_url
+            else:
+                return relative_url
+
+    def get_modified_content(self) -> None:
+        """Get the modified content (Abstract)"""
+        pass
