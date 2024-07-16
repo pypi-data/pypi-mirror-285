04e2b50b8c88fa3a8b808c4d3b043d55292772e6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-16 17:04:13 +0000

    Add package pretty-print-dmeurer/1.1.0/pretty_print_dmeurer-1.1.0.tar.gz

---
+++ packages/pretty-print-dmeurer/pretty_print_dmeurer-1.1.0.tar.gz/pretty_print_dmeurer-1.1.0/main.py
@@ -0,0 +1,7 @@
+from src import pretty_print_dmeurer
+
+if __name__ == '__main__':
+    pretty_print_dmeurer.color_demo()
+    print(pretty_print_dmeurer.Colors.FG.green + "HEY")
+    print(pretty_print_dmeurer.Colors.FG.more(142) + "HEY")
+
