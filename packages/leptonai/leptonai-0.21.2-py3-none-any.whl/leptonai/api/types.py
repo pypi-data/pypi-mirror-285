52ec37e38c4e7c9db755fd051cc21f90b9f2e68f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-16 04:04:05 +0000

    Add package leptonai/0.21.2/leptonai-0.21.2-py3-none-any.whl

---
+++ packages/leptonai/leptonai-0.21.2-py3-none-any.whl/leptonai/api/types.py
@@ -0,0 +1,44 @@
+# flake8: noqa
+"""
+Types for the Lepton AI API.
+
+These types are used as wrappers of the json payloads used by the API.
+"""
+
+from enum import Enum
+from typing import List, Optional, Union
+import warnings
+from pydantic import BaseModel, Field
+
+from leptonai.config import LEPTON_RESERVED_ENV_NAMES, VALID_SHAPES
+
+from .v1.types.common import Metadata
+
+from .v1.types.deployment import (
+    ResourceRequirement,
+    TokenValue,
+    TokenVar,
+    EnvValue,
+    EnvVar,
+    MountOptions,
+    Mount,
+    ScaleDown,
+    AutoScaler,
+    HealthCheckLiveness,
+    HealthCheck,
+    LeptonDeploymentState,
+    ContainerPort,
+    LeptonContainer,
+    AutoscalerCondition,
+    AutoScalerStatus,
+    LeptonResourceAffinity,
+)
+
+from .v1.types.deployment import (
+    LeptonDeploymentUserSpec as DeploymentUserSpec,
+    DeploymentEndpoint,
+    LeptonDeploymentStatus as DeploymentStatus,
+    LeptonDeployment as Deployment,
+)
+
+from .v1.types.job import LeptonJob, LeptonJobUserSpec, LeptonJobState, LeptonJobStatus
