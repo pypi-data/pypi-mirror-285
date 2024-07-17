ba38a1e093ffac3a907ec72d0a6c3d7e7c530fc9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-17 16:05:03 +0000

    Add package MeUtils/2024.7.17.16.41.58/meutils-2024.7.17.16.41.58.tar.gz

---
+++ packages/MeUtils/meutils-2024.7.17.16.41.58.tar.gz/meutils-2024.7.17.16.41.58/meutils/serving/fastapi/dependencies/auth.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# @Project      : AI.  @by PyCharm
+# @File         : auth
+# @Time         : 2023/12/19 17:12
+# @Author       : betterme
+# @WeChat       : meutils
+# @Software     : PyCharm
+# @Description  : 
+
+from meutils.pipe import *
+
+from fastapi.security import HTTPBearer, HTTPAuthorizationCredentials
+
+get_bearer_token = HTTPBearer()
