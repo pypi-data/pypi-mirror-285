885a369c3bd89c3d98a71e14e0a4ccc3f19cd364
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-17 15:04:00 +0000

    Add package shotstack-sdk/0.2.7/shotstack-sdk-0.2.7.tar.gz

---
+++ packages/shotstack-sdk/shotstack-sdk-0.2.7.tar.gz/shotstack-sdk-0.2.7/setup.py
@@ -0,0 +1,36 @@
+from setuptools import setup, find_packages  # noqa: H301
+from pathlib import Path
+
+NAME = "shotstack-sdk"
+VERSION = "0.2.7"
+# To install the library, run the following
+#
+# python setup.py install
+#
+# prerequisite: setuptools
+# http://pypi.python.org/pypi/setuptools
+
+REQUIRES = [
+  "urllib3 >= 1.25.3",
+  "python-dateutil",
+]
+
+this_directory = Path(__file__).parent
+long_description = (this_directory / "README.md").read_text()
+
+setup(
+    name=NAME,
+    version=VERSION,
+    description="Shotstack",
+    author="Shotstack",
+    author_email="pypi@shotstack.io",
+    url="https://shotstack.io/product/sdk/python/",
+    keywords=["shotstack", "video", "video editing", "video editor", "video editing api", "video generation", "video manipulation", "ffmpeg"],
+    python_requires=">=3.6",
+    install_requires=REQUIRES,
+    packages=find_packages(exclude=["test", "tests"]),
+    include_package_data=True,
+    license="MIT",
+    long_description=long_description,
+    long_description_content_type="text/markdown"
+)
