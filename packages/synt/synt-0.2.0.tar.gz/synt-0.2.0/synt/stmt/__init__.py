1bc0f787e2bf062c9544957bc9167c6800632dd4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-17 16:04:49 +0000

    Add package synt/0.2.0/synt-0.2.0.tar.gz

---
+++ packages/synt/synt-0.2.0.tar.gz/synt-0.2.0/synt/stmt/__init__.py
@@ -0,0 +1,42 @@
+from __future__ import annotations
+
+
+__all__ = [
+    "block",
+    "stmt",
+    "fn",
+    "returns",
+    "cls",
+    "decorator",
+    "keyword",
+    "delete",
+    "assign",
+    "assertion",
+    "raising",
+    "importing",
+    "branch",
+    "loop",
+    "try_catch",
+    "context",
+    "namespace",
+    "expression",
+]
+
+from . import assertion
+from . import assign
+from . import block
+from . import branch
+from . import cls
+from . import context
+from . import decorator
+from . import delete
+from . import expression
+from . import fn
+from . import importing
+from . import keyword
+from . import loop
+from . import namespace
+from . import raising
+from . import returns
+from . import stmt
+from . import try_catch
