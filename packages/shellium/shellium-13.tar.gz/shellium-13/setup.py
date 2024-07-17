fcf4c83c5b62027a1ad7f800418ade270229c387
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-17 03:04:10 +0000

    Add package shellium/13/shellium-13.tar.gz

---
+++ packages/shellium/shellium-13.tar.gz/shellium-13/setup.py
@@ -0,0 +1,32 @@
+from setuptools import setup
+
+with open('README.md', encoding='utf-8') as f:
+    long_description = f.read()
+
+name = 'shellium'
+version = '13'
+author = 'GooseG4G'
+email = 'danilnegusev@inbox.ru'
+desc = 'The package provides a wrapper for working with chrome driver.'
+url = 'https://github.com/GooseG4G/shellium'
+packages = ['shellium']
+install_requires = [
+    'psutil',
+    'selenium==4.22.0',
+    'undetected_chromedriver==3.5.5',
+]
+python_requires = '>=3.10.6'
+
+setup(
+    name=name,
+    author=author,
+    author_email=email,
+    url=url,
+    version=version,
+    description=desc,
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    packages=packages,
+    install_requires=install_requires,
+    python_requires=python_requires,
+)
