15db8b4edd10479157f80728a6db50b19e246285
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-17 03:03:50 +0000

    Add package theid/1.0.6/theid-1.0.6.tar.gz

---
+++ packages/theid/theid-1.0.6.tar.gz/theid-1.0.6/setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup
+
+setup(
+    name='theid',
+    version='1.0.6',
+    author='Xizhen Du',
+    author_email='xizhendu@gmail.com',
+    url='https://github.com/xizhendu/theid-sdk-python',
+    long_description=open('README.md', 'r').read(),
+    long_description_content_type='text/markdown',
+    description='Identity Service - https://devnull.cn/identity',
+    # packages=['thedns'],
+    install_requires=[
+        "requests",
+    ]
+)
