3970285d43c165100086228aa9d9f4a0245c6216
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-17 15:04:20 +0000

    Add package dynamic-links/0.2.1/dynamic_links-0.2.1-py3-none-any.whl

---
+++ packages/dynamic-links/dynamic_links-0.2.1-py3-none-any.whl/dlframe/Logger.py
@@ -0,0 +1,35 @@
+class Logger:
+    def __init__(self, name, trigger=None) -> None:
+        self.name = name
+        self.trigger = trigger
+
+    def print(self, *x, **kwargs):
+        if self.trigger is not None:
+            self.trigger({
+                'type': 'print', 
+                'name': self.name, 
+                'args': x, 
+                'kwargs': kwargs
+            })
+            return
+        print('[{}]: '.format(self.name), end=' ')
+        print(*x, **kwargs)
+
+    def imshow(self, img):
+        if self.trigger is not None:
+            self.trigger({
+                'type': 'imshow', 
+                'name': self.name, 
+                'args': img
+            })
+            return
+        print(img)
+
+    @classmethod
+    def get_logger(cls, name):
+        logger = cls(name, trigger=Logger.global_trigger)
+        cls.loggers.setdefault(id(logger), logger)
+        return logger
+
+Logger.loggers = {}
+Logger.global_trigger = None
