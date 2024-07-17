e45e59553b2069ccd6a0a5943c588da3c2b60f74
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-17 15:04:19 +0000

    Add package canadapostwrapper/0.4.0/canadapostwrapper-0.4.0.tar.gz

---
+++ packages/canadapostwrapper/canadapostwrapper-0.4.0.tar.gz/canadapostwrapper-0.4.0/setup.py
@@ -0,0 +1,22 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='canadapostwrapper',
+    version='0.4.0',
+    packages=find_packages(),
+    install_requires=[
+        'requests',
+    ],
+    author='shibakek',
+    author_email='wilhemnorman732@gmail.com',
+    description='A wrapper for tracking packages with Canada Post',
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
+    url='https://github.com/shibakek2/canada-post',
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+    ],
+    python_requires='>=3.6',
+)
