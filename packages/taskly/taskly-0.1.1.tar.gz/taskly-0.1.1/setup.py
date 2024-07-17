f2bd3f278edd7af92d97b63d4e70da39c09b3a38
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-17 03:04:28 +0000

    Add package taskly/0.1.1/taskly-0.1.1.tar.gz

---
+++ packages/taskly/taskly-0.1.1.tar.gz/taskly-0.1.1/setup.py
@@ -0,0 +1,21 @@
+from setuptools import setup, find_packages
+from src.Version import VERSION
+
+if __name__ == "__main__":
+    try:
+        setup(
+            version=VERSION,
+            use_scm_version={"version_scheme": "no-guess-dev"},
+            # packages=find_packages(where='src'),
+            # package_dir={'': 'src'},
+            package_data={'': ['scripts/reset.py']},
+        )
+    except Exception as e:
+        print(
+            "\n\nAn error occurred while building the project:\n",
+            str(e),
+            "\n\nPlease ensure you have the most updated version of setuptools, "
+            "setuptools_scm, and wheel with:\n"
+            "   pip install -U setuptools setuptools_scm wheel\n\n"
+        )
+        raise
