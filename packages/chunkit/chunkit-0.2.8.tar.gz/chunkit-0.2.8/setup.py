a37dd6e86cf2074dcc98ffafc9390f93d6163caf
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-17 05:04:55 +0000

    Add package chunkit/0.2.8/chunkit-0.2.8.tar.gz

---
+++ packages/chunkit/chunkit-0.2.8.tar.gz/chunkit-0.2.8/setup.py
@@ -0,0 +1,31 @@
+import os
+from setuptools import (
+   setup,
+   find_packages,
+)
+
+this_dir = os.path.abspath(os.path.dirname(__file__))
+with open(os.path.join(this_dir, 'README.md'), encoding='utf-8') as f:
+    long_desc = f.read()
+
+setup(
+   name='chunkit',
+   version='0.2.8',
+   description='Convert URLs and files into LLM-friendly markdown chunks',
+   long_description=long_desc,
+   long_description_content_type='text/markdown',
+   author='hypergrok',
+   author_email='173556723+hypergrok@users.noreply.github.com',
+   url='https://github.com/hypergrok/chunkit',
+   project_urls={
+      'Source Code': 'https://github.com/hypergrok/chunkit'
+   },
+   packages=find_packages(),
+   include_package_data=True,
+   package_data={
+      'chunkit': ['config.toml'],
+   },
+   install_requires=[
+      'requests', 'html2text', 'beautifulsoup4', 'toml'
+   ]
+)
