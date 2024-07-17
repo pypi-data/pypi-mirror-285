b669f0ca939b1b78928acfa1058160dde2ae116a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-17 06:04:39 +0000

    Add package tecton/0.8.17/tecton-0.8.17-py3-none-any.whl

---
+++ packages/tecton/tecton-0.8.17-py3-none-any.whl/tecton/_stamp.py
@@ -0,0 +1,1 @@
+BUILD_STATUS = {'BUILD_ID': 'uncommitted-buildkite-agent-1721157102', 'BUILD_TIMESTAMP': '1721157102', 'FORMATTED_DATE': '2024 Jul 16 19 11 42 Tue', 'GIT_COMMIT': 'fae8113ea7e4d18a4e336cbb323bbd2c878a0e28', 'BUILD_EMBED_LABEL': '', 'BUILD_HOST': 'buildkite-agent-medium-5f69b5489-tczdf', 'BUILD_USER': 'buildkite-agent'}
