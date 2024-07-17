69d46294d9b9c5ea553a112708a029e3c75414fe
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-17 03:04:20 +0000

    Add package rendercv/1.12/rendercv-1.12.tar.gz

---
+++ packages/rendercv/rendercv-1.12.tar.gz/rendercv-1.12/docs/update_entry_figures.py
@@ -0,0 +1,277 @@
+"""This script generates the example entry figures and creates an environment for
+documentation templates using `mkdocs-macros-plugin`. For example, the content of the
+example entries found in
+"[Structure of the YAML Input File](https://docs.rendercv.com/user_guide/structure_of_the_yaml_input_file/)"
+are coming from this script.
+"""
+
+import io
+import pathlib
+import shutil
+import tempfile
+from typing import Any
+
+import pdfCropMargins
+import ruamel.yaml
+
+import rendercv.data as data
+import rendercv.renderer as renderer
+
+repository_root = pathlib.Path(__file__).parent.parent
+rendercv_path = repository_root / "rendercv"
+image_assets_directory = pathlib.Path(__file__).parent / "assets" / "images"
+
+# The entries below will be pasted into the documentation as YAML, and their
+# corresponding figures will be generated with this script.
+education_entry = {
+    "institution": "Boğaziçi University",
+    "location": "Istanbul, Turkey",
+    "degree": "BS",
+    "area": "Mechanical Engineering",
+    "start_date": "2015-09",
+    "end_date": "2020-06",
+    "highlights": [
+        "GPA: 3.24/4.00 ([Transcript](https://example.com))",
+        "Awards: Dean's Honor List, Sportsperson of the Year",
+    ],
+}
+
+experience_entry = {
+    "company": "Some Company",
+    "location": "TX, USA",
+    "position": "Software Engineer",
+    "start_date": "2020-07",
+    "end_date": "2021-08-12",
+    "highlights": [
+        (
+            "Developed an [IOS application](https://example.com) that has received"
+            " more than **100,000 downloads**."
+        ),
+        "Managed a team of **5** engineers.",
+    ],
+}
+
+normal_entry = {
+    "name": "Some Project",
+    "date": "2021-09",
+    "highlights": [
+        "Developed a web application with **React** and **Django**.",
+        "Implemented a **RESTful API**",
+    ],
+}
+
+publication_entry = {
+    "title": (
+        "Magneto-Thermal Thin Shell Approximation for 3D Finite Element Analysis of"
+        " No-Insulation Coils"
+    ),
+    "authors": ["J. Doe", "***H. Tom***", "S. Doe", "A. Andsurname"],
+    "date": "2021-12-08",
+    "journal": "IEEE Transactions on Applied Superconductivity",
+    "doi": "10.1109/TASC.2023.3340648",
+}
+
+one_line_entry = {
+    "label": "Programming",
+    "details": "Python, C++, JavaScript, MATLAB",
+}
+
+bullet_entry = {
+    "bullet": "This is a bullet entry.",
+}
+
+text_entry = (
+    "This is a *TextEntry*. It is only a text and can be useful for sections like"
+    " **Summary**. To showcase the TextEntry completely, this sentence is added, but it"
+    " doesn't contain any information."
+)
+
+
+def dictionary_to_yaml(dictionary: dict[str, Any]):
+    """Converts a dictionary to a YAML string.
+
+    Args:
+        dictionary (dict[str, Any]): The dictionary to be converted to YAML.
+    Returns:
+        str: The YAML string.
+    """
+    yaml_object = ruamel.yaml.YAML()
+    yaml_object.width = 60
+    yaml_object.indent(mapping=2, sequence=4, offset=2)
+    with io.StringIO() as string_stream:
+        yaml_object.dump(dictionary, string_stream)
+        yaml_string = string_stream.getvalue()
+    return yaml_string
+
+
+def define_env(env):
+    # See https://mkdocs-macros-plugin.readthedocs.io/en/latest/macros/
+    entries = [
+        "education_entry",
+        "experience_entry",
+        "normal_entry",
+        "publication_entry",
+        "one_line_entry",
+        "bullet_entry",
+        "text_entry",
+    ]
+
+    entries_showcase = dict()
+    for entry in entries:
+        proper_entry_name = entry.replace("_", " ").title()
+        entries_showcase[proper_entry_name] = {
+            "yaml": dictionary_to_yaml(eval(entry)),
+            "figures": [
+                {
+                    "path": f"../assets/images/{theme}/{entry}.png",
+                    "alt_text": f"{proper_entry_name} in {theme}",
+                    "theme": theme,
+                }
+                for theme in data.available_themes
+            ],
+        }
+
+    env.variables["showcase_entries"] = entries_showcase
+
+    # For theme templates reference docs
+    themes_path = rendercv_path / "themes"
+    theme_templates = dict()
+    for theme in data.available_themes:
+        theme_templates[theme] = dict()
+        for theme_file in themes_path.glob(f"{theme}/*.tex"):
+            theme_templates[theme][theme_file.stem] = theme_file.read_text()
+
+        # Update ordering of theme templates
+        order = [
+            "Preamble.j2",
+            "Header.j2",
+            "SectionBeginning.j2",
+            "SectionEnding.j2",
+            "TextEntry.j2",
+            "BulletEntry.j2",
+            "OneLineEntry.j2",
+            "EducationEntry.j2",
+            "ExperienceEntry.j2",
+            "NormalEntry.j2",
+            "PublicationEntry.j2",
+        ]
+        theme_templates[theme] = {key: theme_templates[theme][key] for key in order}
+
+        if theme != "markdown":
+            theme_templates[theme] = {
+                f"{key}.tex": value for key, value in theme_templates[theme].items()
+            }
+        else:
+            theme_templates[theme] = {
+                f"{key}.md": value for key, value in theme_templates[theme].items()
+            }
+
+    env.variables["theme_templates"] = theme_templates
+
+    # Available themes strings (put available themes between ``)
+    themes = [f"`{theme}`" for theme in data.available_themes]
+    env.variables["available_themes"] = ", ".join(themes)
+
+    # Available social networks strings (put available social networks between ``)
+    social_networks = [
+        f"`{social_network}`" for social_network in data.available_social_networks
+    ]
+    env.variables["available_social_networks"] = ", ".join(social_networks)
+
+
+def generate_entry_figures():
+    """Generate an image for each entry type and theme."""
+    # Generate PDF figures for each entry type and theme
+    entries = {
+        "education_entry": data.EducationEntry(**education_entry),
+        "experience_entry": data.ExperienceEntry(**experience_entry),
+        "normal_entry": data.NormalEntry(**normal_entry),
+        "publication_entry": data.PublicationEntry(**publication_entry),
+        "one_line_entry": data.OneLineEntry(**one_line_entry),
+        "text_entry": f"{text_entry}",
+        "bullet_entry": data.BulletEntry(**bullet_entry),
+    }
+    themes = data.available_themes
+
+    with tempfile.TemporaryDirectory() as temporary_directory:
+        # Create temporary directory
+        temporary_directory_path = pathlib.Path(temporary_directory)
+        for theme in themes:
+            design_dictionary = {
+                "theme": theme,
+                "disable_page_numbering": True,
+                "disable_last_updated_date": True,
+            }
+            if theme == "moderncv":
+                # moderncv theme does not support these options
+                del design_dictionary["disable_page_numbering"]
+                del design_dictionary["disable_last_updated_date"]
+
+            for entry_type, entry in entries.items():
+                # Create data model with only one section and one entry
+                data_model = data.RenderCVDataModel(
+                    **{
+                        "cv": data.CurriculumVitae(sections={entry_type: [entry]}),
+                        "design": design_dictionary,
+                    }
+                )
+
+                # Render
+                latex_file_path = renderer.create_a_latex_file_and_copy_theme_files(
+                    data_model, temporary_directory_path
+                )
+                pdf_file_path = renderer.render_a_pdf_from_latex(latex_file_path)
+
+                # Prepare output directory and file path
+                output_directory = image_assets_directory / theme
+                output_directory.mkdir(parents=True, exist_ok=True)
+                output_pdf_file_path = output_directory / f"{entry_type}.pdf"
+
+                # Remove file if it exists
+                if output_pdf_file_path.exists():
+                    output_pdf_file_path.unlink()
+
+                # Crop margins
+                pdfCropMargins.crop(
+                    argv_list=[
+                        "-p4",
+                        "100",
+                        "0",
+                        "100",
+                        "0",
+                        "-a4",
+                        "0",
+                        "-30",
+                        "0",
+                        "-30",
+                        "-o",
+                        str(output_pdf_file_path.absolute()),
+                        str(pdf_file_path.absolute()),
+                    ]
+                )
+
+                # Convert PDF to image
+                png_file_path = renderer.render_pngs_from_pdf(output_pdf_file_path)[0]
+                desired_png_file_path = output_pdf_file_path.with_suffix(".png")
+
+                # If image exists, remove it
+                if desired_png_file_path.exists():
+                    desired_png_file_path.unlink()
+
+                # Move image to desired location
+                png_file_path.rename(desired_png_file_path)
+
+                # Remove PDF file
+                output_pdf_file_path.unlink()
+
+
+def update_index():
+    """Update index.md file by copying README.md file."""
+    index_file_path = repository_root / "docs" / "index.md"
+    readme_file_path = repository_root / "README.md"
+    shutil.copy(readme_file_path, index_file_path)
+
+
+if __name__ == "__main__":
+    generate_entry_figures()
+    print("Entry figures generated successfully.")
---
+++ packages/rendercv/rendercv-1.12.tar.gz/rendercv-1.12/rendercv/cli/utilities.py
@@ -0,0 +1,282 @@
+"""
+The `rendercv.cli.utilities` module contains utility functions that are required by CLI.
+"""
+
+import json
+import pathlib
+import re
+import shutil
+import urllib.request
+from typing import Optional
+
+import pydantic
+import typer
+
+from .. import data
+
+
+def string_to_file_path(string: Optional[str]) -> Optional[pathlib.Path]:
+    """Convert a string to a pathlib.Path object. If the string is None, then return
+    None.
+
+    Args:
+        string (str): The string to be converted to a pathlib.Path object.
+
+    Returns:
+        pathlib.Path: The pathlib.Path object.
+    """
+    if string is not None:
+        return pathlib.Path(string).absolute()
+    else:
+        return None
+
+
+def set_or_update_a_value(
+    data_model: pydantic.BaseModel,
+    key: str,
+    value: str,
+    sub_model: Optional[pydantic.BaseModel | dict | list] = None,
+) -> pydantic.BaseModel:  # type: ignore
+    """Set or update a value in a data model for a specific key. For example, a key can
+    be `cv.sections.education.3.institution` and the value can be "Bogazici University".
+
+    Args:
+        data_model (pydantic.BaseModel | dict | list): The data model to set or update
+            the value.
+        key (str): The key to set or update the value.
+        value (Any): The value to set or update.
+        sub_model (pydantic.BaseModel | dict | list, optional): The sub model to set or
+            update the value. This is used for recursive calls. When the value is set
+            to a sub model, the original data model is validated. Defaults to None.
+    """
+    # Recursively call this function until the last key is reached:
+
+    # Rename `sections` with `sections_input` since the key is `sections` is an alias:
+    key = key.replace("sections.", "sections_input.")
+    keys = key.split(".")
+
+    if sub_model is not None:
+        model = sub_model
+    else:
+        model = data_model
+
+    if len(keys) == 1:
+        # Set the value:
+        if value.startswith("{") and value.endswith("}"):
+            # Allow users to assign dictionaries:
+            value = eval(value)
+        elif value.startswith("[") and value.endswith("]"):
+            # Allow users to assign lists:
+            value = eval(value)
+
+        if isinstance(model, pydantic.BaseModel):
+            setattr(model, key, value)
+        elif isinstance(model, dict):
+            model[key] = value
+        elif isinstance(model, list):
+            model[int(key)] = value
+        else:
+            raise ValueError(
+                "The data model should be either a Pydantic data model, dictionary, or"
+                " list.",
+            )
+
+        data_model = type(data_model).model_validate(
+            (data_model.model_dump(by_alias=True))
+        )
+        return data_model
+    else:
+        # get the first key and call the function with remaining keys:
+        first_key = keys[0]
+        key = ".".join(keys[1:])
+        if isinstance(model, pydantic.BaseModel):
+            sub_model = getattr(model, first_key)
+        elif isinstance(model, dict):
+            sub_model = model[first_key]
+        elif isinstance(model, list):
+            sub_model = model[int(first_key)]
+        else:
+            raise ValueError(
+                "The data model should be either a Pydantic data model, dictionary, or"
+                " list.",
+            )
+
+        set_or_update_a_value(data_model, key, value, sub_model)
+
+
+def set_or_update_values(
+    data_model: data.RenderCVDataModel,
+    key_and_values: dict[str, str],
+) -> data.RenderCVDataModel:
+    """Set or update values in `RenderCVDataModel` for specific keys. It uses the
+    `set_or_update_a_value` function to set or update the values.
+
+    Args:
+        data_model (data.RenderCVDataModel): The data model to set or update the values.
+        key_and_values (dict[str, str]): The key and value pairs to set or update.
+    """
+    for key, value in key_and_values.items():
+        try:
+            data_model = set_or_update_a_value(data_model, key, value)  # type: ignore
+        except pydantic.ValidationError as e:
+            raise e
+        except (ValueError, KeyError, IndexError, AttributeError):
+            raise ValueError(f'The key "{key}" does not exist in the data model!')
+
+    return data_model
+
+
+def copy_files(paths: list[pathlib.Path] | pathlib.Path, new_path: pathlib.Path):
+    """Copy files to the given path. If there are multiple files, then rename the new
+    path by adding a number to the end of the path.
+
+    Args:
+        paths (list[pathlib.Path]): The paths of the files to be copied.
+        new_path (pathlib.Path): The path to copy the files to.
+    """
+    if isinstance(paths, pathlib.Path):
+        paths = [paths]
+
+    if len(paths) == 1:
+        shutil.copy2(paths[0], new_path)
+    else:
+        for i, file_path in enumerate(paths):
+            # append a number to the end of the path:
+            number = i + 1
+            png_path_with_page_number = (
+                pathlib.Path(new_path).parent
+                / f"{pathlib.Path(new_path).stem}_{number}.png"
+            )
+            shutil.copy2(file_path, png_path_with_page_number)
+
+
+def get_latest_version_number_from_pypi() -> Optional[str]:
+    """Get the latest version number of RenderCV from PyPI.
+
+    Example:
+        ```python
+        get_latest_version_number_from_pypi()
+        ```
+        returns
+        `#!python "1.1"`
+
+    Returns:
+        Optional[str]: The latest version number of RenderCV from PyPI. Returns None if
+            the version number cannot be fetched.
+    """
+    version = None
+    url = "https://pypi.org/pypi/rendercv/json"
+    try:
+        with urllib.request.urlopen(url) as response:
+            data = response.read()
+            encoding = response.info().get_content_charset("utf-8")
+            json_data = json.loads(data.decode(encoding))
+            version = json_data["info"]["version"]
+    except Exception:
+        pass
+
+    return version
+
+
+def get_error_message_and_location_and_value_from_a_custom_error(
+    error_string: str,
+) -> tuple[Optional[str], Optional[str], Optional[str]]:
+    """Look at a string and figure out if it's a custom error message that has been
+    sent from `rendercv.data.reader.read_input_file`. If it is, then return the custom
+    message, location, and the input value.
+
+    This is done because sometimes we raise an error about a specific field in the model
+    validation level, but Pydantic doesn't give us the exact location of the error
+    because it's a model-level error. So, we raise a custom error with three string
+    arguments: message, location, and input value. Those arguments then combined into a
+    string by Python. This function is used to parse that custom error message and
+    return the three values.
+
+    Args:
+        error_string (str): The error message.
+
+    Returns:
+        tuple[Optional[str], Optional[str], Optional[str]]: The custom message,
+            location, and the input value.
+    """
+    pattern = r"""\(['"](.*)['"], '(.*)', '(.*)'\)"""
+    match = re.search(pattern, error_string)
+    if match:
+        return match.group(1), match.group(2), match.group(3)
+    else:
+        return None, None, None
+
+
+def copy_templates(
+    folder_name: str,
+    copy_to: pathlib.Path,
+    new_folder_name: Optional[str] = None,
+) -> Optional[pathlib.Path]:
+    """Copy one of the folders found in `rendercv.templates` to `copy_to`.
+
+    Args:
+        folder_name (str): The name of the folder to be copied.
+        copy_to (pathlib.Path): The path to copy the folder to.
+
+    Returns:
+        Optional[pathlib.Path]: The path to the copied folder.
+    """
+    # copy the package's theme files to the current directory
+    template_directory = pathlib.Path(__file__).parent.parent / "themes" / folder_name
+    if new_folder_name:
+        destination = copy_to / new_folder_name
+    else:
+        destination = copy_to / folder_name
+
+    if destination.exists():
+        return None
+    else:
+        # copy the folder but don't include __init__.py:
+        shutil.copytree(
+            template_directory,
+            destination,
+            ignore=shutil.ignore_patterns("__init__.py", "__pycache__"),
+        )
+
+        return destination
+
+
+def parse_render_command_override_arguments(
+    extra_arguments: typer.Context,
+) -> dict["str", "str"]:
+    """Parse extra arguments given to the `render` command as data model key and value
+    pairs and return them as a dictionary.
+
+    Args:
+        extra_arguments (typer.Context): The extra arguments context.
+
+    Returns:
+        dict["str", "str"]: The key and value pairs.
+    """
+    key_and_values: dict["str", "str"] = dict()
+
+    # `extra_arguments.args` is a list of arbitrary arguments that haven't been
+    # specified in `cli_render_command` function's definition. They are used to allow
+    # users to edit their data model in CLI. The elements with even indexes in this list
+    # are keys that start with double dashed, such as
+    # `--cv.sections.education.0.institution`. The following elements are the
+    # corresponding values of the key, such as `"Bogazici University"`. The for loop
+    # below parses `ctx.args` accordingly.
+
+    if len(extra_arguments.args) % 2 != 0:
+        raise ValueError(
+            "There is a problem with the extra arguments! Each key should have"
+            " a corresponding value."
+        )
+
+    for i in range(0, len(extra_arguments.args), 2):
+        key = extra_arguments.args[i]
+        value = extra_arguments.args[i + 1]
+        if not key.startswith("--"):
+            raise ValueError(f"The key ({key}) should start with double dashes!")
+
+        key = key.replace("--", "")
+
+        key_and_values[key] = value
+
+    return key_and_values
