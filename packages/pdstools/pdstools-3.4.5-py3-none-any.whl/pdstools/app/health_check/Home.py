74e17e6b22e53652bb3f663c86e8d096daa3204f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-17 16:06:12 +0000

    Add package pdstools/3.4.5/pdstools-3.4.5-py3-none-any.whl

---
+++ packages/pdstools/pdstools-3.4.5-py3-none-any.whl/pdstools/app/health_check/Home.py
@@ -0,0 +1,17 @@
+import streamlit as st
+
+st.set_page_config(
+    page_title="Home",
+    menu_items={
+        "Report a bug": "https://github.com/pegasystems/pega-datascientist-tools/issues",
+        "Get help": "https://pegasystems.github.io/pega-datascientist-tools/Python/examples.html",
+    },
+)
+"""
+# Welcome to the Health Check app!
+
+This app helps you analyze ADMDatamart data by allowing you to generate a Health Check document with visuals as well as excel
+export for self exploration of the data. 
+
+To be up to date with changes in the app, make sure to regularly run a `pip install --upgrade pdstools`!
+"""
