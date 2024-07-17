c8d549c17b5475ecabd746f2335019ef42271116
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-17 05:03:38 +0000

    Add package radio-pyse/0.3.1/radio_pyse-0.3.1-py3-none-any.whl

---
+++ packages/radio-pyse/radio_pyse-0.3.1-py3-none-any.whl/sourcefinder/accessors/lofaraccessor.py
@@ -0,0 +1,26 @@
+from sourcefinder.accessors.requiredatts import RequiredAttributesMetaclass
+from six import with_metaclass
+
+
+class LofarAccessor(with_metaclass(RequiredAttributesMetaclass, object)):
+    """
+    Additional metadata required for processing LOFAR images through QC
+    checks.
+
+    Attributes:
+        antenna_set (string): Antenna set in use during observation.
+            String; 'LBA_INNER', 'LBA_OUTER', 'LBA_SPARSE', 'LBA' or 'HBA'
+        ncore(int): Number of core stations in use during observation.
+        nremote(int): Number of remote stations in use during observation.
+        nintl(int): Number of international stations in use during observation.
+        subbandwidth(float): Width of a subband in Hz.
+        subbands(int): Number of subbands.
+    """
+    _required_attributes = [
+        'antenna_set',
+        'ncore',
+        'nremote',
+        'nintl',
+        'subbandwidth',
+        'subbands',
+    ]
