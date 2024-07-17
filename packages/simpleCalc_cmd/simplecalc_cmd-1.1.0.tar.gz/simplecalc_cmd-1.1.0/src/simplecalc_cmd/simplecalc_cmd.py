b6e571a01ca8422fc4e05fbbb41206bd7bc6eb02
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-17 14:03:25 +0000

    Add package simpleCalc_cmd/1.1.0/simplecalc_cmd-1.1.0.tar.gz

---
+++ packages/simpleCalc_cmd/simplecalc_cmd-1.1.0.tar.gz/simplecalc_cmd-1.1.0/src/simplecalc_cmd/simplecalc_cmd.py
@@ -0,0 +1,13 @@
+from hc_add.hc_add import add
+from hc_div.hc_div import div
+from hc_mul.hc_mul import mul
+
+def call_add():
+    add()
+
+def call_div():
+    div()
+
+def call_mul():
+    mul() 
+
