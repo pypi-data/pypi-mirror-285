7d7a0de52320b841216ea9ad1f4fbb65e2ed1319
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-17 06:04:46 +0000

    Add package openplugin-sdk/0.1.0a5/openplugin_sdk-0.1.0a5-py3-none-any.whl

---
+++ packages/openplugin-sdk/openplugin_sdk-0.1.0a5-py3-none-any.whl/openplugin/resources/__init__.py
@@ -0,0 +1,61 @@
+# File generated from our OpenAPI spec by Stainless. See CONTRIBUTING.md for details.
+
+from .api import (
+    APIResource,
+    AsyncAPIResource,
+    APIResourceWithRawResponse,
+    AsyncAPIResourceWithRawResponse,
+    APIResourceWithStreamingResponse,
+    AsyncAPIResourceWithStreamingResponse,
+)
+from .openapi_parsers import (
+    OpenAPIParsersResource,
+    AsyncOpenAPIParsersResource,
+    OpenAPIParsersResourceWithRawResponse,
+    AsyncOpenAPIParsersResourceWithRawResponse,
+    OpenAPIParsersResourceWithStreamingResponse,
+    AsyncOpenAPIParsersResourceWithStreamingResponse,
+)
+from .plugin_validators import (
+    PluginValidatorsResource,
+    AsyncPluginValidatorsResource,
+    PluginValidatorsResourceWithRawResponse,
+    AsyncPluginValidatorsResourceWithRawResponse,
+    PluginValidatorsResourceWithStreamingResponse,
+    AsyncPluginValidatorsResourceWithStreamingResponse,
+)
+from .openapi_param_parsers import (
+    OpenAPIParamParsersResource,
+    AsyncOpenAPIParamParsersResource,
+    OpenAPIParamParsersResourceWithRawResponse,
+    AsyncOpenAPIParamParsersResourceWithRawResponse,
+    OpenAPIParamParsersResourceWithStreamingResponse,
+    AsyncOpenAPIParamParsersResourceWithStreamingResponse,
+)
+
+__all__ = [
+    "APIResource",
+    "AsyncAPIResource",
+    "APIResourceWithRawResponse",
+    "AsyncAPIResourceWithRawResponse",
+    "APIResourceWithStreamingResponse",
+    "AsyncAPIResourceWithStreamingResponse",
+    "PluginValidatorsResource",
+    "AsyncPluginValidatorsResource",
+    "PluginValidatorsResourceWithRawResponse",
+    "AsyncPluginValidatorsResourceWithRawResponse",
+    "PluginValidatorsResourceWithStreamingResponse",
+    "AsyncPluginValidatorsResourceWithStreamingResponse",
+    "OpenAPIParsersResource",
+    "AsyncOpenAPIParsersResource",
+    "OpenAPIParsersResourceWithRawResponse",
+    "AsyncOpenAPIParsersResourceWithRawResponse",
+    "OpenAPIParsersResourceWithStreamingResponse",
+    "AsyncOpenAPIParsersResourceWithStreamingResponse",
+    "OpenAPIParamParsersResource",
+    "AsyncOpenAPIParamParsersResource",
+    "OpenAPIParamParsersResourceWithRawResponse",
+    "AsyncOpenAPIParamParsersResourceWithRawResponse",
+    "OpenAPIParamParsersResourceWithStreamingResponse",
+    "AsyncOpenAPIParamParsersResourceWithStreamingResponse",
+]
