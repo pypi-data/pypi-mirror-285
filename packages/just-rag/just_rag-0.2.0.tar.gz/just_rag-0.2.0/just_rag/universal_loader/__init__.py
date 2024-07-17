ecd7ed12253d57d2aeb070267407bc3311296b28
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-17 06:04:12 +0000

    Add package just-rag/0.2.0/just_rag-0.2.0.tar.gz

---
+++ packages/just-rag/just_rag-0.2.0.tar.gz/just_rag-0.2.0/just_rag/universal_loader/__init__.py
@@ -0,0 +1,13 @@
+from .universal_document_loader import UniversalDocumentLoader
+from .markdown_loader import MarkdownLoader
+from .pdf_loader import PDFLoader
+from .website_loader import WebsiteLoader
+from .unstructured_file_loader import UnstructuredFileLoader
+
+__all__ = [
+    "UniversalDocumentLoader",
+    "MarkdownLoader",
+    "PDFLoader",
+    "WebsiteLoader",
+    "UnstructuredFileLoader",
+]
