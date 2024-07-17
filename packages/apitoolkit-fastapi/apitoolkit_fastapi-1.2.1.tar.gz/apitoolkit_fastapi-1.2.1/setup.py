f02a048c0090e5622cb59b2a5d391c3954f7687b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-17 14:03:30 +0000

    Add package apitoolkit-fastapi/1.2.1/apitoolkit_fastapi-1.2.1.tar.gz

---
+++ packages/apitoolkit-fastapi/apitoolkit_fastapi-1.2.1.tar.gz/apitoolkit_fastapi-1.2.1/setup.py
@@ -0,0 +1,24 @@
+from setuptools import setup, find_packages
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setup(
+    name="apitoolkit_fastapi",
+    version='1.2.1',
+    packages=find_packages(),
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    author_email='hello@apitoolkit.io',
+    author='APIToolkit',
+    install_requires=[
+        'fastapi',
+        'google-cloud-pubsub',
+        'google-auth',
+        'starlette',
+        'httpx',
+        'jsonpath-ng',
+        'apitoolkit-python',
+        'pytz'
+    ]
+)
