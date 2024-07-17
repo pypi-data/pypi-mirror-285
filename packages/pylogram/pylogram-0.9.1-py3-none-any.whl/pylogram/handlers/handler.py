09b44a30a09984742b92122365121f1cbf1fb088
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-17 17:03:51 +0000

    Add package pylogram/0.9.1/pylogram-0.9.1-py3-none-any.whl

---
+++ packages/pylogram/pylogram-0.9.1-py3-none-any.whl/pylogram/handlers/handler.py
@@ -0,0 +1,44 @@
+#  Pylogram - Telegram MTProto API Client Library for Python
+#  Copyright (C) 2017-2023 Dan <https://github.com/delivrance>
+#  Copyright (C) 2023-2024 Pylakey <https://github.com/pylakey>
+#
+#  This file is part of Pylogram.
+#
+#  Pylogram is free software: you can redistribute it and/or modify
+#  it under the terms of the GNU Lesser General Public License as published
+#  by the Free Software Foundation, either version 3 of the License, or
+#  (at your option) any later version.
+#
+#  Pylogram is distributed in the hope that it will be useful,
+#  but WITHOUT ANY WARRANTY; without even the implied warranty of
+#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#  GNU Lesser General Public License for more details.
+#
+#  You should have received a copy of the GNU Lesser General Public License
+#  along with Pylogram.  If not, see <http://www.gnu.org/licenses/>.
+
+import inspect
+from typing import Callable
+
+import pylogram
+from pylogram.filters import Filter
+from pylogram.types import Update
+
+
+class Handler:
+    def __init__(self, callback: Callable, filters: Filter = None):
+        self.callback = callback
+        self.filters = filters
+
+    async def check(self, client: "pylogram.Client", update: Update):
+        if callable(self.filters):
+            if inspect.iscoroutinefunction(self.filters.__call__):
+                return await self.filters(client, update)
+            else:
+                return await client.loop.run_in_executor(
+                    client.executor,
+                    self.filters,
+                    client, update
+                )
+
+        return True
