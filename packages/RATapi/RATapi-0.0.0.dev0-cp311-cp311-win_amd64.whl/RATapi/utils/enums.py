7a4c2a91e72406bc9ad88889c6db0153a9081724
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-17 03:03:48 +0000

    Add package RATapi/0.0.0.dev0/RATapi-0.0.0.dev0-cp311-cp311-win_amd64.whl

---
+++ packages/RATapi/RATapi-0.0.0.dev0-cp311-cp311-win_amd64.whl/RATapi/utils/enums.py
@@ -0,0 +1,101 @@
+from enum import Enum
+
+try:
+    from enum import StrEnum
+except ImportError:
+    from strenum import StrEnum
+
+
+# Controls
+class Parallel(StrEnum):
+    """Defines the available options for parallelization"""
+
+    Single = "single"
+    Points = "points"
+    Contrasts = "contrasts"
+
+
+class Procedures(StrEnum):
+    """Defines the available options for procedures"""
+
+    Calculate = "calculate"
+    Simplex = "simplex"
+    DE = "de"
+    NS = "ns"
+    Dream = "dream"
+
+
+class Display(StrEnum):
+    """Defines the available options for display"""
+
+    Off = "off"
+    Iter = "iter"
+    Notify = "notify"
+    Final = "final"
+
+
+class BoundHandling(StrEnum):
+    """Defines the available options for bound handling"""
+
+    Off = "off"
+    Reflect = "reflect"
+    Bound = "bound"
+    Fold = "fold"
+
+
+class Strategies(Enum):
+    """Defines the available options for strategies"""
+
+    Random = 1
+    LocalToBest = 2
+    BestWithJitter = 3
+    RandomWithPerVectorDither = 4
+    RandomWithPerGenerationDither = 5
+    RandomEitherOrAlgorithm = 6
+
+
+# Models
+class Hydration(StrEnum):
+    None_ = "none"
+    BulkIn = "bulk in"
+    BulkOut = "bulk out"
+    Oil = "oil"
+
+
+class Languages(StrEnum):
+    Cpp = "cpp"
+    Python = "python"
+    Matlab = "matlab"
+
+
+class Priors(StrEnum):
+    Uniform = "uniform"
+    Gaussian = "gaussian"
+
+
+class TypeOptions(StrEnum):
+    Constant = "constant"
+    Data = "data"
+    Function = "function"
+
+
+class BackgroundActions(StrEnum):
+    Add = "add"
+    Subtract = "subtract"
+
+
+# Project
+class Calculations(StrEnum):
+    NonPolarised = "non polarised"
+    Domains = "domains"
+
+
+class Geometries(StrEnum):
+    AirSubstrate = "air/substrate"
+    SubstrateLiquid = "substrate/liquid"
+
+
+class LayerModels(StrEnum):
+    CustomLayers = "custom layers"
+    CustomXY = "custom xy"
+    StandardLayers = "standard layers"
