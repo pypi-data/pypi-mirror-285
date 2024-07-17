d02134586328412a35a89a5252cbcfb5a53bea7a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-17 06:04:11 +0000

    Add package DecimalCalcul/2.2/decimalcalcul-2.2.tar.gz

---
+++ packages/DecimalCalcul/decimalcalcul-2.2.tar.gz/decimalcalcul-2.2/DecimalCalcul/base.py
@@ -0,0 +1,43 @@
+
+
+class Decimal:
+
+    @staticmethod
+    def add(nb1, nb2):
+        p1 = Decimal.__getPow10(nb1)
+        p2 = Decimal.__getPow10(nb2)
+        pMax = max(p1, p2)
+        return float(int(nb1 * pMax) + int(nb2 * pMax)) / pMax
+
+    @staticmethod
+    def __getPow10(number: float):
+        string = str('{:.16f}'.format(float(number))).rstrip("0")
+        indexDot = string.index(".")
+        return 10 ** (len(string) - 1 - indexDot)
+
+    @staticmethod
+    def times(nb1, nb2):
+        p1 = Decimal.__getPow10(nb1)
+        p2 = Decimal.__getPow10(nb2)
+        return float(int(nb1 * p1) * int(nb2 * p2)) / (p1 * p2)
+
+    @staticmethod
+    def divide(nb1, nb2):
+        p1 = Decimal.__getPow10(nb1)
+        p2 = Decimal.__getPow10(nb2)
+        pMax = max(p1, p2)
+        return float(int(nb1 * pMax) / int(nb2 * pMax))
+
+    @staticmethod
+    def minus(nb1, nb2):
+        p1 = Decimal.__getPow10(nb1)
+        p2 = Decimal.__getPow10(nb2)
+        pMax = max(p1, p2)
+        return float(int(nb1 * pMax) - int(nb2 * pMax)) / pMax
+
+def main():
+    pass
+
+
+if __name__ == "__main__":
+    main()
---
+++ packages/DecimalCalcul/decimalcalcul-2.2.tar.gz/decimalcalcul-2.2/setup.py
@@ -0,0 +1,23 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='DecimalCalcul',
+    version='2.2',
+    packages=find_packages(),
+    include_package_data=True,
+    install_requires=[
+        # Liste des dépendances
+    ],
+    tests_require=['pytest'],
+    author='Bouquet Cédric',
+    author_email='cedric-bouquet-7@outlook.fr',
+    description='Fournit une classe dédiée aux calculs sur des nombres décimaux',
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
+    url='https://github.com/votre_utilisateur/mon_projet',
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'Operating System :: OS Independent',
+    ],
+    python_requires='>=3.0',
+)
