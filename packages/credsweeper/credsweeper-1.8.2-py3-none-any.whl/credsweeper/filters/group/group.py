19d7f08761e4dd260132af7b5674dbc15e9f7367
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-17 05:03:34 +0000

    Add package credsweeper/1.8.2/credsweeper-1.8.2-py3-none-any.whl

---
+++ packages/credsweeper/credsweeper-1.8.2-py3-none-any.whl/credsweeper/filters/group/group.py
@@ -0,0 +1,64 @@
+from abc import ABC
+from typing import List
+
+from credsweeper.common.constants import GroupType
+from credsweeper.config import Config
+from credsweeper.filters import (Filter, LineSpecificKeyCheck, ValueAllowlistCheck, ValueArrayDictionaryCheck,
+                                 ValueBlocklistCheck, ValueCamelCaseCheck, ValueFilePathCheck, ValueFirstWordCheck,
+                                 ValueLastWordCheck, ValueLengthCheck, ValueMethodCheck, ValueNotAllowedPatternCheck,
+                                 ValuePatternCheck, ValueSimilarityCheck, ValueStringTypeCheck, ValueTokenCheck,
+                                 VariableNotAllowedPatternCheck, ValuePatternLengthCheck, ValueHexNumberCheck)
+
+
+class Group(ABC):
+    """Abstract Group class"""
+
+    def __init__(self, config: Config, rule_type: GroupType = GroupType.DEFAULT) -> None:
+        if rule_type == GroupType.KEYWORD:
+            self.filters: List[Filter] = self.get_keyword_base_filters(config)
+        elif rule_type == GroupType.PATTERN:
+            self.filters: List[Filter] = self.get_pattern_base_filters(config)
+        else:
+            self.filters: List[Filter] = []
+
+    @property
+    def filters(self) -> List[Filter]:
+        """property getter"""
+        return self.__filters
+
+    @filters.setter
+    def filters(self, filters: List[Filter]) -> None:
+        """property setter"""
+        self.__filters = filters
+
+    @staticmethod
+    def get_keyword_base_filters(config: Config) -> List[Filter]:
+        """returns base filters"""
+        filters = [  #
+            ValueAllowlistCheck(),
+            ValueArrayDictionaryCheck(),
+            ValueBlocklistCheck(),
+            ValueCamelCaseCheck(),
+            ValueFilePathCheck(),
+            ValueFirstWordCheck(),
+            ValueHexNumberCheck(),
+            ValueLastWordCheck(),
+            ValueLengthCheck(config),
+            ValueMethodCheck(),
+            ValueSimilarityCheck(),
+            ValueStringTypeCheck(config),
+            ValueTokenCheck(),
+            VariableNotAllowedPatternCheck(),
+        ]
+        if not config.doc:
+            filters.extend([ValuePatternCheck(config), ValueNotAllowedPatternCheck()])
+        return filters
+
+    @staticmethod
+    def get_pattern_base_filters(config: Config) -> List[Filter]:
+        """return base filters for pattern"""
+        return [  #
+            LineSpecificKeyCheck(),  #
+            ValuePatternCheck(config),  #
+            ValuePatternLengthCheck(config)
+        ]
---
+++ packages/credsweeper/credsweeper-1.8.2-py3-none-any.whl/credsweeper/filters/value_dictionary_value_length_check.py
@@ -0,0 +1,27 @@
+from credsweeper.config import Config
+from credsweeper.credentials import LineData
+from credsweeper.file_handler.analysis_target import AnalysisTarget
+from credsweeper.filters import Filter
+
+
+class ValueDictionaryValueLengthCheck(Filter):
+    """Check that candidate length is between 5 and 30."""
+
+    def __init__(self, config: Config = None) -> None:
+        pass
+
+    def run(self, line_data: LineData, target: AnalysisTarget) -> bool:
+        """Run filter checks on received credential candidate data 'line_data'.
+
+        Args:
+            line_data: credential candidate data
+            target: multiline target from which line data was obtained
+
+        Return:
+            True, if need to filter candidate and False if left
+
+        """
+        if 4 <= len(line_data.value) <= 31:
+            return False
+        else:
+            return True
