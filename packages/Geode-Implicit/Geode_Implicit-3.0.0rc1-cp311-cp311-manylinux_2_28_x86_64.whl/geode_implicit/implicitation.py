16100859044113ddbba71b652ff62b29c5ff1033
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-17 07:03:45 +0000

    Add package Geode-Implicit/3.0.0rc1/Geode_Implicit-3.0.0rc1-cp311-cp311-manylinux_2_28_x86_64.whl

---
+++ packages/Geode-Implicit/Geode_Implicit-3.0.0rc1-cp311-cp311-manylinux_2_28_x86_64.whl/geode_implicit/implicitation.py
@@ -0,0 +1,15 @@
+#
+# Copyright (c) 2019 - 2024 Geode-solutions. All rights reserved.
+#
+
+import opengeode
+import opengeode_geosciences
+import geode_common
+import geode_conversion
+import geode_background
+import geode_explicit
+import geode_numerics
+
+from .lib64.geode_implicit_py_implicitation import *
+
+ImplicitImplicitationLibrary.initialize()
