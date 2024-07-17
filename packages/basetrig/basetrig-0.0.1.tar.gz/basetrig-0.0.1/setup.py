8b77f9b71a84e48372467b23ff825667c7a294c2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-17 04:04:33 +0000

    Add package basetrig/0.0.1/basetrig-0.0.1.tar.gz

---
+++ packages/basetrig/basetrig-0.0.1.tar.gz/basetrig-0.0.1/setup.py
@@ -0,0 +1,13 @@
+import setuptools
+
+setuptools.setup(
+    name = "basetrig",                                                                                                                              
+    version = "0.0.1",
+    author = "Cheng",
+    license='MIT license',
+    author_email="cjenf844747@gmail.com",
+    description="Basic operations on trigonometric functions",
+    url="https://github.com/cjenf/trigfunc", 
+    python_requires='>=3.11.4'
+)
+
