eea41e8deec70dff21b9ffb78d0ae6163732d712
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-17 03:04:00 +0000

    Add package musemodalities/0.0.2/musemodalities-0.0.2.tar.gz

---
+++ packages/musemodalities/musemodalities-0.0.2.tar.gz/musemodalities-0.0.2/src/musemodalities/__init__.py
@@ -0,0 +1,21 @@
+"""Find out more at https://github.com/GDCC/pyDataverse.
+
+Copyright 2024 Slava Tykhonov
+
+Licensed under the MIT License.
+"""
+
+from __future__ import absolute_import
+
+import urllib3
+
+urllib3.disable_warnings()  # noqa
+
+__author__ = "Slava Tykhonov"
+__email__ = "4tykhonov@gmail.com"
+__copyright__ = "Copyright (c) 2024 Slava Tykhonov"
+__license__ = "MIT License"
+__version__ = "0.0.1"
+__url__ = "https://github.com/MuseIT-project/musemodalities"
+__download_url__ = "https://pypi.python.org/pypi/musemodalities"
+__description__ = "A Python module for MuseIT Modalities."
