4faf72744e7cf2ebc49e821fa0f9c904cfb43e62
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-17 14:03:40 +0000

    Add package python-shukujitsu/2024.7.17.post1/python_shukujitsu-2024.7.17.post1.tar.gz

---
+++ packages/python-shukujitsu/python_shukujitsu-2024.7.17.post1.tar.gz/python_shukujitsu-2024.7.17.post1/shukujitsu/data.py
@@ -0,0 +1,33 @@
+from importlib import import_module
+from warnings import warn
+
+
+def load_txt(*args, **kwargs):
+    return load("TXT", *args, **kwargs)
+
+
+def load_bin(*args, **kwargs):
+    return load("BIN", *args, **kwargs)
+
+
+_DEFAULT_BUNDLES = """\
+asof20240717
+asof20230204
+asof20230201
+asof20220201
+asof20210201
+asof20201127
+asof20201118
+""".split()
+
+
+def load(type_, bundles=_DEFAULT_BUNDLES):
+    for asof in bundles:
+        try:
+            mod = import_module("shukujitsu.%s" % asof)
+        except ImportError as e:
+            warn(repr(e), category=ImportWarning, stacklevel=2)
+        else:
+            return getattr(mod, type_)
+    else:
+        raise RuntimeError("No holidays data is available")
