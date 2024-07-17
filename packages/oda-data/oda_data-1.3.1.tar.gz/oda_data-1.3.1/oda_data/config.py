031b0ee758cafe2fab3fc867232f61999d45a945
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-17 07:03:43 +0000

    Add package oda-data/1.3.1/oda_data-1.3.1.tar.gz

---
+++ packages/oda-data/oda_data-1.3.1.tar.gz/oda_data-1.3.1/oda_data/config.py
@@ -0,0 +1,28 @@
+from pathlib import Path
+
+
+class OdaPATHS:
+    """Class to store the paths to the data and output folders."""
+
+    project = Path(__file__).resolve().parent.parent
+    scripts = project / "oda_data"
+    raw_data = scripts / ".raw_data"
+    pydeflate = raw_data / ".pydeflate"
+    indicators = scripts / "indicators"
+    cleaning = scripts / "clean_data"
+    settings = scripts / "settings"
+    sectors = indicators / "sectors"
+    tests = project / "tests"
+    test_files = tests / "files"
+
+
+# ------------------------------ Key URLs ------------------------------ #
+
+BASE_URL: str = "https://stats.oecd.org/DownloadFiles.aspx?DatasetCode="
+
+CRS_URL: str = f"{BASE_URL}CRS1"
+TABLE1_URL: str = f"{BASE_URL}TABLE1"
+TABLE2A_URL: str = f"{BASE_URL}TABLE2A"
+MULTISYSTEM_URL: str = f"{BASE_URL}MULTISYSTEM"
+
+CODES_URL: str = "https://webfs.oecd.org/crs-iati-xml/Lookup/DAC-CRS-CODES.xml"
