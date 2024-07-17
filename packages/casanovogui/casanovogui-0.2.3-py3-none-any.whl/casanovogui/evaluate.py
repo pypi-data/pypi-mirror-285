f6913fb62c365a8f8166cb23ae4d529ef0cc3164
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-17 07:03:45 +0000

    Add package casanovogui/0.2.3/casanovogui-0.2.3-py3-none-any.whl

---
+++ packages/casanovogui/casanovogui-0.2.3-py3-none-any.whl/casanovogui/evaluate.py
@@ -0,0 +1,6 @@
+import streamlit as st
+
+def run():
+    st.set_page_config(page_title="Evaluate", layout="wide")
+    st.title('Evaluate')
+    st.warning('This page is under construction')
---
+++ packages/casanovogui/casanovogui-0.2.3-py3-none-any.whl/casanovogui/pages/spectra.py
@@ -0,0 +1,3 @@
+from spectra import run
+
+run()
