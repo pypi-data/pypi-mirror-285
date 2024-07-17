e6424bc8d47c9915bb279fe8980e28eb73927924
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-17 05:04:59 +0000

    Add package coherent.cli/0.3.1/coherent_cli-0.3.1.tar.gz

---
+++ packages/coherent.cli/coherent_cli-0.3.1.tar.gz/coherent_cli-0.3.1/__main__.py
@@ -0,0 +1,39 @@
+import pathlib
+import runpy
+import subprocess
+import sys
+
+import typer
+from coherent.build import bootstrap
+
+
+app = typer.Typer()
+
+passthrough_command = app.command(
+    context_settings={
+        'allow_extra_args': True,
+        'ignore_unknown_options': True,
+    },
+)
+
+
+@app.command()
+def install(target: pathlib.Path):
+    with bootstrap.write_pyproject(target):
+        subprocess.run([sys.executable, '-m', 'pip', 'install', target])
+
+
+@passthrough_command
+def build():
+    del sys.argv[1]
+    runpy.run_module('coherent.build', run_name='__main__')
+
+
+@passthrough_command
+def test():
+    del sys.argv[1]
+    runpy.run_module('coherent.test', run_name='__main__')
+
+
+if __name__ == '__main__':
+    app()
