478bc6fb7748580589739233a8be7b7b82e690f1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-17 15:03:32 +0000

    Add package time-limitation-module/1.0.1/time-limitation-module-1.0.1.tar.gz

---
+++ packages/time-limitation-module/time-limitation-module-1.0.1.tar.gz/time-limitation-module-1.0.1/setup.py
@@ -0,0 +1,13 @@
+from setuptools import find_packages, setup
+
+
+setup(
+    name="time-limitation-module",
+    version="1.0.1",
+    author="leooyang",
+    author_email="leooyang@futunn.com",
+    description="time limitation module",
+    python_requires="==3.7.5",
+    packages=find_packages(exclude=["tests"]),
+    install_requires=["pydantic==1.10.7", "cmlb_api_client==0.3.1"],
+)
