bfc5b3ab740a7632bcb542278133510a3d5ef0cc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-17 14:03:43 +0000

    Add package calculatepy/0.0.5/calculatepy-0.0.5.tar.gz

---
+++ packages/calculatepy/calculatepy-0.0.5.tar.gz/calculatepy-0.0.5/calculatepy/add.py
@@ -0,0 +1,13 @@
+from helpers.operand_type import check_operand_type
+
+def add(*ops):
+    total = 0
+    for op in ops:
+        if type(op) == list or type(op) == tuple or type(op) == set:
+            for o in op:
+                num = check_operand_type(o)
+                total = total + num
+        else:
+            num = check_operand_type(op)
+            total = total + num
+    return total
