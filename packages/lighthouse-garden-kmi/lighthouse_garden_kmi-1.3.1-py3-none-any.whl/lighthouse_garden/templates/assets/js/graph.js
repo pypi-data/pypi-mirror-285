4982fed3004e6964980e14afa68990ee7c1d4b49
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-17 03:04:31 +0000

    Add package lighthouse-garden-kmi/1.3.1/lighthouse_garden_kmi-1.3.1-py3-none-any.whl

---
+++ packages/lighthouse-garden-kmi/lighthouse_garden_kmi-1.3.1-py3-none-any.whl/lighthouse_garden/templates/assets/js/graph.js
@@ -0,0 +1,65 @@
+document.addEventListener('DOMContentLoaded', function() {
+    var classes = document.getElementsByClassName('graph');
+
+    var layout = {
+      showlegend: false,
+      height: 80,
+      width: 300,
+      xaxis: {
+        showline: false,
+        showgrid: false,
+        showticklabels: false,
+        linecolor: 'rgb(204,204,204)',
+        linewidth: 2,
+        autotick: false,
+        ticks: 'outside',
+        tickcolor: 'rgb(204,204,204)',
+        tickwidth: 0,
+        ticklen: 0,
+      },
+      yaxis: {
+        showgrid: false,
+        zeroline: false,
+        showline: false,
+        showticklabels: false
+      },
+      autosize: false,
+      margin: {
+        autoexpand: false,
+        l: 0,
+        r: 0,
+        t: 0,
+        b: 30
+      },
+    };
+
+    var i;
+    for (i = 0; i < classes.length; i++) {
+      var values_y = classes[i].getAttribute('data-values-y').split(',');
+      var values_x = classes[i].getAttribute('data-values-x').split(',');
+      var values_text = classes[i].getAttribute('data-values-text').split(',');
+      var id = classes[i].getAttribute('id');
+      var trace1 = {
+          y: values_y,
+          x: values_x,
+          z: values_text,
+          type: 'lines'
+      };
+      var data = [trace1];
+      var myPlot = document.getElementById(id);
+
+      Plotly.newPlot(id, data, layout, {displayModeBar: false});
+
+        myPlot.on('plotly_click', function(data){
+            data.points.map(function(d){
+              var key = Object.keys(d.data.x).find(key => d.data.x[key] === d.x);
+              var modal = $('#frameModal');
+              modal.find('.modal-title').text('Report ' + d.data.x[key]);
+              modal.find('.modal-body').html('<iframe src="' + d.data.z[key] + '">');
+              modal.find('.modal-body iframe').height(0.8 * $(document).height());
+              modal.modal('show');
+            });
+        });
+    }
+});
+
