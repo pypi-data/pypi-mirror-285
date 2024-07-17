29bb15081a88384f78ef579f0cc8e628650d4ddd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-17 16:05:40 +0000

    Add package pygame-ecs/0.3.0/pygame_ecs-0.3.0-py3-none-any.whl

---
+++ packages/pygame-ecs/pygame_ecs-0.3.0-py3-none-any.whl/pygame_ecs/managers/component_manager.py
@@ -0,0 +1,34 @@
+import typing
+
+from pygame_ecs.components.base_component import BaseComponent
+from pygame_ecs.entity import Entity
+from pygame_ecs.exceptions import EntityDoesNotHaveComponent
+
+ComponentInstanceType = typing.TypeVar("ComponentInstanceType", bound=BaseComponent)
+
+
+class ComponentManager:
+    __slots__ = ("components",)
+    def __init__(self) -> None:
+        self.components: dict[typing.Type[BaseComponent], dict[Entity, ComponentInstanceType]] = {}  # type: ignore
+
+    def init_components(self):
+        # get all subclasses using BaseComponent
+        component_subclasses = BaseComponent.__subclasses__()
+        for component_subclass in component_subclasses:
+            self.components[component_subclass] = {}
+
+    def add_component(self, entity, component):
+        """Adds a component to an entity
+
+        Args:
+            entity (Entity): Entity instance
+            component (BaseComponent): Component that subclasses BaseComponent
+        """
+        self.components[type(component)][entity] = component
+
+    def remove_component(self, entity, component_type):
+        try:
+            del self.components[component_type][entity]
+        except KeyError:
+            raise EntityDoesNotHaveComponent(entity, component_type)
