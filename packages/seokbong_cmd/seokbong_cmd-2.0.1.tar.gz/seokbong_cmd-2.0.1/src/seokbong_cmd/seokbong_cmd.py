3443e5b80955b9d0d4a9e96548183b149e673ad4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-17 14:03:24 +0000

    Add package seokbong_cmd/2.0.1/seokbong_cmd-2.0.1.tar.gz

---
+++ packages/seokbong_cmd/seokbong_cmd-2.0.1.tar.gz/seokbong_cmd-2.0.1/src/seokbong_cmd/seokbong_cmd.py
@@ -0,0 +1,12 @@
+from seokbong_add.add import seokbong_plus
+from seokbong_division.division import seokbong_divide
+from seokbong_mul.mul import seokbong_mul
+
+def call_plus():
+    seokbong_plus()
+
+def call_mul():
+    seokbong_mul()
+
+def call_div():
+    seokbong_divide()
