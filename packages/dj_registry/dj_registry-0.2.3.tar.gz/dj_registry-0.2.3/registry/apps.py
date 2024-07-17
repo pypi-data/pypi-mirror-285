01ce4997c254f7cfb515b108867bcbd326c3cf4a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-17 16:05:53 +0000

    Add package dj_registry/0.2.3/dj_registry-0.2.3.tar.gz

---
+++ packages/dj_registry/dj_registry-0.2.3.tar.gz/dj_registry-0.2.3/registry/apps.py
@@ -0,0 +1,7 @@
+from django.apps import AppConfig
+
+
+class RegistryConfig(AppConfig):
+    default_auto_field = 'django.db.models.AutoField'
+    name = 'registry'
+    verbose_name = "Django Registry"
