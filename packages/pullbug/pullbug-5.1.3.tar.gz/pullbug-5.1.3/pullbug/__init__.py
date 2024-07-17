3dbff291465a39a46a39f051fe3a3854d819e1cb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-17 07:03:59 +0000

    Add package pullbug/5.1.3/pullbug-5.1.3.tar.gz

---
+++ packages/pullbug/pullbug-5.1.3.tar.gz/pullbug-5.1.3/pullbug/__init__.py
@@ -0,0 +1,8 @@
+from pullbug.bug import Pullbug
+from pullbug.messages import Message
+
+
+__all__ = [
+    'Pullbug',
+    'Message',
+]
---
+++ packages/pullbug/pullbug-5.1.3.tar.gz/pullbug-5.1.3/setup.py
@@ -0,0 +1,72 @@
+import re
+
+import setuptools
+
+
+with open('README.md', 'r') as readme_file:
+    long_description = readme_file.read()
+
+# Inspiration: https://stackoverflow.com/a/7071358/6064135
+with open('pullbug/_version.py', 'r') as version_file:
+    version_groups = re.search(r"^__version__ = ['\"]([^'\"]*)['\"]", version_file.read(), re.M)
+    if version_groups:
+        version = version_groups.group(1)
+    else:
+        raise RuntimeError('Unable to find version string!')
+
+REQUIREMENTS = [
+    'PyGithub >= 1.59.0',
+    'requests == 2.*',
+    'slack_sdk == 3.*',
+    'woodchips == 1.*',
+]
+
+DEV_REQUIREMENTS = [
+    'bandit == 1.7.*',
+    'black == 24.*',
+    'build == 1.2.*',
+    'flake8 == 7.*',
+    'isort == 5.*',
+    'mypy == 1.10.*',
+    'pytest == 8.*',
+    'pytest-cov == 4.*',
+    'twine == 4.*',
+    'types-requests',
+]
+
+setuptools.setup(
+    name='pullbug',
+    version=version,
+    description='Get bugged via Discord or Slack to merge your GitHub pull requests or close open issues.',
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url='http://github.com/justintime50/pullbug',
+    author='Justintime50',
+    license='MIT',
+    packages=setuptools.find_packages(
+        exclude=[
+            'examples',
+            'test',
+        ]
+    ),
+    package_data={
+        'pullbug': [
+            'py.typed',
+        ]
+    },
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    install_requires=REQUIREMENTS,
+    extras_require={
+        'dev': DEV_REQUIREMENTS,
+    },
+    entry_points={
+        'console_scripts': [
+            'pullbug=pullbug.cli:main',
+        ],
+    },
+    python_requires='>=3.8, <4',
+)
