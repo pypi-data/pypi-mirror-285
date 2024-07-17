925e63ce99f7377a4cf37dc0059efb1b0dd9e335
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-17 17:03:51 +0000

    Add package tong/0.1.6/tong-0.1.6-py3-none-any.whl

---
+++ packages/tong/tong-0.1.6-py3-none-any.whl/tong/__init__.py
@@ -0,0 +1,26 @@
+from .infer import BaseInfer
+from .types import BaseInputs, BaseOutputs, FileInput, StringInput, FloatInput, IntegerInput, BooleanInput, FileOutput, StringOutput, FloatOutput, IntegerOutput, BooleanOutput, FileOutputList, StringOutputList, FloatOutputList, IntegerOutputList, BooleanOutputList
+
+# export all the classes
+
+__all__ = [
+    'BaseInfer',
+    'BaseInputs',
+    'BaseOutputs',
+    'FileInput',
+    'StringInput',
+    'FloatInput',
+    'IntegerInput',
+    'BooleanInput',
+    'FileOutput',
+    'StringOutput',
+    'FloatOutput',
+    'IntegerOutput',
+    'BooleanOutput',
+    'FileOutputList',
+    'StringOutputList',
+    'FloatOutputList',
+    'IntegerOutputList',
+    'BooleanOutputList'
+]
+
