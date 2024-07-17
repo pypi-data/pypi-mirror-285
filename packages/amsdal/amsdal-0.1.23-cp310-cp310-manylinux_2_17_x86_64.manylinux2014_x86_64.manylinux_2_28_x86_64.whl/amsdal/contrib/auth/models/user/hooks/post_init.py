f4a00651f64ebf8ad0d14015b7ba59a779f2a13b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-17 16:06:15 +0000

    Add package amsdal/0.1.23/amsdal-0.1.23-cp310-cp310-manylinux_2_17_x86_64.manylinux2014_x86_64.manylinux_2_28_x86_64.whl

---
+++ packages/amsdal/amsdal-0.1.23-cp310-cp310-manylinux_2_17_x86_64.manylinux2014_x86_64.manylinux_2_28_x86_64.whl/amsdal/contrib/auth/models/user/hooks/post_init.py
@@ -0,0 +1,25 @@
+from typing import Any
+
+
+def post_init(self, *, is_new_object: bool, kwargs: dict[str, Any]) -> None:  # type: ignore[no-untyped-def]
+    import bcrypt
+
+    from amsdal.contrib.auth.errors import UserCreationError
+
+    email = kwargs.get('email', None)
+    password = kwargs.get('password', None)
+
+    if email is None or email == '':
+        msg = "Email can't be empty"
+        raise UserCreationError(msg)
+
+    if password is None or password == '':
+        msg = "Password can't be empty"
+        raise UserCreationError(msg)
+
+    kwargs['email'] = email.lower()
+
+    if is_new_object and '_metadata' not in kwargs:
+        hashed_password = bcrypt.hashpw(password.encode('utf-8'), bcrypt.gensalt())
+        self.password = hashed_password
+        self._object_id = email.lower()
