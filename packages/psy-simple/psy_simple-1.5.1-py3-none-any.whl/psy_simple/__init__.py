d1daeb22b72f89e0869a3cc4ba664de8991e4d8e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-17 04:03:32 +0000

    Add package psy-simple/1.5.1/psy_simple-1.5.1-py3-none-any.whl

---
+++ packages/psy-simple/psy_simple-1.5.1-py3-none-any.whl/psy_simple/__init__.py
@@ -0,0 +1,32 @@
+# SPDX-FileCopyrightText: 2016-2024 University of Lausanne
+# SPDX-FileCopyrightText: 2020-2021 Helmholtz-Zentrum Geesthacht
+# SPDX-FileCopyrightText: 2021-2024 Helmholtz-Zentrum hereon GmbH
+#
+# SPDX-License-Identifier: LGPL-3.0-only
+
+"""psy-simple
+
+Psyplot plugin for simple visualization tasks
+"""
+
+from __future__ import annotations
+
+from . import _version
+
+__version__ = _version.get_versions()["version"]
+
+__author__ = "Philipp S. Sommer"
+__copyright__ = """
+2016-2024 University of Lausanne
+2020-2021 Helmholtz-Zentrum Geesthacht
+2021-2024 Helmholtz-Zentrum hereon GmbH
+"""
+__credits__ = [
+    "Philipp S. Sommer",
+]
+__license__ = "LGPL-3.0-only"
+
+__maintainer__ = "Philipp S. Sommer"
+__email__ = "philipp.sommer@hereon.de"
+
+__status__ = "Pre-Alpha"
