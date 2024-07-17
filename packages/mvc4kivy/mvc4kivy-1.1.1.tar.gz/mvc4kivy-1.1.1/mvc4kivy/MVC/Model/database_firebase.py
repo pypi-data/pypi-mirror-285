51a7d312fcc2f90798dc343f94e44f044209250a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-17 05:04:35 +0000

    Add package mvc4kivy/1.1.1/mvc4kivy-1.1.1.tar.gz

---
+++ packages/mvc4kivy/mvc4kivy-1.1.1.tar.gz/mvc4kivy-1.1.1/mvc4kivy/MVC/Model/database_firebase.py
@@ -0,0 +1,31 @@
+from __future__ import annotations
+
+import socket
+
+
+def get_connect(func, host="8.8.8.8", port=53, timeout=3):
+    """Checks for an active Internet connection."""
+
+    def wrapped(*args):
+        try:
+            socket.setdefaulttimeout(timeout)
+            socket.socket(socket.AF_INET, socket.SOCK_STREAM).connect(
+                (host, port)
+            )
+            return func(*args)
+        except Exception:
+            return False
+
+    return wrapped
+
+
+class DataBase:
+    """
+    Your methods for working with the database should be implemented in this
+    class.
+    """
+
+    name = "Firebase"
+
+    def __init__(self):
+        ...
