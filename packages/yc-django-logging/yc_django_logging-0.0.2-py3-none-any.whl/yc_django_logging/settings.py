18e5476f7be111ef10335f7944957c8efcb827c7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-17 14:03:43 +0000

    Add package yc-django-logging/0.0.2/yc_django_logging-0.0.2-py3-none-any.whl

---
+++ packages/yc-django-logging/yc_django_logging-0.0.2-py3-none-any.whl/yc_django_logging/settings.py
@@ -0,0 +1,26 @@
+"""
+Created on 2024/7/16 17:06
+@author:刘飞
+@description: 一些配置信息
+"""
+from django.utils.translation import gettext_lazy as _
+
+logging_menu_list = [
+    {
+        'name': _('日志模块'),
+        'models': [
+            {
+                'name': _('登录日志'),
+                'url': 'yc_django_logging/operationlog/'
+            },  # 操作日志
+            {
+                'name': _('操作日志'),
+                'url': 'yc_django_logging/loginlog/'
+            },  # 登录日志
+            {
+                'name': _('后台日志'),
+                'url': 'admin/logentry/'
+            },  # 后台日志
+        ]
+    }
+]
