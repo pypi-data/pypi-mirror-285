587f1aef2681c92903739be8956c2d09ba46633e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-17 05:04:23 +0000

    Add package intellikit/0.0.6/intellikit-0.0.6.tar.gz

---
+++ packages/intellikit/intellikit-0.0.6.tar.gz/intellikit-0.0.6/intellikit/__init__.py
@@ -0,0 +1,10 @@
+"""Top-level package for intellikit."""
+
+__author__ = """Arthur Kakande"""
+__email__ = "kaksarthur@gmail.com"
+__version__ = "0.0.6"
+
+from .intellikit import *
+from .sim import *
+from .ir import *
+from .common import *
