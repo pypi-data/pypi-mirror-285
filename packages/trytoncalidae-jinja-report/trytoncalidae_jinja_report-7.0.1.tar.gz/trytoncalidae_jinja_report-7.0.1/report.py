8db5a9c7e1be2445ba3866f4070b2e97f3a7bb44
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-17 03:03:56 +0000

    Add package trytoncalidae-jinja-report/7.0.1/trytoncalidae_jinja_report-7.0.1.tar.gz

---
+++ packages/trytoncalidae-jinja-report/trytoncalidae_jinja_report-7.0.1.tar.gz/trytoncalidae_jinja_report-7.0.1/report.py
@@ -0,0 +1,115 @@
+__all__ = [
+    'JinjaTranslator',
+    'Jinja2Report',
+    'SetTranslationJinja2',
+]
+
+from base64 import b64encode
+import jinja2
+
+from trytond.pool import Pool, PoolMeta
+from trytond.report.report import TranslateFactory
+
+
+class JinjaExtensionsMixin():
+    JINJA_EXTENSIONS = [
+        'jinja2.ext.i18n',
+        'jinja2.ext.do',
+        'jinja2.ext.loopcontrols',
+    ]
+
+    @classmethod
+    def get_extensions(cls):
+        return cls.JINJA_EXTENSIONS
+
+
+class JinjaTranslator(object):
+
+    def __init__(self, method):
+        self.gettext = method
+        self.ngettext = method
+        self.ugettext = method
+        self.ungettext = method
+
+
+class Jinja2Report(JinjaExtensionsMixin, metaclass=PoolMeta):
+
+    @classmethod
+    def render(cls, action, context):
+        env = cls.get_environ()
+        template = env.from_string(action.report_content.decode())
+        return template.render(**context)
+
+    @classmethod
+    def get_environ(cls):
+        env = jinja2.Environment(extensions=cls.get_extensions())
+        translations = cls.get_translations()
+        env.install_gettext_translations(translations)
+        env.filters['b64encode'] = b64encode
+        env.filters['decode'] = bytes.decode
+        return env
+
+    @classmethod
+    def get_context(cls, records, header, data):
+        report_context = super().get_context(records, header, data)
+        report_context['attachment'] = cls.get_attachment
+        report_context['attachments'] = cls.get_attachments
+        return report_context
+
+    @classmethod
+    def get_translations(cls):
+        translate = TranslateFactory(
+            cls.__name__,
+            Pool().get('ir.translation'),
+        )
+        return JinjaTranslator(lambda text: translate(text))
+
+    @classmethod
+    def get_attachment(cls, record, name):
+        Attachment = Pool().get('ir.attachment')
+        try:
+            attachment, = Attachment.search([
+                ('resource', '=', record),
+                ('name', '=', name),
+            ])
+            data = attachment.data
+        except ValueError:
+            data = None
+        finally:
+            return data
+
+    @classmethod
+    def get_attachments(cls, record, name):
+        Attachment = Pool().get('ir.attachment')
+        try:
+            attachments = Attachment.search([
+                ('resource', '=', record),
+                ('name', 'ilike', f"{name}%"),
+            ])
+            data = [attachment.data for attachment in attachments]
+        except ValueError:
+            data = []
+        finally:
+            return data
+
+
+class SetTranslationJinja2(JinjaExtensionsMixin, metaclass=PoolMeta):
+
+    __name__ = 'ir.translation.set'
+
+    def extract_report_html(self, content):
+        try:
+            yield from super().extract_report_html(content)
+        except Exception:
+            # Because trytond tries to manage any (x|ht)ml report
+            # content with the picky genshi translation extractor,
+            # the set-translations process will fail in presence of
+            # reports tailored for other template languages like jinja2.
+            env = jinja2.Environment(extensions=self.get_extensions())
+            for _, _, str_ in env.extract_translations(content.decode()):
+                if str_:
+                    yield str_
+
+
+def register():
+    Pool.register(SetTranslationJinja2, module='jinja_report', type_='wizard')
---
+++ packages/trytoncalidae-jinja-report/trytoncalidae_jinja_report-7.0.1.tar.gz/trytoncalidae_jinja_report-7.0.1/tests/test_jinja_report.py
@@ -0,0 +1,146 @@
+
+import trytond_factories
+
+from trytond.pool import Pool
+from trytond.tests.test_tryton import activate_module
+from trytond.tests.test_tryton import ModuleTestCase
+from trytond.tests.test_tryton import with_transaction
+
+from report import Jinja2Report
+
+
+class JinjaReportTestCase(ModuleTestCase):
+    'Test Jinja Report module'
+    module = 'jinja_report'
+
+    class TestReport(Jinja2Report):
+        __name__ = 'jinja.test_report'
+
+    @classmethod
+    def setUpClass(cls):
+        Pool.register(cls.TestReport, module='jinja_report', type_='report')
+        activate_module(['tests', 'jinja_report'])
+
+    @with_transaction()
+    def test_jinja_report(self):
+        # GIVEN
+        report = trytond_factories.Report.create(
+            model='test.model',
+            report_name='jinja.test_report',
+            extension='html',
+            template_extension='html',
+            report_content_custom=(
+                b"{{ record.name }} {{ data.get('data') }}"
+            ),
+        )
+
+        record = trytond_factories.TestModel.create(name='R3C0RD')
+
+        # WHEN
+        Report = Pool().get(report.report_name, type='report')
+        (_, report_data, _, _) = Report.execute(
+            [record.id], data={'data': 'D4T4'}
+        )
+
+        # THEN
+        self.assertEqual(report_data, 'R3C0RD D4T4')
+
+    @with_transaction()
+    def test_jinja_report_attachment(self):
+        # GIVEN
+        record = trytond_factories.TestModel.create()
+        trytond_factories.DataAttachment.create(
+            resource=record,
+            name='example.png',
+            data=b'\x89PNG\r\n\x1a\n\x00\x00\x00\rIHDR\x00\x00\x00\x02\x00'
+            b'\x00\x00\x02\x08\x02\x00\x00\x00\xfd\xd4\x9as\x00\x00\x00\x01'
+            b'sRGB\x00\xae\xce\x1c\xe9\x00\x00\x00\x04gAMA\x00\x00\xb1\x8f'
+            b'\x0b\xfca\x05\x00\x00\x00\tpHYs\x00\x00\x0e\xc3\x00\x00\x0e\xc3'
+            b'\x01\xc7o\xa8d\x00\x00\x00\x15IDAT\x18Wcxgd\xf5VN\x8d\x01\x88'
+            b'\xdf\xdb\xb9\x02\x00&\xc4\x05/C\xee\xb8\xc6\x00\x00\x00\x00'
+            b'IEND\xaeB`\x82',
+        )
+        report = trytond_factories.Report.create(
+            model='test.model',
+            report_name='jinja.test_report',
+            extension='html',
+            template_extension='html',
+            report_content_custom=(
+                b"{{ attachment(record, 'example.png') "
+                b"| b64encode | decode }}"
+            ),
+        )
+        Report = Pool().get(report.report_name, type='report')
+
+        # WHEN
+        (_, report_data, _, _) = Report.execute(ids=[record.id], data={})
+
+        # THEN
+        self.assertEqual(
+            report_data,
+            (
+                "iVBORw0KGgoAAAANSUhEUgAAAAIAAAACCAIAAAD91JpzAAAAAXNSR0IArs"
+                "4c6QAAAARnQU1BAACxjwv8YQUAAAAJcEhZcwAADsMAAA7DAcdvqGQAAAAVSU"
+                "RBVBhXY3hnZPVWTo0BiN/buQIAJsQFL0PuuMYAAAAASUVORK5CYII="
+            )
+        )
+
+    @with_transaction()
+    def test_jinja_report_attachments(self):
+        # GIVEN
+        record = trytond_factories.TestModel.create()
+        trytond_factories.DataAttachment.create(
+            resource=record,
+            name='image example.png',
+            data=b'\x89PNG\r\n\x1a\n\x00\x00\x00\rIHDR\x00\x00\x00\x02\x00'
+            b'\x00\x00\x02\x08\x02\x00\x00\x00\xfd\xd4\x9as\x00\x00\x00\x01'
+            b'sRGB\x00\xae\xce\x1c\xe9\x00\x00\x00\x04gAMA\x00\x00\xb1\x8f'
+            b'\x0b\xfca\x05\x00\x00\x00\tpHYs\x00\x00\x0e\xc3\x00\x00\x0e\xc3'
+            b'\x01\xc7o\xa8d\x00\x00\x00\x15IDAT\x18Wcxgd\xf5VN\x8d\x01\x88'
+            b'\xdf\xdb\xb9\x02\x00&\xc4\x05/C\xee\xb8\xc6\x00\x00\x00\x00'
+            b'IEND\xaeB`\x82',
+        )
+        trytond_factories.DataAttachment.create(
+            resource=record,
+            name='image example2.png',
+            data=b'\x89PNG\r\n\x1a\n\x00\x00\x00\rIHDR\x00\x00\x00\x02\x00'
+            b'\x00\x00\x02\x08\x02\x00\x00\x00\xfd\xd4\x9as\x00\x00\x00\x01'
+            b'sRGB\x00\xae\xce\x1c\xe9\x00\x00\x00\x04gAMA\x00\x00\xb1\x8f'
+            b'\x0b\xfca\x05\x00\x00\x00\tpHYs\x00\x00\x0e\xc3\x00\x00\x0e\xc3'
+            b'\x01\xc7o\xa8d\x00\x00\x00\x15IDAT\x18Wcxgd\xf5VN\x8d\x01\x88'
+            b'\xdf\xdb\xb9\x02\x00&\xc4\x05/C\xee\xb8\xc6\x00\x00\x00\x00'
+            b'IEND\xaeB`\x82',
+        )
+        report = trytond_factories.Report.create(
+            model='test.model',
+            report_name='jinja.test_report',
+            extension='html',
+            template_extension='html',
+            report_content_custom=(
+                b"{% for attachment in attachments(record, 'image') %}"
+                b"{{ attachment | b64encode | decode }}"
+                b"{% endfor %}"
+            ),
+        )
+        Report = Pool().get(report.report_name, type='report')
+
+        # WHEN
+        (_, report_data, _, _) = Report.execute(ids=[record.id], data={})
+
+        # THEN
+        self.assertEqual(
+            report_data,
+            (
+                # first attachment
+                "iVBORw0KGgoAAAANSUhEUgAAAAIAAAACCAIAAAD91JpzAAAAAXNSR0IArs"
+                "4c6QAAAARnQU1BAACxjwv8YQUAAAAJcEhZcwAADsMAAA7DAcdvqGQAAAAVSU"
+                "RBVBhXY3hnZPVWTo0BiN/buQIAJsQFL0PuuMYAAAAASUVORK5CYII="
+                # second attachment
+                "iVBORw0KGgoAAAANSUhEUgAAAAIAAAACCAIAAAD91JpzAAAAAXNSR0IArs"
+                "4c6QAAAARnQU1BAACxjwv8YQUAAAAJcEhZcwAADsMAAA7DAcdvqGQAAAAVSU"
+                "RBVBhXY3hnZPVWTo0BiN/buQIAJsQFL0PuuMYAAAAASUVORK5CYII="
+            )
+        )
+
+
+del ModuleTestCase
