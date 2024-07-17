65035cde4f7c41f9dee8543fe6752d8b3b099b3f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-17 15:04:17 +0000

    Add package PgQueuer/0.5.8/PgQueuer-0.5.8-py3-none-any.whl

---
+++ packages/PgQueuer/PgQueuer-0.5.8-py3-none-any.whl/PgQueuer/listeners.py
@@ -0,0 +1,52 @@
+from __future__ import annotations
+
+import asyncio
+
+from . import models
+from .db import Driver
+from .logconfig import logger
+
+
+class PGEventListener(asyncio.Queue[models.Event]):
+    """
+    A PostgreSQL event queue that listens to a specified
+    channel and stores incoming events.
+    """
+
+
+async def initialize_event_listener(
+    connection: Driver,
+    channel: models.PGChannel,
+) -> PGEventListener:
+    """
+    This method establishes a listener on a PostgreSQL channel using
+    the provided connection and channel.
+    """
+
+    def parse_and_queue(
+        payload: str | bytes | bytearray,
+        queue: PGEventListener,
+    ) -> None:
+        """
+        Parses a JSON payload and inserts it into the queue as an `models.Event` object.
+        """
+        try:
+            parsed_event = models.Event.model_validate_json(payload)
+        except Exception:
+            logger.critical(
+                "Failed to parse payload: `%s`.",
+                payload,
+            )
+            return
+
+        try:
+            queue.put_nowait(parsed_event)
+        except Exception:
+            logger.critical(
+                "Unexpected error inserting event into queue: `%s`.",
+                parsed_event,
+            )
+
+    listener = PGEventListener()
+    await connection.add_listener(channel, lambda x: parse_and_queue(x, listener))
+    return listener
---
+++ packages/PgQueuer/PgQueuer-0.5.8-py3-none-any.whl/PgQueuer/models.py
@@ -0,0 +1,109 @@
+from __future__ import annotations
+
+from datetime import datetime, timedelta, timezone
+from typing import Literal, NewType
+
+from pydantic import AwareDatetime, BaseModel, Field
+
+###### Events ######
+
+PGChannel = NewType(
+    "PGChannel",
+    str,
+)
+
+
+OPERATIONS = Literal[
+    "insert",
+    "update",
+    "delete",
+    "truncate",
+]
+
+
+class Event(BaseModel):
+    """
+    A class representing an event in a PostgreSQL channel.
+
+    Attributes:
+        channel: The PostgreSQL channel the event belongs to.
+        operation: The type of operation performed (insert, update or delete).
+        sent_at: The timestamp when the event was sent.
+        table: The table the event is associated with.
+        received_at: The timestamp when the event was received.
+    """
+
+    channel: PGChannel
+    operation: OPERATIONS
+    sent_at: AwareDatetime
+    table: str
+    received_at: AwareDatetime = Field(
+        init=False,
+        default_factory=lambda: datetime.now(
+            tz=timezone.utc,
+        ),
+    )
+
+    @property
+    def latency(self) -> timedelta:
+        """
+        Calculate the latency between when the event was sent and received.
+        """
+        return self.received_at - self.sent_at
+
+
+###### Jobs ######
+JobId = NewType(
+    "JobId",
+    int,
+)
+
+STATUS = Literal[
+    "queued",
+    "picked",
+]
+
+
+class Job(BaseModel):
+    """
+    Represents a job with attributes such as ID, priority,
+    creation time, status, entrypoint, and optional payload.
+    """
+
+    id: int
+    priority: int
+    created: AwareDatetime
+    status: STATUS
+    entrypoint: str
+    payload: bytes | None
+
+
+###### Statistics ######
+STATUS_LOG = Literal[
+    "exception",
+    "successful",
+]
+
+
+class QueueStatistics(BaseModel):
+    """
+    Represents the number of jobs per entrypoint and priority in the queue.
+    """
+
+    count: int
+    entrypoint: str
+    priority: int
+    status: STATUS
+
+
+class LogStatistics(BaseModel):
+    """
+    Represents log statistics for jobs based on status, entrypoint, and priority.
+    """
+
+    count: int
+    created: AwareDatetime
+    entrypoint: str
+    priority: int
+    status: STATUS_LOG
+    time_in_queue: timedelta
