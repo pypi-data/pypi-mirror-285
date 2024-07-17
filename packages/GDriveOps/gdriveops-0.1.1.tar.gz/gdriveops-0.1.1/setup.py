9949704cf061a4dbe807b877025071de86d921d6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-17 05:04:03 +0000

    Add package GDriveOps/0.1.1/gdriveops-0.1.1.tar.gz

---
+++ packages/GDriveOps/gdriveops-0.1.1.tar.gz/gdriveops-0.1.1/setup.py
@@ -0,0 +1,37 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='GDriveOps',
+    version='0.1.1',
+    packages=find_packages(),
+    install_requires=[
+        "google-api-python-client==1.7.2",
+        "google-auth==2.14.1",
+        "google-auth-httplib2==0.0.3",
+        "google-auth-oauthlib==0.4.1",
+        'PyMuPDF',
+        'python-docx'
+    ],
+    dependency_links=[
+        'https://pypi.org/simple/'
+    ],
+    entry_points={
+        'console_scripts': [
+            'GDriveOps=GDriveOps.GDhandler:main',
+        ],
+    },
+    author='Hammed A. Akande',
+    author_email='akandehammedadedamola@gmail.com',
+    description='A package to handle Google Drive uploads and downloads.',
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
+    url='https://github.com/drhammed/GDriveOps',
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Intended Audience :: Developers',
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+    ],
+    python_requires='>=3.6',
+)
