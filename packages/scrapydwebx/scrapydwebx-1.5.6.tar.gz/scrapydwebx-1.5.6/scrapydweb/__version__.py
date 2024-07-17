961ae0ec85fff92cac09084746ae8784fccd864b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-17 04:03:59 +0000

    Add package scrapydwebx/1.5.6/scrapydwebx-1.5.6.tar.gz

---
+++ packages/scrapydwebx/scrapydwebx-1.5.6.tar.gz/scrapydwebx-1.5.6/scrapydweb/__version__.py
@@ -0,0 +1,10 @@
+# coding: utf-8
+
+__title__ = 'scrapydwebx'
+__version__ = '1.5.6'
+__author__ = 'payala'
+__author_email__ = 'ppayala@gmail.com'
+__url__ = 'https://github.com/payala/scrapydwebx'
+__license__ = 'GNU General Public License v3.0'
+__description__ = ("Web app for Scrapyd cluster management, "
+                   "with support for Scrapy log analysis & visualization.")
