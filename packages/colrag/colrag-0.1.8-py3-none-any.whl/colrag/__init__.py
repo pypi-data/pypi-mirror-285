6a37e0a7f73c4f43878438007faa486f399154a4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-17 03:03:42 +0000

    Add package colrag/0.1.8/colrag-0.1.8-py3-none-any.whl

---
+++ packages/colrag/colrag-0.1.8-py3-none-any.whl/colrag/__init__.py
@@ -0,0 +1,17 @@
+__version__ = "0.1.8"  # Update this to your current version
+
+from .indexer import index_documents, read_file, process_file
+from .retriever import load_model_from_index, retrieve_and_rerank_documents, retrieve_and_rerank_multiple_documents
+from .config import config
+from .logger import get_logger
+
+__all__ = [
+    'index_documents',
+    'read_file',
+    'process_file',
+    'load_model_from_index',
+    'retrieve_and_rerank_documents',
+    'retrieve_and_rerank_multiple_documents',
+    'config',
+    'get_logger'
+]
