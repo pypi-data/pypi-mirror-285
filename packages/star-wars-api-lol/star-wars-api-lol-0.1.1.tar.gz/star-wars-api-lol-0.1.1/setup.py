05bbdba2d0bdd8f5e58333b3586703370c1ec8dd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-17 03:03:42 +0000

    Add package star-wars-api-lol/0.1.1/star-wars-api-lol-0.1.1.tar.gz

---
+++ packages/star-wars-api-lol/star-wars-api-lol-0.1.1.tar.gz/star-wars-api-lol-0.1.1/setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup, find_packages
+setup(
+name='star-wars-api-lol',
+version='0.1.1',
+author='suyog',
+author_email='kulkarnisuyog3@gmail.com',
+description='SWAPI, A Star Wars API Package',
+packages=find_packages(),
+classifiers=[
+'Programming Language :: Python :: 3',
+'License :: OSI Approved :: MIT License',
+'Operating System :: OS Independent',
+],
+python_requires='>=3.6',
+)
