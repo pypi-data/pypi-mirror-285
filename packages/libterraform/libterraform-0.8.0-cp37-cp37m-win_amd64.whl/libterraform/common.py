a4df7e919df60b3ae9ef79b1bd8fa5e7bcb4bc92
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-17 15:03:48 +0000

    Add package libterraform/0.8.0/libterraform-0.8.0-cp37-cp37m-win_amd64.whl

---
+++ packages/libterraform/libterraform-0.8.0-cp37-cp37m-win_amd64.whl/libterraform/common.py
@@ -0,0 +1,32 @@
+import json
+import os
+from typing import List, Union
+
+# ===================================================================
+# OS constants
+# ===================================================================
+
+WINDOWS = os.name == "nt"
+
+# ===================================================================
+# Type
+# ===================================================================
+
+CmdType = Union[str, List]
+
+
+# ===================================================================
+# utils
+# ===================================================================
+
+
+def json_loads(string, split=False):
+    if split:
+        value = []
+        for line in string.split("\n"):
+            if not line:
+                continue
+            value.append(json.loads(line))
+    else:
+        value = json.loads(string)
+    return value
