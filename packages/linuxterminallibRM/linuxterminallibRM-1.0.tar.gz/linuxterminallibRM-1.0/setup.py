b14a5fe166fbfb217300ec99c7c3c9fc27cc89dd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-17 03:03:51 +0000

    Add package linuxterminallibRM/1.0/linuxterminallibRM-1.0.tar.gz

---
+++ packages/linuxterminallibRM/linuxterminallibRM-1.0.tar.gz/linuxterminallibRM-1.0/setup.py
@@ -0,0 +1,8 @@
+from setuptools import setup
+
+setup(name='linuxterminallibRM',
+      version='1.0',
+      description='Can run commands in terminal (with sudo too)',
+      packages=[''],
+      author_email='arcanedevstudio@gmail.com',
+      zip_safe=False)
