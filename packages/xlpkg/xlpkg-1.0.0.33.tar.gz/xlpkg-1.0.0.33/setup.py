4fe10a553fc6b1339df40d65896df871dbf44ec4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-17 16:06:10 +0000

    Add package xlpkg/1.0.0.33/xlpkg-1.0.0.33.tar.gz

---
+++ packages/xlpkg/xlpkg-1.0.0.33.tar.gz/xlpkg-1.0.0.33/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup, find_packages
+ 
+setup(
+    name='xlpkg03',
+    version='1.0.0.4',
+    description='My Project Description',
+    packages=find_packages(),
+    python_requires='>=3.6',
+)
