e96d9cc61948ac4a1c076d6a03cddd02c3a3b805
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-17 16:06:06 +0000

    Add package lddya-test/1.0.0/lddya-test-1.0.0.tar.gz

---
+++ packages/lddya-test/lddya-test-1.0.0.tar.gz/lddya-test-1.0.0/setup.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python
+#-*- coding:utf-8 -*-
+
+#############################################
+# File Name: setup.py
+# Author: lidongdong
+# Mail: 927052521@qq.com
+# Created Time: 2022.10.21  19.50
+############################################
+
+
+from setuptools import setup, find_packages
+
+setup(
+    name = "lddya-test",
+    version = "1.0.0",
+    keywords = {"pip", "license","licensetool", "tool", "gm"},
+    description = "* 给栅格图的绘制路径功能添加一个label参数；给算法的run函数添加一个show_process参数；给Map添加一个识别路径的功能；给Clock添加一个获取delta数值的方法；给算法的迭代数据添加一个保存为excel的方法；给Map添加一个size属性。",
+    long_description = "具体功能，请自行挖掘。",
+    license = "MIT Licence",
+
+    url = "https://github.com/not_define/please_wait",
+    author = "lidongdong",
+    author_email = "927052521@qq.com",
+
+    packages = find_packages(),
+    include_package_data = True,
+    platforms = "any",
+    install_requires = ['numpy','matplotlib','pygame','pandas']
+)
