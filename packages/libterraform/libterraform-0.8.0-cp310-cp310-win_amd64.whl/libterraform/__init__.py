7a1286977662085b6edcf50a608d9e144f59ba3e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-17 15:03:36 +0000

    Add package libterraform/0.8.0/libterraform-0.8.0-cp310-cp310-win_amd64.whl

---
+++ packages/libterraform/libterraform-0.8.0-cp310-cp310-win_amd64.whl/libterraform/__init__.py
@@ -0,0 +1,18 @@
+import os
+from ctypes import c_void_p, cdll
+
+from libterraform.common import WINDOWS
+
+__version__ = "0.8.0"
+
+root = os.path.dirname(os.path.abspath(__file__))
+_lib_filename = "libterraform.dll" if WINDOWS else "libterraform.so"
+_lib_tf = cdll.LoadLibrary(os.path.join(root, _lib_filename))
+
+_free = _lib_tf.Free
+_free.argtypes = [c_void_p]
+
+from .cli import TerraformCommand
+from .config import TerraformConfig
+
+__all__ = ["TerraformCommand", "TerraformConfig"]
