131687dcdf16431d3f850c31fbe1b383fdb3191d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-17 04:03:23 +0000

    Add package brigid/0.6.1/brigid-0.6.1-py3-none-any.whl

---
+++ packages/brigid/brigid-0.6.1-py3-none-any.whl/brigid/conftest.py
@@ -0,0 +1,27 @@
+import asyncio
+import os
+from typing import AsyncGenerator, Generator
+
+import fastapi
+import pytest
+import pytest_asyncio
+
+from brigid.application import application
+
+
+@pytest.fixture(scope="session", autouse=True)
+def mark_tests_running():
+    os.environ["BRIGID_TESTS_RUNNING"] = "True"
+
+
+@pytest.fixture(scope="session", autouse=True)
+def event_loop() -> Generator[asyncio.AbstractEventLoop, asyncio.AbstractEventLoop, None]:
+    loop = asyncio.get_event_loop_policy().new_event_loop()
+    yield loop
+    loop.close()
+
+
+@pytest_asyncio.fixture(scope="session", autouse=True)
+async def app() -> AsyncGenerator[fastapi.FastAPI, None]:
+    async with application.with_app() as app:
+        yield app
