8ea1d374381284fc5f76339cd79bd1b0dbab8fcd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-17 14:03:31 +0000

    Add package Front11_div/0.5.0/Front11_div-0.5.0-py3-none-any.whl

---
+++ packages/Front11_div/Front11_div-0.5.0-py3-none-any.whl/front11_div/front11_div.py
@@ -0,0 +1,13 @@
+import sys
+
+a = sys.argv[0]
+x = int(sys.argv[1])
+y = int(sys.argv[2])
+
+def divide(x, y):
+    if y == 0:
+        raise ValueError("Division by zero is not allowed")
+    else:
+        return x / y
+
+    
