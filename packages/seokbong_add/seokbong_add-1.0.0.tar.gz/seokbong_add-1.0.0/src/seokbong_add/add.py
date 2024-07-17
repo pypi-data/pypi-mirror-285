ce21132043ade600420077834c4c4f6682e23511
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-17 14:03:24 +0000

    Add package seokbong_add/1.0.0/seokbong_add-1.0.0.tar.gz

---
+++ packages/seokbong_add/seokbong_add-1.0.0.tar.gz/seokbong_add-1.0.0/src/seokbong_add/add.py
@@ -0,0 +1,11 @@
+import sys
+
+def add():
+    first_num = int(input("first_num: "))
+    second_num = int(input("second_num: "))
+    print(first_num+second_num)
+
+def seokbong_plus():
+    a = int(sys.argv[1])
+    b = int(sys.argv[2])
+    print(a+b)
