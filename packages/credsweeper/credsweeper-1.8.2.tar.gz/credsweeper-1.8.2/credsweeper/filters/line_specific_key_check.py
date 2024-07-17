39f1ab05d5c226eb29b0a5f51e530da3a2347d83
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-17 05:03:34 +0000

    Add package credsweeper/1.8.2/credsweeper-1.8.2.tar.gz

---
+++ packages/credsweeper/credsweeper-1.8.2.tar.gz/credsweeper-1.8.2/credsweeper/filters/line_specific_key_check.py
@@ -0,0 +1,36 @@
+import re
+
+from credsweeper.config import Config
+from credsweeper.credentials import LineData
+from credsweeper.file_handler.analysis_target import AnalysisTarget
+from credsweeper.filters import Filter
+from credsweeper.utils import Util
+
+
+class LineSpecificKeyCheck(Filter):
+    """Check that values from list below is not in candidate line."""
+
+    NOT_ALLOWED = [r"example", r"enc\(", r"enc\[", r"true", r"false"]
+    NOT_ALLOWED_PATTERN = re.compile(Util.get_regex_combine_or(NOT_ALLOWED))
+
+    def __init__(self, config: Config = None) -> None:
+        pass
+
+    def run(self, line_data: LineData, target: AnalysisTarget) -> bool:
+        """Run filter checks on received credential candidate data 'line_data'.
+
+        Args:
+            line_data: credential candidate data
+            target: multiline target from which line data was obtained
+
+        Return:
+            True, if need to filter candidate and False if left
+
+        """
+        if line_data.line is None:
+            return True
+
+        if self.NOT_ALLOWED_PATTERN.search(target.line_lower):
+            return True
+
+        return False
