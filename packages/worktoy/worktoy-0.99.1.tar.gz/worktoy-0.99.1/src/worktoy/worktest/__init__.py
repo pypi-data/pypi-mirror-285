2973d6e8135e5f2b7d70ae1ef630ec12539c0662
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-17 04:04:44 +0000

    Add package worktoy/0.99.1/worktoy-0.99.1.tar.gz

---
+++ packages/worktoy/worktoy-0.99.1.tar.gz/worktoy-0.99.1/src/worktoy/worktest/__init__.py
@@ -0,0 +1,6 @@
+"""WorkTest provides a general subclass of TestCase for testing."""
+#  AGPL-3.0 license
+#  Copyright (c) 2024 Asger Jon Vistisen
+from __future__ import annotations
+
+from ._work_test import WorkTest
