13dabc87c09e38754297ac2d4d2c18df8434107e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-17 15:03:37 +0000

    Add package libterraform/0.8.0/libterraform-0.8.0-cp311-cp311-macosx_12_0_x86_64.whl

---
+++ packages/libterraform/libterraform-0.8.0-cp311-cp311-macosx_12_0_x86_64.whl/libterraform/exceptions.py
@@ -0,0 +1,30 @@
+class LibTerraformError(Exception):
+    pass
+
+
+class TerraformCommandError(LibTerraformError):
+    """Raised when TerraformCommand.run() is called with check=True and the process
+    returns a non-zero exit status.
+
+    Attributes:
+      retcode, cmd, stdout, stderr
+    """
+
+    def __init__(self, retcode, cmd, stdout=None, stderr=None):
+        self.retcode = retcode
+        self.cmd = cmd
+        self.stdout = stdout
+        self.stderr = stderr
+
+    def __str__(self):
+        return f"Command {self.cmd!r} returned non-zero exit status {self.retcode}."
+
+
+class TerraformFdReadError(LibTerraformError):
+    """Raised when TerraformCommand.run() is called and cannot read stdout/stderr."""
+
+    def __init__(self, fd):
+        self.fd = fd
+
+    def __str__(self):
+        return f"Read from fd {self.fd} error."
