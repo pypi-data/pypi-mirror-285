a78ec05b511270a36c11ea6f614828939d4e0fb6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-17 16:05:11 +0000

    Add package pyfino/1.0.5/pyfino-1.0.5.tar.gz

---
+++ packages/pyfino/pyfino-1.0.5.tar.gz/pyfino-1.0.5/src/pyfino/__init__.py
@@ -0,0 +1,50 @@
+# coding: utf-8
+from time import sleep
+import json, threading
+
+
+class Fino:
+  """ Fino类， 基于配置文件加载节点
+  @auth       xuwh
+  @comment
+  @date       2024-7-15
+  """
+  
+  def _load_config(self,configPath):
+    
+    with open(configPath) as f:
+      return json.loads(f.read())
+    
+  def _wait_forever(self):
+    while True:
+      sleep(1)
+    
+  def loop_forever(self, config:str='nodes.json'):
+    """ 该函数将无限循环，直到外部主动中断进程
+    @param config:  包含节点配置的文件路径
+    """
+    
+    nodes = self._load_config(config)
+    threads = []
+    for nodename in nodes:
+      node = nodes[nodename]
+      nodeModule = __import__(f'nodes.{nodename}', fromlist=['None'])
+      moduleName = node['module']
+      classInstance = eval(f'nodeModule.{moduleName}(node)')
+      # log.debug(f'Package {moduleName} loaded')
+      threads.append(threading.Thread(target=classInstance.launch, name=nodename, kwargs=node, daemon=True))
+    
+    
+    try:
+      if len(threads) > 0:
+        for t in threads:
+          t.start()
+          
+      self._wait_forever()
+        
+    except KeyboardInterrupt:
+      # log.debug('Fino 退出')
+      pass
+      
+      
+  
