ec2a1b20c31c6d2f339b64b5f0ba993a30b5a353
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-17 06:04:44 +0000

    Add package coveo-stew/3.0.29/coveo_stew-3.0.29.tar.gz

---
+++ packages/coveo-stew/coveo_stew-3.0.29.tar.gz/coveo_stew-3.0.29/coveo_stew/metadata/stew_api.py
@@ -0,0 +1,34 @@
+from typing import Any, List, Mapping, Optional
+
+from coveo_styles.styles import echo
+
+from coveo_stew.metadata.poetry_api import dependencies_factory
+
+
+class StewPackage:
+    """Represents the stew-specific sections of a pyproject.toml file."""
+
+    def __init__(
+        self,
+        *,
+        build: bool = False,  # deprecated
+        build_without_hashes: bool = False,
+        pydev: bool = False,
+        build_dependencies: Optional[Mapping[str, Any]] = None,
+        extras: Optional[List[str]] = None,
+        all_extras: bool = False,
+    ) -> None:
+        # poetry sometimes fail at getting hashes, in which case the export cannot work because pip will complain
+        # that some files have a hash and some don't. This fixes it.
+        self.build_without_hashes = build_without_hashes
+        self.pydev = pydev  # is this a one-ring-to-bind-them-all dev environment?
+        # additional build-time dependencies
+        self.build_dependencies = dependencies_factory(build_dependencies)
+        self.extras = extras
+        self.all_extras = all_extras
+
+        if extras and all_extras:
+            echo.suggest(
+                "Both 'extras' and 'all_extras' are specified. 'extras' will be ignored; "
+                "consider removing it from your pyproject.toml."
+            )
