f9db9b3c808a9305e6f360197b75f670a3c26f67
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-17 15:03:57 +0000

    Add package readabs/0.0.11/readabs-0.0.11-py3-none-any.whl

---
+++ packages/readabs/readabs-0.0.11-py3-none-any.whl/readabs/__init__.py
@@ -0,0 +1,9 @@
+"""Package to download timeseries data from 
+the Australian Bureau of Statistics (ABS) 
+(and elsewhere)."""
+
+from . import readabs
+from .readabs import *
+
+__version__ = "0.0.11"
+__author__ = "Bryan Palmer"
