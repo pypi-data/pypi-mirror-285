9d7a96d809f65370f31b60314e5991e7af1a7daf
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-17 03:03:50 +0000

    Add package bygg/0.5.4/bygg-0.5.4.tar.gz

---
+++ packages/bygg/bygg-0.5.4.tar.gz/bygg-0.5.4/examples/environments/Byggfile2.py
@@ -0,0 +1,9 @@
+from bygg.core.action import ActionContext, action
+from bygg.core.common_types import CommandStatus
+import rich
+
+
+@action("action2_python")
+def example_action(ctx: ActionContext):
+    rich.print("[magenta]Running default action from Python.[/magenta]")
+    return CommandStatus(0, "All ok.", None)
