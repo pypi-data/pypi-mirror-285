05d46621e3916a90b46d8ee45380cd96520190f6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-17 05:04:00 +0000

    Add package sparganothis-vim/0.1.37/sparganothis_vim-0.1.37.tar.gz

---
+++ packages/sparganothis-vim/sparganothis_vim-0.1.37.tar.gz/sparganothis_vim-0.1.37/game/src/lib.rs
@@ -0,0 +1,23 @@
+#![deny(unused_crate_dependencies)]
+// for "wasm-pack test --node"
+use getrandom as _;
+use wasm_bindgen_test as _;
+
+pub mod api;
+pub mod bot;
+pub mod git_version;
+pub mod random;
+pub mod rot;
+pub mod tet;
+pub mod timestamp;
+
+#[cfg(test)]
+pub mod tests {
+    use wasm_bindgen_test::*;
+
+    #[test]
+    #[wasm_bindgen_test]
+    pub fn test_2_plus_2() {
+        assert_eq!(2 + 2, 4)
+    }
+}
