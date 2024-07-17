326749d42349d991b8694233091a8014f4fe9a12
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-17 04:04:32 +0000

    Add package podonos/0.1.16/podonos-0.1.16-py3-none-any.whl

---
+++ packages/podonos/podonos-0.1.16-py3-none-any.whl/podonos/common/constant.py
@@ -0,0 +1,25 @@
+"""
+Default constant values across whole SDK
+"""
+
+
+# Text colors on terminal
+class bcolors:
+    BOLD = '\033[1m'
+    ENDC = '\033[0m'
+    FAIL = '\033[91m'
+    OK = '\033[92m'
+    WARN = '\033[93m'
+
+
+# Podonos homepage
+PODONOS_HOME = 'https://www.podonos.com/'
+
+# Podonos Workspace
+PODONOS_WORKSPACE = "https://workspace.podonos.com"
+
+# Podonos API base URL
+PODONOS_API_BASE_URL = "https://prod.podonosapi.com"
+
+# Contact email
+PODONOS_CONTACT_EMAIL = "hello@podonos.com"
