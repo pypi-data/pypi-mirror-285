97a37f3bed0c12abf90c426c4b0274faa7931b61
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-17 16:05:41 +0000

    Add package pygame-ecs/0.3.0/pygame_ecs-0.3.0.tar.gz

---
+++ packages/pygame-ecs/pygame_ecs-0.3.0.tar.gz/pygame_ecs-0.3.0/test/tester.py
@@ -0,0 +1,8 @@
+import subprocess
+import sys
+
+for _ in range(5):
+    subprocess.run("py test/speed_test.py perfect")
+print()
+for _ in range(5):
+    subprocess.run("py test/speed_test.py imperfect")
