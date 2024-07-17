dc2432ba5e3b00f729c5b5375d45e2f24bab3a51
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-17 14:03:36 +0000

    Add package Agently/3.3.1.9a1/Agently-3.3.1.9a1.tar.gz

---
+++ packages/Agently/Agently-3.3.1.9a1.tar.gz/Agently-3.3.1.9a1/Agently/__init__.py
@@ -0,0 +1,25 @@
+#import nest_asyncio
+from .Request import Request
+from .Agent import AgentFactory
+from .Facility import FacilityManager
+from .WebSocket import WebSocketServer, WebSocketClient
+from .Workflow import Workflow, Schema as WorkflowSchema
+from ._global import global_plugin_manager, global_settings, global_storage, global_tool_manager, global_websocket_server
+from .utils import *
+
+#nest_asyncio.apply()
+
+def create_agent(*args, **kwargs):
+    return AgentFactory().create_agent(*args, **kwargs)
+
+facility = FacilityManager()
+lib = facility
+
+set_settings = global_settings.set
+
+def register_plugin(module_name:str, plugin_name: str, plugin: callable):
+    global_plugin_manager.register(module_name, plugin_name, plugin)
+    facility.refresh_plugins()
+
+def set_plugin_settings(module_name: str, plugin_name: str, key: str, value: any):
+    global_plugin_manager.set_settings(f"plugin_settings.{ module_name }.{ plugin_name }", key, value)
