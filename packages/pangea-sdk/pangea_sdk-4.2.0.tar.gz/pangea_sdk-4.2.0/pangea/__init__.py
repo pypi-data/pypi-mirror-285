77266580c6023901c20a3868da8962d4b86e9fc3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-17 04:04:14 +0000

    Add package pangea-sdk/4.2.0/pangea_sdk-4.2.0.tar.gz

---
+++ packages/pangea-sdk/pangea_sdk-4.2.0.tar.gz/pangea_sdk-4.2.0/pangea/__init__.py
@@ -0,0 +1,6 @@
+__version__ = "4.2.0"
+
+from pangea.asyncio.request import PangeaRequestAsync
+from pangea.config import PangeaConfig
+from pangea.request import PangeaRequest
+from pangea.response import PangeaResponse
