0164ba7063f7b6e63f811b4880660d27aa197d19
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-17 16:04:44 +0000

    Add package lddya/5.0.0/lddya-5.0.0.tar.gz

---
+++ packages/lddya/lddya-5.0.0.tar.gz/lddya-5.0.0/setup.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python
+#-*- coding:utf-8 -*-
+
+#############################################
+# File Name: setup.py
+# Author: lidongdong
+# Mail: 927052521@qq.com
+# Created Time: 2022.10.21  19.50
+############################################
+
+
+from setuptools import setup, find_packages
+
+setup(
+    name = "lddya",
+    version = "5.0.0",
+    keywords = {"pip", "license","licensetool", "tool", "gm"},
+    description = "将Algorithm中的几个算法进行分类，暂分两类：Heuristic与Traditional。这将为后续强化学习的添加提供支持。",
+    long_description = "具体功能，请自行挖掘。",
+    license = "MIT Licence",
+
+    url = "https://github.com/not_define/please_wait",
+    author = "lidongdong",
+    author_email = "927052521@qq.com",
+
+    packages = find_packages(),
+    include_package_data = True,
+    platforms = "any",
+    install_requires = ['numpy','matplotlib','pygame','pandas']
+)
