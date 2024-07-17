5fc732091a56a3400ce7c8796eb23077fd55c1cf
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-17 06:05:12 +0000

    Add package kagglehub/0.2.8/kagglehub-0.2.8-py3-none-any.whl

---
+++ packages/kagglehub/kagglehub-0.2.8-py3-none-any.whl/kagglehub/__init__.py
@@ -0,0 +1,13 @@
+__version__ = "0.2.8"
+
+import kagglehub.logger  # configures the library logger.
+from kagglehub import colab_cache_resolver, http_resolver, kaggle_cache_resolver, registry
+from kagglehub.auth import login, whoami
+from kagglehub.datasets import dataset_download
+from kagglehub.models import model_download, model_upload
+
+registry.model_resolver.add_implementation(http_resolver.ModelHttpResolver())
+registry.model_resolver.add_implementation(kaggle_cache_resolver.ModelKaggleCacheResolver())
+registry.model_resolver.add_implementation(colab_cache_resolver.ModelColabCacheResolver())
+
+registry.dataset_resolver.add_implementation(http_resolver.DatasetHttpResolver())
