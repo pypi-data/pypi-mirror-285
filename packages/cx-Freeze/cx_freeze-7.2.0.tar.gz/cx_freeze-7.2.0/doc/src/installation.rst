b2c741a7ec6bbb830af66c0b8f1c11e3829b39c8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-17 07:03:56 +0000

    Add package cx-Freeze/7.2.0/cx_freeze-7.2.0.tar.gz

---
+++ packages/cx-Freeze/cx_freeze-7.2.0.tar.gz/cx_freeze-7.2.0/doc/src/installation.rst
@@ -0,0 +1,73 @@
+
+Installation
+============
+
+Pip
+---
+
+You should install the latest version of :pypi:`cx_Freeze` using :pypi:`pip`:
+
+  .. code-block:: console
+
+    pip install --upgrade cx_Freeze
+
+.. note::
+  The recommended way to use cx_Freeze is in a
+  :pythondocs:`virtual environment <library/venv.html>`.
+  If you're unfamiliar with Python virtual environments, check out the
+  :packaging:`packaging user guide <guides/installing-using-pip-and-virtual-environments>`.
+
+.. important::
+  Please note that some operating systems might be equipped with the python3
+  and pip3 commands instead of python and pip (but they should be equivalent).
+
+Pipenv
+------
+
+Using pipenv, install or update by issuing one of the following commands:
+
+  .. code-block:: console
+
+    pipenv install cx_Freeze
+    pipenv update cx_Freeze
+
+Conda-forge
+-----------
+
+Directly from the conda-forge channel:
+
+  .. code-block:: console
+
+    conda install conda-forge::cx_freeze
+
+.. seealso:: `cx_freeze-feedstock
+   <https://github.com/conda-forge/cx_freeze-feedstock#installing-cx_freeze>`_.
+
+Python requirements
+-------------------
+
+Python requirements are installed automatically by pip, pipenv, or conda.
+
+  .. code-block:: console
+
+   setuptools >= 62.6
+   typing_extensions >= 4.10.0 (Python 3.8, 3.9)
+   wheel >= 0.42.0
+   cx_Logging >= 3.1           (Windows only)
+   lief >= 0.12.0              (Windows only)
+   filelock >=3.11.0           (Linux)
+   patchelf >= 0.14            (Linux)
+   C compiler                  (required only if installing from sources)
+
+.. note:: If you have trouble with patchelf, check :ref:`patchelf`.
+
+Download the source code
+------------------------
+
+You can download and extract the source code found on :repository:`Github <>`
+to do a manual installation. Check :doc:`development/index`.
+
+Issue tracking
+--------------
+
+Bug report and issue tracking on :repository:`Github issues <issues>`.
---
+++ packages/cx-Freeze/cx_freeze-7.2.0.tar.gz/cx_freeze-7.2.0/tests/test_winmsvcr.py
@@ -0,0 +1,71 @@
+"""Test winmsvcr."""
+
+from __future__ import annotations
+
+import sys
+from pathlib import Path
+
+import pytest
+from generate_samples import run_command
+
+from cx_Freeze._compat import BUILD_EXE_DIR, EXE_SUFFIX
+from cx_Freeze.winmsvcr import FILES
+
+EXPECTED = (
+    "api-ms-win-*.dll",
+    # VC 2015 and 2017
+    "concrt140.dll",
+    "msvcp140_1.dll",
+    "msvcp140_2.dll",
+    "msvcp140.dll",
+    "ucrtbase.dll",
+    "vcamp140.dll",
+    "vccorlib140.dll",
+    "vcomp140.dll",
+    "vcruntime140.dll",
+    # VS 2019
+    "msvcp140_atomic_wait.dll",
+    "msvcp140_codecvt_ids.dll",
+    "vcruntime140_1.dll",
+    # VS 2022
+    "vcruntime140_threads.dll",
+)
+
+SAMPLES_DIR = Path(__file__).resolve().parent.parent / "samples"
+BUILD_EXE_CMD = "python setup.py build_exe --silent --excludes=tkinter"
+
+
+@pytest.mark.skipif(sys.platform != "win32", reason="Windows tests")
+def test_files() -> None:
+    """Test winmsvcr.FILES."""
+    assert EXPECTED == FILES
+
+
+@pytest.mark.skipif(sys.platform != "win32", reason="Windows tests")
+@pytest.mark.parametrize("include_msvcr", [False, True], ids=["no", "yes"])
+@pytest.mark.datafiles(SAMPLES_DIR / "sqlite")
+def test_build_exe_with_include_msvcr(
+    datafiles: Path, include_msvcr: bool
+) -> None:
+    """Test the simple sample with include_msvcr option."""
+    command = BUILD_EXE_CMD
+    if include_msvcr:
+        command += " --include-msvcr"
+    output = run_command(datafiles, command)
+
+    build_exe_dir = datafiles / BUILD_EXE_DIR
+
+    executable = build_exe_dir / f"test_sqlite3{EXE_SUFFIX}"
+    assert executable.is_file()
+    output = run_command(datafiles, executable, timeout=10)
+    assert output.startswith("dump.sql created")
+
+    names = [
+        file.name.lower()
+        for file in build_exe_dir.glob("*.dll")
+        if any(filter(file.match, EXPECTED))
+    ]
+    if include_msvcr:
+        assert names != []
+    else:
+        assert not names
