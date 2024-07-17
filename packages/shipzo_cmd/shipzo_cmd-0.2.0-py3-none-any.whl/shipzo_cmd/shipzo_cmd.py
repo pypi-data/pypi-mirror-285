9cc48fdf42cdea3f1c5bed4f33b8039f849b0e78
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-17 14:03:36 +0000

    Add package shipzo_cmd/0.2.0/shipzo_cmd-0.2.0-py3-none-any.whl

---
+++ packages/shipzo_cmd/shipzo_cmd-0.2.0-py3-none-any.whl/shipzo_cmd/shipzo_cmd.py
@@ -0,0 +1,22 @@
+from shipzo_plus.shipzo_plus import plus
+from shipzo_mult.shipzo_mult import mult
+from shipzo_divide.shipzo_divide import divide
+
+def call():
+    plus()
+    mult()
+    divide()
+
+#    sum_result = sum(a, b)
+#    mult_result = mult(a, b)
+#    divide_result = divide(a, b)
+#
+#    print(f"Sum: {sum_result}")
+#    print(f"Multiplication: {mult_result}")
+#    print(f"Division: {divide_result}")
+#def call():
+#    result = sum(a,b)
+#    print(f"fff: {result}")
+#
+#if __name__=="__main__":
+#    call()
