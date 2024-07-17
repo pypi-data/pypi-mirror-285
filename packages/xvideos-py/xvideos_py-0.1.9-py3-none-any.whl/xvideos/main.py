6da4bb8b61e50ea219cd93ca0fb60361a0c2edc4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-17 06:05:03 +0000

    Add package xvideos-py/0.1.9/xvideos_py-0.1.9-py3-none-any.whl

---
+++ packages/xvideos-py/xvideos_py-0.1.9-py3-none-any.whl/xvideos/main.py
@@ -0,0 +1,9 @@
+from .xvideos import XVideos
+import json
+def main():
+    xvideos = XVideos()
+    print(xvideos.download_low('https://www.xvideos.com/video.ubillud1141/salgo_de_compras_y_termino_cogiendo_con_la_cajera.', "porno/peru"))
+    #print(xvideos.fresh())
+
+if __name__ == "__main__":
+    main()
