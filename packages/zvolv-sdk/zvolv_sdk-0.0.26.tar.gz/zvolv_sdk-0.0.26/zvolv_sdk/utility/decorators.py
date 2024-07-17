21ec92295ef8cdcca5c29eccaf48779e3fb0f7cc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-17 05:04:00 +0000

    Add package zvolv-sdk/0.0.26/zvolv_sdk-0.0.26.tar.gz

---
+++ packages/zvolv-sdk/zvolv_sdk-0.0.26.tar.gz/zvolv_sdk-0.0.26/zvolv_sdk/utility/decorators.py
@@ -0,0 +1,28 @@
+from functools import wraps
+import traceback
+
+def enforce_pydantic_model(model_class):
+    def decorator(func):
+        @wraps(func)
+        def wrapper(*args, **kwargs):
+            for arg in args:
+                if isinstance(arg, model_class):
+                    break
+            else:
+                raise TypeError(f"Expected instance of {model_class.__name__}, got {type(arg)}")
+            return func(*args, **kwargs)
+        return wrapper
+    return decorator
+
+def handler_wrapper(func):
+    def handler(context, event):
+        print('in decorator warpper')
+        try:
+            result = func(context, event)
+            context.client.logger.closeExecutionLog('success', 'Execution completed', result, None)
+            return result
+        except Exception as e:
+            trace = traceback.format_exc()
+            context.client.logger.closeExecutionLog('failure', str(e), None, trace)
+            raise
+    return handler
