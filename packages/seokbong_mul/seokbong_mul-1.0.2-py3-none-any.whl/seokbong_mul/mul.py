1838cd298cd30342c0e51402e93c297b0262159c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-17 14:03:27 +0000

    Add package seokbong_mul/1.0.2/seokbong_mul-1.0.2-py3-none-any.whl

---
+++ packages/seokbong_mul/seokbong_mul-1.0.2-py3-none-any.whl/seokbong_mul/mul.py
@@ -0,0 +1,11 @@
+import sys
+'''
+def mul():
+    first_num = int(input("first_num: "))
+    second_num = int(input("second_num: "))
+    print(first_num * second_num)
+'''
+def seokbong_mul():
+    a = int(sys.argv[1])
+    b = int(sys.argv[2])
+    print(a * b)
