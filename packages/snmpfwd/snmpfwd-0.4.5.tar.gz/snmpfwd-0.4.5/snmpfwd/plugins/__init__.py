07a5e499311570d818ab926ea654629ff3554f78
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-17 15:03:37 +0000

    Add package snmpfwd/0.4.5/snmpfwd-0.4.5.tar.gz

---
+++ packages/snmpfwd/snmpfwd-0.4.5.tar.gz/snmpfwd-0.4.5/snmpfwd/plugins/__init__.py
@@ -0,0 +1,7 @@
+#
+# This file is part of snmpfwd software.
+#
+# Copyright (c) 2014-2019, Ilya Etingof <etingof@gmail.com>
+# License: https://www.pysnmp.com/snmpfwd/license.html
+#
+# This file is necessary to make this directory a package.
