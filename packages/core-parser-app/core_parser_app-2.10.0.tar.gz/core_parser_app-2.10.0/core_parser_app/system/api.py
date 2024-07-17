a618644cd3a3db865e40ddab38fa6255e1679bf4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-17 05:04:33 +0000

    Add package core-parser-app/2.10.0/core_parser_app-2.10.0.tar.gz

---
+++ packages/core-parser-app/core_parser_app-2.10.0.tar.gz/core_parser_app-2.10.0/core_parser_app/system/api.py
@@ -0,0 +1,24 @@
+""" System APIs for the parser app
+"""
+
+
+# TODO: look into using delete cascade
+# FIXME: not removing all data structure elements
+def delete_branch_from_db(element_id):
+    """Delete a branch from the database
+
+    Args:
+        element_id:
+
+    Returns:
+    """
+    from core_parser_app.components.data_structure.models import (
+        DataStructureElement,
+    )
+
+    element = DataStructureElement.get_by_id(element_id)
+
+    for child in element.children.all():
+        delete_branch_from_db(str(child.pk))
+
+    element.delete()
