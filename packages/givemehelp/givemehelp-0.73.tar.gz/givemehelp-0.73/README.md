effeb5a4cda3735ff65aa5255681a28427577d15
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-17 04:04:22 +0000

    Add package givemehelp/0.73/givemehelp-0.73.tar.gz

---
+++ packages/givemehelp/givemehelp-0.73.tar.gz/givemehelp-0.73/README.md
@@ -0,0 +1,4 @@
+GivemeHelp README
+
+Overview
+givemehelp is a Python tool that runs a specified Python program and provides detailed error messages from both OpenAI and Google's Gemini AI. It retrieves the necessary API keys, executes the program, and in case of an error, generates and displays error explanations from both AI services. Additionally, it calculates the similarity between the AI responses to provide validity and ensure that the results can be trusted.
---
+++ packages/givemehelp/givemehelp-0.73.tar.gz/givemehelp-0.73/setup.py
@@ -0,0 +1,31 @@
+from setuptools import setup, find_packages
+from setuptools.command.install import install
+import subprocess
+import sys
+
+class PostInstallCommand(install):
+    """Post-installation for installation mode."""
+    def run(self):
+        install.run(self)
+        subprocess.check_call([sys.executable, '-m', 'spacy', 'download', 'en_core_web_md'])
+
+setup(
+    name='givemehelp',
+    version='0.73',
+    packages=find_packages(),
+    install_requires=[
+        'openai',
+        'boto3',
+        'google-generativeai',
+        'spacy',
+        'jsonschema'
+    ],
+    entry_points={
+        "console_scripts": [
+            "givemehelp = givemehelp:retreiveSecretKey",
+        ],
+    },
+    cmdclass={
+        'install': PostInstallCommand,
+    },
+)
