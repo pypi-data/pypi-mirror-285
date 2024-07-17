12414be3fe750218f5741de4dae0e2869cf89f01
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-17 16:04:47 +0000

    Add package dadude/0.2.4/dadude-0.2.4.tar.gz

---
+++ packages/dadude/dadude-0.2.4.tar.gz/dadude-0.2.4/src/dadude/cli/write_table.py
@@ -0,0 +1,24 @@
+from functools import partial
+from fire import Fire
+from ..writer.delta_writer import write_json_table, overwrite_json_table
+
+
+write_property_entity_v3 = partial(
+    write_json_table,
+    local_json_file_path="data/gold/material_property_entity_v3.json",
+)
+
+write_dc_annotation_spum_cn_clms_2407 = partial(
+    write_json_table,
+    lines=True,
+    local_json_file_path="data/bronze/dc_annotation_spum_cn_clms_2407.json",
+)
+
+overwrite_dc_annotation_spum_cn_clms_2407 = partial(
+    overwrite_json_table,
+    lines=True,
+    local_json_file_path="data/bronze/dc_annotation_spum_cn_clms_2407.json",
+)
+
+if __name__ == "__main__":
+    Fire()
