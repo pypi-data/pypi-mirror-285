589f0934fc6eb53b371367aa410b9c1455e31716
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-17 15:03:34 +0000

    Add package chaiverse/1.12.3/chaiverse-1.12.3.tar.gz

---
+++ packages/chaiverse/chaiverse-1.12.3.tar.gz/chaiverse-1.12.3/__init__.py
@@ -0,0 +1,39 @@
+import sys
+
+# check if the symbol is already imported to avoid recursive import error
+if 'developer_login' not in sys.modules:
+    from chaiverse.cli_utils.login_cli import developer_login
+
+import chaiverse.formatters as formatters
+
+from chaiverse.chat import SubmissionChatbot
+
+from chaiverse.submit import (
+    ModelSubmitter,
+    deactivate_model,
+    redeploy_model,
+    evaluate_model,
+    get_model_info,
+    get_my_submissions,
+)
+
+from chaiverse.feedback import get_feedback
+
+from chaiverse.leaderboard_cli import (
+    display_leaderboard,
+    get_leaderboard,
+    get_leaderboards,
+    auto_deactivate,
+)
+
+from chaiverse.feed import get_feed
+
+from chaiverse.competition_cli import (
+    get_competitions,
+    create_competition,
+    update_competition,
+    enroll_submission,
+    withdraw_submission,
+)
+
+from chaiverse.prometheus_metrics import PrometheusService
