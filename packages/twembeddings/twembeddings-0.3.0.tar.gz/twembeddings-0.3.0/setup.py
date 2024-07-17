c5faf3629f1889b219b267710d2173be00a6e67a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-17 04:03:40 +0000

    Add package twembeddings/0.3.0/twembeddings-0.3.0.tar.gz

---
+++ packages/twembeddings/twembeddings-0.3.0.tar.gz/twembeddings-0.3.0/setup.py
@@ -0,0 +1,28 @@
+from setuptools import setup
+
+setup(name='twembeddings',
+      version='0.3.0',
+      description='event detection in tweets',
+      url='https://github.com/ina-foss/twembeddings',
+      author='Béatrice Mazoyer',
+      author_email='beatrice.mazoyer@sciencespo.fr',
+      license='MIT',
+      packages=['twembeddings'],
+      install_requires=[
+        "gensim",
+        "numpy",
+        "pandas",
+        "protobuf==3.20.3",
+        "pytorch-transformers",
+        "scikit-learn>=1.3",
+        "scipy",
+        "sentence-transformers",
+        "tensorflow>=2.13.0",
+        "tensorflow-text>=2.13.0",
+        "tqdm",
+        "unidecode",
+        "twython",
+        "pyyaml",
+        "sparse_dot_mkl"
+      ],
+      zip_safe=False)
