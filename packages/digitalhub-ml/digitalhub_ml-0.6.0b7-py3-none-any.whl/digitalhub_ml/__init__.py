d96e0c62daff67cd682779476f0a1475f979d4dc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-17 04:04:45 +0000

    Add package digitalhub-ml/0.6.0b7/digitalhub_ml-0.6.0b7-py3-none-any.whl

---
+++ packages/digitalhub-ml/digitalhub_ml-0.6.0b7-py3-none-any.whl/digitalhub_ml/__init__.py
@@ -0,0 +1,16 @@
+from digitalhub_ml.entities.models.crud import (
+    delete_model,
+    get_model,
+    import_model,
+    list_models,
+    log_model,
+    new_model,
+    update_model,
+)
+from digitalhub_ml.entities.projects.crud import (
+    get_or_create_project,
+    get_project,
+    import_project,
+    load_project,
+    new_project,
+)
