74387598e3ba8bd13dcba673906e2cb33a8271bf
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-17 06:04:49 +0000

    Add package mufasa-polimi/0.0.1/mufasa_polimi-0.0.1.tar.gz

---
+++ packages/mufasa-polimi/mufasa_polimi-0.0.1.tar.gz/mufasa_polimi-0.0.1/setup.py
@@ -0,0 +1,25 @@
+from setuptools import setup, find_packages
+
+VERSION = '0.0.1'
+DESCRIPTION = 'Utilities for Polimi MUFASA users'
+LONG_DESCRIPTION = ''
+
+# Setting up
+setup(
+    name="mufasa-polimi",
+    version=VERSION,
+    author="Alberto Rota",
+    author_email="<alberto1.rota@polimi.it>",
+    description=DESCRIPTION,
+    long_description_content_type="text/markdown",
+    long_description=LONG_DESCRIPTION,
+    packages=find_packages(),
+    install_requires=['opencv-python', 'pyautogui', 'pyaudio'],
+    keywords=['python', 'slurm', 'hpc'],
+    classifiers=[
+        "Development Status :: 1 - Planning",
+        "Intended Audience :: Developers",
+        "Programming Language :: Python :: 3",
+        "Operating System :: Unix"
+    ]
+)
