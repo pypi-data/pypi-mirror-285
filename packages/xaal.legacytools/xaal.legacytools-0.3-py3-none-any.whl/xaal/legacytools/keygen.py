d9dbbe40b44dd18187aca864512f7ee7247b7371
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-17 06:05:03 +0000

    Add package xaal.legacytools/0.3/xaal.legacytools-0.3-py3-none-any.whl

---
+++ packages/xaal.legacytools/xaal.legacytools-0.3-py3-none-any.whl/xaal/legacytools/keygen.py
@@ -0,0 +1,22 @@
+""" Tool to build a key pass for xAAL config file"""
+
+from __future__ import print_function
+
+from six.moves import input
+
+
+from xaal.lib import tools
+import binascii
+
+def main():
+    try:
+        temp = input("Please enter your passphrase: ")
+        key  = tools.pass2key(temp)
+        print("Cut & Paste this key in your xAAL config-file")
+        print("key=%s"% binascii.hexlify(key).decode('utf-8'))
+    except KeyboardInterrupt:
+        print("Bye Bye..")
+        
+
+if __name__ == '__main__':
+    main()
