5bcc1f6226f6fbd1fbe74ed5f0f0e3f3a7cd8f9c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-17 03:04:18 +0000

    Add package pyransame/0.4.0/pyransame-0.4.0-py3-none-any.whl

---
+++ packages/pyransame/pyransame-0.4.0-py3-none-any.whl/pyransame/__init__.py
@@ -0,0 +1,10 @@
+"""Python package for random sampling of meshes."""
+
+from numpy.random import default_rng
+
+rng = default_rng()
+
+from .line import random_line_dataset, random_line_points
+from .surface import random_surface_dataset, random_surface_points
+from .vertex import random_vertex_dataset, random_vertex_points
+from .volume import random_volume_dataset, random_volume_points
