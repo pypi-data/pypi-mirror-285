3ec17bda33fe123d83e12dcb7543e75da8385eb2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-17 15:04:18 +0000

    Add package sparrowPythonProjectManager/0.1/sparrowPythonProjectManager-0.1-py3-none-any.whl

---
+++ packages/sparrowPythonProjectManager/sparrowPythonProjectManager-0.1-py3-none-any.whl/pythonProjectManager/content.py
@@ -0,0 +1,16 @@
+# xml文件初始值
+MANAGER_XML_CONTENT = """
+<?xml version="1.0" encoding="UTF-8" ?>
+<pythonProjectManager>
+    <pypiUrls>
+        <pypiUrl>https://mirrors.aliyun.com/pypi/simple/</pypiUrl>
+    </pypiUrls>
+    <savePath>C:\\pythonProjectManager</savePath>
+    <packages>
+        <package>
+            <name>pythonProjectManager</name>
+            <version>latest</version>
+        </package>
+    </packages>
+</pythonProjectManager>
+"""
