0f88b46734176ff3953c6629672c68781c889f69
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-17 07:03:39 +0000

    Add package powermake/1.3.0/powermake-1.3.0.tar.gz

---
+++ packages/powermake/powermake-1.3.0.tar.gz/powermake-1.3.0/tests/test.py
@@ -0,0 +1,15 @@
+import powermake
+
+
+def on_build(config: powermake.Config):
+
+    config.add_c_cpp_flags("-Weverything")
+
+    files = powermake.get_files("*.c", "*.cpp")
+
+    objects = powermake.compile_files(config, files)
+
+    print(powermake.link_files(config, objects))
+
+
+powermake.run("program_test", build_callback=on_build)
