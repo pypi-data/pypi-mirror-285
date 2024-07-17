343023145a9c34f79c7e48362e87065e003e52fe
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-17 04:03:23 +0000

    Add package psy-reg/1.5.0/psy_reg-1.5.0-py3-none-any.whl

---
+++ packages/psy-reg/psy_reg-1.5.0-py3-none-any.whl/psy_reg/__init__.py
@@ -0,0 +1,28 @@
+# SPDX-FileCopyrightText: 2021-2024 Helmholtz-Zentrum hereon GmbH
+# SPDX-FileCopyrightText: 2020-2021 Helmholtz-Zentrum Geesthacht
+# SPDX-FileCopyrightText: 2016-2024 University of Lausanne
+#
+# SPDX-License-Identifier: LGPL-3.0-only
+
+"""psy-reg
+
+Psyplot plugin for visualizing and calculating regression plots
+"""
+
+from __future__ import annotations
+
+from . import _version
+
+__version__ = _version.get_versions()["version"]
+
+__author__ = "Philipp S. Sommer"
+__copyright__ = "2021-2024 Helmholtz-Zentrum hereon GmbH"
+__credits__ = [
+    "Philipp S. Sommer",
+]
+__license__ = "LGPL-3.0-only"
+
+__maintainer__ = "Philipp S. Sommer"
+__email__ = "philipp.sommer@hereon.de"
+
+__status__ = "Production"
