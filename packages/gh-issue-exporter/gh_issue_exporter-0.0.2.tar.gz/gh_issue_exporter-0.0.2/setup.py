cfa3935869383118e6f0dd1bac8fe4f27761e52b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-17 04:03:57 +0000

    Add package gh-issue-exporter/0.0.2/gh_issue_exporter-0.0.2.tar.gz

---
+++ packages/gh-issue-exporter/gh_issue_exporter-0.0.2.tar.gz/gh_issue_exporter-0.0.2/setup.py
@@ -0,0 +1,12 @@
+"""Setup file"""
+from setuptools import setup
+
+setup(
+    name='gh-issue-exporter',
+    version='0.0.1',
+    description='A way to export github issues',
+    author='Joakim Loxdal',
+    author_email='loxdal@proton.me',
+    packages=['gh_issue_exporter'],
+    install_requires=[],
+)
