08a0226f49f4983b43a9d16f1e5909e90a3ce2be
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-17 07:03:57 +0000

    Add package pySecDec/1.6.4/pySecDec-1.6.4-py3-none-manylinux2014_x86_64.whl

---
+++ packages/pySecDec/pySecDec-1.6.4-py3-none-manylinux2014_x86_64.whl/pySecDec/metadata.py
@@ -0,0 +1,3 @@
+__version__ = "1.6.4"
+__authors__ = authors = "Sophia Borowka, Gudrun Heinrich, Stephan Jahn, Stephen Jones, Matthias Kerner, Florian Langer, Vitaly Magerya, Anton Olsson, Andres Poldaru, Johannes Schlenk, Emilio Villa, Tom Zirke"
+__commit__ = git_id = "1964173610d87564922814cdc951cee675b0149c"
