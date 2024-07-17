3307bfb922fb65359c722bc7f43ebbf84028e278
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-17 03:03:44 +0000

    Add package clusterfudge/0.42.0/clusterfudge-0.42.0.tar.gz

---
+++ packages/clusterfudge/clusterfudge-0.42.0.tar.gz/clusterfudge-0.42.0/clusterfudge/__init__.py
@@ -0,0 +1,29 @@
+from .clusterfudge import (
+    Client,
+    ClusterfudgeConfig,
+    CreateLaunchRequest,
+    GitRepo,
+    Job,
+    LaunchStatus,
+    LocalDir,
+    OnReplicaFailureOtherReplicasAreStopped,
+    OnReplicaFailureOtherReplicasContinue,
+    Process,
+    Resources,
+    SchedulingError,
+)
+
+__all__ = [
+    "Client",
+    "ClusterfudgeConfig",
+    "CreateLaunchRequest",
+    "GitRepo",
+    "Job",
+    "LaunchStatus",
+    "LocalDir",
+    "OnReplicaFailureOtherReplicasAreStopped",
+    "OnReplicaFailureOtherReplicasContinue",
+    "Process",
+    "Resources",
+    "SchedulingError",
+]
