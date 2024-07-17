6ebc5d2d69df2cbb393b08be2818d59aa11e731e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-17 03:03:58 +0000

    Add package cbrkit/0.12.1/cbrkit-0.12.1.tar.gz

---
+++ packages/cbrkit/cbrkit-0.12.1.tar.gz/cbrkit-0.12.1/cbrkit/cli.py
@@ -0,0 +1,53 @@
+"""
+.. include:: ../cli.md
+"""
+
+import os
+from pathlib import Path
+
+try:
+    from rich import print
+    from typer import Typer
+except ModuleNotFoundError:
+    print(
+        "Please install cbrkit with the [cli] extra to use the command line interface."
+    )
+    raise
+
+import cbrkit
+
+app = Typer()
+
+
+@app.callback()
+def app_callback():
+    pass
+
+
+@app.command()
+def retrieve(casebase_path: Path, queries_path: Path, retriever: str):
+    casebase = cbrkit.loaders.path(casebase_path)
+    queries = cbrkit.loaders.path(queries_path)
+    retrievers = cbrkit.retrieval.load(retriever)
+
+    for query_name, query in queries.items():
+        result = cbrkit.retrieval.apply(casebase, query, retrievers)
+        print(f"Query: {query_name}")
+        print(result.ranking)
+        print()
+
+
+@app.command()
+def serve(retriever: str, reload: bool = False) -> None:
+    import uvicorn
+
+    os.environ["CBRKIT_RETRIEVER"] = retriever
+
+    uvicorn.run(
+        "cbrkit.api:app",
+        reload=reload,
+    )
+
+
+if __name__ == "__main__":
+    app()
