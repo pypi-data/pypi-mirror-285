dd70e29cbca7cab036e048f5bd476d2649f27c08
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-17 16:04:56 +0000

    Add package tsf-model/1.0.0/tsf_model-1.0.0.tar.gz

---
+++ packages/tsf-model/tsf_model-1.0.0.tar.gz/tsf_model-1.0.0/setup.py
@@ -0,0 +1,29 @@
+from distutils.core import setup
+from setuptools import find_packages
+
+setup(name='tsf_model',  # 包名
+      version='1.0.0',  # 版本号
+      description='Common time series forecasting models',
+      long_description='Designing statistical models may require modifications to the data format',
+      author='Yuanjian Zhang',
+      author_email='yuanjianzhang2003@163.com',
+      url='https://qianyongdeyu.top/',
+      install_requires=['numpy','tensorflow','xgboost','scikit-learn','statsmodels'],
+      license='BSD License',
+      packages=find_packages(),
+      platforms=["all"],
+      classifiers=[
+          'Intended Audience :: Developers',
+          'Operating System :: OS Independent',
+          'Natural Language :: Chinese (Simplified)',
+          'Programming Language :: Python',
+          'Programming Language :: Python :: 2',
+          'Programming Language :: Python :: 2.7',
+          'Programming Language :: Python :: 3',
+          'Programming Language :: Python :: 3.5',
+          'Programming Language :: Python :: 3.6',
+          'Programming Language :: Python :: 3.7',
+          'Programming Language :: Python :: 3.8',
+          'Topic :: Software Development :: Libraries'
+      ],
+      )
