80d3b506abccaf53227e8d9869b199e17f840d0a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-18 04:04:22 +0000

    Add package asdk-plugin-portfolio/2.0.0b2/asdk_plugin_portfolio-2.0.0b2-py3-none-any.whl

---
+++ packages/asdk-plugin-portfolio/asdk_plugin_portfolio-2.0.0b2-py3-none-any.whl/asdk_plugin_portfolio/agraph/portfolio/configuration/portfolio_group/v1/PortfolioGroupAPI/api/__init__.py
@@ -0,0 +1,5 @@
+# flake8: noqa
+
+# import apis into api package
+from asdk_plugin_portfolio.agraph.portfolio.configuration.portfolio_group.v1.PortfolioGroupAPI.api.default_portfolio_group_api import DefaultPortfolioGroupAPI
+
