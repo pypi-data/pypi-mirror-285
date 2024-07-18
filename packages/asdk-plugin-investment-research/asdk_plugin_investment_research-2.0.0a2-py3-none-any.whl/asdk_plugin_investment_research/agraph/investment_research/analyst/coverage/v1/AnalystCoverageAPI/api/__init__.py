f527893f9964f75b1c20893556f9bfb4a1ded444
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-18 04:05:40 +0000

    Add package asdk-plugin-investment-research/2.0.0a2/asdk_plugin_investment_research-2.0.0a2-py3-none-any.whl

---
+++ packages/asdk-plugin-investment-research/asdk_plugin_investment_research-2.0.0a2-py3-none-any.whl/asdk_plugin_investment_research/agraph/investment_research/analyst/coverage/v1/AnalystCoverageAPI/api/__init__.py
@@ -0,0 +1,5 @@
+# flake8: noqa
+
+# import apis into api package
+from asdk_plugin_investment_research.agraph.investment_research.analyst.coverage.v1.AnalystCoverageAPI.api.default_analyst_coverage_api import DefaultAnalystCoverageAPI
+
