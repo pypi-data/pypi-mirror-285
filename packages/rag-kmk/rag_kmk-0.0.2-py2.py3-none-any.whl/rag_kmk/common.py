d5e14852ec3f8c497bc6c8835434a7285efe014f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-18 06:03:31 +0000

    Add package rag-kmk/0.0.2/rag_kmk-0.0.2-py2.py3-none-any.whl

---
+++ packages/rag-kmk/rag_kmk-0.0.2-py2.py3-none-any.whl/rag_kmk/common.py
@@ -0,0 +1,15 @@
+"""The common module contains common functions and classes used by the other modules.
+"""
+
+def hello_world():
+    """Prints "Hello World!" to the console.
+    """
+    print("Hello World!")
+
+def info():
+    """Prints information about the module.
+    """
+    print("This is the common module.")
+    print("It contains common functions and classes used by the other modules.")
+
+    
