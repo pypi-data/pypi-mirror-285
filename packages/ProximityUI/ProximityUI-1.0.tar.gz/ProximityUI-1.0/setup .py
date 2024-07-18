9fdbf132ddcc9590b801ffa69c0c17f53a911eb5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-18 05:05:13 +0000

    Add package ProximityUI/1.0/ProximityUI-1.0.tar.gz

---
+++ packages/ProximityUI/ProximityUI-1.0.tar.gz/ProximityUI-1.0/setup .py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[ ]:
+
+
+from setuptools import setup, find_packages
+
+
+# In[ ]:
+
+
+setup(
+    name = 'ProximityUI',
+    author = 'Providence Adu,Ph.D.',
+    author_email = '<padu@charlotte.edu>',
+    version = '1.0',
+    url='https://github.com/ProvidenceAdu/ProximityUI',
+    description = 'library code execute various proximity analyses using ArcPy',
+    long_description = open('README.md').read(),
+    long_description_content_type = 'text/markdown',
+    packages = find_packages(),
+    install_requires = ['numpy>=1.18.0', 'pandas>=1.0.0',],
+    keywords = ['urban institute','python', 'QOL','quality of life explorer']
+)
+
