6e08bb016e3fff8aac3578a907bf066efb411ea7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-18 03:05:21 +0000

    Add package django-spar/0.1/django-spar-0.1.tar.gz

---
+++ packages/django-spar/django-spar-0.1.tar.gz/django-spar-0.1/django_render/test/apps.py
@@ -0,0 +1,7 @@
+from django.apps import AppConfig
+
+
+class DjangoRenderTestAppConfig(AppConfig):
+    label = "django_render_test"
+    name = "django_render.test"
+    verbose_name = "Django Render tests"
