c586116847b6734bb2beccf8657012bdaa692b1f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-18 14:04:11 +0000

    Add package EMAtools/0.3.0/EMAtools-0.3.0.tar.gz

---
+++ packages/EMAtools/EMAtools-0.3.0.tar.gz/EMAtools-0.3.0/src/ema/__init__.py
@@ -0,0 +1,14 @@
+from .emc import *
+from .signal import *
+from .results import *
+
+from .file import File
+from .emin import Emin
+from .coupled import CoupledSim
+from .inp import Inp
+from .cin import Cin
+
+from . import testing
+
+### Aliases for backward compatibility ###
+import ema.results as probes
