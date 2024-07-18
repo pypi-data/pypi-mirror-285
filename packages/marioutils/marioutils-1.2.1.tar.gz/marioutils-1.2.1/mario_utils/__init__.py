99945f220d4c54754827c208ec3c631b6cc09807
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-18 06:03:43 +0000

    Add package marioutils/1.2.1/marioutils-1.2.1.tar.gz

---
+++ packages/marioutils/marioutils-1.2.1.tar.gz/marioutils-1.2.1/mario_utils/__init__.py
@@ -0,0 +1,4 @@
+from .databasemanagement import MariaDatabase, MsSQLDatabase
+from .logger import logger, Levels
+
+__all__ = ['MariaDatabase', 'MsSQLDatabase', 'logger', 'Levels']
---
+++ packages/marioutils/marioutils-1.2.1.tar.gz/marioutils-1.2.1/mario_utils/databasemanagement/__init__.py
@@ -0,0 +1,2 @@
+from .src import MsSQLDatabase, MariaDatabase
+__all__ = ['MsSQLDatabase', 'MariaDatabase']
