2798ff1c5b83690456453b1eae731b5846cf6e0d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-18 06:04:04 +0000

    Add package treelite/4.3.0/treelite-4.3.0-py3-none-manylinux2014_aarch64.whl

---
+++ packages/treelite/treelite-4.3.0-py3-none-manylinux2014_aarch64.whl/treelite/sklearn/__init__.py
@@ -0,0 +1,17 @@
+"""Model loader ingest scikit-learn models into Treelite"""
+
+from .importer import import_model
+
+
+def import_model_with_model_builder(sklearn_model):
+    """
+    This function was removed in Treelite 4.0; please use :py:meth:`~treelite.sklearn.import_model`
+    instead.
+    """
+    raise NotImplementedError(
+        "treelite.sklearn.import_model_with_model_builder() was removed in Treelite 4.0. "
+        "Please use treelite.sklearn.import_model() instead."
+    )
+
+
+__all__ = ["import_model", "import_model_with_model_builder"]
