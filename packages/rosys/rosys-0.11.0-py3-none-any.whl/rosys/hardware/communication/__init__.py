1a3e3eca22e6399b6cdb37b699f9797b3951daf6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-18 03:04:02 +0000

    Add package rosys/0.11.0/rosys-0.11.0-py3-none-any.whl

---
+++ packages/rosys/rosys-0.11.0-py3-none-any.whl/rosys/hardware/communication/__init__.py
@@ -0,0 +1,3 @@
+from .communication import Communication
+from .serial_communication import SerialCommunication
+from .web_communication import WebCommunication
---
+++ packages/rosys/rosys-0.11.0-py3-none-any.whl/rosys/testing/__init__.py
@@ -0,0 +1,7 @@
+import pytest
+
+pytest.register_assert_rewrite('rosys.testing.helpers')  # nopep8
+
+# pylint: disable=wrong-import-position
+from . import log_configuration
+from .helpers import approx, assert_point, assert_pose, automate_drive_to, forward
