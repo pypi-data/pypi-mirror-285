a5a9726bba62deddbb70518972193daaf22a5d8c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-18 05:04:33 +0000

    Add package scan-pdf/0.4.4/scan_pdf-0.4.4.tar.gz

---
+++ packages/scan-pdf/scan_pdf-0.4.4.tar.gz/scan_pdf-0.4.4/scan_pdf/combine.py
@@ -0,0 +1,24 @@
+import logging
+
+import pypdf
+
+logger = logging.getLogger(__name__)
+
+
+class Combiner:
+    def __init__(self, options):
+        self.options = options
+
+    def combine(self, page_file_names):
+        output_file_name = self.options.output_file_name[0]
+        logger.info("combine %d pages into %s", len(page_file_names), output_file_name)
+
+        merger = pypdf.PdfMerger()
+
+        for pdf in page_file_names:
+            merger.append(pdf)
+
+        merger.write(output_file_name)
+        merger.close()
+
+        return 0
