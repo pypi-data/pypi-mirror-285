226f1cb10f4cec8103b6851d483c7b272b835c6e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-18 05:05:31 +0000

    Add package notificationhubs-rest-python/1.0.1/notificationhubs_rest_python-1.0.1.tar.gz

---
+++ packages/notificationhubs-rest-python/notificationhubs_rest_python-1.0.1.tar.gz/notificationhubs_rest_python-1.0.1/setup.py
@@ -0,0 +1,26 @@
+from setuptools import setup
+
+
+
+setup(
+    name="notificationhubs-rest-python",
+    version="1.0.1",
+    include_package_data=True,
+    license="Apache-2.0 License",
+    description="A Python library for sending notifications using Azure Notification Hubs REST API.",
+    long_description=open("README.md").read(),
+    long_description_content_type="text/markdown",
+    url="https://github.com/panevo/notificationhubs-rest-python",
+    author="Karlo Krakan",
+    author_email="karlo.krakan@panevo.com",
+    classifiers=[
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: Apache Software License",
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
+        "Programming Language :: Python :: 3.10",
+        "Programming Language :: Python :: 3.11",
+        "Programming Language :: Python :: 3.12",
+    ],
+)
