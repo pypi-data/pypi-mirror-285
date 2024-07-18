b81acf73accc8bd95c4f932d0b8dcd31ca51d4c7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-18 06:03:46 +0000

    Add package python-incidentio-client/0.48.0/python_incidentio_client-0.48.0-py3-none-any.whl

---
+++ packages/python-incidentio-client/python_incidentio_client-0.48.0-py3-none-any.whl/incident_io_client/models/catalog_v2_update_type_request_body_icon.py
@@ -0,0 +1,31 @@
+from enum import Enum
+
+
+class CatalogV2UpdateTypeRequestBodyIcon(str, Enum):
+    BOLT = "bolt"
+    BOX = "box"
+    BRIEFCASE = "briefcase"
+    BROWSER = "browser"
+    BULB = "bulb"
+    CALENDAR = "calendar"
+    CLOCK = "clock"
+    COG = "cog"
+    COMPONENTS = "components"
+    DATABASE = "database"
+    DOC = "doc"
+    EMAIL = "email"
+    FILES = "files"
+    FLAG = "flag"
+    FOLDER = "folder"
+    GLOBE = "globe"
+    MONEY = "money"
+    SERVER = "server"
+    SEVERITY = "severity"
+    STAR = "star"
+    STORE = "store"
+    TAG = "tag"
+    USER = "user"
+    USERS = "users"
+
+    def __str__(self) -> str:
+        return str(self.value)
