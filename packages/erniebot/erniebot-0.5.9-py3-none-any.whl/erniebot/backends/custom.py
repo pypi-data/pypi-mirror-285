bcc280c40229096b8ac6863075ee9af2272aef94
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-18 15:04:03 +0000

    Add package erniebot/0.5.9/erniebot-0.5.9-py3-none-any.whl

---
+++ packages/erniebot/erniebot-0.5.9-py3-none-any.whl/erniebot/backends/custom.py
@@ -0,0 +1,102 @@
+# Copyright (c) 2023 PaddlePaddle Authors. All Rights Reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+import os
+from typing import Any, AsyncIterator, ClassVar, Dict, Iterator, Optional, Union
+
+import erniebot.utils.logging as logging
+from erniebot.api_types import APIType
+from erniebot.backends.bce import QianfanLegacyBackend
+from erniebot.response import EBResponse
+from erniebot.types import HeadersType, ParamsType
+
+from .base import EBBackend
+
+
+class CustomBackend(EBBackend):
+    """Custom backend for debugging purposes."""
+
+    api_type: ClassVar[APIType] = APIType.CUSTOM
+
+    def __init__(self, config_dict: Dict[str, Any]) -> None:
+        super().__init__(config_dict=config_dict)
+        access_token = self._cfg.get("access_token", None)
+        if access_token is None:
+            access_token = os.environ.get("AISTUDIO_ACCESS_TOKEN", None)
+        self._access_token = access_token
+
+    def request(
+        self,
+        method: str,
+        path: str,
+        stream: bool,
+        *,
+        params: Optional[ParamsType] = None,
+        headers: Optional[HeadersType] = None,
+        request_timeout: Optional[float] = None,
+    ) -> Union[EBResponse, Iterator[EBResponse]]:
+        url, headers, data = self._client.prepare_request(
+            method,
+            path,
+            supplied_headers=headers,
+            params=params,
+        )
+        return self._client.send_request(
+            method,
+            url,
+            stream,
+            data=data,
+            headers=headers,
+            request_timeout=request_timeout,
+        )
+
+    async def arequest(
+        self,
+        method: str,
+        path: str,
+        stream: bool,
+        *,
+        params: Optional[ParamsType] = None,
+        headers: Optional[HeadersType] = None,
+        request_timeout: Optional[float] = None,
+    ) -> Union[EBResponse, AsyncIterator[EBResponse]]:
+        url, headers, data = self._client.prepare_request(
+            method,
+            path,
+            supplied_headers=headers,
+            params=params,
+        )
+        if self._access_token is not None:
+            headers = self._add_aistudio_fields_to_headers(headers)
+        return await self._client.asend_request(
+            method,
+            url,
+            stream,
+            data=data,
+            headers=headers,
+            request_timeout=request_timeout,
+        )
+
+    @classmethod
+    def handle_response(cls, resp: EBResponse) -> EBResponse:
+        return QianfanLegacyBackend.handle_response(resp)
+
+    def _add_aistudio_fields_to_headers(self, headers: HeadersType) -> HeadersType:
+        if "Authorization" in headers:
+            logging.warning(
+                "Key 'Authorization' already exists in `headers`: %r",
+                headers["Authorization"],
+            )
+        headers["Authorization"] = f"{self._access_token}"
+        return headers
---
+++ packages/erniebot/erniebot-0.5.9-py3-none-any.whl/erniebot/intro.py
@@ -0,0 +1,38 @@
+# Copyright (c) 2023 PaddlePaddle Authors. All Rights Reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+from typing import List, Tuple
+
+__all__ = ["Model"]
+
+
+class Model(object):
+    """A dummy resource class."""
+
+    @staticmethod
+    def list() -> List[Tuple[str, str]]:
+        """Lists the available models."""
+        return [
+            ("ernie-3.5", "文心大模型（ernie-3.5）"),
+            ("ernie-turbo", "文心大模型（ernie-turbo）"),
+            ("ernie-4.0", "文心大模型（ernie-4.0）"),
+            ("ernie-4.0-turbo-8k", "文心大模型（ernie-4.0-turbo-8k）"),
+            ("ernie-3.5-8k", "文心大模型（ernie-3.5-8k）"),
+            ("ernie-speed", " 文心大模型（ernie-speed）"),
+            ("ernie-speed-128k", " 文心大模型（ernie-speed-128k）"),
+            ("ernie-tiny-8k", " 文心大模型（ernie-tiny-8k）"),
+            ("ernie-char-8k", " 文心大模型（ernie-char-8k）"),
+            ("ernie-text-embedding", "文心百中语义模型"),
+            ("ernie-vilg-v2", "文心一格模型"),
+        ]
