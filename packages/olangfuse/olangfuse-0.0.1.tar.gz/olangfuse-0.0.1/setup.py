5adf8da4ae18edfff6f270f07e8035a46a8cd304
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-18 15:04:07 +0000

    Add package olangfuse/0.0.1/olangfuse-0.0.1.tar.gz

---
+++ packages/olangfuse/olangfuse-0.0.1.tar.gz/olangfuse-0.0.1/setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name="olangfuse",
+    version="0.0.1",
+    packages=["olangfuse"],
+    url="",
+    license="",
+    author="Trinh Do Duy Hung",
+    author_email="trinhhungsss492@gmail.com",
+    description="A wrapper for Langfuse to manage Trace with full CRUD operations.",
+)
