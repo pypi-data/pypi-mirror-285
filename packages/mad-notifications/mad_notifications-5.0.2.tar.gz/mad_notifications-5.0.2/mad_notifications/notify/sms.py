bc68bd39a17000dfafca4ad44e200b06af39f82b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-18 16:04:27 +0000

    Add package mad-notifications/5.0.2/mad_notifications-5.0.2.tar.gz

---
+++ packages/mad-notifications/mad_notifications-5.0.2.tar.gz/mad_notifications-5.0.2/mad_notifications/notify/sms.py
@@ -0,0 +1,31 @@
+from celery import shared_task
+import logging
+from mad_notifications.models import get_notification_model
+from mad_notifications.settings import notification_settings
+
+logger = logging.getLogger(__name__)
+
+
+# Tasks to send respective notifications
+
+
+@shared_task(name="Non-Periodic: SMS notification")
+def sms_notification(notification_id):
+    try:
+        notification_obj = get_notification_model().objects.get(id=notification_id)
+
+        sender_method_name = (
+            f"send{notification_settings.DEFAULT_SMS_PROVIDER}SMSNotification"
+        )
+        sender_method = globals().get(sender_method_name)
+        if sender_method:
+            sender_method(notification_obj)
+            return f"SMS notifications sent via {sender_method_name}"
+        else:
+            msg = f"Couldn't call: {sender_method_name}"
+            logger.warning(msg)
+            return msg
+    #
+    except Exception as e:
+        logger.error(str(e))
+        return "Unable to send SMS notification: " + str(e)
