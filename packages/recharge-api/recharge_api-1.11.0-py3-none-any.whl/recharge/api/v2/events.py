87a8fffd4283e3332b80b2802259e1f428c4b135
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-18 03:05:10 +0000

    Add package recharge-api/1.11.0/recharge_api-1.11.0-py3-none-any.whl

---
+++ packages/recharge-api/recharge_api-1.11.0-py3-none-any.whl/recharge/api/v2/events.py
@@ -0,0 +1,31 @@
+from typing import TypedDict, Optional
+
+from recharge.api import RechargeResource, RechargeScope, RechargeVersion
+
+
+class EventListQuery(TypedDict, total=False):
+    create_at_min: str
+    created_at_max: str
+    object_type: str
+    object_id: int
+    verbs: str
+    customer_id: int
+    origin: str
+
+
+class EventResource(RechargeResource):
+    """
+    https://developer.rechargepayments.com/2021-11/events
+    """
+
+    object_list_key = "events"
+    recharge_version: RechargeVersion = "2021-11"
+
+    def list_(self, query: Optional[EventListQuery] = None):
+        """List events.
+        https://developer.rechargepayments.com/2021-11/events/events_list
+        """
+        required_scopes: list[RechargeScope] = ["read_events"]
+        self._check_scopes(f"GET /{self.object_list_key}", required_scopes)
+
+        return self._http_get(self._url, query)
