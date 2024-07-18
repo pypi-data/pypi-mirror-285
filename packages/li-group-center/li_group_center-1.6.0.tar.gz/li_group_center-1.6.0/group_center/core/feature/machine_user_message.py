b4e95f41ad25746132990d7c6dae5c19f833395b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-18 03:05:51 +0000

    Add package li-group-center/1.6.0/li_group_center-1.6.0.tar.gz

---
+++ packages/li-group-center/li_group_center-1.6.0.tar.gz/li_group_center-1.6.0/group_center/core/feature/machine_user_message.py
@@ -0,0 +1,13 @@
+from group_center.core.feature.machine_message import new_message_enqueue
+
+
+def machine_user_message_directly(
+        user_name: str,
+        content: str,
+):
+    data_dict: dict = {
+        "userName": user_name,
+        "content": content,
+    }
+
+    new_message_enqueue(data_dict, "/api/client/user/message")
