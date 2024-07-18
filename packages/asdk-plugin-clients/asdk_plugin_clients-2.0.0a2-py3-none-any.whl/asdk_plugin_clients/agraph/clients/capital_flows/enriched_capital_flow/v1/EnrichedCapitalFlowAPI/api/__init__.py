0e1e7d0b799ab8edb1c551e0e0a8474009499cb2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-18 04:05:40 +0000

    Add package asdk-plugin-clients/2.0.0a2/asdk_plugin_clients-2.0.0a2-py3-none-any.whl

---
+++ packages/asdk-plugin-clients/asdk_plugin_clients-2.0.0a2-py3-none-any.whl/asdk_plugin_clients/agraph/clients/capital_flows/enriched_capital_flow/v1/EnrichedCapitalFlowAPI/api/__init__.py
@@ -0,0 +1,5 @@
+# flake8: noqa
+
+# import apis into api package
+from asdk_plugin_clients.agraph.clients.capital_flows.enriched_capital_flow.v1.EnrichedCapitalFlowAPI.api.default_enriched_capital_flow_api import DefaultEnrichedCapitalFlowAPI
+
