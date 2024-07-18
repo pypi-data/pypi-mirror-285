eceda49eec677237c46721af8e432b230a3ec4b9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-18 03:04:01 +0000

    Add package Easebuzz/1.0.4/Easebuzz-1.0.4-py3-none-any.whl

---
+++ packages/Easebuzz/Easebuzz-1.0.4-py3-none-any.whl/Easebuzz/constant.py
@@ -0,0 +1,12 @@
+"""
+This module contains constant values.
+"""
+
+PAYMENT_URL_TEST = "https://testpay.easebuzz.in/"
+PAYMENT_URL_PROD  = "https://pay.easebuzz.in/"
+
+URL_TEST = "https://testdashboard.easebuzz.in/"
+URL_PROD = "https://dashboard.easebuzz.in/"
+
+AMOUNT_REGEX = r"^([\d]+)\.([\d]?[\d])$"
+EMAIL_REGEX  = r"^([\w\.-]+)@([\w-]+)\.([\w]{2,8})(\.[\w]{2,8})?"
