7ef58ebd1c7ceefc6a28868291ed928eb0763c18
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-18 06:03:32 +0000

    Add package manufacture/0.6.1/manufacture-0.6.1.tar.gz

---
+++ packages/manufacture/manufacture-0.6.1.tar.gz/manufacture-0.6.1/setup.py
@@ -0,0 +1,24 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="manufacture",
+    version="0.6.1",
+    packages=find_packages(),
+    entry_points={
+        'console_scripts': [
+            'manufacture = manufacture.manufacture:main',
+        ],
+    },
+    author="Spandan Chavan",
+    author_email="spandanchavan727477@gmail.com",
+    description="A simple command-line tool to create and manage files.",
+    long_description=open('README.md').read(),
+    long_description_content_type="text/markdown",
+    url="https://github.com/Spandan7724/manufacture.git",
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+)
