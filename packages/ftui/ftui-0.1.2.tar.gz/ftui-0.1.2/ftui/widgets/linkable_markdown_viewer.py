e36750458024bf7d70206c2ee9e4b22161e1cbdc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-18 04:05:37 +0000

    Add package ftui/0.1.2/ftui-0.1.2.tar.gz

---
+++ packages/ftui/ftui-0.1.2.tar.gz/ftui-0.1.2/ftui/widgets/linkable_markdown_viewer.py
@@ -0,0 +1,14 @@
+from pathlib import Path
+import webbrowser
+
+from textual import on
+from textual.widgets import Markdown
+
+
+class LinkableMarkdown(Markdown):
+
+    @on(Markdown.LinkClicked)
+    def handle_link(self, event: Markdown.LinkClicked) -> None:
+        if not Path(event.href).exists():
+            event.prevent_default()
+            webbrowser.open(event.href)
