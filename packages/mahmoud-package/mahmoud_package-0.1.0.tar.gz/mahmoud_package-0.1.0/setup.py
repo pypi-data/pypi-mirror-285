d565dc26be5044fbcd08425e31fa0499d0568714
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-18 06:03:38 +0000

    Add package mahmoud-package/0.1.0/mahmoud_package-0.1.0.tar.gz

---
+++ packages/mahmoud-package/mahmoud_package-0.1.0.tar.gz/mahmoud_package-0.1.0/setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup, find_packages
+
+setup(
+name='mahmoud_package',
+version='0.1.0',
+author='Your Name',
+author_email='mahmoudsegni1997@gmail.com',
+description='This a package for testing',
+packages=find_packages(),
+classifiers=[
+'Programming Language :: Python :: 3',
+'License :: OSI Approved :: MIT License',
+'Operating System :: OS Independent',
+],
+python_requires='>=3.6',
+)
