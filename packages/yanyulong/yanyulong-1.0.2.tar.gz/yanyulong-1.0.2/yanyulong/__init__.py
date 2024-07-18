a07e5958d7f3e5101e90566d42b75b51a6af096a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-18 03:05:46 +0000

    Add package yanyulong/1.0.2/yanyulong-1.0.2.tar.gz

---
+++ packages/yanyulong/yanyulong-1.0.2.tar.gz/yanyulong-1.0.2/yanyulong/__init__.py
@@ -0,0 +1,19 @@
+
+def get_yan():
+    yan='''
+    yan
+    '''
+    print(yan)
+
+# endregion
+def get_yu():
+    yu='''
+    yu/
+    '''
+    print(yu)
+
+def get_long():
+    long='''
+    long/
+    '''
+    print(long)
