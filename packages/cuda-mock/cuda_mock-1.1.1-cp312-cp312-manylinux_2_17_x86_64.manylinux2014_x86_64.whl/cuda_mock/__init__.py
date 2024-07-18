e895688fa39819bc78d716c6223e52d1e8e421f8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-18 03:04:34 +0000

    Add package cuda-mock/1.1.1/cuda_mock-1.1.1-cp312-cp312-manylinux_2_17_x86_64.manylinux2014_x86_64.whl

---
+++ packages/cuda-mock/cuda_mock-1.1.1-cp312-cp312-manylinux_2_17_x86_64.manylinux2014_x86_64.whl/cuda_mock/__init__.py
@@ -0,0 +1,17 @@
+from .cuda_mock_impl import *
+from .dynamic_obj import *
+from .triton_mock import *
+
+import atexit
+
+# check version
+import sys, re
+versions = re.findall(r"major=(\d+), minor=(\d+), micro=(\d+)", str(sys.version_info))[0]
+int_version = (int(versions[0]) << 24) + (int(versions[1]) << 16) + (int(versions[2]) << 8)
+# ignore lower 8 byte
+if (get_build_version_int() >> 8) != (int_version >> 8):
+    print(f"warning: {hex(get_build_version_int())} vs {hex(int_version)} mismatch")
+
+atexit.register(uninitialize)
+
+initialize()
