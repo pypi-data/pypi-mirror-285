d105dfd9b99faf8636934391027151a3537c87f1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-18 05:06:08 +0000

    Add package ksgpu/1.0.0/ksgpu-1.0.0.tar.gz

---
+++ packages/ksgpu/ksgpu-1.0.0.tar.gz/ksgpu-1.0.0/README.md
@@ -0,0 +1,5 @@
+### GPU C++/CUDA core utils
+
+Some day there will be useful documentation here.
+
+Contact: Kendrick Smith <kmsmith@perimeterinstitute.ca>
---
+++ packages/ksgpu/ksgpu-1.0.0.tar.gz/ksgpu-1.0.0/src_python/ksgpu/__init__.py
@@ -0,0 +1,20 @@
+import numpy
+import cupy
+
+# FIXME this makes dir(ksgpu) look weird, since it consists entirely of ad hoc functions
+# for testing. I'll probably clean this up when there's more python functionality in ksgpu.
+
+from .ksgpu_pybind11 import *
+
+
+def launch_busy_wait_kernel(arr, a40_seconds):
+    """
+    Launches a "busy wait" kernel with one threadblock and 32 threads.
+    Useful for testing stream/device synchronization.
+
+    The 'arr' argument is a caller-allocated length-32 uint32 array.
+    The 'a40_seconds' arg determines the amount of work done by the kernel,
+    normalized to "seconds on an NVIDIA A40".
+    """
+    
+    ksgpu_pybind11._launch_busy_wait_kernel(arr, a40_seconds, cupy.cuda.get_current_stream().ptr)
