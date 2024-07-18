6159f623ec9b65794c4dbd839d8240280d0d2674
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-18 04:04:43 +0000

    Add package tracelynx-cli/0.1.15/tracelynx_cli-0.1.15-py3-none-any.whl

---
+++ packages/tracelynx-cli/tracelynx_cli-0.1.15-py3-none-any.whl/cli/links.py
@@ -0,0 +1,40 @@
+import json
+import os
+
+import click
+
+from xml_report.link_prediction_example import json_data
+
+import logging
+logger = logging.getLogger(__name__)
+logger.setLevel(logging.DEBUG)
+
+
+@click.command("get-links-between-items-of-type")
+@click.argument("resource-types", nargs=+1)
+@click.argument("output", type=click.Path(file_okay=True, dir_okay=False))
+@click.pass_context
+def get_link_mapping(ctx: click.Context, resource_types: tuple, output: str) -> None:
+    """
+    Retrieving from TraceLynx API the link mapping from Test Cases and Test Functions
+    :param ctx:
+    :param resource_types:
+    :param output:
+    :return:
+    """
+
+    for rt in resource_types:
+        logger.info(f"Getting link mapping from {rt}")
+        print(rt)
+
+
+    if not os.path.exists(os.path.dirname(output)):
+        os.makedirs(os.path.dirname(output))
+
+    # json_data2 = get_predicted_link(
+    #     ctx=ctx,
+    #     traceability_id=3,
+    # )
+
+    with open(f"{output}", 'w') as file:
+        json.dump(json_data, file)
