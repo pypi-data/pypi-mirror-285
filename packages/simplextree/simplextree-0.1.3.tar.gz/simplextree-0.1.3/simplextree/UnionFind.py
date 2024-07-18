b5b69eaafd3defbb29b2812c624fce02bf295126
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-18 04:04:16 +0000

    Add package simplextree/0.1.3/simplextree-0.1.3.tar.gz

---
+++ packages/simplextree/simplextree-0.1.3.tar.gz/simplextree-0.1.3/simplextree/UnionFind.py
@@ -0,0 +1,8 @@
+from __future__ import annotations
+from typing import *
+from _unionfind import UnionFind as UnionFindCpp
+
+class UnionFind(UnionFindCpp):
+  """ Union Find data structure """
+  def __init__(n: int):
+    pass 
