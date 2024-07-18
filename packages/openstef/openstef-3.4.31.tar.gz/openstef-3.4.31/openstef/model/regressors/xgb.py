60421d1ccbbf59e78a1b8aa546ee4574b1eb20f7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-18 03:04:43 +0000

    Add package openstef/3.4.31/openstef-3.4.31.tar.gz

---
+++ packages/openstef/openstef-3.4.31.tar.gz/openstef-3.4.31/openstef/model/regressors/xgb.py
@@ -0,0 +1,29 @@
+# SPDX-FileCopyrightText: 2017-2023 Contributors to the OpenSTEF project <korte.termijn.prognoses@alliander.com> # noqa E501>
+#
+# SPDX-License-Identifier: MPL-2.0
+
+from xgboost import XGBRegressor
+
+from openstef.model.regressors.regressor import OpenstfRegressor
+
+
+class XGBOpenstfRegressor(XGBRegressor, OpenstfRegressor):
+    """XGB Regressor which implements the Openstf regressor API."""
+
+    gain_importance_name = "total_gain"
+    weight_importance_name = "weight"
+
+    @property
+    def feature_names(self):
+        return self._Booster.feature_names
+
+    @property
+    def can_predict_quantiles(self):
+        return False
+
+    @staticmethod
+    def _get_importance_names():
+        return {
+            "gain_importance_name": "total_gain",
+            "weight_importance_name": "weight",
+        }
