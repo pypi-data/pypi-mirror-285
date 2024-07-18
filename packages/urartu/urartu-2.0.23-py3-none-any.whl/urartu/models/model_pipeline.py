45891fe86bd5dee6d9a1bd9338c95b067a6a24b6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-18 16:04:08 +0000

    Add package urartu/2.0.23/urartu-2.0.23-py3-none-any.whl

---
+++ packages/urartu/urartu-2.0.23-py3-none-any.whl/urartu/models/model_pipeline.py
@@ -0,0 +1,37 @@
+from typing import Tuple
+
+import torch
+from transformers import AutoModelForCausalLM, AutoTokenizer, pipeline
+
+from urartu.common.device import Device
+from urartu.common.model import Model
+
+
+class ModelPipeline(Model):
+    def __init__(self, cfg) -> None:
+        super().__init__(cfg)
+
+    def _get_model(self) -> Tuple[AutoModelForCausalLM, AutoTokenizer]:
+        model = AutoModelForCausalLM.from_pretrained(
+            self.cfg.name,
+            cache_dir=self.cfg.get("cache_dir"),
+            device_map=Device.get_device(),
+            torch_dtype=eval(self.cfg.get("dtype")),
+            token=self.cfg.get("api_token"),
+        )
+        self.tokenizer = AutoTokenizer.from_pretrained(self.cfg.name)
+
+        self.model = pipeline(
+            "text-generation",
+            model=model,
+            tokenizer=self.tokenizer,
+            torch_dtype=eval(self.cfg.get("dtype")),
+            device_map=Device.get_device(),
+            eos_token_id=self.tokenizer.eos_token_id,
+        )
+
+    def generate(self, prompt: str, generate_cfg):
+        output = self.model(prompt, **generate_cfg)
+        output_first_resp = output[0]["generated_text"]
+
+        return output_first_resp
