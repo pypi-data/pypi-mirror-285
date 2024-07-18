b750c57d995015e95344a993f9408f094e3446a5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-18 14:04:51 +0000

    Add package chemllmhack/0.1.4/chemllmhack-0.1.4-py3-none-any.whl

---
+++ packages/chemllmhack/chemllmhack-0.1.4-py3-none-any.whl/chemllmhack/test1.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+"""
+File name: test1.py
+Author: Bowen
+Date created: 18/7/2024
+Description: This Python file provides an example of mathematical operations.
+
+Copyright information: © 2024 QDX
+"""
+
+import chemllmhack
+
+chemllmhack.query("how to write rex language expression for prepare protein")
