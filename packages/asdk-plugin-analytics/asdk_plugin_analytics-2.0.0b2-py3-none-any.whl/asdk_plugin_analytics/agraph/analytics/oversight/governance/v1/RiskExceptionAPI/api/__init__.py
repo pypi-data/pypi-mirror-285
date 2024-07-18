4b797a25716acfeae2bb1eab8f3c54bb4ebedccc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-18 04:04:22 +0000

    Add package asdk-plugin-analytics/2.0.0b2/asdk_plugin_analytics-2.0.0b2-py3-none-any.whl

---
+++ packages/asdk-plugin-analytics/asdk_plugin_analytics-2.0.0b2-py3-none-any.whl/asdk_plugin_analytics/agraph/analytics/oversight/governance/v1/RiskExceptionAPI/api/__init__.py
@@ -0,0 +1,5 @@
+# flake8: noqa
+
+# import apis into api package
+from asdk_plugin_analytics.agraph.analytics.oversight.governance.v1.RiskExceptionAPI.api.default_risk_exception_api import DefaultRiskExceptionAPI
+
---
+++ packages/asdk-plugin-analytics/asdk_plugin_analytics-2.0.0b2-py3-none-any.whl/asdk_plugin_analytics/agraph/analytics/oversight/governance/v1/RiskTaskAPI/api/__init__.py
@@ -0,0 +1,5 @@
+# flake8: noqa
+
+# import apis into api package
+from asdk_plugin_analytics.agraph.analytics.oversight.governance.v1.RiskTaskAPI.api.default_risk_task_api import DefaultRiskTaskAPI
+
