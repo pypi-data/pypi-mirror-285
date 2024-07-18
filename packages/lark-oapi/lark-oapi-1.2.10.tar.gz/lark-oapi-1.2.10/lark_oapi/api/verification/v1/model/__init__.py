53185c09f463f51feb4fc79e0faa0b0fe944ccad
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-18 16:04:18 +0000

    Add package lark-oapi/1.2.10/lark-oapi-1.2.10.tar.gz

---
+++ packages/lark-oapi/lark-oapi-1.2.10.tar.gz/lark-oapi-1.2.10/lark_oapi/api/verification/v1/model/__init__.py
@@ -0,0 +1,5 @@
+from .department_id import *
+from .verification import *
+from .get_verification_request import *
+from .get_verification_response_body import *
+from .get_verification_response import *
