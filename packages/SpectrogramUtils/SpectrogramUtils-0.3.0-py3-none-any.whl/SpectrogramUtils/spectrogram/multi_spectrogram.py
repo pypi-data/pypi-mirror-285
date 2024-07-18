52938ff2b8a959ae3c8779e57fe971e490a9acd2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-18 03:05:56 +0000

    Add package SpectrogramUtils/0.3.0/SpectrogramUtils-0.3.0-py3-none-any.whl

---
+++ packages/SpectrogramUtils/SpectrogramUtils-0.3.0-py3-none-any.whl/SpectrogramUtils/spectrogram/multi_spectrogram.py
@@ -0,0 +1,258 @@
+from typing import Union
+
+import numpy as np
+import numpy.typing as npt
+import librosa
+import soundfile as sf
+from matplotlib.axes import Axes
+
+from ..data.data import DisplayType, ListOrdering
+from ..data.config import Config
+from ..processors.abstract_data_processor import AbstractDataProcessor
+from ..exceptions.lib_exceptions import NoProcessorException, NoIndexException, WrongDisplayTypeException, UnknownWavTypeException
+
+class MultiSpectrogram:
+    @classmethod
+    def from_stfts(cls, config : Config, processor : AbstractDataProcessor = None, ordering : ListOrdering = None, *stfts : np.ndarray):
+        # assert same shape
+        assert all(stft.shape == stfts[0].shape for stft in stfts), "All stfts shape should be the same"
+
+        # create a data
+        data = np.zeros(shape = (2 * len(stfts), *stfts[0].shape))
+        for i, stft in enumerate(stfts):
+            data[2*i] = stft.real
+            data[2*i + 1] = stft.imag
+        
+        # Instanciate the class with the multi spectro 
+        return cls(config, processor, ordering, data)
+
+    def __init__(self, config : Config, processor : AbstractDataProcessor,  ordering : ListOrdering, data : np.ndarray) -> None:
+        # define config
+        assert isinstance(config, Config), f"config must be a Config object, not {type(config)}"
+        self.__conf = config
+
+        # Set processor
+        assert isinstance(processor, AbstractDataProcessor) or processor is None, f"processor must heritate from AbstractDataProcessor, found type : {type(processor)}"
+        self.__processor = processor 
+
+        # define data
+        assert isinstance(data, np.ndarray), f"data must be a NDArray object, not {type(data)}"
+        assert data.dtype == np.float64 or data.dtype == np.float32, f"data type must be float64 or float32, not {data.dtype}"
+        self.__data = data
+
+        # Define ordering
+        assert isinstance(ordering, ListOrdering), f"ordering must be a ListOrdering object, not {type(data)}"
+        self.__ordering = ordering
+
+    
+    def to_data(self, process_data : bool = False) -> npt.NDArray[np.float64]:
+        """return a stereo spectrogram. Indexes precisions :
+        - 2*n : amplitude of stft at index n
+        - 2*n + 1 : phase of stft at index n
+
+        Args:
+            process (bool): if data need to be processed
+
+        Returns:
+            np.ndarray: multi spectrogram
+        """
+        if process_data:
+            if self.__processor is not None:
+                data = self.__processor._forward(self.__data)
+            else:
+                raise NoProcessorException("Askep processed data without providing a processor")
+        else:
+            data = self.__data
+        return data
+    
+    def to_rearanged_data(self, process_data : bool = False):
+        """return a stereo spectrogram with rearanged indexes. Indexes precisions :
+        - 2*n : amplitude of stft at index n
+        - 2*n + 1 : phase of stft at index n
+
+        Args:
+            process (bool): if data need to be processed
+
+        Returns:
+            np.ndarray: multi spectrogram
+        """
+        data = self.to_data(process_data)
+        if self.__ordering == ListOrdering.AMPLITUDE_PHASE:
+            data = np.concatenate((data[::2], data[1::2]), axis = 0) # Rearange the order of the amplitudes and phases
+        return data
+        
+    
+    def get_amplitudes(self) -> np.ndarray:
+        # TODO
+        raise NotImplementedError()
+        
+    @property
+    def ordering(self) -> ListOrdering:
+        """Return the list ordering set for this spectrogram
+        """
+        return self.__ordering
+    
+    
+    @property
+    def num_stfts(self) -> int:
+        """Number of stfts in the spectrogram, equivalent to the number of channels in the audio.
+        """
+        return self.__data.shape[0] // 2
+    
+    @property
+    def shape(self):
+        """return the shape of the spectrogram
+        """
+        return self.__data.shape
+    
+    def show_image_on_axis(self, 
+                           axis : Axes, 
+                           display_type : DisplayType = DisplayType.MEAN, 
+                           index : Union[int, None] = None, 
+                           use_processor : bool = False,
+                           *axes_args, 
+                           **axes_kwargs) -> None:
+        """Show the stft on the given axis (If the processor make the mean of the stft not 0, it RECENTER on 0 !)
+
+        Args:
+            axis (axes.Axes): Axis to display the stereo spectrogram
+            display_type (DisplayType, optional): How to display the stereo spectrogram. Defaults to DisplayType.MEAN.
+            
+        Raises:
+            NoIndexException: DisplayType is INDEX but no index provided
+            WrongDisplayTypeException: this DisplayType isn't handled for this method
+        """
+        # init new data
+        display_data = np.zeros_like(self.to_data(use_processor)[0], dtype=np.complex128)
+
+        if display_type == DisplayType.MEAN:
+            data = self.to_data(use_processor)
+            mean_real = data[::2] - np.mean(data[::2])
+            mean_imag = data[1::2] - np.mean(data[1::2])
+            display_data += np.mean(mean_real, axis = 0) + 1j * np.mean(mean_imag, axis = 0)
+        
+        elif display_type == DisplayType.INDEX:
+            if index is not None:
+                index_data = self.get_stft(index, use_processor)
+                full_data = self.to_data(use_processor)
+                mean_real = np.mean(full_data[::2])
+                mean_imag = np.mean(full_data[1::2])
+                display_data += (index_data - mean_real - 1j * mean_imag)
+            else:
+                raise NoIndexException("Can't display index if no index is provided")
+            
+        elif display_type == DisplayType.MAX:
+            data = self.to_data(use_processor)
+            mean_real = data[::2] - np.mean(data[::2])
+            mean_imag = data[1::2] - np.mean(data[1::2])
+            display_data += np.max(mean_real, axis = 0) + 1j * np.max(mean_imag, axis = 0)
+
+        elif display_type == DisplayType.MIN:
+            data = self.to_data(use_processor)
+            mean_real = data[::2] - np.mean(data[::2])
+            mean_imag = data[1::2] - np.mean(data[1::2])
+            display_data += np.min(mean_real, axis = 0) + 1j * np.min(mean_imag, axis = 0)
+        
+        else:
+            raise WrongDisplayTypeException(f"Cannot use display type {display_type.name} for image display")
+
+        if self.__conf.power_to_db_intensity is not None:
+            db_data = librosa.power_to_db(np.abs(display_data)**self.__conf.power_to_db_intensity)
+            axis.imshow(db_data, *axes_args, **axes_kwargs)
+            axis.set_xlabel('Time')
+            axis.set_ylabel('Frequency')
+        else:
+            axis.imshow(np.abs(display_data), *axes_args, **axes_kwargs)
+            axis.set_xlabel('Time')
+            axis.set_ylabel('Frequency')
+
+    def show_wave_on_axis(self, 
+                          axis : Axes, 
+                          display_type : DisplayType = DisplayType.STACK, 
+                          index : Union[int, None] = None, 
+                          *axes_args, 
+                          **axes_kwargs) -> None:
+        """Show the wave shape on a given axis 
+
+        Args:
+            axis (axes.Axes): Axis to display the stereo spectrogram
+            display_type (DisplayType, optional): How to display the wave. Defaults to DisplayType.STACK.
+
+        Raises:
+            NoIndexException: DisplayType is INDEX but no index provided
+            WrongDisplayTypeException: this DisplayType isn't handled for this method
+        """
+        axis.set_xlabel('Time')
+        axis.set_ylabel('Amplitude')
+        if display_type == DisplayType.STACK:
+            axis.plot(self.get_waves().transpose(), *axes_args, **axes_kwargs)            
+
+        elif display_type == DisplayType.MEAN:
+            waves = self.get_waves()
+            avg_wave = np.mean(waves.transpose(), axis = 0)
+            axis.plot(avg_wave, *axes_args, **axes_kwargs)
+
+        elif display_type == DisplayType.INDEX:
+            if index is not None:
+                wave = self.get_wave(index)
+                axis.plot(wave, *axes_args, **axes_kwargs)
+            else:
+                raise NoIndexException("Can't display index if no index is provided")
+
+        else:
+            raise WrongDisplayTypeException(f"Cannot use display type {display_type.name} for image display")
+    
+    def get_stft(self, index : int, use_processor : bool = False) -> npt.NDArray[np.complex128]:
+        """Get a stft at a specified index
+
+        Args:
+            index (int): index of channel to get the stft
+
+        Returns:
+            np.ndarray: stft at the requested index
+        """
+        return self.to_data(use_processor)[2*index] + 1j * self.to_data(use_processor)[2*index + 1]
+
+    def get_wave(self, index : int) -> npt.NDArray[np.float64]:
+        """Get the wave shape for the channel at the requested index
+
+        Args:
+            index (int): requested channel index
+
+        Returns:
+            np.ndarray: 1D wave shape
+        """
+        stft = self.get_stft(index, False)
+        wave = librosa.istft(stft, **self.__conf.get_istft_kwargs())
+        return wave
+    
+    def get_waves(self) -> npt.NDArray[np.float64]:
+        """Get the wave shape for all channels
+
+        Returns:
+            np.ndarray: n-Dimentional wave shape
+        """
+        waves = None
+        for i in range(self.num_stfts): 
+            wave = self.get_wave(i)
+            if waves is None:
+                waves = np.zeros(shape=(self.num_stfts, *wave.shape))
+            waves[i] = wave
+        return waves
+    
+    def save_as_file(self, file_name : str) -> None:
+        """save the file as wav
+
+        Args:
+            file_name (str): file name
+
+        Raises:
+            UnknownWavTypeException: Cannot save as wav file that has more than 2 channels. please call method 
+            get_waves and setup a custom save function.
+        """
+        if not file_name.endswith(".wav"):
+            file_name += ".wav"
+        if self.num_stfts <=2:
+            sf.write(file_name, self.get_waves().transpose(), samplerate=self.__conf.sample_rate)
+        else:
+            raise UnknownWavTypeException("Cannot save audio if it isn't mono or stereo")
---
+++ packages/SpectrogramUtils/SpectrogramUtils-0.3.0-py3-none-any.whl/SpectrogramUtils/spectrogram/spectrogram_factory.py
@@ -0,0 +1,217 @@
+from typing import Callable, Union, Optional, List, Any
+import os
+
+import numpy as np
+import numpy.typing as npt
+import soundfile as sf
+
+from ..data.config import Config
+from ..processors.abstract_data_processor import AbstractDataProcessor
+from .multi_spectrogram import MultiSpectrogram
+from ..data.data import AudioPadding, ListOrdering
+from ..misc.utils import get_multi_stft
+from ..exceptions.lib_exceptions import WrongConfigurationException, BadTypeException
+
+
+class SpectrogramFactory:
+    def __init__(self, 
+                config : Config,
+                data_processor : Optional[AbstractDataProcessor] = None,
+                audio_padder : Optional[Callable[[np.ndarray, int], np.ndarray]] = AudioPadding.NONE,
+                ordering : ListOrdering = ListOrdering.ALTERNATE
+                ) -> None:
+        # Set the config
+        self.__config = config
+
+        # Set the data processor
+        self.__processor = data_processor
+
+        # Set the padding functino
+        self.__audio_padder = audio_padder
+
+        # set ordering
+        self.__ordering = ordering
+
+        # Check if config is correct
+        if self.__audio_padder is not None and self.__config.audio_length is None:
+            raise WrongConfigurationException("Audio Padder can't be userd without a audio_length configured in the config object.")
+        
+    @property
+    def num_channel(self) -> int:
+        return self.__config.num_channel
+
+    def get_spectrogram_from_audio(self, audio_array : np.ndarray) -> MultiSpectrogram:
+        """
+        Converts an audio array to a MultiSpectrogram instance.
+
+        Args:
+            audio_array (np.ndarray): 
+                A numpy array representing the audio data. The shape of the array should be (num_channels, samples).
+
+        Raises:
+            WrongConfigurationException: 
+                If the number of channels in the audio array does not match the expected number of channels in the configuration.
+            WrongConfigurationException: 
+                If the audio length or audio padding function is not provided in the configuration.
+
+        Returns:
+            MultiSpectrogram: 
+                A MultiSpectrogram instance created from the given audio array.
+        """
+        # Get the channel number
+        num_channels = audio_array.shape[0]
+
+        # padding
+        if self.__audio_padder is not None:
+            audio_array = self.__audio_padder(audio_array, self.__config.audio_length)
+
+        # If audio is mono, we can copy 
+        if num_channels == 1 and self.__config.num_channel > 1:
+            audio_array = np.stack([audio_array]*self.__config.num_channel, axis = 0)
+
+        # If number of channels and desired channel number fit, we can do stfts
+        if num_channels == self.__config.num_channel:
+            stfts = get_multi_stft(audio_array, **self.__config.get_istft_kwargs())
+            return MultiSpectrogram.from_stfts(self.__config, self.__processor, self.__ordering, *stfts)
+        
+        else:
+            raise WrongConfigurationException(f"Cannot handle data with {num_channels} channels. Configuration is set for {self.__config.num_channel} channels.")
+        
+    def get_spectrogram_from_path(self, file_path : str) -> MultiSpectrogram:
+        """
+        Converts an audio file from the given file path to a MultiSpectrogram instance.
+
+        Args:
+            file_path (str): 
+                The path to the audio file.
+
+        Returns:
+            MultiSpectrogram: 
+                A MultiSpectrogram instance created from the audio file.
+
+        Raises:
+            AssertionError: 
+                If the file does not exist or if the sample rate of the file does not match the configured sample rate.
+        """
+        # assert file exist
+        assert os.path.isfile(file_path), f"The file {file_path} doesn't exist"
+
+        # Load the audio
+        audio, sample_rate = sf.read(file_path, always_2d=True)
+
+        # assert sample rate is correct
+        assert sample_rate == self.__config.sample_rate, "Sample rate of the file is different from the one configured in the Config onbject"
+
+        # return the spectro
+        return self.get_spectrogram_from_audio(audio.transpose())
+    
+    def get_spectrograms_from_files(self, audio_or_file_list : List[Union[str, np.ndarray]]) -> List[MultiSpectrogram]:
+        """
+        Converts a list of audio file paths or numpy arrays to a list of MultiSpectrogram instances.
+
+        Args:
+            audio_or_file_list (List[Union[str, np.ndarray]]): 
+                A list containing either file paths to audio files (as strings) or numpy arrays representing audio data.
+
+        Raises:
+            BadTypeException: 
+                If an element in the list is neither a string nor a numpy array.
+
+        Returns:
+            List[MultiSpectrogram]: 
+                A list of MultiSpectrogram instances created from the audio files or arrays.
+        """
+        spectros : list[MultiSpectrogram] = []
+        for audio_or_file in audio_or_file_list:
+            if isinstance(audio_or_file, str):
+                spectros.append(
+                    self.get_spectrogram_from_path(audio_or_file))
+            elif isinstance(audio_or_file, np.ndarray):
+                spectros.append(
+                    self.get_spectrogram_from_audio(audio_or_file))
+            else:
+                raise BadTypeException(f"Couldn't handle type : {type(audio_or_file)}. Can only get file_path as str and audio_file as NDArray")
+        return spectros
+    
+    def get_spectrogram_from_model_output(self, model_output : npt.NDArray[np.float64]) -> List[MultiSpectrogram]:
+        """From model output, recreate a spectrogram, rearrange the amplitude phase if needed.
+        /!\\ the model output should be shaped like (batch, channel, h, w)
+
+        Args:
+            model_output (npt.NDArray[np.float64]): [description]
+
+        Returns:
+            list[MultiSpectrogram]: Spectrograms from the model output
+        """
+        # Check if need to be reordered
+        if self.__ordering == ListOrdering.AMPLITUDE_PHASE:
+            new_order = np.arange(model_output.shape[1]).reshape(2, -1).flatten("F")
+            model_output = model_output[:, new_order, :, :] # Rearange the data in correct order
+
+        # Make spectrograms once it's on the correct order
+        spectros : list[MultiSpectrogram] = []
+        for x in model_output:
+            # Backward process
+            backward_processed = self.__processor._backward(x)
+
+            # Get the spectrogram
+            spectros.append(
+                MultiSpectrogram(self.__config, self.__processor, self.__ordering, backward_processed))
+            
+        return spectros
+    
+    def get_numpy_dataset(self, 
+                          audio_or_file_list : Union[List[Union[str, np.ndarray]], List[MultiSpectrogram]], 
+                          use_processor : bool
+                          ) -> npt.NDArray[np.float64]:
+        """
+        Converts a list of audio files or MultiSpectrogram instances to a numpy dataset.
+
+        Args:
+            audio_or_file_list (Union[List[Union[str, np.ndarray]], List[MultiSpectrogram]]): 
+                A list containing either file paths to audio files, numpy arrays representing audio data,
+                or a list of MultiSpectrogram instances.
+            use_processor (bool): 
+                A boolean flag indicating whether to process the data before converting it to a numpy dataset.
+
+        Raises:
+            WrongConfigurationException: 
+                If the audio length is not provided in the configuration.
+            WrongConfigurationException: 
+                If the audio padding function is not provided in the configuration.
+
+        Returns:
+            npt.NDArray[np.float64]: 
+                A numpy array containing the processed audio data.
+        """
+        if self.__config.audio_length is None:
+            raise WrongConfigurationException("Cannot create a numpy dataset with no audio length provided. \n" + \
+                            "Set the audio_length field in the configuration.")
+        if self.__audio_padder is None:
+            raise WrongConfigurationException("Cannot create a numpy dataset with no audio padding function provided. \n" + \
+                            "Set audio_padder argument in the factory constructor.")
+
+        spectros = self.get_spectrograms_from_files(audio_or_file_list)
+        X_data = np.zeros((len(spectros), *spectros[0].shape))
+        for i, spec in enumerate(spectros):
+            X_data[i] = spec.to_rearanged_data(use_processor)
+        return X_data
+    
+    def _get_stft_shape(self):
+        """#### Return the shape that a stft would have 
+
+        #### Returns:
+            - tuple[int, int]: stft shape
+            >>> 
+        """
+        stft_config = self.__config.get_istft_kwargs()
+        audio_length = stft_config.get("audio_length")
+        window_length = stft_config.get("window_length")
+        hop_length = stft_config.get("hop_length")
+        n_fft = stft_config.get("n_fft")
+        num_frames = (audio_length - window_length) // hop_length + 1
+        num_frequency_bins = n_fft // 2 + 1
+        return num_frequency_bins, num_frames
+
+
+    
