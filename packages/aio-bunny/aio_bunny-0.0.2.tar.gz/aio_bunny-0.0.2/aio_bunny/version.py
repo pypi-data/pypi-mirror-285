ef34d432218d04de197909e897d98cb21c887db1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-18 04:04:42 +0000

    Add package aio-bunny/0.0.2/aio_bunny-0.0.2.tar.gz

---
+++ packages/aio-bunny/aio_bunny-0.0.2.tar.gz/aio_bunny-0.0.2/aio_bunny/version.py
@@ -0,0 +1,14 @@
+author_info = (
+    ("Mehdi Kamani", "mkamani@sheypoor.com"),)
+
+package_info = """
+Asyncio compatible wraper around the aio-pika library for consumining messages.
+"""
+package_license = "MIT License"
+
+team_email = ""
+
+version_info = (0, 0, 2)
+
+__author__ = ", ".join("{} <{}>".format(*info) for info in author_info)
+__version__ = ".".join(map(str, version_info))
