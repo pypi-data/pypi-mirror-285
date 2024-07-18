3794fb49e110a2088e5c8edab11665856ed781fc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-18 04:04:30 +0000

    Add package sofapal/0.0.5/sofapal-0.0.5.tar.gz

---
+++ packages/sofapal/sofapal-0.0.5.tar.gz/sofapal-0.0.5/pal/main.py
@@ -0,0 +1,118 @@
+import argparse
+import importlib
+import json
+import os
+import pkgutil
+import re
+import sys
+from pathlib import Path
+
+from halo import Halo
+from InquirerPy import inquirer
+from InquirerPy.base.control import Choice
+
+from .ai_util import format_response, prompt
+
+configs = Path.home().joinpath(".pal")
+
+
+parser = argparse.ArgumentParser(
+    prog="pal",
+    description="pal - A friend; a chum.",
+    epilog='Use "%(prog)s {command} --help" for more information about a command.',
+)
+commands = parser.add_subparsers(dest="command", title="Available Commands")
+
+plugins = {}
+
+
+def load_plugins(path: str):
+    sys.path.insert(0, path)
+
+    for _, name, _ in pkgutil.iter_modules([path]):
+        module = importlib.import_module(name)
+        if hasattr(module, "Plugin"):
+            klass = getattr(module, "Plugin")
+            instance = klass(commands)
+            instance.setup()
+
+            plugins[name] = instance
+
+
+load_plugins(f"{os.path.dirname(os.path.abspath(__file__))}/plugins")
+load_plugins(f"{configs}/plugins")
+
+do = commands.add_parser("do", help="Pal AI", description="Pal AI")
+do.add_argument("prompt", nargs="*", help="Prompt inputted to the AI", action="store")
+
+args = parser.parse_args()
+
+
+def run():
+
+    if args.command in plugins:
+        plugins[args.command].run(args, parser)
+
+    elif args.command == "do":
+        config = (
+            configs.joinpath("ai.json")
+            if os.path.exists(configs.joinpath("ai.json"))
+            else None
+        )
+
+        if config:
+            if args.prompt:
+                data = json.loads(config.read_text(encoding="utf-8"))
+                functions = [plugin.function for plugin in list(plugins.values())]
+                query = " ".join(args.prompt)
+
+                with Halo("Thinking..."):
+                    response = format_response(prompt(query, functions))
+
+                if re.match(r"^fallback$", response[0]["name"], re.IGNORECASE):
+                    print(response[0]["arguments"]["response"])
+                    parser.exit(
+                        1,
+                    )
+
+                commands = []
+
+                for cmd in response:
+                    arguments = ""
+                    for name, dict_ in cmd["arguments"].items():
+                        arguments += f"--{name} '{dict_}' "
+
+                    commands.append(
+                        Choice(
+                            f"pal {cmd["name"]} {arguments}",
+                            name=str(cmd["name"]).capitalize(),
+                            enabled=(True if data["trust_mode"] else False),
+                        ),
+                    )
+
+                runs = inquirer.checkbox(
+                    message="Select commands to run:",
+                    choices=commands,
+                    cycle=False,
+                    raise_keyboard_interrupt=False,
+                    mandatory=False,
+                    transformer=lambda result: "Running %s command%s..."
+                    % (len(result), "s" if len(result) > 1 else ""),
+                ).execute()
+
+                if runs:
+                    for run in runs:
+                        os.system(run)
+        else:
+
+            if not os.path.exists(configs):
+                os.mkdir(configs)
+
+            parser.exit(
+                1,
+                message="Ai config not found. \n > Learn more: https://github.com/SlumberDemon/pal#ai ",
+            )
+
+    else:
+        parser.print_help()
+        parser.exit()
---
+++ packages/sofapal/sofapal-0.0.5.tar.gz/sofapal-0.0.5/setup.py
@@ -0,0 +1,48 @@
+#!/usr/bin/env python3
+from pathlib import Path
+
+from setuptools import find_packages, setup
+
+HERE = Path(__file__).parent
+README = (HERE / "README.md").read_text("utf-8")
+
+
+setup(
+    name="sofapal",
+    version="0.0.5",
+    description="A friend; a chum.",
+    long_description=README,
+    long_description_content_type="text/markdown",
+    url="https://github.com/slumberdemon/pal",
+    author="SlumberDemon",
+    author_email="hi@sofa.sh",
+    license="MIT",
+    classifiers=[
+        "License :: OSI Approved :: MIT License",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
+        "Programming Language :: Python :: 3.10",
+        "Programming Language :: Python :: 3.11",
+        "Programming Language :: Python :: 3.12",
+    ],
+    packages=find_packages(where="."),
+    install_requires=[
+        "inquirerpy",
+        "pytermgui",
+    ],
+    package_dir={"": "."},
+    package_data={
+        "pal": ["plugins/*.py"],
+    },
+    entry_points={
+        "console_scripts": [
+            "pal=pal.main:run",
+        ],
+    },
+    project_urls={
+        "Bug Reports": "https://github.com/slumberdemon/pal/issues",
+        "Source": "https://github.com/slumberdemon/pal",
+    },
+)
