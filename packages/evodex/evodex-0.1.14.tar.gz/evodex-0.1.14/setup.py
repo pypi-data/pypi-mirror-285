4a1dceaa96e2c82f2dbba8c7a381be1b09e12db7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-18 06:04:08 +0000

    Add package evodex/0.1.14/evodex-0.1.14.tar.gz

---
+++ packages/evodex/evodex-0.1.14.tar.gz/evodex-0.1.14/setup.py
@@ -0,0 +1,25 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='evodex',
+    version='0.1.14',
+    packages=find_packages(include=['evodex', 'evodex.*']),
+    install_requires=[
+        'rdkit-pypi',
+        'pandas',
+        'numpy',
+    ],
+    include_package_data=True,
+    author='J. Christopher Anderson',
+    author_email='jcanderson@berkeley.edu',
+    description='A project to process enzymatic reactions',
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
+    url='https://github.com/jcaucb/evodex',
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+    ],
+    python_requires='>=3.6',
+)
