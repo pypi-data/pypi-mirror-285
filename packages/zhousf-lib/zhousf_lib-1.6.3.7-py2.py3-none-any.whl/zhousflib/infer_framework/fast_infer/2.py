dfe9791d12ccb53e05cb7929eb60e54dbe6658aa
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-18 16:04:34 +0000

    Add package zhousf-lib/1.6.3.7/zhousf_lib-1.6.3.7-py2.py3-none-any.whl

---
+++ packages/zhousf-lib/zhousf_lib-1.6.3.7-py2.py3-none-any.whl/zhousflib/infer_framework/fast_infer/2.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+# @Author  : zhousf-a
+# @Function:
+import fastdeploy as fd
+import os
+
+if __name__ == "__main__":
+    input = "Hello"
+    cipher, key = fd.encryption.encrypt(input)
+    output = fd.encryption.decrypt(cipher, key)
+    assert input == output, "test encryption failed"
+    print("test encryption success")
