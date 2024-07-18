7b52cb477b9091ed1f4d5dda0c3f995f66d650e1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-18 16:04:08 +0000

    Add package urartu/2.0.23/urartu-2.0.23.tar.gz

---
+++ packages/urartu/urartu-2.0.23.tar.gz/urartu-2.0.23/urartu/models/model_causal_language.py
@@ -0,0 +1,46 @@
+from typing import Tuple
+
+import torch
+from transformers import AutoModelForCausalLM, AutoTokenizer
+
+from urartu.common.device import Device
+from urartu.common.model import Model
+
+
+class ModelCausalLanguage(Model):
+    def __init__(self, cfg) -> None:
+        super().__init__(cfg)
+
+    def _get_model(self) -> AutoModelForCausalLM:
+        self.model = AutoModelForCausalLM.from_pretrained(
+            self.cfg.name,
+            cache_dir=self.cfg.get("cache_dir"),
+            device_map=Device.get_device(),
+            torch_dtype=eval(self.cfg.get("dtype")),
+            token=self.cfg.get("api_token"),
+        )
+
+        for param in self.model.parameters():
+            param.requires_grad = False
+
+        self.tokenizer = AutoTokenizer.from_pretrained(self.cfg.name)
+        if self.tokenizer.pad_token is None:
+            self.tokenizer.pad_token = self.tokenizer.eos_token
+
+    def generate(self, prompt: str, generate_cfg=None):
+        if not generate_cfg:
+            generate_cfg = self.cfg.get("generate")
+        self.model.eval()
+
+        prompt_tokenized = self.tokenizer(
+            prompt, return_tensors="pt", padding=True, truncation=True
+        )
+        prompt_tensor = prompt_tokenized["input_ids"].to(self.model.device)
+        attention_mask = prompt_tokenized["attention_mask"].to(self.model.device)
+        with torch.no_grad():
+            output_tokenized = self.model.generate(
+                input_ids=prompt_tensor, attention_mask=attention_mask, **generate_cfg
+            )
+        output = self.tokenizer.decode(output_tokenized[0], skip_special_tokens=True)
+
+        return output
