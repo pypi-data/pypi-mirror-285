7c432db3a73788be928b26b1332ca02efdf79504
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-18 04:04:21 +0000

    Add package asdk-plugin-data/2.0.0b2/asdk_plugin_data-2.0.0b2-py3-none-any.whl

---
+++ packages/asdk-plugin-data/asdk_plugin_data-2.0.0b2-py3-none-any.whl/asdk_plugin_data/agraph/data/reference_data/asset/asset_creation/v1/SecurityCreationAPI/api/__init__.py
@@ -0,0 +1,5 @@
+# flake8: noqa
+
+# import apis into api package
+from asdk_plugin_data.agraph.data.reference_data.asset.asset_creation.v1.SecurityCreationAPI.api.default_security_creation_api import DefaultSecurityCreationAPI
+
