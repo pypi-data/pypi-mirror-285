f84b2649030d1c548d043134f5137f938708ae56
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-18 16:04:19 +0000

    Add package python-runtime-metrics/1.1.0/python_runtime_metrics-1.1.0.tar.gz

---
+++ packages/python-runtime-metrics/python_runtime_metrics-1.1.0.tar.gz/python_runtime_metrics-1.1.0/setup.py
@@ -0,0 +1,19 @@
+from setuptools import setup, find_packages
+
+with open("README.md", "r") as f:
+    description = f.read()
+
+setup(
+    name='python_runtime_metrics',
+    version='1.1.0',
+    description='A Prometheus metrics collector for runtime metrics',
+    author='medhun',
+    author_email='medhun@example.com',
+    packages=find_packages(),
+    install_requires=[
+        'prometheus_client',
+    ],
+    python_requires='>=3.6',
+    long_description=description,
+    long_description_content_type="text/markdown"
+)
