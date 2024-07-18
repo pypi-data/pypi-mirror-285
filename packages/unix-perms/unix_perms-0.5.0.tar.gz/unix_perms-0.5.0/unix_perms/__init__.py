461eae8e6ccf33a9dd2488d85b7c2b0a45aff818
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-18 03:04:58 +0000

    Add package unix-perms/0.5.0/unix_perms-0.5.0.tar.gz

---
+++ packages/unix-perms/unix_perms-0.5.0.tar.gz/unix_perms-0.5.0/unix_perms/__init__.py
@@ -0,0 +1,20 @@
+from unix_perms.exceptions import InvalidOctalError
+from unix_perms.octals import (OctalConfig, from_octal_digit_to_config,
+                               from_octal_to_permissions_mode,
+                               is_permissions_mode)
+from unix_perms.permissions import OctalPermissions
+from unix_perms.types import (PermissionsByte, PermissionsConfig,
+                              PermissionsMode)
+
+__version__ = '0.5.0'
+__all__ = [
+    'InvalidOctalError',
+    'OctalPermissions',
+    'from_octal_digit_to_config',
+    'from_octal_to_permissions_mode',
+    'is_permissions_mode',
+    'OctalConfig',
+    'PermissionsByte',
+    'PermissionsMode',
+    'PermissionsConfig'
+]
---
+++ packages/unix-perms/unix_perms-0.5.0.tar.gz/unix_perms-0.5.0/unix_perms/models.py
@@ -0,0 +1,23 @@
+from pydantic import BaseModel
+
+
+class Authority(BaseModel):
+    """
+    Represents octals for all Unix file permission settings for a specific authority.
+
+    Args:
+        read_write_execute (int): The octal for read, write, and execute permissions.
+        read_write (int): The octal for read and write permissions.
+        read (int): The octal for read only permissions.
+        read_execute (int): The octal for read and execute permissions.
+        write_execute (int): The octal for write and execute permissions.
+        write (int): The octal for write only permissions.
+        execute (int): The octal for execute only permissions.
+    """
+    read_write_execute: int
+    read_write: int
+    read: int
+    read_execute: int
+    write_execute: int
+    write: int
+    execute: int
