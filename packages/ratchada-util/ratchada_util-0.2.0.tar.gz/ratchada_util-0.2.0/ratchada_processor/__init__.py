7b650a4e4f0ba21f0b62f7da862805a541bce84c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-18 03:05:28 +0000

    Add package ratchada-util/0.2.0/ratchada_util-0.2.0.tar.gz

---
+++ packages/ratchada-util/ratchada_util-0.2.0.tar.gz/ratchada_util-0.2.0/ratchada_processor/__init__.py
@@ -0,0 +1,4 @@
+from .process import tokenize_text
+from .english import EnglishTextNormalizer as EnglishTextNormalizer
+from .basic import BasicTextNormalizer as BasicTextNormalizer
+from .postfix import remove_longest_repeating_words
---
+++ packages/ratchada-util/ratchada_util-0.2.0.tar.gz/ratchada_util-0.2.0/setup.py
@@ -0,0 +1,23 @@
+from distutils.core import setup
+
+setup(
+    name = 'ratchada_util',         
+    packages = ['ratchada_processor'],   
+    version = '0.2.0',      
+    license='MIT',        
+    description = 'A Python lib/util for Ratchada Whisper model',   
+    author = 'tm-zoon',                   
+    author_email = 'zoon_p@thinkingmachin.es',      
+    url = 'https://github.com/tm-zoon-patcharawiwatpong/ratchada_util',     
+    keywords = ['RATCAHA-WHISPER'],   
+    install_requires=[            
+            'regex==2023.10.3 ; python_version >= "3.10" and python_version < "3.12"',
+            'more-itertools==10.1.0 ; python_version >= "3.10" and python_version < "3.12"',
+        ],
+    classifiers=[
+            'Programming Language :: Python :: 3.10',
+            'Programming Language :: Python :: 3.11',
+            'License :: OSI Approved :: MIT License',
+            'Operating System :: OS Independent',
+    ],
+)
