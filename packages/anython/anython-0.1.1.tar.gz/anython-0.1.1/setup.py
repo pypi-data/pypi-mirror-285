71818fb9b0b9abea1b0fd4668f846b099980289e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-18 04:05:06 +0000

    Add package anython/0.1.1/anython-0.1.1.tar.gz

---
+++ packages/anython/anython-0.1.1.tar.gz/anython-0.1.1/setup.py
@@ -0,0 +1,18 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='anython',
+    version='0.1.1',
+    author='defzaid',
+    description='no description',
+    packages=find_packages(),
+    install_requires=[
+        #'anython>=1.1',
+        #'kivy>=2.3.0', 
+    ],
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+    ]
+)
