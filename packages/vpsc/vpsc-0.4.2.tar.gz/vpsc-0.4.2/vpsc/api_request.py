ec3359f9c8cf2b7fdde5705540a9a62efd091c8f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-18 04:05:06 +0000

    Add package vpsc/0.4.2/vpsc-0.4.2.tar.gz

---
+++ packages/vpsc/vpsc-0.4.2.tar.gz/vpsc-0.4.2/vpsc/api_request.py
@@ -0,0 +1,101 @@
+"""リクエストモジュール
+
+APIへのリクエストを直接的に行なっているモジュールです。
+ページングなどの処理もこちらで対応。
+
+"""
+from collections.abc import Sized, Iterator
+from types import MappingProxyType
+from typing import Literal, Optional, Type, TYPE_CHECKING
+
+import requests
+from pydantic import BaseModel
+
+from .exceptions import APIException
+
+if TYPE_CHECKING:
+    from client import APIConfig
+
+
+class APIRequest(Iterator, Sized):
+    unsafe_methods = ["post", "put", "delete"]
+    generator = None
+    count = 0
+
+    def __init__(self, config: "APIConfig", header: MappingProxyType):
+        self.config = config
+        self.headers = dict(header)
+        self.headers["Authorization"] = f"Bearer {self.config.api_key}"
+
+    def __len__(self):
+        if self.count is None:
+            raise ValueError("request first")
+
+        return self.count
+
+    def __iter__(self):
+        if self.generator is None:
+            raise NotImplementedError()
+        return self
+
+    def __next__(self):
+        if self.generator is None:
+            raise NotImplementedError()
+        return next(self.generator)
+
+    def __generator(self, prefetch_data: dict, response_obj: Optional[Type[BaseModel]], per_page: int, **request_args):
+        for item in prefetch_data["results"]:
+            yield response_obj(**item)
+        next_url = prefetch_data.get("next")
+        if not next_url:
+            return
+
+        # 2ページ目から取得
+        while next_url:
+            request_args["url"] = next_url
+            results = self._fetch(**request_args)
+            for item in results["results"]:
+                yield response_obj(**item)
+            next_url = results.get("next", False)
+
+    def request(
+        self,
+        endpoint: str,
+        method: Literal["get", "post", "put", "delete"],
+        data: Optional[BaseModel] = None,
+        response_obj: Optional[Type[BaseModel]] = None,
+    ):
+        content = None
+        if method in self.unsafe_methods:
+            self.headers["content-type"] = "application/json"
+            if data:
+                content = data.model_dump_json(exclude_unset=True).encode("utf-8")
+
+        req_data = {
+            "method": method,
+            "url": f"{self.config.host}{endpoint}",
+            "headers": self.headers,
+        }
+        if content:
+            req_data["data"] = content
+
+        result = self._fetch(**req_data)
+        if result is None:
+            return None
+
+        if result.get("results"):
+            self.generator = self.__generator(prefetch_data=result, response_obj=response_obj, per_page=10, **req_data)
+            return self
+        elif self.count == 1:
+            return response_obj(**result)
+        return []
+
+    def _fetch(self, **req_data) -> Optional[dict]:
+        res = requests.request(**req_data)
+        if 400 <= res.status_code < 600:
+            raise APIException(res.status_code, res.json())
+        if res.content is not None and len(res.content) > 3:
+            data = res.json()
+            self.count = data.get("count", 1)
+            return data
+        return None
---
+++ packages/vpsc/vpsc-0.4.2.tar.gz/vpsc-0.4.2/vpsc/client.py
@@ -0,0 +1,285 @@
+from types import MappingProxyType
+from typing import Optional, Iterable, List
+
+from pydantic_settings import BaseSettings, SettingsConfigDict
+
+from .models.custom import (
+    server_sort_query,
+    UpdateServer,
+    ShutdownServer,
+    UpdateHost,
+    UpdateNfsServer,
+    UpdateNfsServerIpv4,
+    CreateSwitch,
+    UpdateSwitch,
+    Ptr,
+)
+from .models.generated import Server, ServerPowerStatus, NfsServer, NfsServerPowerStatus, Switch
+from .api_request import APIRequest
+
+
+class APIConfig(BaseSettings):
+    model_config = SettingsConfigDict(env_file="~/.vpsc", env_file_encoding="utf-8", env_prefix="vps_")
+    host: str = "https://secure.sakura.ad.jp/vps/api/v7"
+    api_key: str
+
+
+class Client:
+    def __init__(self, config: APIConfig):
+        self.config = config
+        self.header = MappingProxyType({"Authorization": f"Bearer {self.config.api_key}"})
+        self.client = APIRequest(config=self.config, header=self.header)
+
+    def get_servers(self, sort: Optional[server_sort_query] = None) -> Iterable[Server]:
+        """
+        サーバー一覧を取得する
+
+        :param sort: ソート情報
+        :return:
+        """
+        return self.client.request(
+            endpoint="/servers",
+            method="get",
+            response_obj=Server,
+        )
+
+    def get_server(self, server_id: int) -> Server:
+        """
+        個別のサーバー情報を取得する
+
+        :param server_id: サーバーID
+        :return:
+        """
+        return self.client.request(
+            endpoint=f"/servers/{server_id}",
+            method="get",
+            response_obj=Server,
+        )
+
+    def update_server(self, server_id: int, data: UpdateServer) -> Server:
+        """
+        個別のサーバー情報を更新する
+
+        :param server_id: サーバーID
+        :param data: 更新データ
+        :return:
+        """
+        return self.client.request(
+            endpoint=f"/servers/{server_id}",
+            method="put",
+            data=data,
+            response_obj=Server,
+        )
+
+    def get_server_power_status(self, server_id: int) -> ServerPowerStatus:
+        """
+        サーバーの電源状態を取得する
+
+        :param server_id:　サーバーID
+        :return:
+        """
+        return self.client.request(
+            endpoint=f"/servers/{server_id}/power-status",
+            method="get",
+            response_obj=ServerPowerStatus,
+        )
+
+    def power_on_server(self, server_id: int):
+        """
+        サーバーを起動する
+
+        :param server_id: サーバーID
+        :return:
+        """
+        return self.client.request(
+            endpoint=f"/servers/{server_id}/power-on",
+            method="post",
+        )
+
+    def shutdown_server(self, server_id: int, force: bool = False):
+        """
+        サーバーをシャットダウンする
+
+        :param force: 強制停止を行うか
+        :param server_id: サーバーID
+        :return:
+        """
+        data = ShutdownServer(force=force)
+        return self.client.request(
+            endpoint=f"/servers/{server_id}/shutdown",
+            method="post",
+            data=data,
+        )
+
+    def force_force_reboot_server(self, server_id: int):
+        """
+        サーバーを強制再起動する
+
+        :param server_id: サーバーID
+        :return:
+        """
+        return self.client.request(
+            endpoint=f"/servers/{server_id}/force-reboot",
+            method="post",
+        )
+
+    def update_server_ipv4_ptr(self, server_id: int, data: UpdateHost):
+        """
+        サーバーのipv4の逆引きホスト名を設定する
+
+        :param server_id: サーバーID
+        :param data: 設定データ
+        :return:
+        """
+        return self.client.request(
+            endpoint=f"/servers/{server_id}/ipv4-ptr",
+            method="put",
+            data=data,
+            response_obj=Ptr,
+        )
+
+    def update_server_ipv6_ptr(self, server_id: int, data: UpdateHost):
+        """
+        サーバーのipv6の逆引きホスト名を設定する
+
+        :param server_id: サーバーID
+        :param data: 設定データ
+        :return:
+        """
+        return self.client.request(
+            endpoint=f"/servers/{server_id}/ipv6-ptr",
+            method="put",
+            data=data,
+            response_obj=Ptr,
+        )
+
+    def get_nfs_servers(self) -> List[NfsServer]:
+        """
+        NFSサーバー情報一覧を取得する
+
+        :return:
+        """
+        return self.client.request(
+            endpoint=f"/nfs-servers",
+            method="get",
+            response_obj=NfsServer,
+        )
+
+    def get_nfs_server(self, nfs_server_id: int) -> NfsServer:
+        """
+        個別のNFSサーバー情報を取得する
+
+
+        :param nfs_server_id: NFSサーバーID
+        :return:
+        """
+        return self.client.request(
+            endpoint=f"/nfs-servers/{nfs_server_id}",
+            method="get",
+            response_obj=NfsServer,
+        )
+
+    def update_nfs_server(self, nfs_server_id: int, data: UpdateNfsServer):
+        """
+        個別のNFSサーバー情報を更新する
+        :param nfs_server_id:  NFSサーバーID
+        :param data: 更新データ
+        :return:
+        """
+        return self.client.request(
+            endpoint=f"/nfs-servers/{nfs_server_id}",
+            method="put",
+            data=data,
+            response_obj=NfsServer,
+        )
+
+    def update_nfs_server_ipv4(self, nfs_server_id: int, data: UpdateNfsServerIpv4):
+        """
+        NFSサーバーのipv4を設定する
+
+        :param nfs_server_id: NFSサーバーID
+        :param data: 設定情報
+        :return:
+        """
+        return self.client.request(
+            endpoint=f"/nfs-servers/{nfs_server_id}/ipv4",
+            method="put",
+            data=data,
+        )
+
+    def get_nfs_server_power_status(self, nfs_server_id: int) -> NfsServerPowerStatus:
+        """
+        NFSサーバーの電源状態を取得する
+
+        :param nfs_server_id:
+        :return:
+        """
+        return self.client.request(
+            endpoint=f"/nfs-servers/{nfs_server_id}/power-status", method="get", response_obj=NfsServerPowerStatus
+        )
+
+    def create_switch(self, data: CreateSwitch) -> Switch:
+        """
+        スイッチを作成する
+
+        :param data: 作成データ
+        :return:
+        """
+        return self.client.request(
+            endpoint=f"/switches",
+            method="post",
+            data=data,
+            response_obj=Switch,
+        )
+
+    def get_switches(self) -> List[Switch]:
+        """
+        スイッチ情報一覧を取得する
+
+        :return:
+        """
+        return self.client.request(
+            endpoint=f"/switches",
+            method="get",
+            response_obj=Switch,
+        )
+
+    def get_switch(self, switch_id: int) -> Switch:
+        """
+        個別のスイッチ情報を取得する
+
+        :param switch_id: スイッチID
+        :return:
+        """
+        return self.client.request(
+            endpoint=f"/switches/{switch_id}",
+            method="get",
+            response_obj=Switch,
+        )
+
+    def update_switch(self, switch_id: int, data: UpdateSwitch) -> Switch:
+        """
+        個別のスイッチ情報を更新する
+
+        :param switch_id: スイッチID
+        :param data: 更新データ
+        :return:
+        """
+        return self.client.request(
+            endpoint=f"/switches/{switch_id}",
+            method="put",
+            data=data,
+            response_obj=Switch,
+        )
+
+    def delete_switch(self, switch_id: int):
+        """
+        個別のスイッチを削除する
+
+        :param switch_id: スイッチID
+        :return:
+        """
+        return self.client.request(
+            endpoint=f"/switches/{switch_id}",
+            method="delete",
+        )
