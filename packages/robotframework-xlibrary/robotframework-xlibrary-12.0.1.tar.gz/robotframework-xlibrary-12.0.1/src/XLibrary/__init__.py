afefaaed2e0f5c8fb934159cbe380b582c4330f0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-18 15:03:59 +0000

    Add package robotframework-xlibrary/12.0.1/robotframework-xlibrary-12.0.1.tar.gz

---
+++ packages/robotframework-xlibrary/robotframework-xlibrary-12.0.1.tar.gz/robotframework-xlibrary-12.0.1/src/XLibrary/__init__.py
@@ -0,0 +1,33 @@
+#XLibrary/__init__.py
+# -*- coding: utf-8 -*-
+from robot.api.deco import keyword
+from .main import MainClass
+from .submodule1 import XAppiumFlutter
+from .submodule1 import XMongoDB
+from .submodule2 import XLog
+from .submodule2 import XImages
+from .submodule2 import GoogleSheet
+
+
+ 
+__all__ = [ 'XLibrary','MainClass', 'XAppiumFlutter', 'XMongoDB','XLog', 'XImages','GoogleSheet']
+
+
+
+class XLibrary(
+    MainClass,
+    XAppiumFlutter, 
+    XMongoDB,
+    XLog,
+    XImages,
+    GoogleSheet
+    ):
+    """ 
+    *` XLibrary `* เป็นไลบรารีที่พัฒนาขึ้นเฉพาะ เพื่อรองรับการใช้งานส่วนบุคคลในงานพัฒนา Script Automate
+    """
+
+    @keyword("Hello World")
+    def TTTTTT(self):
+        """ พิมพ์ข้อความ 'Hello, world!' ลงในคอนโซล """
+        print("Hello, world!")
+
