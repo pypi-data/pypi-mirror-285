46e930ee6dbb4af2465ef8db99cd180105721541
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-18 15:04:02 +0000

    Add package cherab-inversion/0.3.0/cherab_inversion-0.3.0.tar.gz

---
+++ packages/cherab-inversion/cherab_inversion-0.3.0.tar.gz/cherab_inversion-0.3.0/cherab/inversion/version.py
@@ -0,0 +1,8 @@
+
+version = "0.3.0"
+__version__ = version
+full_version = version
+
+git_revision = "c66bc32d4ee73b990775a71973101fb673c91ba3"
+release = "dev" not in version and "+" not in version
+short_version = version.split("+")[0]
