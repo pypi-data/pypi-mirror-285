28304fda3dafecf5b96b53ed7aa6500cd2013249
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-18 14:04:26 +0000

    Add package hierarchical-review-plots/0.1.6/hierarchical_review_plots-0.1.6-py3-none-any.whl

---
+++ packages/hierarchical-review-plots/hierarchical_review_plots-0.1.6-py3-none-any.whl/hierarchical_review_plots/__init__.py
@@ -0,0 +1,7 @@
+"""Module of hierarchical review plots"""
+
+__version__ = "0.1.6"
+from .tilt_ppda_and_build_up_disruption_altair import *  # noqa
+from .polar_bar_plotly import *  # noqa
+from .heat_map_consistent_altair import *  # noqa
+from .weighted_xg_plotly import *  # noqa
---
+++ packages/hierarchical-review-plots/hierarchical_review_plots-0.1.6-py3-none-any.whl/hierarchical_review_plots/polar_bar_plotly.py
@@ -0,0 +1,91 @@
+import plotly.express as px
+from PIL import Image
+
+
+def make_bar_plot_player(larga, radar_player, minutes_played, team, league_logo, team_logo):
+    fig = _set_up_bar_polar(larga, radar_player, minutes_played, team)
+    fig = _update_bar_polar(fig)
+    return add_nies_logo(fig, league_logo, team_logo)
+
+
+def make_bar_plot_player_2(larga, radar_player, minutes_played, team, league_logo, team_logo):
+    fig = _set_up_bar_polar_2(larga, radar_player, minutes_played, team)
+    fig = _update_bar_polar(fig)
+    return add_nies_logo(fig, league_logo, team_logo)
+
+
+def add_nies_logo(fig, league_logo: str, team_logo: str):
+    fig.add_layout_image(
+        dict(
+            source=Image.open(f"static/logos/{league_logo}.png"),
+            xref="paper",
+            yref="paper",
+            x=0.9,
+            y=1.05,
+            sizex=0.2,
+            sizey=0.2,
+            xanchor="right",
+            yanchor="bottom",
+        )
+    ).add_layout_image(
+        dict(
+            source=Image.open("static/logos/logo_nies.png"),
+            xref="paper",
+            yref="paper",
+            x=0.05,
+            y=0.05,
+            sizex=0.2,
+            sizey=0.2,
+        )
+    ).add_layout_image(
+        dict(
+            source=Image.open(f"static/logos/{team_logo}.png"),
+            xref="paper",
+            yref="paper",
+            x=0.9,
+            y=0.1,
+            sizex=0.2,
+            sizey=0.2,
+            xanchor="right",
+        )
+    )
+    return fig
+
+
+def _set_up_bar_polar(larga, radar_player, minutes_played, team):
+    player_t = larga[larga.Player == radar_player]
+    fig = px.bar_polar(
+        player_t,
+        r="deciles",
+        theta="variable",
+        color="type_variable",
+        title=f"{radar_player}, {team} ({minutes_played} minutes played)",
+    )
+    return fig
+
+
+def _set_up_bar_polar_2(larga, radar_player, minutes_played, team):
+    fig = px.bar_polar(
+        larga,
+        r="deciles",
+        theta="variable",
+        color="type_variable",
+        title=f"{radar_player}, {team} ({minutes_played} minutes played)",
+    )
+    return fig
+
+
+def _update_bar_polar(fig):
+    fig.update_traces(showlegend=True)
+    fig.update_polars(
+        radialaxis_showticklabels=True, angularaxis_griddash="dash", angularaxis_showline=False
+    )
+    fig.update_layout(
+        legend_title_text="Game phase",
+        polar_radialaxis_ticksuffix="",
+        polar_angularaxis_rotation=90,
+        polar_angularaxis_direction="clockwise",
+        polar_radialaxis_dtick=20,
+        polar_hole=0.30,
+    )
+    return fig
