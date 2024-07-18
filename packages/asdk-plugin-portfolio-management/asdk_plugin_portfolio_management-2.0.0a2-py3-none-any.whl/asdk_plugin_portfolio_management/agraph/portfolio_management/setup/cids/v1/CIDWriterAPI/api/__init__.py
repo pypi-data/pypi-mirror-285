37740f4d41e2f25efefbaf334fff537fa86ae1d1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-18 04:05:40 +0000

    Add package asdk-plugin-portfolio-management/2.0.0a2/asdk_plugin_portfolio_management-2.0.0a2-py3-none-any.whl

---
+++ packages/asdk-plugin-portfolio-management/asdk_plugin_portfolio_management-2.0.0a2-py3-none-any.whl/asdk_plugin_portfolio_management/agraph/portfolio_management/setup/cids/v1/CIDWriterAPI/api/__init__.py
@@ -0,0 +1,5 @@
+# flake8: noqa
+
+# import apis into api package
+from asdk_plugin_portfolio_management.agraph.portfolio_management.setup.cids.v1.CIDWriterAPI.api.default_cid_writer_api import DefaultCIDWriterAPI
+
---
+++ packages/asdk-plugin-portfolio-management/asdk_plugin_portfolio_management-2.0.0a2-py3-none-any.whl/asdk_plugin_portfolio_management/agraph/portfolio_management/target/strategy/v1/StrategyAPI/api/__init__.py
@@ -0,0 +1,5 @@
+# flake8: noqa
+
+# import apis into api package
+from asdk_plugin_portfolio_management.agraph.portfolio_management.target.strategy.v1.StrategyAPI.api.default_strategy_api import DefaultStrategyAPI
+
