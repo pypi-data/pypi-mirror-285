dc17e5b92c996a0301c999373664ffda79df52ca
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-18 14:04:23 +0000

    Add package qolproximity/1.1/qolproximity-1.1.tar.gz

---
+++ packages/qolproximity/qolproximity-1.1.tar.gz/qolproximity-1.1/setup.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+
+from setuptools import setup,find_packages
+
+
+
+setup(
+    name = 'qolproximity',
+    author = 'Providence Adu,Ph.D.',
+    author_email = 'padu@charlotte.edu',
+    version = '1.1',
+    url='https://github.com/ProvidenceAdu/qolproximity',
+    description = 'library code execute various proximity analyses using ArcPy for qol proximity analyses',
+    long_description_content_type='text/markdown',
+    long_description=open('README.md').read(),
+    packages = find_packages(),
+    install_requires = ['pandas>=1.0.0']
+)
+
