03aaf3469a26dad47ad9810173074cf15a6d36c1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-18 15:03:54 +0000

    Add package SlopeStability/0.8/SlopeStability-0.8.tar.gz

---
+++ packages/SlopeStability/SlopeStability-0.8.tar.gz/SlopeStability-0.8/setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='SlopeStability',
+    version='0.8',
+    description='A package for determining factor of safety for critical slip circle using stability charts',
+    packages=find_packages(),
+    install_requires=[
+        'numpy',
+        'pandas',
+        'openpyxl',
+        'matplotlib',
+        # Add other dependencies here
+    ],
+)
