847bbbe2d9c988b7aa3687fb8941ede2aceda776
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-18 05:06:05 +0000

    Add package evo-framework/2024.7.172222/evo_framework-2024.7.172222-py3-none-any.whl

---
+++ packages/evo-framework/evo_framework-2024.7.172222-py3-none-any.whl/evo_framework/core/evo_core_bridge/utility/IuBridge.py
@@ -0,0 +1,11 @@
+import asyncio
+import sys
+
+class IuBridge:
+    @staticmethod
+    async def doGetEApiConfig():
+        pass
+    
+    @staticmethod
+    def DoPathAppend(packageName:str):
+        pass
