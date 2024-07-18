0b215ad928256a0c6b34449e9ff608bb6af5e2a8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-18 04:04:38 +0000

    Add package AbhaySTT/0.2/abhaystt-0.2.tar.gz

---
+++ packages/AbhaySTT/abhaystt-0.2.tar.gz/abhaystt-0.2/setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup,find_packages
+setup(
+    name='AbhaySTT',
+    version='0.2',
+    author='Abhay Agnihotri',
+    author_email='abhayagnihotri976@gmail.com',
+    description='This is the awesome speech to text package created by Me(Abhay Agnihori)'
+
+)
+packages=find_packages(),
+install_requirements=[
+    'selenium',
+    'webdriver-manager'
+]
