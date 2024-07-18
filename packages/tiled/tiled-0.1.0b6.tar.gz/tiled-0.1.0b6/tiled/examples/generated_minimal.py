01d017beb335de807147b21dd32d177dd5a7bcc7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-18 05:05:05 +0000

    Add package tiled/0.1.0b6/tiled-0.1.0b6.tar.gz

---
+++ packages/tiled/tiled-0.1.0b6.tar.gz/tiled-0.1.0b6/tiled/examples/generated_minimal.py
@@ -0,0 +1,32 @@
+import numpy
+import pandas
+import xarray
+
+from tiled.adapters.array import ArrayAdapter
+from tiled.adapters.dataframe import DataFrameAdapter
+from tiled.adapters.mapping import MapAdapter
+from tiled.adapters.xarray import DatasetAdapter
+
+tree = MapAdapter(
+    {
+        "A": ArrayAdapter.from_array(numpy.ones((100, 100))),
+        "B": ArrayAdapter.from_array(numpy.ones((100, 100, 100))),
+        "C": DataFrameAdapter.from_pandas(
+            pandas.DataFrame(
+                {
+                    "x": 1 * numpy.ones(100),
+                    "y": 2 * numpy.ones(100),
+                    "z": 3 * numpy.ones(100),
+                }
+            ),
+            npartitions=3,
+        ),
+        "D": DatasetAdapter.from_dataset(
+            xarray.Dataset(
+                data_vars={"temperature": ("time", [100, 99, 98])},
+                coords={"time": [1, 2, 3]},
+            )
+        ),
+    },
+    metadata={"thing": "stuff"},
+)
