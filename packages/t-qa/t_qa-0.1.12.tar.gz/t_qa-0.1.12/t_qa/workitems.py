e39c02382df42b2e5d1ec03c1ba808f3a77487a3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-18 16:04:29 +0000

    Add package t-qa/0.1.12/t_qa-0.1.12.tar.gz

---
+++ packages/t-qa/t_qa-0.1.12.tar.gz/t_qa-0.1.12/t_qa/workitems.py
@@ -0,0 +1,13 @@
+"""WorkItems module to get the work items from Robocorp WorkItems library or from local file."""
+try:
+    from RPA.Robocorp.WorkItems import WorkItems
+
+    work_items = WorkItems()
+    work_items.get_input_work_item()
+    work_item = work_items.get_work_item_variables()
+
+    METADATA = work_item.get("metadata", dict())
+    VARIABLES = work_item.get("variables", dict())
+except (ImportError, KeyError):
+    VARIABLES = {}
+    METADATA = {}
