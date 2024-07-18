9374adf5729490352b8601100738fa872cbb3ed5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-18 14:04:57 +0000

    Add package ciervo/0.1.7/ciervo-0.1.7-py3-none-any.whl

---
+++ packages/ciervo/ciervo-0.1.7-py3-none-any.whl/ciervo/io/load.py
@@ -0,0 +1,39 @@
+import pandas as pd
+import os
+import numpy as np
+import importlib
+
+def load_csv(file_path):
+    # df : Elapsed Time,Isquio,Cuadriceps,GLMedio,AductorLargo,Angle
+    dtypes = {
+        "Elapsed Time": "str",
+        "Isquio": float,
+        "Cuadriceps": float,
+        "GLMedio": float,
+        "AductorLargo": float,
+        "Angle": float,
+    }
+
+    # Load csv
+    df = pd.read_csv(file_path)
+
+    # Replace Nan.NaN with np.nan
+    df = df.replace("NaN.NaN", 0)
+    df = df.replace("NaN.0", 0)
+
+    # Set dtypes
+    df = df.astype(dtypes)
+
+    # Set datetime
+    df["Elapsed Time"] = np.linspace(0, len(df) / 1000, len(df))
+
+    return df
+
+
+def example_marcha():
+    #path = "ciervo/tests/data/marcha"
+    path = os.path.join(os.path.dirname(__file__), "tests/data/marcha")
+    files = os.listdir(path)
+    files = [f for f in files if f.endswith(".csv")]
+    files.sort()
+    return [load_csv(os.path.join(path, f)) for f in files]
