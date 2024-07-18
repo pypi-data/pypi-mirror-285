3ab19e9f534ce658947d2c310594453abe801a23
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-18 06:04:08 +0000

    Add package temporals/0.1.0/temporals-0.1.0-py3-none-any.whl

---
+++ packages/temporals/temporals-0.1.0-py3-none-any.whl/temporals/__init__.py
@@ -0,0 +1,33 @@
+from datetime import time, date, datetime
+from .utils import get_datetime
+from .periods import DatePeriod, TimePeriod, DatetimePeriod
+
+
+__all__ = [
+    'PeriodFactory',
+    'DatePeriod',
+    'TimePeriod',
+    'DatetimePeriod',
+    'get_datetime',
+]
+
+
+class PeriodFactory:
+
+    # TODO: Docs
+    def __new__(cls,
+                start,
+                end,
+                force_datetime: bool = False,
+                **kwargs) -> TimePeriod | DatePeriod | DatetimePeriod:
+        if isinstance(start, str):
+            start = get_datetime(start, force_datetime)
+        if isinstance(end, str):
+            end = get_datetime(end, force_datetime)
+        if type(start) is time and type(end) is time:
+            return TimePeriod(start, end, **kwargs)
+        if type(start) is date and type(end) is date:
+            return DatePeriod(start, end, **kwargs)
+        if type(start) is datetime and type(end) is datetime:
+            return DatetimePeriod(start, end, **kwargs)
+        raise ValueError(f"Could not find suitable period type for the provided values")
