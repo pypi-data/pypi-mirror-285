d116ffa26d499d0aea3263a7ba33528b6f0c2059
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-18 04:05:40 +0000

    Add package asdk-plugin-compliance/2.0.0a2/asdk_plugin_compliance-2.0.0a2-py3-none-any.whl

---
+++ packages/asdk-plugin-compliance/asdk_plugin_compliance-2.0.0a2-py3-none-any.whl/asdk_plugin_compliance/agraph/compliance/state/compliance_rule/v1/ComplianceRuleAPI/api/__init__.py
@@ -0,0 +1,5 @@
+# flake8: noqa
+
+# import apis into api package
+from asdk_plugin_compliance.agraph.compliance.state.compliance_rule.v1.ComplianceRuleAPI.api.default_compliance_rule_api import DefaultComplianceRuleAPI
+
