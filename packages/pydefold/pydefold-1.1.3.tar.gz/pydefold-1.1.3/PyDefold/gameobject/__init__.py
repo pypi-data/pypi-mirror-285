ca21ae46af9fb9d20ff9aeb3ab20c69244f3669f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-18 03:05:21 +0000

    Add package pydefold/1.1.3/pydefold-1.1.3.tar.gz

---
+++ packages/pydefold/pydefold-1.1.3.tar.gz/pydefold-1.1.3/PyDefold/gameobject/__init__.py
@@ -0,0 +1,3 @@
+from .lua_ddf_pb2 import LuaModule 
+from .gameobject_ddf_pb2 import AcquireInputFocus,CollectionDesc,CollectionInstanceDesc,ComponenTypeDesc,ComponentDesc,ComponentPropertyDesc,Disable,EmbeddedComponentDesc,EmbeddedInstanceDesc,Enable,InstanceDesc,InstancePropertyDesc,PropertyDesc,PrototypeDesc,ReleaseInputFocus,ScriptMessage,SetParent 
+from .properties_ddf_pb2 import PropertyDeclarationEntry,PropertyDeclarations 
