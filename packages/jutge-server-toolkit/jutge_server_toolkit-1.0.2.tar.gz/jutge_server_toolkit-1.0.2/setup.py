4bb73f30d0063f329075f5591276df7b58011da3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-18 15:04:03 +0000

    Add package jutge-server-toolkit/1.0.2/jutge_server_toolkit-1.0.2.tar.gz

---
+++ packages/jutge-server-toolkit/jutge_server_toolkit-1.0.2.tar.gz/jutge_server_toolkit-1.0.2/setup.py
@@ -0,0 +1,36 @@
+#!/usr/bin/env python3
+# coding=utf-8
+
+from setuptools import setup
+from os import system
+
+version = '1.0.2'
+
+setup(
+    name='jutge-server-toolkit',
+    packages=['jutge.servertoolkit'],
+    install_requires=[
+        'jutge-toolkit',
+    ],
+    version=version,
+    description='Server Toolkit for Jutge.org',
+    long_description='Server Toolkit for Jutge.org',
+    author='Jordi Petit et al',
+    author_email='jpetit@cs.upc.edu',
+    url='https://github.com/jutge-org/jutge-server-toolkit',
+    download_url='https://github.com/jutge-org/jutge-server-toolkit/tarball/{}'.format(version),
+    keywords=['jutge', 'jutge.org', 'education', 'toolkit'],
+    license='Apache',
+    zip_safe=False,
+    include_package_data=True,
+    setup_requires=['setuptools'],
+    entry_points={
+        'console_scripts': [
+        ]
+    },
+    scripts=[
+        'scripts/jutge-run',
+        'scripts/jutge-submit',
+        'scripts/jutge-install-sudo-tools',
+    ]
+)
