6f9a0108ed59c2fbb4726e2157e1bcb2238bdd59
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-18 16:04:24 +0000

    Add package luckyrobots/0.1.28/luckyrobots-0.1.28.tar.gz

---
+++ packages/luckyrobots/luckyrobots-0.1.28.tar.gz/luckyrobots-0.1.28/setup.py
@@ -0,0 +1,26 @@
+from setuptools import setup, find_packages
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setup(
+    name="luckyrobots",
+    version="0.1.28",
+    description="Robotics-AI Training in Hyperrealistic Game Environments",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    author="Devrim Yasar",
+    author_email="braces.verbose03@icloud.com",
+    url="https://github.com/lucky-robots/lucky-robots",
+    package_dir={"": "src"},
+    packages=find_packages(where="src"),
+    install_requires=[
+        "watchdog", "Flask==2.3.2","fastapi"
+    ],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.8',
+)
