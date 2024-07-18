dccd0d1f4b25c200f0ec391eb7b22d1e9bebf100
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-18 16:04:13 +0000

    Add package hfer/0.0.5/hfer-0.0.5.tar.gz

---
+++ packages/hfer/hfer-0.0.5.tar.gz/hfer-0.0.5/setup.py
@@ -0,0 +1,14 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+setuptools.setup(
+    name="hfer",
+    version="0.0.5",
+    author="Ryan",
+    author_email="xuyangshen1122@gmail.com",
+    description="one-line hf model",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/XuyangShen/HFer",
+)
