171b0551366711ab64b53adf73b5ea1e54759bda
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-18 04:04:22 +0000

    Add package asdk-plugin-accounting/2.0.0b2/asdk_plugin_accounting-2.0.0b2-py3-none-any.whl

---
+++ packages/asdk-plugin-accounting/asdk_plugin_accounting-2.0.0b2-py3-none-any.whl/asdk_plugin_accounting/agraph/accounting/configuration/attribute/portfolio/records/v1/PortfolioConfigurationAPI/api/__init__.py
@@ -0,0 +1,5 @@
+# flake8: noqa
+
+# import apis into api package
+from asdk_plugin_accounting.agraph.accounting.configuration.attribute.portfolio.records.v1.PortfolioConfigurationAPI.api.default_portfolio_configuration_api import DefaultPortfolioConfigurationAPI
+
---
+++ packages/asdk-plugin-accounting/asdk_plugin_accounting-2.0.0b2-py3-none-any.whl/asdk_plugin_accounting/agraph/accounting/transactions/abor_lot/v1/AborLotAPI/api/__init__.py
@@ -0,0 +1,5 @@
+# flake8: noqa
+
+# import apis into api package
+from asdk_plugin_accounting.agraph.accounting.transactions.abor_lot.v1.AborLotAPI.api.default_abor_lot_api import DefaultAborLotAPI
+
