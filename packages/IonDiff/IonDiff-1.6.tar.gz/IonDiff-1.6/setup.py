3d4694fc695daeda8ba0ff726e2d66b182a6d627
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-18 16:04:23 +0000

    Add package IonDiff/1.6/IonDiff-1.6.tar.gz

---
+++ packages/IonDiff/IonDiff-1.6.tar.gz/IonDiff-1.6/setup.py
@@ -0,0 +1,31 @@
+from distutils.core import setup
+
+with open('LICENSE') as f:
+    license = f.read()
+
+setup(
+  name = 'IonDiff',
+  packages = ['IonDiff'],
+  version = '1.6',
+  license=license,
+  description = 'Unsupervised identification and analysis of ion-hopping events in solid state electrolytes.',
+  author = 'Cibrán López Álvarez',
+  author_email = 'cibran.lopez@upc.edu',
+  url = 'https://github.com/IonRepo/IonDiff',
+  download_url = 'https://github.com/IonRepo/IonDiff/archive/refs/tags/0.1.tar.gz',
+  keywords = ['Ionic Diffusion', 'Unsupervised Clustering', 'Machine Learning'],
+  install_requires=[
+          'numpy',
+          'scipy',
+          'scikit-learn',
+          'multiprocess',
+          'seaborn',
+      ],
+  classifiers=[
+    'Development Status :: 3 - Alpha',
+    'Intended Audience :: Developers',
+    'Topic :: Software Development :: Build Tools',
+    'License :: OSI Approved :: MIT License',
+    'Programming Language :: Python :: 3',
+  ],
+)
