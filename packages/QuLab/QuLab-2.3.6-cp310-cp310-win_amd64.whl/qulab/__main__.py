591bb0ad8e92a64f4370a94c4ab5f70a7dae284c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-18 14:04:52 +0000

    Add package QuLab/2.3.6/QuLab-2.3.6-cp310-cp310-win_amd64.whl

---
+++ packages/QuLab/QuLab-2.3.6-cp310-cp310-win_amd64.whl/qulab/__main__.py
@@ -0,0 +1,26 @@
+import click
+
+from .monitor.__main__ import main as monitor
+from .scan.server import server
+from .sys.net.cli import dht
+from .visualization.__main__ import plot
+
+
+@click.group()
+def main():
+    pass
+
+
+@main.command()
+def hello():
+    """Print hello world."""
+    click.echo('hello, world')
+
+
+main.add_command(monitor)
+main.add_command(plot)
+main.add_command(dht)
+main.add_command(server)
+
+if __name__ == '__main__':
+    main()
