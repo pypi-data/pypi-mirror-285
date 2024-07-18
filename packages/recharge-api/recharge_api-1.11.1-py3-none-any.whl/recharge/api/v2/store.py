00d0767d3f7f140b55a1d25877a4daf2e1d48958
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-18 16:04:22 +0000

    Add package recharge-api/1.11.1/recharge_api-1.11.1-py3-none-any.whl

---
+++ packages/recharge-api/recharge_api-1.11.1-py3-none-any.whl/recharge/api/v2/store.py
@@ -0,0 +1,19 @@
+from recharge.api import RechargeResource, RechargeScope, RechargeVersion
+
+
+class StoreResource(RechargeResource):
+    """
+    https://developer.rechargepayments.com/2021-11/store
+    """
+
+    object_list_key = "store"
+    recharge_version: RechargeVersion = "2021-11"
+
+    def get(self):
+        """Get store information.
+        https://developer.rechargepayments.com/2021-11/store/store_retrieve
+        """
+        required_scopes: list[RechargeScope] = ["store_info"]
+        self._check_scopes(f"GET /{self.object_list_key}", required_scopes)
+
+        return self._http_get(self._url)
