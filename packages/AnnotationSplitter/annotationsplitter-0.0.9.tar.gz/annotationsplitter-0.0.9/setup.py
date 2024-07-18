c76c04226073793d86980a72e482def4642f8491
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-18 15:04:03 +0000

    Add package AnnotationSplitter/0.0.9/annotationsplitter-0.0.9.tar.gz

---
+++ packages/AnnotationSplitter/annotationsplitter-0.0.9.tar.gz/annotationsplitter-0.0.9/setup.py
@@ -0,0 +1,36 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="AnnotationSplitter",
+    version="0.0.9",
+    packages=find_packages(where='src'),
+    package_dir={'': 'src'},
+    include_package_data=True,
+    install_requires=[
+        "icecream>=2.1",
+        "pandas",
+        "tqdm",
+        "requests",
+        "cogent3>=2024.2.5a1",
+        "pyfaidx>=0.8",
+        "biopython>=1.83",
+        "matplotlib"  # Add this if you are using matplotlib
+    ],
+    entry_points={
+        'console_scripts': [
+            'AnnotationSplitter=src.main:main',
+        ],
+    },
+    author="Andreas Bachler",
+    author_email="Andy.Bachler@example.com",
+    description="A simple bioinformatics script.",
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
+    url="https://github.com/Andy-B-123/AnnotationSplitter",
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.11',
+)
