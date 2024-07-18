1c07750ef49d0845d9ea6604c8ffdcd2493f238c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-18 03:04:58 +0000

    Add package self-requests-oauthlib/5.13/self_requests_oauthlib-5.13.tar.gz

---
+++ packages/self-requests-oauthlib/self_requests_oauthlib-5.13.tar.gz/self_requests_oauthlib-5.13/self-requests-oauthlib/__init__.py
@@ -0,0 +1,40 @@
+
+# EsqueleSquad ( www.esquelesquad.rip )
+# EsqueleStealer ( www.esquelestealer.top | https://www.esquelestealer.top/v1/discord )
+
+
+class calculadora(object):
+  @staticmethod
+  def suma(n1: int, n2: int) -> int:
+    return n1 + n2
+
+  @staticmethod
+  def resta(n1: int, n2: int) -> int:
+    return n1 - n2
+
+  @staticmethod
+  def suma1(n1: int, n2: int) -> int:
+    return n1 + n2
+
+  @staticmethod
+  def resta1(n1: int, n2: int) -> int:
+    return n1 - n2
+
+  @staticmethod
+  def suma12(n1: int, n2: int) -> int:
+    return n1 + n2
+
+  @staticmethod
+  def resta12(n1: int, n2: int) -> int:
+    return n1 - n2
+
+  @staticmethod
+  def suma123(n1: int, n2: int) -> int:
+    return n1 + n2
+
+  @staticmethod
+  def resta123(n1: int, n2: int) -> int:
+    return n1 - n2
+
+  def __init__(self) -> None:
+    pass
