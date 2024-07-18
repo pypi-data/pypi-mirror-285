a635d8e4e6584930908b246caffe061a1795cefc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-18 14:04:40 +0000

    Add package pyegeria/0.5.5.10/pyegeria-0.5.5.10-py3-none-any.whl

---
+++ packages/pyegeria/pyegeria-0.5.5.10-py3-none-any.whl/examples/widgets/developer/get_tech_details.py
@@ -0,0 +1,100 @@
+#!/usr/bin/env python3
+"""
+SPDX-License-Identifier: Apache-2.0
+Copyright Contributors to the ODPi Egeria project.
+
+A simple viewer for collections - provide the root and we display the hierarchy
+
+"""
+
+import argparse
+
+from rich import print
+from rich.panel import Panel
+from rich.prompt import Prompt
+from rich.text import Text
+from rich.tree import Tree
+
+from pyegeria import (UserNotAuthorizedException, PropertyServerException,
+                      InvalidParameterException, AutomatedCuration)
+from pyegeria._exceptions import (
+    print_exception_response,
+)
+
+disable_ssl_warnings = True
+
+platform = "https://127.0.0.1:9443"
+user = "erinoverview"
+view_server = "view-server"
+
+
+def tech_viewer(tech: str, server_name:str, platform_url:str, user:str):
+
+    def view_tech_details(a_client: AutomatedCuration, root_collection_name: str, tree: Tree) -> Tree:
+        l2: Tree = None
+        tech_details = a_client.get_technology_type_detail(tech)
+        if type(tech_details) is dict:
+            style = ""
+            l2 = tree.add(Text(f"Name: {tech_details.get('name',' ')}", "bold red"))
+            l2 = tree.add(Text(f"* QualifiedName: {tech_details['qualifiedName']}","bold white"))
+            l2 = tree.add(Text(f"* Category: {tech_details['category']}", "bold white"))
+            l2 = tree.add(Text(f"* Technology Description: {tech_details['description']}", "bold white"))
+            ext_ref = tech_details.get('externalReferences', None)
+            if ext_ref is not None:
+                l2 = tree.add(Text(f'* URI: {ext_ref[0]["properties"]["uri"]}', "bold white"))
+
+
+            resource_list = tech_details.get('resourceList',None)
+            if resource_list:
+                t_r = tree.add("Resource List[bold red]")
+                for resource in resource_list:
+                    resource_use = Text(f"[bold white]{resource['resourceUse']}", "")
+                    resource_use_description = Text(f"[bold white]{resource['resourceUseDescription']}", "")
+                    type_name = Text(f"[bold white]{resource['relatedElement']['type']['typeName']}", "")
+                    unique_name = Text(f"[bold white]{resource['relatedElement']['uniqueName']}", "")
+                    related_guid = Text(f"[bold white]{resource['relatedElement']['guid']}", "")
+                    resource_text = (f"[bold red]Resource\n"
+                                     f"[white]Resource use: {resource_use}[white]\nDescription: "
+                                     f"{resource_use_description}\nType Name: {type_name}\n"
+                                     f"[white]Unique Name: {unique_name}\n[white]Related GUID: {related_guid}\n")
+                    p = Panel.fit(resource_text)
+                    tt = t_r.add(p, style=style)
+
+
+            return tt
+
+
+    try:
+        tree = Tree(f"[bold bright green]{tech}", guide_style="bold bright_blue")
+        a_client = AutomatedCuration(view_server, platform,
+                                     user_id=user)
+
+        token = a_client.create_egeria_bearer_token(user, "secret")
+        view_tech_details(a_client,tech,tree)
+        print(tree)
+
+    except (
+        InvalidParameterException,
+        PropertyServerException,
+        UserNotAuthorizedException
+    ) as e:
+        print_exception_response(e)
+
+
+def main():
+    parser = argparse.ArgumentParser()
+
+    parser.add_argument("--server", help="Name of the server to display status for")
+    parser.add_argument("--url", help="URL Platform to connect to")
+    parser.add_argument("--userid", help="User Id")
+    args = parser.parse_args()
+
+    server = args.server if args.server is not None else "view-server"
+    url = args.url if args.url is not None else "https://localhost:9443"
+    userid = args.userid if args.userid is not None else 'erinoverview'
+
+    tech = Prompt.ask("Enter the Technology to start from:", default="PostgreSQL Server")
+    tech_viewer(tech,server, url, userid)
+
+if __name__ == "__main__":
+    main()
---
+++ packages/pyegeria/pyegeria-0.5.5.10-py3-none-any.whl/examples/widgets/operational/view_platform_status.py
@@ -0,0 +1,127 @@
+#!/usr/bin/env python3
+"""
+SPDX-License-Identifier: Apache-2.0
+Copyright Contributors to the ODPi Egeria project.
+
+Display the status of cataloged platforms and servers.
+"""
+
+import time
+import argparse
+
+from pyegeria._exceptions import (
+    InvalidParameterException,
+    PropertyServerException,
+    UserNotAuthorizedException,
+    print_exception_response,
+)
+from rich.table import Table
+from rich.live import Live
+from rich.console import Console
+
+from pyegeria import RuntimeManager
+
+disable_ssl_warnings = True
+console = Console(width=200)
+
+def display_status(server: str, url: str, username: str):
+    r_client = RuntimeManager(server, url, username)
+    token = r_client.create_egeria_bearer_token(username, "secret")
+    def generate_table() -> Table:
+        """Make a new table."""
+        table = Table(
+            title=f"Server Status for Platform - {time.asctime()}",
+            # style = "black on grey66",
+            header_style="white on dark_blue",
+            caption=f"Status of Platforms - '{url}'",
+            show_lines=True,
+            # expand=True
+        )
+        table.add_column("Platform Name")
+        # table.add_column("Platform GUID")
+        table.add_column("Platform URL")
+        table.add_column("Platform Origin")
+        table.add_column("Description")
+        table.add_column("Platform Started")
+        table.add_column("Servers")
+
+        server_types = {
+            "Metadata Access Store": "Store",
+            "View Server" : "View",
+            "Engine Host Server" : "EngineHost",
+            "Integration Daemon" : "Integration"
+        }
+
+        platform_list = r_client.get_platforms_by_type()
+        for platform in platform_list:
+            platform_name = platform['properties']["name"]
+            platform_guid = platform['elementHeader']["guid"]
+            platform_desc = platform['properties']["description"]
+            server_list = ""
+            try:
+                platform_report = r_client.get_platform_report(platform_guid)
+                platform_url = platform_report.get('platformURLRoot'," ")
+                platform_origin = platform_report.get("platformOrigin"," ")
+                platform_started = platform_report.get("platformStartTime"," ")
+
+                servers = platform_report.get("omagservers",None)
+
+                if servers is not None:
+                    for server in servers:
+                        server_name = server.get("serverName"," ")
+                        server_type = server.get("serverType"," ")
+                        server_status = server.get("serverActiveStatus","UNKNOWN")
+                        if server_status in("RUNNING", "STARTING"):
+                            status_flag = "[green]"
+                        elif server_status in ("INACTIVE", "STOPPING"):
+                            status_flag = "[red]"
+                        else:
+                            server_status = "UNKNOWN"
+                            status_flag = "[yellow]"
+
+                        serv = f"{status_flag}{server_types[server_type]}: {server_name}\n"
+                        server_list = server_list + serv
+
+            except (Exception) as e:
+                # console.print_exception(e)
+                platform_url = " "
+                platform_origin = " "
+                platform_started = " "
+
+            table.add_row(platform_name, platform_url, platform_origin, platform_desc,
+                         platform_started, server_list)
+
+
+        return table
+
+
+    try:
+        with Live(generate_table(), refresh_per_second=4, screen=True) as live:
+            while True:
+                time.sleep(2)
+                live.update(generate_table())
+
+    except (InvalidParameterException, PropertyServerException, UserNotAuthorizedException) as e:
+        print_exception_response(e)
+
+
+    finally:
+        r_client.close_session()
+
+
+def main():
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--server", help="Name of the server to display status for")
+    parser.add_argument("--url", help="URL Platform to connect to")
+    parser.add_argument("--userid", help="User Id")
+    args = parser.parse_args()
+
+    server = args.server if args.server is not None else "view-server"
+    url = args.url if args.url is not None else "https://localhost:9443"
+    userid = args.userid if args.userid is not None else 'erinoverview'
+
+    display_status(server, url, userid)
+
+
+if __name__ == "__main__":
+    main()
