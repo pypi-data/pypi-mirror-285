322e85e033ec48f04ac21777a4403837797441ab
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-19 17:04:43 +0000

    Add package pymath_compute/0.3.2/pymath_compute-0.3.2-cp39-none-win32.whl

---
+++ packages/pymath_compute/pymath_compute-0.3.2-cp39-none-win32.whl/pymath_compute/__init__.py
@@ -0,0 +1,15 @@
+"""
+Mathematical Solver Module.
+
+This module provides various imports related to mathematical modeling and solving.
+To better documentation, please refer to the Github Page.
+    > https://github.com/ricardoleal20/pymath_compute
+"""
+from pymath_compute.model import Variable, MathExpression, MathFunction
+
+# Include the all module
+__all__ = [
+    "Variable",
+    "MathExpression",
+    "MathFunction"
+]
