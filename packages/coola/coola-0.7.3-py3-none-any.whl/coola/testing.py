1b5e377b67aa7e636a9489d4b933598a3a14b63d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-19 17:05:05 +0000

    Add package coola/0.7.3/coola-0.7.3-py3-none-any.whl

---
+++ packages/coola/coola-0.7.3-py3-none-any.whl/coola/testing.py
@@ -0,0 +1,37 @@
+r"""Define some utility functions for testing."""
+
+from __future__ import annotations
+
+__all__ = [
+    "jax_available",
+    "numpy_available",
+    "pandas_available",
+    "polars_available",
+    "torch_available",
+    "torch_cuda_available",
+    "torch_mps_available",
+    "xarray_available",
+]
+
+import pytest
+
+from coola.utils.imports import (
+    is_jax_available,
+    is_numpy_available,
+    is_pandas_available,
+    is_polars_available,
+    is_torch_available,
+    is_xarray_available,
+)
+from coola.utils.tensor import is_cuda_available, is_mps_available
+
+jax_available = pytest.mark.skipif(not is_jax_available(), reason="Requires JAX")
+numpy_available = pytest.mark.skipif(not is_numpy_available(), reason="Requires NumPy")
+pandas_available = pytest.mark.skipif(not is_pandas_available(), reason="Requires pandas")
+polars_available = pytest.mark.skipif(not is_polars_available(), reason="Requires polars")
+torch_available = pytest.mark.skipif(not is_torch_available(), reason="Requires PyTorch")
+torch_cuda_available = pytest.mark.skipif(
+    not is_cuda_available(), reason="Requires PyTorch and CUDA"
+)
+torch_mps_available = pytest.mark.skipif(not is_mps_available(), reason="Requires PyTorch and MPS")
+xarray_available = pytest.mark.skipif(not is_xarray_available(), reason="Requires xarray")
