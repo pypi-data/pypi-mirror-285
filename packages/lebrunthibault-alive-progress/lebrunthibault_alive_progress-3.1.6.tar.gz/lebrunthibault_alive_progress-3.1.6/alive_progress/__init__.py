2c48f1d18915ffc7a22c50b6c3b74a1a2b6fdaf8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-19 18:05:00 +0000

    Add package lebrunthibault-alive-progress/3.1.6/lebrunthibault_alive_progress-3.1.6.tar.gz

---
+++ packages/lebrunthibault-alive-progress/lebrunthibault_alive_progress-3.1.6.tar.gz/lebrunthibault_alive_progress-3.1.6/alive_progress/__init__.py
@@ -0,0 +1,12 @@
+from .core.configuration import config_handler
+from .core.progress import alive_bar, alive_it
+
+VERSION = (3, 1, 6)
+
+__author__ = 'Thibault Lebrun'
+__email__ = 'thibaultlebrun@live.fr'
+__version__ = '.'.join(map(str, VERSION))
+__description__ = 'A new kind of Progress Bar, with real-time throughput, ' \
+                  'ETA, and very cool animations!'
+
+__all__ = ('alive_bar', 'alive_it', 'config_handler')
