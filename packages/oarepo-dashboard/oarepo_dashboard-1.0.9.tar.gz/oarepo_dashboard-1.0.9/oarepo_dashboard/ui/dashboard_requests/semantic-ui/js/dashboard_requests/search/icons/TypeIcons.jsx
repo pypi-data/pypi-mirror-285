25fd388c72e10721702482a0d109739f8de8bf47
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-19 18:05:06 +0000

    Add package oarepo-dashboard/1.0.9/oarepo_dashboard-1.0.9.tar.gz

---
+++ packages/oarepo-dashboard/oarepo_dashboard-1.0.9.tar.gz/oarepo_dashboard-1.0.9/oarepo_dashboard/ui/dashboard_requests/semantic-ui/js/dashboard_requests/search/icons/TypeIcons.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import { Icon } from "semantic-ui-react";
+
+export const EditRecordIcon = () => (
+  <Icon name="unlock alternate" className="neutral" />
+);
+export const DeleteRecordIcon = () => (
+  <Icon name="delete" className="neutral" />
+);
+export const PublishRecordIcon = () => (
+  <Icon name="upload" className="neutral" />
+);
