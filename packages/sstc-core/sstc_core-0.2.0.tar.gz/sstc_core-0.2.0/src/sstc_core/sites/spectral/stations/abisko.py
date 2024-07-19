b7a812ecc9333340bed7b02cac31405006ca3e34
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-19 14:04:49 +0000

    Add package sstc-core/0.2.0/sstc_core-0.2.0.tar.gz

---
+++ packages/sstc-core/sstc_core-0.2.0.tar.gz/sstc_core-0.2.0/src/sstc_core/sites/spectral/stations/abisko.py
@@ -0,0 +1,10 @@
+version = '2024_v0.1'
+
+meta ={
+    "acronym": "ANS",
+    "name": "Abisko Scientific Research Station",
+    "is_active": True,
+    "short_name": "Abisko"
+}
+
+# Active platforms confirmed 2024 
