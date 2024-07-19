de21059f2dad09c7dd94b9aefbda2a5b732a681c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-19 14:04:49 +0000

    Add package unitize/0.0.4/unitize-0.0.4.tar.gz

---
+++ packages/unitize/unitize-0.0.4.tar.gz/unitize-0.0.4/setup.py
@@ -0,0 +1,28 @@
+from setuptools import setup, find_packages
+from pathlib import Path
+this_directory = Path(__file__).parent
+
+VERSION = '0.0.4' 
+DESCRIPTION = 'A package for unit conversion and casting.'
+LONG_DESCRIPTION = (this_directory / "README.md").read_text()
+
+# Setting up
+setup(
+        name="unitize", 
+        version=VERSION,
+        author="Naren Dawar",
+        description=DESCRIPTION,
+        long_description=LONG_DESCRIPTION,
+        long_description_content_type='text/markdown',
+        packages=find_packages(),
+        install_requires=[],  
+        
+        keywords=['python', 'conversion', 'units', 'data', 'time', 'data conversion'],
+        classifiers= [
+            "Intended Audience :: Education",
+            "Programming Language :: Python :: 2",
+            "Programming Language :: Python :: 3",
+            "Operating System :: MacOS :: MacOS X",
+            "Operating System :: Microsoft :: Windows",
+        ]
+)
