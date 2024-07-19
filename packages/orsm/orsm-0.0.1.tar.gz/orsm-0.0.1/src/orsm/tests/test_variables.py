bb11e6f4bf6f0422985e5fa9a29633eac88a01e2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-19 15:05:21 +0000

    Add package orsm/0.0.1/orsm-0.0.1.tar.gz

---
+++ packages/orsm/orsm-0.0.1.tar.gz/orsm-0.0.1/src/orsm/tests/test_variables.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+import unittest
+
+
+class MyTestCase(unittest.TestCase):
+    def test_something(self):
+        return
+        raise NotImplemented
+
+
+if __name__ == '__main__':
+    unittest.main()
