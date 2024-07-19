98c38b48deb3de35b63b628c584097f5b621973d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-19 14:05:53 +0000

    Add package end-of-life/0.2/end_of_life-0.2.tar.gz

---
+++ packages/end-of-life/end_of_life-0.2.tar.gz/end_of_life-0.2/setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup, find_packages
+setup(
+    name='end-of-life',
+    version='0.2',
+    packages=find_packages(),
+    install_requires=[
+        #Add dependencies here.
+        #e.g 'numpy>=1.11.1'
+    ],
+    #entry_points={
+    #    "console_scripts": [
+    #        "end-of-life = end-of-life:hello",
+    #    ],
+    #},
+)
