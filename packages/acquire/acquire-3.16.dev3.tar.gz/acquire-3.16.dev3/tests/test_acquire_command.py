a3a32032d765c4f624721b4c50d323711ec00f8d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-19 03:05:42 +0000

    Add package acquire/3.16.dev3/acquire-3.16.dev3.tar.gz

---
+++ packages/acquire/acquire-3.16.dev3.tar.gz/acquire-3.16.dev3/tests/test_acquire_command.py
@@ -0,0 +1,41 @@
+from argparse import Namespace
+from typing import List
+from unittest.mock import patch
+
+import pytest
+
+from acquire.acquire import (
+    CONFIG,
+    MODULES,
+    PROFILES,
+    VOLATILE,
+    create_argument_parser,
+    parse_acquire_args,
+)
+
+
+@pytest.fixture
+def acquire_parser_args(config: List, argument_list: List) -> Namespace:
+    CONFIG["arguments"] = config
+    with patch("argparse._sys.argv", [""] + argument_list):
+        return parse_acquire_args(create_argument_parser(PROFILES, VOLATILE, MODULES), config=CONFIG)[0]
+
+
+@pytest.mark.parametrize("config, argument_list", [([], [])])
+def test_no_defaults_in_config(acquire_parser_args):
+    assert not acquire_parser_args.force_fallback
+
+
+@pytest.mark.parametrize("config, argument_list", [(["--force-fallback"], [])])
+def test_one_config_default_argument(acquire_parser_args):
+    assert acquire_parser_args.force_fallback
+
+
+@pytest.mark.parametrize("config, argument_list", [(["-f", "test"], ["-f", "best"])])
+def test_config_default_argument_override(acquire_parser_args):
+    assert acquire_parser_args.file == ["best"]
+
+
+@pytest.mark.parametrize("config, argument_list", [([], ["target1", "target2"])])
+def test_local_target_fallbactargets(acquire_parser_args):
+    assert acquire_parser_args.targets == ["target1", "target2"]
