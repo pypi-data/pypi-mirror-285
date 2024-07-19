91c726ecbc15ab5824ec2151157d23cffd0ef4ed
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-19 16:05:37 +0000

    Add package thestage-core/0.0.9/thestage_core-0.0.9-py3-none-any.whl

---
+++ packages/thestage-core/thestage_core-0.0.9-py3-none-any.whl/thestage_core/config/__init__.py
@@ -0,0 +1,6 @@
+import os
+
+THESTAGE_CONFIG_DIR = os.getenv('THESTAGE_CONFIG_DIR', '.thestage')
+THESTAGE_CONFIG_FILE = os.getenv('THESTAGE_CONFIG_FILE', 'config.json')
+THESTAGE_AUTH_TOKEN = os.getenv('THESTAGE_AUTH_TOKEN', None)
+THESTAGE_API_URL = os.getenv('THESTAGE_API_URL', '')
