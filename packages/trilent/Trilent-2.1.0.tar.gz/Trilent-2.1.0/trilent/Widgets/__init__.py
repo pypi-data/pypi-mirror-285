d038f8a26edac6daad3db05dea6eec39dfe7bf52
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-19 18:05:04 +0000

    Add package trilent/2.1.0/Trilent-2.1.0.tar.gz

---
+++ packages/trilent/Trilent-2.1.0.tar.gz/Trilent-2.1.0/trilent/Widgets/__init__.py
@@ -0,0 +1,14 @@
+__all__ = ['PositionTypes', 'Box', 'Widget', 'Text', 'Button', 'Entry', 'Slider', 'CheckBox', 'Switch', 'Canvas', 'Image']
+
+from .widget import PositionTypes, Widget
+from .box import Box
+from .text import Text
+from .button import Button
+from .entry import Entry
+from .slider import Slider
+from .check_box import CheckBox
+from .switch import Switch
+from .canvas import Canvas
+from .image import Image
+
+_ = PositionTypes, Box, Widget, Text, Button, Entry, Slider, CheckBox, Switch, Canvas, Image
