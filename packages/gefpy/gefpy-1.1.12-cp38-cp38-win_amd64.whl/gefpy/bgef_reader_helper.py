b94f1e25206f74e23d48b3ba8a9692959295b07e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-19 17:05:07 +0000

    Add package gefpy/1.1.12/gefpy-1.1.12-cp38-cp38-win_amd64.whl

---
+++ packages/gefpy/gefpy-1.1.12-cp38-cp38-win_amd64.whl/gefpy/bgef_reader_helper.py
@@ -0,0 +1,7 @@
+# the value for sampling,is same as cpp!
+class SamplingPointKind(object):
+    left_top: int = 0
+    right_top: int = 1
+    left_bottom: int = 2
+    right_bottom: int = 3
+    center: int = 5
