be316d08a558bd81241a0c9e9a97d24ad65a1f99
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-19 18:05:08 +0000

    Add package send-area/0.0.1/send_area-0.0.1.tar.gz

---
+++ packages/send-area/send_area-0.0.1.tar.gz/send_area-0.0.1/tests/testMain.py
@@ -0,0 +1,13 @@
+import unittest
+
+from src.send_area import Calculate
+class TestCalculate(unittest.TestCase):
+  def setUp(self):
+    self.calculator = Calculate()
+  def test_circle(self):
+    self.assertEqual(self.calculator.circle(5), 15.71)
+  def test_triangle(self):
+    self.assertEqual(self.calculator.triangle(3,4, 5), 6)
+
+if __name__ == "__init__":
+  unittest.main()
