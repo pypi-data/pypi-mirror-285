a1c4dd666eb7a66394336a7e22a42c35051550ee
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-19 15:05:49 +0000

    Add package pySISF/0.1.0rc59.post1/pysisf-0.1.0rc59.post1-py3-none-any.whl

---
+++ packages/pySISF/pysisf-0.1.0rc59.post1-py3-none-any.whl/pySISF/__init__.py
@@ -0,0 +1,11 @@
+#   -------------------------------------------------------------
+#   Copyright (c) Microsoft Corporation. All rights reserved.
+#   Licensed under the MIT License. See LICENSE in project root for information.
+#   -------------------------------------------------------------
+"""Python Package Template"""
+from __future__ import annotations
+
+__version__ = "0.1.0""-rc59-post1"
+
+from pySISF import sisf
+from pySISF import vidlib
