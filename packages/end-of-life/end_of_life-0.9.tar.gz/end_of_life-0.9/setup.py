0971eac5bd1f110b1158e4adc80fe9db09481cd2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-19 15:05:06 +0000

    Add package end-of-life/0.9/end_of_life-0.9.tar.gz

---
+++ packages/end-of-life/end_of_life-0.9.tar.gz/end_of_life-0.9/setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup, find_packages
+setup(
+    name='end-of-life',
+    version='0.9',
+    packages=find_packages(),
+    install_requires=[
+        #Malicious actor compromised
+        #Add dependencies here.
+        #e.g 'numpy>=1.11.1'
+    ],
+    #entry_points={
+    #    "console_scripts": [
+    #        "end-of-life = end-of-life:hello",
+    #    ],
+    #},
+)
