0fe73b49681fac37243c7dac3146e99a3da82f3c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-19 14:05:05 +0000

    Add package rootchecker/0.3.0/rootchecker-0.3.0.tar.gz

---
+++ packages/rootchecker/rootchecker-0.3.0.tar.gz/rootchecker-0.3.0/rootchecker/rootchecker.py
@@ -0,0 +1,53 @@
+import os
+
+device = {
+        "magisk" : "unchecked",
+        "root" : "unchecked",
+        "su_binary" : "unchecked",
+        "running_on_termux" : "unchecked",
+        }
+
+def trigger_root():
+    global device
+    device["root"] = True
+
+
+def check_for_su_binary():
+    global device
+    if os.path.isfile("/system/bin/su") == True:
+        device["su_binary"] = True
+        trigger_root()
+    else:
+        device["su_binary"] = False
+    return 0
+
+
+def check_for_magisk():
+    global device
+    if os.path.isfile("/system/bin/magisk") == True:
+        device["magisk"] = True
+        trigger_root()
+    else:
+        device["magisk"] = False
+    return 0
+
+
+def check_for_termux():
+    global device
+    if os.path.isdir("/data/data/com.termux/files/home"):
+        device["running_on_termux"] = True
+    else:
+        device["running_on_termux"] = False
+
+
+def check_root():
+    global device
+    check_for_su_binary()
+    check_for_magisk()
+    check_for_termux()
+    if device["root"] != True:
+        device["root"] = False
+    return device
+
+
+
---
+++ packages/rootchecker/rootchecker-0.3.0.tar.gz/rootchecker-0.3.0/setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+from setuptools import find_packages
+
+
+setup(
+        name = "rootchecker",
+        version = "0.3.0",
+        description = "Simple rootchecker for Android that written in python",
+        author = "yakupaslantas",
+        packages = find_packages(),
+)
+
+
