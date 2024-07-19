533c5b851dcedefc19d4e32f7be639700af04953
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-19 03:04:54 +0000

    Add package mov-cli/4.4.8/mov_cli-4.4.8-py3-none-any.whl

---
+++ packages/mov-cli/mov_cli-4.4.8-py3-none-any.whl/mov_cli/cli/__main__.py
@@ -0,0 +1,169 @@
+from __future__ import annotations
+from typing import List, Optional, TYPE_CHECKING
+
+if TYPE_CHECKING:
+    ...
+
+import typer
+import shutil
+import logging
+from pathlib import Path
+
+from .play import play
+from .search import search
+from .ui import welcome_msg
+from .episode import handle_episode
+from .plugins import show_all_plugins
+from .scraper import select_scraper, use_scraper, scrape, steal_scraper_args
+from .configuration import open_config_file, set_cli_config
+
+from ..config import Config
+from ..download import Download
+from ..media import MetadataType
+from ..logger import mov_cli_logger
+from ..http_client import HTTPClient
+from ..utils import hide_ip, get_temp_directory, what_platform, get_cache_directory
+
+__all__ = ("mov_cli",)
+
+uwu_app = typer.Typer(pretty_exceptions_enable = False) # NOTE: goldy has an uwu complex.
+
+def mov_cli(
+    query: Optional[List[str]] = typer.Argument(None, help = "A film, tv show or anime you would like to Query."), 
+    debug: Optional[bool] = typer.Option(None, help = "Enable extra logging details. Useful for bug reporting."), 
+    player: Optional[str] = typer.Option(None, "--player", "-p", help = "Player you would like to stream with. E.g. mpv, vlc"), 
+    scraper: Optional[str] = typer.Option(None, "--scraper", "-s", help = "Scraper you would like to scrape with. E.g. test, youtube, jellyplex"), 
+    fzf: Optional[bool] = typer.Option(None, help = "Toggle fzf on/off for all user selection prompts."), 
+    preview: Optional[bool] = typer.Option(None, help = "Toggle fzf's preview (image preview, etc) on/off for fzf prompts."), 
+    episode: Optional[str] = typer.Option(None, "--episode", "-ep", help = "Episode and season you wanna scrape. E.g. {episode}:{season} like -> 26:3"), 
+    auto_select: Optional[int] = typer.Option(None, "--choice", "-c", help = "Auto select the search results. E.g. Setting it to 1 with query 'nyan cat' will pick " \
+        "the first nyan cat video to show up in search results."
+    ), 
+    limit: Optional[int] = typer.Option(None, "--limit", "-l", help = "Specify the maximum number of results"), 
+
+    version: bool = typer.Option(False, "--version", help = "Display what version mov-cli is currently on."), 
+    edit: bool = typer.Option(False, "--edit", "-e", help = "Opens the mov-cli config with your respective editor."), 
+    download: bool = typer.Option(False, "--download", "-d", help = "Downloads the media instead of playing."), 
+    continue_watching: bool = typer.Option(False, "--continue", "-co", help = "Continue where you left off in a series."), 
+    list_plugins: bool = typer.Option(False, "--list-plugins", "-lp", help = "Prints all configured plugins and their scrapers."), 
+    clear_cache: bool = typer.Option(False, "--no-cache", "--clear-cache", help = "Clears ALL cache stored by mov-cli, including the temp directory cache.")
+):
+    config = Config()
+    platform = what_platform()
+
+    config = set_cli_config(
+        config, 
+        debug = debug, 
+        player = player, 
+        scraper = (scraper, ["scrapers", "default"]), 
+        fzf = (fzf, ["ui", "fzf"]), 
+        preview = (preview, ["ui", "preview"]), 
+        limit = (limit, ["ui", "limit"])
+    )
+
+    if config.debug:
+        mov_cli_logger.setLevel(logging.DEBUG)
+
+    if clear_cache:
+        mov_cli_logger.info("Clearing cache...")
+        shutil.rmtree(get_temp_directory(platform))
+        shutil.rmtree(get_cache_directory(platform))
+
+        # return right away after clearing cache if only --clear-cache or --no-cache is passed.
+        if query is None: 
+            return None
+
+    mov_cli_logger.debug(f"Config -> {config.data}")
+
+    if edit:
+        file_path = None if query is None else Path(query[0])
+        open_config_file(config, file_path)
+        return None
+
+    plugins = config.plugins
+
+    if list_plugins:
+        show_all_plugins(plugins, platform)
+        return None
+
+    welcome_message = welcome_msg(
+        plugins = plugins, 
+        platform = platform, 
+        check_for_updates = True if query is None and config.skip_update_checker is False else False, 
+        display_tip = True if query is None else False, 
+        display_version = version
+    )
+
+    print(welcome_message)
+
+    if query is not None:
+        scrape_options = steal_scraper_args(query) 
+        # This allows passing arguments to scrapers like this: 
+        # https://github.com/mov-cli/mov-cli-youtube/commit/b538d82745a743cd74a02530d6a3d476cd60b808#diff-4e5b064838aa74a5375265f4dfbd94024b655ee24a191290aacd3673abed921a
+
+        query: str = " ".join(query)
+
+        http_client = HTTPClient(
+            headers = config.http_headers, 
+            timeout = config.http_timeout, 
+            hide_ip = config.hide_ip
+        )
+
+        selected_scraper = select_scraper(plugins, config.scrapers, config.fzf_enabled, config.default_scraper)
+
+        if selected_scraper is None:
+            mov_cli_logger.error(
+                "You must choose a scraper to scrape with! " \
+                    "You can set a default scraper with the default key in config.toml."
+            )
+            return False
+
+        selected_scraper[2].update(scrape_options)
+
+        chosen_scraper = use_scraper(selected_scraper, config, http_client)
+
+        choice = search(query, auto_select, chosen_scraper, config.fzf_enabled, config.preview, config.limit)
+
+        if choice is None:
+            mov_cli_logger.error("There was no results or you didn't select anything.")
+            return False
+
+        chosen_episode = handle_episode(
+            episode_string = episode, 
+            scraper = chosen_scraper, 
+            choice = choice, 
+            fzf_enabled = config.fzf_enabled,
+            continue_watching = continue_watching
+        )
+
+        if chosen_episode is None:
+            mov_cli_logger.error("You didn't select a season/episode.")
+            return False
+
+        media = scrape(choice, chosen_episode, chosen_scraper)
+
+        if media is None:
+            episode_details_string = f" ep {chosen_episode.episode} season {chosen_episode.season} of" if choice.type == MetadataType.MULTI else ""
+
+            mov_cli_logger.error(
+                f"The scraper '{chosen_scraper.__class__.__name__}' couldn't find{episode_details_string} '{choice.title}'! " \
+                    "Don't report this to mov-cli, report this to the plugin itself."
+            )
+            return False
+
+        if download:
+            dl = Download(config)
+
+            mov_cli_logger.debug(f"Downloading from this url -> '{hide_ip(media.url, config.hide_ip)}'")
+
+            popen = dl.download(media)
+            
+            if popen:
+                popen.wait()
+
+        else:
+            play(media, choice, chosen_scraper, chosen_episode, config)
+
+def app():
+    uwu_app.command()(mov_cli)
+    uwu_app()
---
+++ packages/mov-cli/mov_cli-4.4.8-py3-none-any.whl/mov_cli/cli/scraper.py
@@ -0,0 +1,174 @@
+from __future__ import annotations
+from typing import TYPE_CHECKING
+
+if TYPE_CHECKING:
+    from typing import Type, Optional, Tuple, List, Dict
+
+    from .plugins import PluginsDataT
+
+    from ..media import Metadata, Media
+    from ..http_client import HTTPClient
+    from ..config import Config, ScrapersConfigT
+    from ..utils.episode_selector import EpisodeSelector
+    from ..scraper import Scraper, ScraperOptionsT
+
+from thefuzz import fuzz
+from devgoldyutils import Colours
+
+from .ui import prompt
+from .plugins import get_plugins_data, handle_internal_plugin_error
+
+from ..utils import what_platform
+from ..logger import mov_cli_logger
+
+def scrape(choice: Metadata, episode: EpisodeSelector, scraper: Scraper) -> Optional[Media]:
+    mov_cli_logger.info(f"Scraping '{Colours.CLAY.apply(choice.title)}'...")
+
+    try:
+        media = scraper.scrape(choice, episode)
+    except Exception as e:
+        handle_internal_plugin_error(e)
+
+    return media
+
+def use_scraper(
+    selected_scraper: Tuple[str, Type[Scraper], ScraperOptionsT], 
+    config: Config, 
+    http_client: HTTPClient
+) -> Scraper:
+    scraper_name, scraper_class, scraper_options = selected_scraper
+
+    mov_cli_logger.info(f"Using '{Colours.BLUE.apply(scraper_name)}' scraper...")
+
+    try:
+        chosen_scraper = scraper_class(config, http_client, scraper_options)
+    except Exception as e:
+        handle_internal_plugin_error(e)
+
+    return chosen_scraper
+
+def select_scraper(plugins: Dict[str, str], scrapers: ScrapersConfigT, fzf_enabled: bool, default_scraper: Optional[str] = None) -> Optional[Tuple[str, Type[Scraper], ScraperOptionsT]]:
+    plugins_data = get_plugins_data(plugins)
+
+    if default_scraper is not None:
+        scraper_name, scraper_or_available_scrapers, scraper_options = get_scraper(default_scraper, plugins_data, scrapers)
+
+        if scraper_name is None:
+            mov_cli_logger.error(
+                f"Could not find a scraper by the id '{default_scraper}'! Are you sure the plugin is installed and in your config? " \
+                    "Read the wiki for more on that: 'https://github.com/mov-cli/mov-cli/wiki#plugins'."
+            )
+
+            scrapers_beautifully_formatted = Colours.ORANGE.apply(f"{f'{Colours.RESET} | {Colours.ORANGE}'.join(scraper_or_available_scrapers)}")
+
+            print(f"\nAvailable Scrapers -> {scrapers_beautifully_formatted}")
+
+            did_you_mean_these = [(x, fuzz.ratio(default_scraper, x)) for x in scraper_or_available_scrapers]
+            did_you_mean_these.sort(key = lambda x: x[1], reverse = True)
+
+            if len(did_you_mean_these) > 0:
+                print(Colours.PURPLE.apply(f"\n* Did you mean: {Colours.GREEN}{did_you_mean_these[0][0]}\n"))
+
+            return None
+
+        return scraper_name, scraper_or_available_scrapers, scraper_options
+
+    chosen_plugin = prompt(
+        "Select a plugin", 
+        choices = plugins_data, 
+        display = lambda x: f"{Colours.ORANGE.apply(x[0])} [{Colours.PINK_GREY.apply(x[1])}]", 
+        fzf_enabled = fzf_enabled
+    )
+
+    plaform = what_platform()
+
+    if chosen_plugin is not None:
+        plugin_namespace, _, plugin = chosen_plugin
+
+        plugin_default_scraper = plugin.default_scraper(plaform)
+
+        chosen_scraper = prompt(
+            "Select a scraper", 
+            choices = [scraper for scraper in plugin.scrapers], 
+            display = (
+                lambda x: Colours.BLUE.apply(x[0].lower()) + Colours.CLAY.apply(" [DEFAULT]") if x[1] == plugin_default_scraper else Colours.BLUE.apply(x[0].lower())
+            ), 
+            fzf_enabled = fzf_enabled
+        )
+
+        if chosen_scraper is None:
+            return None
+
+        scraper_name, scraper = chosen_scraper
+
+        return f"{plugin_namespace}.{scraper_name}".lower(), scraper, {}
+
+    return None
+
+def steal_scraper_args(query: List[str]) -> ScraperOptionsT:
+    args_to_kidnap: List[str] = []
+    arg_values_to_kidnap: List[str] = []
+
+    scraper_options_args: List[Tuple[str, str | bool]] = []
+
+    for index, arg in enumerate(query):
+
+        if arg.startswith("--"):
+            arg_value = True
+
+            try:
+                arg_value_maybe = query[index + 1]
+
+                if not arg_value_maybe.startswith("--"):
+                    arg_value = arg_value_maybe
+
+                    arg_values_to_kidnap.append(arg_value)
+
+            except IndexError as e:
+                mov_cli_logger.debug(
+                    f"No scraper option argument value was found after '{arg}' so we'll assume this argument is a flag. \nError: {e}"
+                )
+
+            args_to_kidnap.append(arg)
+            scraper_options_args.append((arg.replace("--", "").replace("-", "_"), arg_value))
+
+    # KIDNAP THEM ARGS!!!!!
+    for arg_or_arg_value in args_to_kidnap + arg_values_to_kidnap:
+        query.remove(arg_or_arg_value)
+
+    mov_cli_logger.debug(f"Scraper args picked up on --> {scraper_options_args}")
+
+    return dict(scraper_options_args)
+
+def get_scraper(scraper_id: str, plugins_data: PluginsDataT, user_defined_scrapers: ScrapersConfigT) -> Tuple[str, Type[Scraper] | Tuple[None, List[str]], ScraperOptionsT]:
+    scraper_options = {}
+    available_scrapers = []
+
+    # scraper namespace override.
+    for scraper_namespace, scraper_data in user_defined_scrapers.items():
+
+        if scraper_id.lower() == scraper_namespace.lower():
+            mov_cli_logger.debug(f"Using the scraper overridden namespace '{scraper_namespace}'...")
+            scraper_id = scraper_data["namespace"]
+            scraper_options = scraper_data["options"]
+
+    platform = what_platform().upper()
+
+    for plugin_namespace, _, plugin in plugins_data:
+        plugin_scrapers = plugin.hook_data["scrapers"]
+
+        if scraper_id.lower() == plugin_namespace.lower() and f"{platform}.DEFAULT" in plugin_scrapers:
+            return f"{plugin_namespace}.{platform}.DEFAULT", plugin_scrapers[f"{platform}.DEFAULT"], scraper_options
+
+        elif scraper_id.lower() == plugin_namespace.lower() and "DEFAULT" in plugin_scrapers:
+            return f"{plugin_namespace}.DEFAULT", plugin_scrapers["DEFAULT"], scraper_options
+
+        for scraper_name, scraper in plugin_scrapers.items():
+            id = f"{plugin_namespace}.{scraper_name}".lower()
+
+            available_scrapers.append(id)
+
+            if scraper_id.lower() == id:
+                return id, scraper, scraper_options
+
+    return None, available_scrapers, scraper_options
