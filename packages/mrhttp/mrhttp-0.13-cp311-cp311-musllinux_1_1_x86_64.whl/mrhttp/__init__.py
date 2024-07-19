b7b283d0a18b2b6199fd23ddca3a3aa151d02f47
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-19 14:06:04 +0000

    Add package mrhttp/0.13/mrhttp-0.13-cp311-cp311-musllinux_1_1_x86_64.whl

---
+++ packages/mrhttp/mrhttp-0.13-cp311-cp311-musllinux_1_1_x86_64.whl/mrhttp/__init__.py
@@ -0,0 +1,25 @@
+
+from .internals import Protocol
+from .internals import Request as CRequest
+from .internals import Response
+from .internals import Router as CRouter
+from .internals import MrhttpApp as CApp
+from .httputil import HTTPError, HTTPRedirect
+
+from .internals import MemcachedProtocol
+from .internals import MemcachedClient as CMemcachedClient
+from .memcachedclient import MemcachedClient
+
+from .internals import MrqProtocol
+from .internals import MrqClient as CMrqClient
+from .mrqclient import MrqClient 
+
+from .internals import MrcacheProtocol
+from .internals import MrcacheClient as CMrcacheClient
+from .mrcacheclient import MrcacheClient 
+
+from .app import *
+from .internals import randint, escape, to64, from64, timesince
+
+__version__=0.13
+
