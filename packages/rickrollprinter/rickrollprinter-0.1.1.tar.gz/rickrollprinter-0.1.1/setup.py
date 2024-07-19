e411355e95f19005e4463325e8bbef9fdb8e3060
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-19 15:05:47 +0000

    Add package rickrollprinter/0.1.1/rickrollprinter-0.1.1.tar.gz

---
+++ packages/rickrollprinter/rickrollprinter-0.1.1.tar.gz/rickrollprinter-0.1.1/setup.py
@@ -0,0 +1,25 @@
+from setuptools import setup, find_packages
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setup(
+    name="rickrollprinter",
+    version="0.1.1",
+    packages=find_packages(),
+    install_requires=[
+        'pygame',
+    ],
+    author="Juan Andrés Young Hoyos",
+    author_email="juanandresyounghoyos@gmail.com",
+    description="A Python package that prints Rickroll lyrics and plays the song.",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/jayounghoyos/rickrollprinter",
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+)
