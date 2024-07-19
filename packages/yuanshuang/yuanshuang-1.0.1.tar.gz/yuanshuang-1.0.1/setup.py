df6bdb3e1b8aa8c00900642a974a5f40a7bcb032
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-19 17:04:35 +0000

    Add package yuanshuang/1.0.1/yuanshuang-1.0.1.tar.gz

---
+++ packages/yuanshuang/yuanshuang-1.0.1.tar.gz/yuanshuang-1.0.1/setup.py
@@ -0,0 +1,18 @@
+import setuptools
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="yuanshuang",
+    version="1.0.1",
+    author="yuanshuang",
+    url='',
+    author_email="18872142367@qq.com",
+    description="sk_pg package",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    package_dir={"": "src"},
+    packages=setuptools.find_packages(where="src"),
+    python_requires=">=3.7",
+)
