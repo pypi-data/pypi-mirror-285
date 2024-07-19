0db664eee17ad94b5eeaf3a68148c5da3e56d28f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-19 14:05:07 +0000

    Add package heytextual/0.1.0/heytextual-0.1.0-py3-none-any.whl

---
+++ packages/heytextual/heytextual-0.1.0-py3-none-any.whl/heytextual/endpoints.py
@@ -0,0 +1,6 @@
+class Endpoints:
+    base_url = "https://api.heytextual.com"
+    extract = f"{base_url}/extract"
+    documents = f"{base_url}/documents"
+    document = f"{base_url}/api/documents"
+    templates = f"{base_url}/templates"
