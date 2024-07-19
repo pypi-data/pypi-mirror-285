662506704a73c16c319383b24d645483a3acecd9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-19 03:06:04 +0000

    Add package haskellian/0.4.4/haskellian-0.4.4-py3-none-any.whl

---
+++ packages/haskellian/haskellian-0.4.4-py3-none-any.whl/haskellian/dicts/__init__.pyi
@@ -0,0 +1,14 @@
+from .basics import Mapper, map, map_k, map_kv, filter, filter_k, flatmap, flatmap_k
+from .grouping import group_by, zip, unzip, aggregate
+from .dict import Dict
+from .lifting import lift
+
+__all__ = [
+  'Mapper',
+  'map', 'map_k', 'map_kv',
+  'filter', 'filter_k',
+  'flatmap', 'flatmap_k',
+  'group_by', 'zip', 'unzip', 'aggregate',
+  'Dict',
+  'lift',
+]
