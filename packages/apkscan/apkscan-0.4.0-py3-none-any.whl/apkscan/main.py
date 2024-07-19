dcfa515c35e0aaacc7af5c4c64a0c3cc7631a142
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-19 04:05:15 +0000

    Add package apkscan/0.4.0/apkscan-0.4.0-py3-none-any.whl

---
+++ packages/apkscan/apkscan-0.4.0-py3-none-any.whl/apkscan/main.py
@@ -0,0 +1,137 @@
+# © 2024 Lucas Faudman.
+# Licensed under the MIT License (see LICENSE for details).
+# For commercial use, see LICENSE for additional terms.
+from argparse import ArgumentParser, BooleanOptionalAction
+from pathlib import Path
+
+from .apkscan import APKScanner
+from .secret_scanner import INCLUDED_SECRET_LOCATOR_FILES
+from .decompiler import DEFAULT_CONFIG
+
+DEFAULT_RULES = [
+    INCLUDED_SECRET_LOCATOR_FILES['default']
+]
+
+BANNER_ART = """\033[1;32m   ('-.      _ (`-. .-. .-')   .-')             ('-.         .-')
+  ( \033[0mOO\033[1;32m ).-. ( (\033[0mOO\033[1;32m  )\  ( \033[0mOO\033[1;32m ) ( \033[0mOO\033[1;32m ).          ( \033[0mOO\033[1;32m ).-.    ( \033[0mOO\033[1;32m )
+  / . --. /_.`     \,--. ,--.(_)---\_) .-----. / . --. /,--./ ,--,
+  | \-.  \(__...--''|  .'   //    _ | '  .--./ | \-.  \ |   \ |  |
+  | '  |  ||  /  | ||      / \  :` `. |  |('-. | '  |  ||    \|  |
+  | |_.'  ||  |_.' ||     '   '..`''. |  |\033[0mOO\033[1;32m  )| |_.'  ||  .     |
+  |  .-.  ||  .___.'|  .   \ .-._)   \|  |`-'| |  .-.  ||  |\    |
+  |  | |  ||  |     |  |\   \\\       /'  '--'\ |  | |  ||  | \   |
+  `--' `--'`--'     `--' '--' `-----'  `-----' `--' `--'`--'  `--'   """
+
+BANNER_TEXT = """
+  \033[1;92mAPKscan v0.4.0 - \033[3mScan for secrets, endpoints, and other sensitive data
+  after decompiling and deobfuscating Android files.\033[0m\033[0;92m
+  (.apk, .xapk, .dex, .jar, .class, .smali, .zip, .aar, .arsc, .aab, .jadx.kts)
+
+  \033[0m\033[2m(c) Lucas Faudman, 2024. License information in LICENSE file.
+  Credits to the original authors of all dependencies used in this project.
+\033[0m"""
+
+def main():
+    parser = ArgumentParser(description=BANNER_TEXT.strip())
+
+    input_options = parser.add_argument_group("Input Options")
+    input_options.add_argument(dest='files', type=Path, nargs="*", metavar="FILES_TO_SCAN",
+                               help="Path(s) to Java files to decompile and scan.")
+    input_options.add_argument("-r", "--rules", type=Path, nargs="*", default=DEFAULT_RULES, metavar="SECRET_LOCATOR_FILES",
+                               help="Path(s) to secret locator rules/patterns files OR names of included locator sets. "
+                               "\nFiles can be in SecretLocator JSON, secret-patterns-db YAML, or Gitleak TOML formats. "
+                               "\nIncluded locator sets: " + ", ".join(sorted(INCLUDED_SECRET_LOCATOR_FILES.keys())) + ". "
+                               + f"If not provided, default rules will be used. See: {DEFAULT_RULES[0]}"
+                               )
+
+    output_options = parser.add_argument_group("Output Options")
+    output_options.add_argument("-o", "--output", type=Path, metavar="SECRETS_OUTPUT_FILE", default="secrets_output.json", help="Output file for secrets found.")
+    output_options.add_argument("-f", "--format", type=str, choices=["text", "json", "yaml"], default="json", help="Output format for secrets found.")
+    output_options.add_argument("-g", "--groupby", type=str, choices=["file", "locator", "both"], default="both", help="Group secrets by input file or locator. Default is 'both'.")
+    output_options.add_argument("-c", "--cleanup", action=BooleanOptionalAction, default=False, help="Remove decompiled output directories after scanning.")
+    output_options.add_argument("-q", "--quiet", action="store_true", help="Suppress output from subprocesses.")
+
+    decompiler_choices = parser.add_argument_group("Decompiler Choices",
+        description="Choose which decompiler(s) to use. Optionally specify path to decompiler binary. Default is JADX.")
+    decompiler_choices.add_argument('--jadx', "-J", nargs='?', const=None, default=False, help="Use JADX Java decompiler.")
+    decompiler_choices.add_argument('--apktool', "-A", nargs='?', const=None, default=False, help="Use APKTool SMALI disassembler.")
+    decompiler_choices.add_argument('--cfr', "-C", nargs='?', const=None, default=False, help="Use CFR Java decompiler. Requires Enjarify.")
+    decompiler_choices.add_argument('--procyon', "-P", nargs='?', const=None, default=False, help="Use Procyon Java decompiler. Requires Enjarify.")
+    decompiler_choices.add_argument('--krakatau', "-K", nargs='?', const=None, default=False, help="Use Krakatau Java decompiler. Requires Enjarify.")
+    decompiler_choices.add_argument('--fernflower', "-F", nargs='?', const=None, default=False, help="Use Fernflower Java decompiler. Requires Enjarify.")
+    decompiler_choices.add_argument('--enjarify-choice', "-EC", type=str, choices=["auto", "never", "always"], default="auto", help="When to use Enjarify. Default is 'auto' which means use only when needed.")
+    decompiler_choices.add_argument('--unpack-xapks', action=BooleanOptionalAction, default=True, help="Unpack XAPK files into APKs before decompiling. Default is True.")
+
+
+    decompiler_options = parser.add_argument_group("Decompiler Advanced Options", description="Options for Java decompiler.")
+    decompiler_options.add_argument("-d", "--deobfuscate", action=BooleanOptionalAction, default=True, help="Deobfuscate file before scanning.")
+    decompiler_options.add_argument("-w", "--decompiler-working-dir", type=Path, default=Path.cwd(), help="Working directory where files will be decompiled.")
+    decompiler_options.add_argument("--decompiler-output-suffix", type=str, default="-decompiled", help="Suffix for decompiled output directory names. Default is '-decompiled'.")
+    decompiler_options.add_argument("--decompiler-extra-args", type=str, nargs="+", help="Additional arguments to pass to decompilers in form quoted whitespace separated '<DECOMPILER_NAME> <EXTRA_ARGS>...'. For example: --decompiler-extra-args 'jadx --no-debug-info,--no-inline'.")
+    decompiler_options.add_argument("-dct", "--decompiler-concurrency-type", type=str, choices=["thread", "process", "main"], default="thread", help="Type of concurrency to use for decompilation. Default is 'thread'.")
+    decompiler_options.add_argument("-dro", "--decompiler-results-order", type=str, choices=["completed", "submitted"], default="completed", help="Order to process results from decompiler. Default is 'completed'.")
+    decompiler_options.add_argument("-dmw", "--decompiler-max-workers", type=int, default=None, help="Maximum number of workers to use for decompilation.")
+    decompiler_options.add_argument("-dcs", "--decompiler-chunksize", type=int, default=1, help="Number of files to decompile per thread/process.")
+    decompiler_options.add_argument("-dto", "--decompiler-timeout", type=int, help="Timeout for decompilation in seconds.")
+
+    scanner_options = parser.add_argument_group("Secret Scanner Advanced Options", description="Options for secret scanner.")
+    scanner_options.add_argument("-sct", "--scanner-concurrency-type", type=str, choices=["thread", "process", "main"], default="process", help="Type of concurrency to use for scanning. Default is 'process'.")
+    scanner_options.add_argument("-sro", "--scanner-results-order", type=str, choices=["completed", "submitted"], default="completed", help="Order to process results from scanner. Default is 'completed'.")
+    scanner_options.add_argument("-smw", "--scanner-max-workers", type=int, default=None, help="Maximum number of workers to use for scanning.")
+    scanner_options.add_argument("-scs", "--scanner-chunksize", type=int, default=1, help="Number of files to scan per thread/process.")
+    scanner_options.add_argument("-sto", "--scanner-timeout", type=int, help="Timeout for scanning in seconds.")
+
+    args = parser.parse_args()
+    if not args.quiet:
+        print(BANNER_ART + BANNER_TEXT)
+        print('\033[1mStarting APKscan...\033[0m\n')
+
+    if not args.files:
+        parser.error("No files to scan provided. Please provide at least one file to scan.")
+
+    decompiler_kwargs = {
+        "binaries": {},
+        "enjarify_choice": args.enjarify_choice,
+        "unpack_xapks": args.unpack_xapks,
+        "deobfuscate": args.deobfuscate,
+        "remove_failed_output_dirs": args.cleanup,
+        "suppress_output": args.quiet,
+    }
+    scanner_kwargs = {
+        "secret_locator_files": args.rules,
+    }
+
+    for k, v in vars(args).items():
+        if k.startswith("scanner_"):
+            scanner_kwargs[k[8:]] = v
+        elif k.startswith("decompiler_"):
+            decompiler_kwargs[k[11:]] = v
+        elif k in DEFAULT_CONFIG and v is not False:
+            decompiler_kwargs['binaries'][k] = v
+
+    apk_scanner = APKScanner(
+        decompiler_kwargs=decompiler_kwargs,
+        scanner_kwargs=scanner_kwargs,
+        output_file=args.output,
+        output_format=args.format,
+        groupby=args.groupby,
+        cleanup=args.cleanup
+    )
+
+    try:
+        apk_scanner.decompile_and_scan(args.files)
+    except KeyboardInterrupt:
+        print("\n\033[1;34mWriting output, cleaning up, and exiting...\033[0m")
+    finally:
+        apk_scanner.write_output()
+        apk_scanner.do_cleanup()
+
+    if apk_scanner.secrets_results:
+        print(f"\033[1;32m\nAPKscan done. Secrets saved to {apk_scanner.output_file}\033[0m")
+        exit(0)
+    else:
+        print("\033[1;32m\nAPKscan done. \033[1;31mNo secrets found.\033[0m")
+        exit(1)
+
+if __name__ == "__main__":
+    main()
---
+++ packages/apkscan/apkscan-0.4.0-py3-none-any.whl/apkscan/secret_scanner.py
@@ -0,0 +1,245 @@
+# © 2024 Lucas Faudman.
+# Licensed under the MIT License (see LICENSE for details).
+# For commercial use, see LICENSE for additional terms.
+from dataclasses import dataclass, field
+from typing import Optional, Iterator, Tuple
+from pathlib import Path
+from re import (
+    compile as re_compile,
+    Pattern,
+    IGNORECASE,
+    MULTILINE,
+    DOTALL,
+    ASCII,
+    LOCALE,
+    UNICODE,
+    VERBOSE,
+    TEMPLATE,
+)
+
+from yaml import safe_load as yaml_safe_load, YAMLError  # type: ignore
+from json import loads as json_loads, JSONDecodeError
+
+try:
+    # Use built-in tomllib if python 3.11+ otherwise ignore TOML files
+    from tomllib import loads as toml_loads, TOMLDecodeError
+
+    TOML_SUPPORTED = True
+except ImportError:
+    print("tomllib not found. TOML files will be ignored. Use Python 3.11+ to enable TOML support.")
+    TOML_SUPPORTED = False
+
+from .concurrent_executor import ConcurrentExecutor
+from .included_secret_locators import INCLUDED_SECRET_LOCATOR_FILES  # type: ignore
+
+
+@dataclass
+class SecretLocator:
+    id: str
+    name: str
+    pattern: Pattern
+    secret_group: int | str = 0
+    description: Optional[str] = "No description provided."
+    confidence: Optional[str] = "Unknown"
+    severity: Optional[str] = "Unknown"
+    tags: list[str] = field(default_factory=list)
+
+    def __hash__(self) -> int:
+        return hash(self.pattern)
+
+    def __eq__(self, other: object) -> bool:
+        if not isinstance(other, SecretLocator):
+            return NotImplemented
+        return self.pattern == other.pattern
+
+
+@dataclass
+class SecretResult:
+    secret: bytes
+    file_path: Path
+    line_number: int
+    locator: SecretLocator
+
+    def __hash__(self) -> int:
+        return hash(self.secret)
+
+    def __eq__(self, other: object) -> bool:
+        if not isinstance(other, SecretResult):
+            return NotImplemented
+        return self.secret == other.secret
+
+
+def try_load_json_yaml_toml(file_path: Path) -> Optional[dict | list]:
+    if not file_path.exists():
+        print(f"File not found: {file_path}. Skipping.")
+        return None
+
+    with file_path.open("r") as f:
+        contents = f.read()
+
+    try:
+        if (loaded_json := json_loads(contents)) and not isinstance(loaded_json, str):
+            return loaded_json
+    except JSONDecodeError:
+        print(f"Error loading {file_path} as JSON. Trying YAML.")
+
+    try:
+        if (loaded_yaml := yaml_safe_load(contents)) and not isinstance(loaded_yaml, str):
+            return loaded_yaml
+    except YAMLError:
+        print(f"Error loading {file_path} as YAML. Trying TOML.")
+
+    if TOML_SUPPORTED:
+        try:
+            if (loaded_toml := toml_loads(contents)) and not isinstance(loaded_toml, str):
+                return loaded_toml
+        except TOMLDecodeError:
+            print(f"Error loading {file_path} as TOML. Skipping.")
+
+    return None
+
+
+def compile_str_to_bytes_pattern(pattern_str: str) -> Pattern:
+    flag_notation = {
+        "i": IGNORECASE,
+        "m": MULTILINE,
+        "s": DOTALL,
+        "a": ASCII,
+        "l": LOCALE,
+        "u": UNICODE,
+        "x": VERBOSE,
+        "t": TEMPLATE,
+    }
+
+    flags = 0
+    for flag_char, flag in flag_notation.items():
+        if f"(?{flag_char})" in pattern_str or f"(?-{flag_char})" in pattern_str:
+            flags |= flag
+            pattern_str = pattern_str.replace(
+                f"(?{flag_char})",
+                "",
+            ).replace(f"(?-{flag_char})", "")
+    return re_compile(pattern_str.encode(), flags)
+
+
+def load_secrets_patterns_db_format(locator_dicts: list[dict]) -> dict[str, SecretLocator]:
+    secret_locators: dict[str, SecretLocator] = {}
+    for locator_dict in locator_dicts:
+        locator_dict = locator_dict["pattern"]
+        pattern_str = locator_dict.pop("regex")
+        locator_dict["pattern"] = compile_str_to_bytes_pattern(pattern_str)
+        locator_dict["id"] = locator_dict["name"].replace(" ", "-").lower()
+        secret_locators[pattern_str] = SecretLocator(**locator_dict)
+
+    return secret_locators
+
+
+def load_gitleaks_format(locator_dicts: list[dict]) -> dict[str, SecretLocator]:
+    secret_locators: dict[str, SecretLocator] = {}
+    for locator_dict in locator_dicts:
+        pattern_str = locator_dict.pop("regex")
+        locator_dict["pattern"] = compile_str_to_bytes_pattern(pattern_str)
+        locator_dict["name"] = locator_dict["id"].replace("-", " ").title()
+        locator_dict["secret_group"] = locator_dict.pop("secretGroup", 0)
+        locator_dict["tags"] = locator_dict.pop("keywords", [])
+        locator_dict.pop("entropy", None)
+        locator_dict.pop("allowlist", None)
+        secret_locators[pattern_str] = SecretLocator(**locator_dict)
+
+    return secret_locators
+
+
+def load_secret_locators_format(locator_dicts: list[dict]) -> dict[str, SecretLocator]:
+    secret_locators: dict[str, SecretLocator] = {}
+    for locator_dict in locator_dicts:
+        try:
+            pattern_str = locator_dict.pop("pattern")
+            locator_dict["pattern"] = compile_str_to_bytes_pattern(pattern_str)
+            secret_locators[pattern_str] = SecretLocator(**locator_dict)
+        except Exception as e:
+            print(f"Error loading locator: {locator_dict}. Skipping. {e}")
+
+    return secret_locators
+
+
+def load_simple_key_value_format(simple_locator_dict: dict) -> dict[str, SecretLocator]:
+    secret_locators: dict[str, SecretLocator] = {}
+    for name, pattern_strs in simple_locator_dict.items():
+        if isinstance(pattern_strs, str):
+            pattern_strs = [pattern_strs]
+
+        for i, pattern_str in enumerate(pattern_strs):
+            locator_dict = {
+                "id": name.replace(" ", "-").lower() + (f"-{i}" if i else ""),
+                "name": name + (f" {i}" if i else ""),
+                "pattern": compile_str_to_bytes_pattern(pattern_str),
+            }
+            secret_locators[pattern_str] = SecretLocator(**locator_dict)
+    return secret_locators
+
+
+def load_secret_locators(secret_locator_files: list[Path]) -> dict[str, SecretLocator]:
+    print(f"\nLoading secret locators from {len(secret_locator_files)} files.")
+    secret_locators: dict[str, SecretLocator] = {}
+    for secret_locator_file in secret_locator_files:
+        if not (secret_locator_file_data := try_load_json_yaml_toml(secret_locator_file)):
+            continue
+        if isinstance(secret_locator_file_data, list):
+            secret_locators.update(load_secret_locators_format(secret_locator_file_data))
+        elif locator_dicts := secret_locator_file_data.get("patterns"):
+            secret_locators.update(load_secrets_patterns_db_format(locator_dicts))
+        elif locator_dicts := secret_locator_file_data.get("rules"):
+            secret_locators.update(load_gitleaks_format(locator_dicts))
+        else:
+            secret_locators.update(load_simple_key_value_format(secret_locator_file_data))
+
+    print(f"\nLoaded {len(secret_locators)} secret locators.")
+    return secret_locators
+
+
+def find_secret_locator_files_by_name(secret_locator_files: list[Path]):
+    existing = []
+    for secret_locator_file in secret_locator_files:
+        if secret_locator_file.exists():
+            existing.append(secret_locator_file)
+        elif secret_locator_file.stem in INCLUDED_SECRET_LOCATOR_FILES:
+            existing.append(INCLUDED_SECRET_LOCATOR_FILES[secret_locator_file.stem])
+    return existing
+
+
+class SecretScanner:
+    def __init__(
+        self,
+        **concurrent_executor_kwargs,
+    ) -> None:
+        self.secret_locator_files: list[Path] = []
+        self.secret_locators: dict[str, SecretLocator] = {}
+        self.results: dict[SecretLocator, list[SecretResult]] = {}
+        self.concurrent_executor = ConcurrentExecutor(**{"concurrency_type": "process", **concurrent_executor_kwargs})
+
+    def load_secret_locators(self, secret_locator_files: list[Path]) -> Tuple[dict[str, SecretLocator], list[Path]]:
+        secret_locator_files = find_secret_locator_files_by_name(secret_locator_files)
+        self.secret_locator_files.extend(secret_locator_files)
+        self.secret_locators.update(load_secret_locators(secret_locator_files))
+        return self.secret_locators, self.secret_locator_files
+
+    def iterscan_file(self, file_path: Path) -> Iterator[SecretResult]:
+        with file_path.open("rb") as f:
+            for line_number, line in enumerate(f, start=1):
+                for locator in self.secret_locators.values():
+                    if match := locator.pattern.search(line):
+                        yield SecretResult(
+                            secret=match.group(locator.secret_group),
+                            file_path=file_path,
+                            line_number=line_number,
+                            locator=locator,
+                        )
+
+    def scan_file(self, file_path: Path) -> tuple[Path, list[SecretResult]]:
+        return file_path, list(self.iterscan_file(file_path))
+
+    def scan_concurrently(self, file_paths: Iterator[Path]) -> Iterator[tuple[Path, list[SecretResult]]]:
+        yield from self.concurrent_executor.map(self.scan_file, file_paths)
+
+    def __repr__(self) -> str:
+        return f"SecretScanner(secret_locators={len(self.secret_locators)}, concurrent_executor={self.concurrent_executor}))"
