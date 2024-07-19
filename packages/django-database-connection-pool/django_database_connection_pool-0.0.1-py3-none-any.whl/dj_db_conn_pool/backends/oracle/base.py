b75e47c3b18708dd171a0fd5a2b64254136430d7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-19 04:05:00 +0000

    Add package django-database-connection-pool/0.0.1/django_database_connection_pool-0.0.1-py3-none-any.whl

---
+++ packages/django-database-connection-pool/django_database_connection_pool-0.0.1-py3-none-any.whl/dj_db_conn_pool/backends/oracle/base.py
@@ -0,0 +1,12 @@
+from django.db.backends.oracle import base
+from sqlalchemy.dialects.oracle.base import OracleDialect
+
+from dj_db_conn_pool.core.mixins import PersistentDatabaseWrapperMixin
+
+
+class DatabaseWrapper(PersistentDatabaseWrapperMixin, base.DatabaseWrapper):
+    class SQLAlchemyDialect(OracleDialect):
+        pass
+
+    def _set_dbapi_autocommit(self, autocommit):
+        self.connection.driver_connection.autocommit = int(autocommit)
