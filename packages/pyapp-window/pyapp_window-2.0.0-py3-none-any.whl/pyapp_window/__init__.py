0027a0752fd9fcdf9b9070afd95ea88317dd7305
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-19 03:05:47 +0000

    Add package pyapp-window/2.0.0/pyapp_window-2.0.0-py3-none-any.whl

---
+++ packages/pyapp-window/pyapp_window-2.0.0-py3-none-any.whl/pyapp_window/__init__.py
@@ -0,0 +1,9 @@
+# from .launcher import launch
+# from .launcher import open_widow
+# from .webview_window import open_native_window
+# from .webview_window.util import get_center_pos
+# from .webview_window.util import get_screen_size
+
+from .opener import open_window
+
+__version__ = '2.0.0'
