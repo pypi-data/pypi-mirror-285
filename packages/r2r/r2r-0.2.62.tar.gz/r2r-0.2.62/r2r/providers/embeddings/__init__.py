b4113cb8601670eb00b75f3b15838589367ad006
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-19 16:05:57 +0000

    Add package r2r/0.2.62/r2r-0.2.62.tar.gz

---
+++ packages/r2r/r2r-0.2.62.tar.gz/r2r-0.2.62/r2r/providers/embeddings/__init__.py
@@ -0,0 +1,9 @@
+from .ollama import OllamaEmbeddingProvider
+from .openai import OpenAIEmbeddingProvider
+from .sentence_transformer import SentenceTransformerEmbeddingProvider
+
+__all__ = [
+    "OllamaEmbeddingProvider",
+    "OpenAIEmbeddingProvider",
+    "SentenceTransformerEmbeddingProvider",
+]
