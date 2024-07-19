ef0086f0c8a17034c72d9486b93c95dfb0dc28a2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-19 17:04:59 +0000

    Add package formant/1.177.4/formant-1.177.4-py2.py3-none-any.whl

---
+++ packages/formant/formant-1.177.4-py2.py3-none-any.whl/formant/sdk/cloud/v2/src/resources/views/__init__.py
@@ -0,0 +1,4 @@
+
+from formant.sdk.cloud.v2.formant_admin_api_client.models import ViewTags, Filter, ViewConfiguration, Location, LocationViewConfigurationBasemap, Vector3, NumericViewConfiguration, PointCloudViewConfiguration, ViewLayoutType, PartialViewLayoutType, PartialViewTags, LocationViewConfiguration, ImageViewConfigurationMode, ImageViewConfiguration, LocationViewport, View, LocalizationViewConfiguration, TransformTreeViewConfiguration, PartialView, BitsetViewConfiguration, Camera, ViewListResponse, ViewConfigurationType
+from formant.sdk.cloud.v2.formant_admin_api_client.api.view import view_controller_get_one, view_controller_get_all, view_controller_patch
+from formant.sdk.cloud.v2.formant_admin_api_client.types import *
