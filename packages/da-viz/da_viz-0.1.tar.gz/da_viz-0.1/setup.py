38701f943c0ad06537d9b97a8f08d614cd863255
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-19 03:06:26 +0000

    Add package da-viz/0.1/da_viz-0.1.tar.gz

---
+++ packages/da-viz/da_viz-0.1.tar.gz/da_viz-0.1/setup.py
@@ -0,0 +1,28 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='da_viz',
+    version='0.1',
+    description='A package for visualizing data and machine learning model performance',
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
+    author='Sabyasachi',
+    author_email='sabychakraborty08@gmail.com',
+    url='https://github.com/mixter3011/da_vis.git',  # Replace with your actual GitHub repo URL
+    packages=find_packages(),
+    install_requires=[
+        'matplotlib',
+        'seaborn',
+        'scikit-learn',
+        'shap',
+        'pandas',
+        'dash',
+        'plotly'
+    ],
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+    ],
+    python_requires='>=3.6',
+)
