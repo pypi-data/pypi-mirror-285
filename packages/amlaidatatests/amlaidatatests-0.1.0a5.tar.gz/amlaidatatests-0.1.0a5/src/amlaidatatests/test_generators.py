5305da95541a007487cb70ff6c7f569461d0cbd1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-19 04:06:13 +0000

    Add package amlaidatatests/0.1.0a5/amlaidatatests-0.1.0a5.tar.gz

---
+++ packages/amlaidatatests/amlaidatatests-0.1.0a5.tar.gz/amlaidatatests-0.1.0a5/src/amlaidatatests/test_generators.py
@@ -0,0 +1,237 @@
+from typing import List, Union
+
+from amlaidatatests.config import cfg
+from ibis import Schema, literal
+from ibis.expr.datatypes import Array, DataType, Struct
+
+from amlaidatatests.base import (
+    AbstractBaseTest,
+    AbstractColumnTest,
+    AMLAITestSeverity,
+)
+from amlaidatatests.io import get_valid_currency_codes
+from amlaidatatests.schema.base import ResolvedTableConfig, TableType
+from amlaidatatests.schema.v1.common import CurrencyValue, ValueEntity
+from amlaidatatests.tests.common import (
+    AcceptedRangeTest,
+    ColumnValuesTest,
+    ConsecutiveEntityDeletionsTest,
+    CountFrequencyValues,
+    CountValidityStartTimeChangesTest,
+    FieldNeverNullTest,
+    NoMatchingRows,
+    OrphanDeletionsTest,
+    TableCountTest,
+    TableSchemaTest,
+)
+
+
+ENTITIES = {"CurrencyValue": CurrencyValue(), "ValueEntity": ValueEntity()}
+
+
+def get_entity_tests(
+    table_config: ResolvedTableConfig, entity_name: str
+) -> list[AbstractColumnTest]:
+    """_summary_
+
+    Args:
+        table: _description_
+        entity_name: _description_
+
+    Raises:
+        _description_
+
+    Returns:
+        _description_
+    """
+    if entity_name == "CurrencyValue":
+        return [
+            AcceptedRangeTest(
+                table_config=table_config,
+                column="nanos",
+                min_value=0,
+                max_value=1e9,
+            ),
+            AcceptedRangeTest(
+                table_config=table_config,
+                column="units",
+                min_value=0,
+                max_value=None,
+            ),
+            ColumnValuesTest(
+                table_config=table_config,
+                column="currency_code",
+                values=get_valid_currency_codes(),
+            ),
+        ]
+    raise ValueError(f"Unknown Entity {entity_name}")
+
+
+def get_entity_mutation_tests(
+    table_config: ResolvedTableConfig, entity_ids: List[str]
+) -> list[AbstractColumnTest]:
+    """_summary_
+
+    Args:
+        table: _description_
+        primary_keys: _description_
+
+    Returns:
+        _description_
+    """
+    return [
+        CountValidityStartTimeChangesTest(
+            table_config=table_config, warn=500, error=1000, entity_ids=entity_ids
+        ),
+        ConsecutiveEntityDeletionsTest(
+            table_config=table_config, entity_ids=entity_ids
+        ),
+        OrphanDeletionsTest(table_config=table_config, entity_ids=entity_ids),
+    ]
+
+
+def entity_columns(
+    schema: Union[Schema, Array, Struct, DataType],
+    entity_types=ENTITIES.keys(),
+    path: list[str] = [],
+):
+    """_summary_
+
+    Args:
+        schema: _description_
+        entity_types: _description_. Defaults to ENTITIES.keys().
+        path: _description_. Defaults to [].
+
+    Returns:
+        _description_
+    """
+    fields = {}
+    for n, dtype in schema.items():
+
+        if isinstance(dtype, Struct):
+            subfields = entity_columns(
+                schema=dtype, entity_types=entity_types, path=path.copy() + [n]
+            )
+            fields.update(subfields)
+
+        if isinstance(dtype, Array):
+            subfields = entity_columns(
+                schema=dtype.value_type,
+                entity_types=entity_types,
+                path=path.copy() + [n],
+            )
+            fields.update(subfields)
+
+        for name, entity_obj in ENTITIES.items():
+            if name not in entity_types:
+                continue
+            if dtype == entity_obj:
+                fields[".".join(path + [n])] = name
+
+    return fields
+
+
+def non_nullable_fields(
+    schema: Union[Schema, Array, Struct, DataType], path: list[str] = []
+):  # -> list | dict:
+    """_summary_
+
+    Args:
+        schema: _description_
+        path: _description_. Defaults to [].
+
+    Returns:
+        _type_: _description_
+    """
+    if not isinstance(schema, (Schema, Array, Struct)):
+        return True
+
+    fields = []
+    for n, dtype in schema.items():
+        if isinstance(dtype, Struct):
+            subfields = non_nullable_fields(dtype, path=path.copy() + [n])
+            fields += subfields
+
+        if isinstance(dtype, Array):
+            subfields = non_nullable_fields(dtype.value_type, path=path.copy() + [n])
+            fields += subfields
+        # Also include the object itself, even if it's a struct or array.
+        # This is because the Array or Struct could also be nullable.
+        if not dtype.nullable:
+            fields += [".".join(path + [n])]
+
+    return fields
+
+
+def non_nullable_field_tests(
+    table_config: ResolvedTableConfig,
+) -> list[AbstractBaseTest]:
+    """_summary_
+
+    Args:
+        table_config: _description_
+
+    Returns:
+        _description_
+    """
+    fields = non_nullable_fields(schema=table_config.table.schema())
+    tests = []
+    for f in fields:
+        # _, _field = resolve_field(table=table_config.table, column=f)
+        # field_type = _field.type()
+        # if field_type.is_string():
+        #     tests.append(
+        #         FieldNeverWhitespaceOnlyTest(table_config=table_config, column=f)
+        #     )
+
+        tests.append(FieldNeverNullTest(table_config=table_config, column=f))
+    return tests
+
+
+def get_generic_table_tests(
+    table_config: ResolvedTableConfig,
+    max_rows_factor: int,
+    severity: AMLAITestSeverity = AMLAITestSeverity.ERROR,
+) -> list[AbstractBaseTest]:
+    """Generate tests which are completely generic and agnostic to the underlying table
+
+    Args:
+        table_config: _description_
+        max_rows_factor: _description_
+        severity: _description_. Defaults to AMLAITestSeverity.ERROR.
+
+    Returns:
+        _description_
+    """
+    tests = [
+        TableSchemaTest(table_config),
+        TableCountTest(
+            table_config, severity=severity, max_rows_factor=max_rows_factor
+        ),
+    ]
+    if table_config.table_type in (
+        TableType.CLOSED_ENDED_ENTITY,
+        TableType.OPEN_ENDED_ENTITY,
+    ):
+        tests += [
+            NoMatchingRows(
+                table_config=table_config,
+                column="validity_start_time",
+                expression=lambda t: t.validity_start_time.date()
+                > cfg().interval_end_date,
+            ),
+            CountFrequencyValues(
+                table_config=table_config,
+                column="is_entity_deleted",
+                having=lambda c: c.is_entity_deleted == literal(True),
+                proportion=0.4,
+                severity=AMLAITestSeverity.WARN,
+            ),
+            CountFrequencyValues(
+                table_config=table_config,
+                column="validity_start_time",
+                proportion=0.01,
+                severity=AMLAITestSeverity.WARN,
+            ),
+        ]
+    return tests
---
+++ packages/amlaidatatests/amlaidatatests-0.1.0a5.tar.gz/amlaidatatests-0.1.0a5/src/amlaidatatests/tests/test_risk_case_event_table.py
@@ -0,0 +1,293 @@
+#!/usr/bin/env python
+
+from amlaidatatests.config import cfg
+import ibis
+import pytest
+
+from amlaidatatests.base import AMLAITestSeverity, AbstractTableTest
+from amlaidatatests.schema.utils import resolve_table_config
+from amlaidatatests.test_generators import get_generic_table_tests, non_nullable_fields
+from amlaidatatests.tests import common
+
+TABLE_CONFIG = resolve_table_config("risk_case_event")
+TABLE = TABLE_CONFIG.table
+
+
+@pytest.mark.parametrize(
+    "test", get_generic_table_tests(table_config=TABLE_CONFIG, max_rows_factor=10e6)
+)
+def test_table(connection, test: AbstractTableTest):
+    test(connection=connection)
+
+
+def test_primary_keys(connection):
+    test = common.PrimaryKeyColumnsTest(
+        table_config=TABLE_CONFIG, unique_combination_of_columns=["risk_case_event_id"]
+    )
+    test(connection)
+
+
+# For each column in the schema, check all columns are all present
+@pytest.mark.parametrize("column", TABLE_CONFIG.table.schema().fields.keys())
+def test_column_presence(connection, column: str):
+    test = common.ColumnPresenceTest(table_config=TABLE_CONFIG, column=column)
+    test(connection)
+
+
+# For each column in the schema, check all columns are the correct type
+@pytest.mark.parametrize("column", TABLE_CONFIG.table.schema().fields.keys())
+def test_column_type(connection, column):
+    test = common.ColumnTypeTest(table_config=TABLE_CONFIG, column=column)
+    test(connection)
+
+
+# Validate all fields marked in the schema as being non-nullable are
+# non-nullable. This is in addition to the schema level tests, since it's not
+# possible to enforce an embedded struct is non-nullable.
+
+
+@pytest.mark.parametrize("column", non_nullable_fields(TABLE_CONFIG.table.schema()))
+def test_non_nullable_fields(connection, column):
+    test = common.FieldNeverNullTest(table_config=TABLE_CONFIG, column=column)
+    test(connection)
+
+
+@pytest.mark.parametrize(
+    "test",
+    [
+        common.ColumnValuesTest(
+            values=[
+                "AML_SUSPICIOUS_ACTIVITY_START",
+                "AML_SUSPICIOUS_ACTIVITY_END",
+                "AML_PROCESS_START",
+                "AML_PROCESS_END",
+                "AML_ALERT_GOOGLE",
+                "AML_ALERT_LEGACY",
+                "AML_ALERT_ADHOC",
+                "AML_ALERT_EXPLORATORY",
+                "AML_SAR",
+                "AML_EXTERNAL",
+                "AML_EXIT",
+            ],
+            table_config=TABLE_CONFIG,
+            column="type",
+        )
+    ],
+)
+def test_column_values(connection, test):
+    test(connection)
+
+
+def test_referential_integrity_party(connection):
+    to_table_config = resolve_table_config("party")
+    test = common.ReferentialIntegrityTest(
+        table_config=TABLE_CONFIG, to_table_config=to_table_config, keys=["party_id"]
+    )
+    test(connection)
+
+
+w = ibis.window(
+    group_by=[TABLE.risk_case_id, TABLE.party_id], order_by=TABLE.event_time
+)
+
+
+@pytest.mark.parametrize(
+    "test",
+    [
+        common.NoMatchingRows(
+            column="event_time",
+            table_config=TABLE_CONFIG,
+            expression=lambda t: t.event_time >= cfg().interval_end_date,
+            severity=AMLAITestSeverity.ERROR,
+            test_id="DT011"
+        ),
+    ],
+)
+def test_date_consistency(connection, test):
+    test(connection)
+
+
+def test_event_order(connection):
+    t = common.EventOrder(
+        time_column="event_time",
+        column="type",
+        table_config=TABLE_CONFIG,
+        events=["AML_PROCESS_START", "AML_SAR", "AML_EXIT", "AML_PROCESS_END"],
+        severity=AMLAITestSeverity.ERROR,
+    )
+    t(connection)
+
+
+@pytest.mark.parametrize(
+    "test",
+    [
+        common.ColumnCardinalityTest(
+            column="type",
+            table_config=TABLE_CONFIG,
+            group_by=["party_id", "risk_case_event_id"],
+            where=lambda t: t["type"] == "AML_SUSPICIOUS_ACTIVITY_START",
+            max_number=1,
+            severity=AMLAITestSeverity.ERROR,
+            test_id="P060",
+        ),
+        common.ColumnCardinalityTest(
+            column="type",
+            table_config=TABLE_CONFIG,
+            group_by=["party_id", "risk_case_event_id"],
+            where=lambda t: t["type"] == "AML_SUSPICIOUS_ACTIVITY_END",
+            max_number=1,
+            severity=AMLAITestSeverity.ERROR,
+            test_id="P061",
+        ),
+        common.ColumnCardinalityTest(
+            column="type",
+            table_config=TABLE_CONFIG,
+            group_by=["party_id"],
+            where=lambda t: t["type"] == "AML_EXIT",
+            max_number=1,
+            severity=AMLAITestSeverity.WARN,
+            test_id="P066",
+        ),
+        common.ColumnCardinalityTest(
+            column="risk_case_event_id",
+            table_config=TABLE_CONFIG,
+            group_by=["party_id"],
+            max_number=1000,
+            severity=AMLAITestSeverity.WARN,
+            test_id="P056",
+        ),
+        common.ColumnCardinalityTest(
+            column="risk_case_event_id",
+            table_config=TABLE_CONFIG,
+            group_by=["party_id"],
+            max_number=5000,
+            severity=AMLAITestSeverity.ERROR,
+            test_id="P055",
+        ),
+        common.ColumnCardinalityTest(
+            column="risk_case_event_id",
+            table_config=TABLE_CONFIG,
+            group_by=["risk_case_id"],
+            max_number=1000,
+            severity=AMLAITestSeverity.WARN,
+            test_id="P054",
+        ),
+        common.ColumnCardinalityTest(
+            column="risk_case_event_id",
+            table_config=TABLE_CONFIG,
+            group_by=["risk_case_id"],
+            max_number=5000,
+            severity=AMLAITestSeverity.ERROR,
+            test_id="P053",
+        ),
+        common.CountFrequencyValues(
+            column="event_time",
+            table_config=TABLE_CONFIG,
+            max_number=100e3,
+            severity=AMLAITestSeverity.WARN,
+            test_id="P038",
+        ),
+        common.VerifyTypedValuePresence(
+            column="type",
+            table_config=TABLE_CONFIG,
+            min_number=1,
+            group_by=["party_id"],
+            value="AML_PROCESS_START",
+            test_id="P039",
+        ),
+        common.VerifyTypedValuePresence(
+            column="type",
+            table_config=TABLE_CONFIG,
+            min_number=1,
+            group_by=["party_id"],
+            value="AML_PROCESS_END",
+            test_id="P040",
+        ),
+        common.VerifyTypedValuePresence(
+            column="type",
+            table_config=TABLE_CONFIG,
+            min_number=1,
+            group_by=["party_id"],
+            value="AML_EXIT",
+            test_id="P041",
+        ),
+        common.VerifyTypedValuePresence(
+            column="type",
+            table_config=TABLE_CONFIG,
+            min_number=1,
+            group_by=["party_id"],
+            value="AML_SAR",
+            test_id="P043",
+        ),
+        common.VerifyTypedValuePresence(
+            column="type",
+            table_config=TABLE_CONFIG,
+            max_proportion=1,
+            group_by=["party_id"],
+            test_id="P042",
+            value="AML_EXIT",
+        ),
+        common.VerifyTypedValuePresence(
+            column="type",
+            table_config=TABLE_CONFIG,
+            max_proportion=1,
+            group_by=["party_id"],
+            test_id="P044",
+            value="AML_EXIT",
+        ),
+        # TODO: This is not a precise test. We are comparing the number
+        # of values of party_id where AML_PROCESS_START over
+        # the number where AML_EXIT. This could be a co-incidence, but is unlikely
+        common.VerifyTypedValuePresence(
+            column="type",
+            table_config=TABLE_CONFIG,
+            min_proportion=1,
+            group_by=["party_id", "risk_case_id"],
+            where=lambda t: t.type == "AML_EXIT",
+            test_id="P048",
+            value="AML_PROCESS_START",
+        ),
+        common.CountFrequencyValues(
+            column="type",
+            table_config=TABLE_CONFIG,
+            group_by=["risk_case_id", "party_id"],
+            max_number=1,
+            severity=AMLAITestSeverity.ERROR,
+            having=lambda t: t["type"] == "AML_PROCESS_START",
+            test_id="P045",
+        ),
+        common.CountFrequencyValues(
+            column="type",
+            table_config=TABLE_CONFIG,
+            group_by=["risk_case_id", "party_id"],
+            max_number=1,
+            severity=AMLAITestSeverity.ERROR,
+            having=lambda t: t["type"] == "AML_PROCESS_END",
+            test_id="P046",
+        ),
+        common.CountFrequencyValues(
+            column="type",
+            table_config=TABLE_CONFIG,
+            group_by=["risk_case_id", "party_id"],
+            max_number=1,
+            severity=AMLAITestSeverity.ERROR,
+            having=lambda t: t["type"] == "AML_EXIT",
+            test_id="P047",
+        ),
+        common.VerifyTypedValuePresence(
+            column="type",
+            table_config=TABLE_CONFIG,
+            min_proportion=1,
+            group_by=["party_id", "risk_case_id"],
+            where=lambda t: t.type == "AML_SUSPICIOUS_ACTIVITY_END",
+            test_id="P062",
+            value="AML_SUSPICIOUS_ACTIVITY_START",
+        ),
+    ],
+)
+def test_profiling(connection, test):
+    test(connection)
+
+
+if __name__ == "__main__":
+    retcode = pytest.main()
