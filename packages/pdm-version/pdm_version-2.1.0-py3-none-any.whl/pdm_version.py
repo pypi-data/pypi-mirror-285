b91b60722d5602e85775f2d3c92d7454434a659f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-19 18:05:01 +0000

    Add package pdm-version/2.1.0/pdm_version-2.1.0-py3-none-any.whl

---
+++ packages/pdm-version/pdm_version-2.1.0-py3-none-any.whl/pdm_version.py
@@ -0,0 +1,28 @@
+import argparse
+
+from pdm.cli.commands.base import BaseCommand
+from pdm.project import Project
+
+
+class VersionCommand(BaseCommand):
+    """Set project.version to the given version"""
+
+    def add_arguments(self, parser: argparse.ArgumentParser) -> None:
+        """Manipulate the argument parser to add more arguments"""
+        parser.add_argument("version", nargs="?")
+
+    def handle(self, project: Project, options: argparse.Namespace) -> None:
+        """The command handler function.
+
+        :param project: the pdm project instance
+        :param options: the parsed Namespace object
+        """
+        if not options.version:
+            print(project.pyproject.metadata["version"])
+        else:
+            project.pyproject.metadata["version"] = options.version
+            project.pyproject.write()
+
+
+def version(core):
+    core.register_command(VersionCommand, "version")
