316c8c32711e8b0e1a183f1d3638b46a88b890cb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-19 03:05:09 +0000

    Add package nestipy-alchemy/0.1.4/nestipy_alchemy-0.1.4.tar.gz

---
+++ packages/nestipy-alchemy/nestipy_alchemy-0.1.4.tar.gz/nestipy_alchemy-0.1.4/nestipy_alchemy/converter.py
@@ -0,0 +1,32 @@
+from typing import Container, Optional, Type
+
+from pydantic import BaseModel, ConfigDict, create_model
+
+orm_config = ConfigDict(from_attributes=True)
+
+
+def sqlalchemy_to_pydantic(
+        db_model: Type, *, config: Type = None, exclude: Container[str] = None
+) -> Type[BaseModel]:
+    table = db_model.metadata.tables[db_model.__tablename__]
+    fields = {}
+    exclude = exclude or []
+    for column in table.columns:
+        name = column.name
+        if name in exclude:
+            continue
+        python_type: Optional[type] = None
+        if hasattr(column.type, "impl"):
+            if hasattr(column.type.impl, "python_type"):
+                python_type = column.type.impl.python_type
+        elif hasattr(column.type, "python_type"):
+            python_type = column.type.python_type
+        assert python_type, f"Could not infer python_type for {column}"
+
+        if not column.nullable:
+            fields[name] = (python_type, ...)
+        else:
+            fields[name] = (Optional[python_type], None)
+
+    pydantic_model = create_model(db_model.__name__, __config__=config or orm_config, **fields)
+    return pydantic_model
