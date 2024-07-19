03170795616a5f1f3985ab1f55ce4e8ae6a88f47
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-19 03:05:09 +0000

    Add package vpk-custom-functions/0.0.5/vpk_custom_functions-0.0.5.tar.gz

---
+++ packages/vpk-custom-functions/vpk_custom_functions-0.0.5.tar.gz/vpk_custom_functions-0.0.5/vpk_custom_functions/main.py
@@ -0,0 +1,22 @@
+from IPython import get_ipython
+import pandas as pd
+
+def general_settings():
+    '''
+    Function to customize settings for IPython environment.
+    '''
+    # Set the width of dataframe previews to maximum
+    pd.set_option('display.max_rows', None)
+    pd.set_option('display.max_columns', None)
+    pd.set_option('display.float_format', '{:20,.2f}'.format)
+    pd.set_option('display.max_colwidth', None)
+
+    # Enable autoreloading of external functions
+    ip = get_ipython()
+    ip.magic("load_ext autoreload")
+    ip.magic("autoreload 2")
+
+    print("DataFrame width maximized!")
+    print("Autoreloading of functions enabled!")
+
+
