af316b758810927f0a8f26b71fdbf897a32b7a1b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-19 04:05:45 +0000

    Add package rag-kmk/0.0.5/rag_kmk-0.0.5-py2.py3-none-any.whl

---
+++ packages/rag-kmk/rag_kmk-0.0.5-py2.py3-none-any.whl/rag_kmk/__init__.py
@@ -0,0 +1,6 @@
+"""Top-level package for rag-kmk."""
+
+__author__ = """Murat Karakaya"""
+__email__ = "kmkarakaya@gmail.com"
+__version__ = "0.0.5"
+__all__ = ['hello_world', 'info', 'get_version']
