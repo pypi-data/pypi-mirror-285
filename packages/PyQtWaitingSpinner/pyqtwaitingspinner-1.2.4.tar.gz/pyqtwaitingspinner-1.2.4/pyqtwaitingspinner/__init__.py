540185594bf69509926259f9b17f352467cab9f8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-19 04:05:45 +0000

    Add package PyQtWaitingSpinner/1.2.4/pyqtwaitingspinner-1.2.4.tar.gz

---
+++ packages/PyQtWaitingSpinner/pyqtwaitingspinner-1.2.4.tar.gz/pyqtwaitingspinner-1.2.4/pyqtwaitingspinner/__init__.py
@@ -0,0 +1,10 @@
+from pyqtwaitingspinner.configurator import SpinnerConfigurator
+from pyqtwaitingspinner.parameters import SpinnerParameters, from_file
+from pyqtwaitingspinner.spinner import WaitingSpinner
+
+__all__ = [
+    "from_file",
+    "WaitingSpinner",
+    "SpinnerParameters",
+    "SpinnerConfigurator",
+]
