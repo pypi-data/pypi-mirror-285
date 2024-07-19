852fae10fad583ebcb29b27066e03bf5e350f200
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-19 14:05:59 +0000

    Add package EpomakerController/0.0.5/epomakercontroller-0.0.5.tar.gz

---
+++ packages/EpomakerController/epomakercontroller-0.0.5.tar.gz/epomakercontroller-0.0.5/README.md
@@ -0,0 +1,202 @@
+# Epomakercontroller
+![PyPI](https://img.shields.io/pypi/v/EpomakerController.svg)
+
+A CLI tool to interact with an [Epomaker RT100](https://epomaker.com/products/epomaker-rt100) keyboard that comes with a small screen attached. Some features will probably also work with the TH80.
+
+This project is still very rough around the edges!
+
+NOTE!!!: This currently only works over the USB connection!
+
+## Features
+
+- Upload images to the RT100 screen
+- Send arbitrary numbers to the CPU and Temeprature displays on the screen
+- Set RGB patterns on the keyboard
+- Set individual key colours
+- Start a daemon to continuously update the CPU and temperature on the screen
+
+## Requirements
+
+I am using poetry v1.7.1
+
+## Installation
+I suggest using conda or a python virtual env, tested on python 3.10 and 3.12:
+
+_Epomakercontroller_ package is available on [PyPi](https://pypi.org/project/EpomakerController/)
+
+```console
+$ python3.10 -m venv epomaker
+$ source epomaker/bin/activate
+$ pip install EpomakerController
+```
+
+You can also install _Epomakercontroller_ via poetry
+
+```console
+$ sudo apt install poetry
+$ git clone https://github.com/strodgers/epomaker-controller
+$ cd epomaker-controller
+$ python3.10 -m venv epomaker
+$ source epomaker/bin/activate
+$ poetry install --only main
+```
+
+## Usage
+
+```console
+$ epomakercontroller
+Usage: epomakercontroller [OPTIONS] COMMAND [ARGS]...
+
+  A simple CLI for the EpomakerController.
+
+Options:
+  --help  Show this message and exit.
+
+Commands:
+  cycle-light-modes  Cycle through the light modes.
+  dev                Various dev tools.
+  list-temp-devices  List available temperature devices.
+  send-cpu           Send CPU usage percentage to the Epomaker screen.
+  send-temperature   Send temperature to the Epomaker screen.
+  send-time          Send the current time to the Epomaker device.
+  set-rgb-all-keys   Set RGB colour for all keys.
+  start-daemon       Start a daemon to update the CPU usage and...
+  upload-image       Upload an image to the Epomaker device.
+```
+
+### Permissions
+In order to communicate with your keyboard without having to use sudo, you can
+use the tool to generate a udev rule for the connected RT100:
+```console
+epomakercontroller dev --udev
+```
+You will need to enter your password when prompted.
+
+### CPU/Temperature display
+The temperature on the Epomaker screen is supposed to be for the weather, but I thought it was more
+useful to display the temperature of some device on the host machine. You will need to find out
+the label used by a sensor on your machine, which you can do by:
+```console
+$ epomakercontroller list-temp-devices
+
+Temperature key: nvme
+  Label: Composite
+  Current: 35.85°C
+  High: 82.85°C
+  Critical: 89.85°C
+  Label: Composite
+  Current: 44.85°C
+  High: 82.85°C
+  Critical: 89.85°C
+
+Temperature key: amdgpu
+  Label: edge
+  Current: 40.0°C
+  High: N/A°C
+  Critical: N/A°C
+
+Temperature key: k10temp
+  Label: Tctl
+  Current: 44.5°C
+  High: N/A°C
+  Critical: N/A°C
+  Label: Tccd1
+  Current: 39.0°C
+  High: N/A°C
+  Critical: N/A°C
+
+Temperature key: mt7921_phy0
+  Label: N/A
+  Current: 28.0°C
+  High: N/A°C
+  Critical: N/A°C
+```
+
+Then you can start the daemon with the corresponding label, eg:
+```console
+epomakercontroller start-daemon k10temp
+```
+
+Alternatively leave the label blank to disable and only do CPU usage:
+```console
+epomakercontroller start-daemon
+```
+
+The daemon will also update the date and time once when it starts
+
+### Note about interface numbers
+
+I only have my own keyboard to go by, but as far as I can tell there are 3 available HID
+interfaces available when the keyboard is plugged in over USB. All 3 of them will work with
+this controller HOWEVER using interface 0 interferes with regular usage of the keyboard (eg typing).
+
+I have found interface 1 to be the best to use, but some commands take an option for you to set the
+interface number manually if you need to eg:
+```console
+epomakercontroller start-daemon --interface 2
+```
+
+You can also print all the available information about the connected keyboard using the 'dev'
+command. Here is an example of the output from my own keyboard:
+```console
+epomakercontroller dev --print
+Printing all available information about the connected keyboard.
+WARNING: If this program errors out or you cancel early, the keyboard
+              may become unresponsive. It should work fine again if you unplug and plug
+               it back in!
+[{'interface_number': 0,
+  'manufacturer_string': 'ROYUAN',
+  'path': b'5-2.3:1.0',
+  'product_id': 16400,
+  'product_string': 'RT100 Wired',
+  'release_number': 1281,
+  'serial_number': '',
+  'usage': 0,
+  'usage_page': 0,
+  'vendor_id': 12625},
+ {'interface_number': 1,
+  'manufacturer_string': 'ROYUAN',
+  'path': b'5-2.3:1.1',
+  'product_id': 16400,
+  'product_string': 'RT100 Wired',
+  'release_number': 1281,
+  'serial_number': '',
+  'usage': 0,
+  'usage_page': 0,
+  'vendor_id': 12625},
+ {'interface_number': 2,
+  'manufacturer_string': 'ROYUAN',
+  'path': b'5-2.3:1.2',
+  'product_id': 16400,
+  'product_string': 'RT100 Wired',
+  'release_number': 1281,
+  'serial_number': '',
+  'usage': 0,
+  'usage_page': 0,
+  'vendor_id': 12625}]
+```
+
+## TODO
+- Support bluetooth/2.4Ghz
+- Upload GIFs
+- Macros
+
+## Contributing
+
+Contributions are very welcome.
+
+## License
+
+Distributed under the terms of the [MIT license][license],
+_Epomakercontroller_ is free and open source software.
+
+## Issues
+
+If you encounter any problems,
+please [file an issue] along with a detailed description.
+
+<!-- github-only -->
+
+[license]: https://github.com/imp3ga/EpomakerController/blob/main/LICENSE
+[contributor guide]: https://github.com/imp3ga/EpomakerController/blob/main/CONTRIBUTING.md
+[command-line reference]: https://EpomakerController.readthedocs.io/en/latest/usage.html
---
+++ packages/EpomakerController/epomakercontroller-0.0.5.tar.gz/epomakercontroller-0.0.5/src/epomakercontroller/cli.py
@@ -0,0 +1,276 @@
+# src/epomakercontroller/cli.py
+"""Simple CLI for the EpomakerController package."""
+
+import time
+import click
+from epomakercontroller.commands import (
+    EpomakerKeyRGBCommand,
+    EpomakerProfileCommand,
+)
+from epomakercontroller.commands.data.constants import ALL_KEYBOARD_KEYS, Profile
+from epomakercontroller import EpomakerController
+import psutil
+
+# From testing using my own keyboard, this interface works for the controller whilst
+# not interfering with the keyboard's normal operation.
+INTERFACE_NUMBER = 1
+
+@click.group()
+def cli() -> None:
+    """A simple CLI for the EpomakerController."""
+    pass
+
+
+@cli.command()
+@click.argument("image_path", type=click.Path(exists=True))
+def upload_image(image_path: str) -> None:
+    """Upload an image to the Epomaker device.
+
+    Args:
+        image_path (str): The path to the image file to upload.
+    """
+    try:
+        # Need to use interface 0 for this
+        controller = EpomakerController(interface_number=0, dry_run=False)
+        if controller.open_device():
+            print("Uploading, you should see the status on the keyboard screen.\n"
+                  "The keyboard will be unresponsive during this process.")
+            controller.send_image(image_path)
+            click.echo("Image uploaded successfully.")
+        controller.close_device()
+    except Exception as e:
+        click.echo(f"Failed to upload image: {e}")
+
+
+@cli.command()
+@click.argument("r", type=int)
+@click.argument("g", type=int)
+@click.argument("b", type=int)
+@click.option("-i", "--interface", type=int, default=INTERFACE_NUMBER)
+def set_rgb_all_keys(r: int, g: int, b: int, interface: int) -> None:
+    """Set RGB colour for all keys.
+
+    Args:
+        r (int): The red value (0-255).
+        g (int): The green value (0-255).
+        b (int): The blue value (0-255).
+        interface (int): The HID interface number to use.
+    """
+    try:
+        mapping = EpomakerKeyRGBCommand.KeyMap()
+        for key in ALL_KEYBOARD_KEYS:
+            mapping[key] = (r, g, b)
+        frames = [EpomakerKeyRGBCommand.KeyboardRGBFrame(mapping, 50)]
+        controller = EpomakerController(interface, dry_run=False)
+        if controller.open_device():
+            controller.send_keys(frames)
+            click.echo(f"All keys set to RGB({r}, {g}, {b}) successfully.")
+        controller.close_device()
+    except Exception as e:
+        click.echo(f"Failed to set RGB for all keys: {e}")
+
+
+@cli.command()
+@click.option("-i", "--interface", type=int, default=INTERFACE_NUMBER)
+def cycle_light_modes(interface: int) -> None:
+    """Cycle through the light modes.
+
+    Args:
+        interface (int): The HID interface number to use.
+    """
+    try:
+        controller = EpomakerController(interface, dry_run=False)
+        if not controller.open_device():
+            click.echo("Failed to open device.")
+            return
+
+        print(f"Cycling through {len(Profile.Mode)} modes, waiting 5 seconds on each")
+        counter = 1
+        for mode in Profile.Mode:
+            profile = Profile(
+                mode=mode,
+                speed=Profile.Speed.DEFAULT,
+                brightness=Profile.Brightness.DEFAULT,
+                dazzle=Profile.Dazzle.OFF,
+                option=Profile.Option.OFF,
+                rgb=(180, 180, 180),
+            )
+            command = EpomakerProfileCommand.EpomakerProfileCommand(profile)
+            controller._send_command(command)
+            click.echo(
+                f"[{counter}/{len(Profile.Mode)}] Cycled to light mode: {mode.name}"
+            )
+            time.sleep(5)
+            counter += 1
+
+        controller.close_device()
+        click.echo("Cycled through all light modes successfully.")
+    except Exception as e:
+        click.echo(f"Failed to cycle light modes: {e}")
+
+
+@cli.command()
+@click.option("-i", "--interface", type=int, default=INTERFACE_NUMBER)
+def send_time(interface: int) -> None:
+    """Send the current time to the Epomaker device.
+
+    Args:
+        interface (int): The HID interface number to use.
+    """
+    try:
+        controller = EpomakerController(interface, dry_run=False)
+        if controller.open_device():
+            controller.send_time()
+            click.echo("Time sent successfully.")
+        controller.close_device()
+    except Exception as e:
+        click.echo(f"Failed to send time: {e}")
+
+
+@cli.command()
+@click.argument("temperature", type=int)
+@click.option("-i", "--interface", type=int, default=INTERFACE_NUMBER)
+def send_temperature(temperature: int, interface: int) -> None:
+    """Send temperature to the Epomaker screen.
+
+    Args:
+        temperature (int): The temperature value in C (0-100).
+        interface (int): The HID interface number to use.
+    """
+    try:
+        controller = EpomakerController(interface, dry_run=False)
+        if controller.open_device():
+            controller.send_temperature(temperature)
+            click.echo("Temperature sent successfully.")
+        controller.close_device()
+    except Exception as e:
+        click.echo(f"Failed to send temperature: {e}")
+
+
+@cli.command()
+@click.argument("cpu", type=int)
+@click.option("-i", "--interface", type=int, default=INTERFACE_NUMBER)
+def send_cpu(cpu: int, interface: int) -> None:
+    """Send CPU usage percentage to the Epomaker screen.
+
+    Args:
+        cpu (int): The CPU usage percentage (0-100).
+        interface (int): The HID interface number to use.
+    """
+    try:
+        controller = EpomakerController(interface, dry_run=False)
+        if controller.open_device():
+            controller.send_cpu(cpu)
+            click.echo("CPU usage sent successfully.")
+        controller.close_device()
+    except Exception as e:
+        click.echo(f"Failed to send CPU usage: {e}")
+
+
+@cli.command()
+@click.argument("temp_key", type=str, required=False)
+@click.option("-i", "--interface", type=int, default=INTERFACE_NUMBER)
+def start_daemon(temp_key: str | None, interface: int) -> None:
+    """Start a daemon to update the CPU usage and optionally a temperature.
+
+    Args:
+        temp_key (str): A label corresponding to the device to monitor.
+        interface (int): The HID interface number to use.
+    """
+    try:
+        controller = EpomakerController(interface, dry_run=False)
+        first = True
+        while True:
+            if not controller.open_device():
+                click.echo("Failed to open device.")
+                return
+            if first:
+                # Set current time and date
+                controller.send_time()
+                first = False
+
+            # Get CPU usage
+            cpu_usage = psutil.cpu_percent(interval=1)
+            cpu_usage_rounded = round(cpu_usage)
+            click.echo(f"CPU Usage: {cpu_usage}%, sending {cpu_usage_rounded}%")
+            controller.send_cpu(int(cpu_usage_rounded), from_daemon=True)
+
+            # Get device temperature using the provided key
+            if temp_key:
+                try:
+                    temps = psutil.sensors_temperatures()
+                    if temp_key in temps:
+                        cpu_temp = temps[temp_key][0].current
+                        rounded_cpu_temp = round(cpu_temp)
+                        click.echo(
+                            f"CPU Temperature ({temp_key}): {rounded_cpu_temp}°C"
+                        )
+                        controller.send_temperature(rounded_cpu_temp)
+                    else:
+                        available_keys = list(temps.keys())
+                        click.echo(
+                            (f"Temperature key {temp_key!r} not found."
+                             f"Available keys: {available_keys}")
+                        )
+                except AttributeError:
+                    click.echo("Temperature monitoring not supported on this system.")
+
+            controller.close_device()
+
+            time.sleep(3)
+    except KeyboardInterrupt:
+        click.echo("Daemon interrupted by user.")
+    except Exception as e:
+        click.echo(f"Failed to start daemon: {e}")
+
+
+@cli.command()
+def list_temp_devices() -> None:
+    """List available temperature devices."""
+    try:
+        temps = psutil.sensors_temperatures()
+        if not temps:
+            click.echo("No temperature sensors found.")
+            return
+
+        for key, entries in temps.items():
+            click.echo(f"\nTemperature key: {key}")
+            for entry in entries:
+                click.echo(f"  Label: {entry.label or 'N/A'}")
+                click.echo(f"  Current: {entry.current}°C")
+                click.echo(f"  High: {entry.high or 'N/A'}°C")
+                click.echo(f"  Critical: {entry.critical or 'N/A'}°C")
+    except AttributeError:
+        click.echo("Temperature monitoring not supported on this system.")
+
+
+@cli.command()
+@click.option("--print", "print_info", is_flag=True, help="Print all available information about the connected keyboard.")
+@click.option("--udev", "generate_udev", is_flag=True, help="Generate a udev rule for the connected keyboard.")
+def dev(print_info: bool, generate_udev: bool) -> None:
+    """Various dev tools.
+
+    Args:
+        print_info (bool): Print information about the connected keyboard.
+        generate_udev (bool): Generate a udev rule for the connected keyboard.
+    """
+    if print_info:
+        click.echo("Printing all available information about the connected keyboard.")
+        controller = EpomakerController(INTERFACE_NUMBER, dry_run=False)
+        if not controller.open_device(only_info=True):
+            click.echo("Failed to open device.")
+            return
+    elif generate_udev:
+        click.echo("Generating udev rule for the connected keyboard.")
+        # Init controller to get the PID
+        controller = EpomakerController(INTERFACE_NUMBER, dry_run=False)
+        if not controller.open_device(only_info=True):
+            click.echo("Failed to open device.")
+            return
+        controller.generate_udev_rule()
+    else:
+        click.echo("No dev tool specified.")
+
+
+if __name__ == "__main__":
+    cli()
