63bec4dd4c7f3c0f8055a9761a4e46bffaddf3b5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-19 17:04:55 +0000

    Add package purr-geographix/0.1.0/purr_geographix-0.1.0-py3-none-any.whl

---
+++ packages/purr-geographix/purr_geographix-0.1.0-py3-none-any.whl/purr_geographix/core/database.py
@@ -0,0 +1,20 @@
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+import purr_geographix.core.models as models
+
+SQLALCHEMY_DATABASE_URL = "sqlite:///./purrio.sqlite"
+
+engine = create_engine(
+    SQLALCHEMY_DATABASE_URL, connect_args={"check_same_thread": False}
+)
+SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+
+models.Base.metadata.create_all(bind=engine)
+
+
+def get_db():
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
