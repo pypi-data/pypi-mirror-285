66cdbe7bc62744d8060bc932f7d762b3b77b91d2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-19 17:04:41 +0000

    Add package odoo14-addon-sale-promotion-discount-in-field/14.0.1.1.2.dev3/odoo14_addon_sale_promotion_discount_in_field-14.0.1.1.2.dev3-py3-none-any.whl

---
+++ packages/odoo14-addon-sale-promotion-discount-in-field/odoo14_addon_sale_promotion_discount_in_field-14.0.1.1.2.dev3-py3-none-any.whl/odoo/addons/sale_promotion_discount_in_field/__manifest__.py
@@ -0,0 +1,22 @@
+# Copyright 2022 Ooops404
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
+
+{
+    "name": "Coupon Promotion Discount in Field",
+    "version": "14.0.1.1.1",
+    "summary": "Put promotion discount into discount field",
+    "author": "Ooops, Cetmix, Odoo Community Association (OCA)",
+    "maintainers": ["CetmixGitDrone", "Volodiay622"],
+    "license": "AGPL-3",
+    "category": "Sales Management",
+    "website": "https://github.com/OCA/sale-promotion",
+    "depends": ["sale_coupon"],
+    "external_dependencies": {},
+    "demo": [],
+    "data": [
+        "views/coupon_program_view.xml",
+    ],
+    "qweb": [],
+    "installable": True,
+    "application": False,
+}
