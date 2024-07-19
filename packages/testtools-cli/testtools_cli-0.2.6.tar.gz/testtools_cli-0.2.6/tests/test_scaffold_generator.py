21edeb7628b0929d327888315cc84e8b8075833b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-19 03:05:23 +0000

    Add package testtools-cli/0.2.6/testtools_cli-0.2.6.tar.gz

---
+++ packages/testtools-cli/testtools_cli-0.2.6.tar.gz/testtools_cli-0.2.6/tests/test_scaffold_generator.py
@@ -0,0 +1,6 @@
+from src.testtools_cli.generator.scaffold_generator import ScaffoldGenerator, LangType
+
+
+def test_create_pytest_scaffold() -> None:
+    gen = ScaffoldGenerator(lang=LangType.Python, testtool_name="pytest", workdir="/tmp/tool/pytest")
+    gen.generate()
