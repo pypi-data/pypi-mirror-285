aef3460ddb4d14f642ce79d2e01e33c20fe224dd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-19 03:05:57 +0000

    Add package pygcloud/0.0.50/pygcloud-0.0.50-py3-none-any.whl

---
+++ packages/pygcloud/pygcloud-0.0.50-py3-none-any.whl/pygcloud/gcp/services/__init__.py
@@ -0,0 +1,16 @@
+__all__ = [
+    "addresses",
+    "backend",
+    "certificate",
+    "firestore",
+    "fwd_rules",
+    "iam",
+    "identity",
+    "proxies",
+    "pubsub",
+    "run",
+    "scheduler",
+    "services",
+    "storage",
+    "urlmap"
+]
