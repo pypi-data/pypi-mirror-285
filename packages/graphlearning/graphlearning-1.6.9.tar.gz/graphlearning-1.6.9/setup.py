2d89fbd0e2a07225110c41cacda0c7dcaa5d28c0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-19 03:05:51 +0000

    Add package graphlearning/1.6.9/graphlearning-1.6.9.tar.gz

---
+++ packages/graphlearning/graphlearning-1.6.9.tar.gz/graphlearning-1.6.9/setup.py
@@ -0,0 +1,17 @@
+from setuptools import setup, Extension
+import numpy 
+
+setup_args = dict(
+    ext_modules=[Extension('graphlearning.cextensions', 
+                            sources=['c_code/cextensions.c',
+                                     'c_code/lp_iterate.c',
+                                     'c_code/hjsolvers.c',
+                                     'c_code/memory_allocation.c',
+                                     'c_code/mnist_benchmark.c',
+                                     'c_code/mbo_speedy_volume_preserving.c'],
+                            include_dirs=[numpy.get_include()],
+                            #extra_compile_args = ['-Ofast','-std=c11'],
+                            extra_compile_args = ['-Ofast'],
+                            extra_link_args = ['-lm'])])
+
+setup(**setup_args)
