dc20bbc2ccc90985f933f192f9bd51c9ef552b94
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-19 16:05:57 +0000

    Add package ElgatoWavePy/0.1/ElgatoWavePy-0.1-py3-none-any.whl

---
+++ packages/ElgatoWavePy/ElgatoWavePy-0.1-py3-none-any.whl/ElgatoWavePy/__init__.py
@@ -0,0 +1,8 @@
+# PyWaveLink/__init__.py
+
+from .SetVolLoc import SetVolumeLocal
+from .SetVolStre import SetVolumeStream
+from .SetOutput import SetOutput
+from .SetVoliin import SetVolumeInput
+from .SetMuteOut import SetMuteOutput
+from.SetMuteIn import SetMuteInput
