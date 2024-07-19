0c7bf6bb51db3fac2a00c83b4f09e37f0f82c218
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-19 16:06:13 +0000

    Add package alertmanagermeshtastic/2024.7.18.7/alertmanagermeshtastic-2024.7.18.7.tar.gz

---
+++ packages/alertmanagermeshtastic/alertmanagermeshtastic-2024.7.18.7.tar.gz/alertmanagermeshtastic-2024.7.18.7/src/alertmanagermeshtastic/signals.py
@@ -0,0 +1,17 @@
+"""
+alertmanagermeshtastic.signals
+~~~~~~~~~~~~~~~~~~~
+
+Signals
+
+:Copyright: 2007-2022 Jochen Kupperschmidt
+:Copyright: 2023 Alexander Volz
+:License: MIT, see LICENSE for details.
+"""
+
+from blinker import Signal
+
+
+message_received = Signal()
+queue_size_updated = Signal('queue_size')
+meshtastic_connected = Signal('meshtastic_connected')
