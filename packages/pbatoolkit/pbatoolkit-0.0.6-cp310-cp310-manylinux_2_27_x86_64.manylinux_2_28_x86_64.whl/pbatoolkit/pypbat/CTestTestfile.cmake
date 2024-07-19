58e282154de8b58e059d1aa12a4584e9e5431a6d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-19 15:05:45 +0000

    Add package pbatoolkit/0.0.6/pbatoolkit-0.0.6-cp310-cp310-manylinux_2_27_x86_64.manylinux_2_28_x86_64.whl

---
+++ packages/pbatoolkit/pbatoolkit-0.0.6-cp310-cp310-manylinux_2_27_x86_64.manylinux_2_28_x86_64.whl/pbatoolkit/pypbat/CTestTestfile.cmake
@@ -0,0 +1,10 @@
+# CMake generated Testfile for 
+# Source directory: /project/bindings/pypbat
+# Build directory: /project/build/cp310-cp310-linux_x86_64/bindings/pypbat
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+subdirs("fem")
+subdirs("geometry")
+subdirs("math")
+subdirs("profiling")
