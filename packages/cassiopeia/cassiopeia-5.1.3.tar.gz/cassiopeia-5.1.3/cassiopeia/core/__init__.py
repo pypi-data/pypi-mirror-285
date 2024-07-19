4f12fa9d232e1d3ffa48cf2bfb743934b7984481
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-19 17:04:58 +0000

    Add package cassiopeia/5.1.3/cassiopeia-5.1.3.tar.gz

---
+++ packages/cassiopeia/cassiopeia-5.1.3.tar.gz/cassiopeia-5.1.3/cassiopeia/core/__init__.py
@@ -0,0 +1,34 @@
+from .staticdata import (
+    Champion,
+    Champions,
+    Rune,
+    Item,
+    Map,
+    SummonerSpell,
+    Realms,
+    ProfileIcon,
+    ProfileIcons,
+    LanguageStrings,
+    Locales,
+    Versions,
+    Runes,
+    SummonerSpells,
+    Maps,
+    Items,
+)
+from .champion import ChampionRotation
+from .summoner import Summoner
+from .account import Account
+from .championmastery import ChampionMastery, ChampionMasteries
+from .match import Match, MatchHistory
+from .spectator import CurrentMatch, FeaturedMatches
+from .status import ShardStatus
+from .league import (
+    League,
+    ChallengerLeague,
+    GrandmasterLeague,
+    MasterLeague,
+    LeagueSummonerEntries,
+    LeagueEntries,
+)
+from .patch import Patch
