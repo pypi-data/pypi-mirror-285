595ecc75093830bc37aae898e7554814d11101d9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-19 17:05:09 +0000

    Add package windmill-artifact/1.0.0.18/windmill_artifact-1.0.0.18-py3-none-any.whl

---
+++ packages/windmill-artifact/windmill_artifact-1.0.0.18-py3-none-any.whl/windmillartifactv1/client/__init__.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+"""
+Copyright(C) 2023 baidu, Inc. All Rights Reserved
+
+# @Time : 2023/8/15 19:38
+# @File : __init__.py.py
+# @Software: PyCharm
+"""
