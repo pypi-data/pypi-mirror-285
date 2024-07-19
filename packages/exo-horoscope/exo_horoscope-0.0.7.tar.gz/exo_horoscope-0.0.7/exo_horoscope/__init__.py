6041ac19b22a0802488297e02fdbefa488aa8786
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-19 17:04:59 +0000

    Add package exo-horoscope/0.0.7/exo_horoscope-0.0.7.tar.gz

---
+++ packages/exo-horoscope/exo_horoscope-0.0.7.tar.gz/exo_horoscope-0.0.7/exo_horoscope/__init__.py
@@ -0,0 +1,3 @@
+#from astroquery.ipac.nexsci.nasa_exoplanet_archive import NasaExoplanetArchive
+#exoplanets_table = NasaExoplanetArchive.query_criteria(table="pscomppars", select="*")
+#load_the_table.py
