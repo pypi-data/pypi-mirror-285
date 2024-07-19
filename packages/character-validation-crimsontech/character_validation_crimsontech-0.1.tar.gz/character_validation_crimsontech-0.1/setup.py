b84a0933727e8ec3e86a05129a41941ef23ae713
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-19 18:04:37 +0000

    Add package character-validation-crimsontech/0.1/character_validation_crimsontech-0.1.tar.gz

---
+++ packages/character-validation-crimsontech/character_validation_crimsontech-0.1.tar.gz/character_validation_crimsontech-0.1/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='character_validation_crimsontech',
+    version='0.1',
+    packages=find_packages(exclude=['tests*', "debug_inputs"]),
+    author="Crimson Tech",
+    description="Character validation using morphological operations",
+)
