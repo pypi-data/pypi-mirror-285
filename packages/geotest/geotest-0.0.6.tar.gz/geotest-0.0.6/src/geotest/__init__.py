d15dad55a317d47fc94c74c3c71ebc13055bfeba
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-19 17:05:01 +0000

    Add package geotest/0.0.6/geotest-0.0.6.tar.gz

---
+++ packages/geotest/geotest-0.0.6.tar.gz/geotest-0.0.6/src/geotest/__init__.py
@@ -0,0 +1,22 @@
+import typer
+from datetime import date
+
+app = typer.Typer()
+
+@app.command()
+def hello(name: str = typer.Option(default="World", help="Name to greet")) -> str:
+    typer.echo(f"Hello {name}!")
+
+def ask_age() -> int:
+    return typer.prompt("How old are you?")
+
+@app.command()
+def year_born(age: int = typer.Argument(ask_age)):
+    typer.echo(f"You were born in {date.today().year - age}")
+    
+@app.command()
+def age_today(age: int = typer.Argument(ask_age)):
+    typer.echo(f"You are {age} years old today")
+
+def main():
+    app()
