07ba251eac0bc0676f753c651bd19ce4db1583ad
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-19 14:05:48 +0000

    Add package pyjess/0.3.1/pyjess-0.3.1-cp37-cp37m-manylinux_2_17_x86_64.manylinux2014_x86_64.whl

---
+++ packages/pyjess/pyjess-0.3.1-cp37-cp37m-manylinux_2_17_x86_64.manylinux2014_x86_64.whl/pyjess/__init__.py
@@ -0,0 +1,23 @@
+# noqa: D104
+
+__version__ = "0.3.1"
+__author__ = "Martin Larralde <martin.larralde@embl.de>"
+__all__ = ["Atom", "Hit", "Jess", "Query", "Molecule", "Template", "TemplateAtom"]
+
+from . import _jess
+from ._jess import Atom, Hit, Jess, Query, Molecule, Template, TemplateAtom
+
+__doc__ = _jess.__doc__
+
+# Small addition to the docstring: we want to show a link redirecting to the
+# rendered version of the documentation, but this can only work when Python
+# is running with docstrings enabled
+if __doc__ is not None:
+    __doc__ += """See Also:
+    An online rendered version of the documentation for this version
+    of the library on
+    `Read The Docs <https://pyjess.readthedocs.io/en/v{}/>`_.
+
+    """.format(
+        __version__
+    )
