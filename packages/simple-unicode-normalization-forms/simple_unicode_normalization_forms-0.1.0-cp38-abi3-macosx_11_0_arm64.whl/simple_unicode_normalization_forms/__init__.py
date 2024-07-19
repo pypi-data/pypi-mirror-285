c333b0ac30c2522f1e94d10b4a032a57693ee74c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-19 03:05:03 +0000

    Add package simple-unicode-normalization-forms/0.1.0/simple_unicode_normalization_forms-0.1.0-cp38-abi3-macosx_11_0_arm64.whl

---
+++ packages/simple-unicode-normalization-forms/simple_unicode_normalization_forms-0.1.0-cp38-abi3-macosx_11_0_arm64.whl/simple_unicode_normalization_forms/__init__.py
@@ -0,0 +1,5 @@
+from .simple_unicode_normalization_forms import *
+
+__doc__ = simple_unicode_normalization_forms.__doc__
+if hasattr(simple_unicode_normalization_forms, "__all__"):
+    __all__ = simple_unicode_normalization_forms.__all__
