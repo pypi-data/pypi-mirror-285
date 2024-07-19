b459583b5952b93ac3bfd36ac5d246254059362d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-19 18:05:10 +0000

    Add package TesAPI/1.1/tesapi-1.1.tar.gz

---
+++ packages/TesAPI/tesapi-1.1.tar.gz/tesapi-1.1/TesAPI/Exceptions.py
@@ -0,0 +1,7 @@
+class SettingError(Exception):
+    def __init__(self, message): super().__init__(message)
+        
+
+class RequestError(Exception):
+    def __init__(self, message): super().__init__(message)
+
