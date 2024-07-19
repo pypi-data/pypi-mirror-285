dc7b7189e6a784e605a2f251750f16f80c2238d8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-19 17:05:08 +0000

    Add package ign8/4.12.24/ign8-4.12.24-py3-none-any.whl

---
+++ packages/ign8/ign8-4.12.24-py3-none-any.whl/ign8/aap/serve.py
@@ -0,0 +1,16 @@
+# main file for semaphore
+
+import os
+import sys
+import time
+import json
+import requests
+
+from  ..common import prettyllog
+
+
+
+
+def main():
+    prettyllog("status", "check", "login", "dsv", "0", "Testing", "ERROR")
+
