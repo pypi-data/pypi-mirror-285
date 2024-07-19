d323f820c942399c099b9abfb0545567c88711b9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-19 18:05:04 +0000

    Add package myfunx/0.5.7/myfunx-0.5.7-py3-none-any.whl

---
+++ packages/myfunx/myfunx-0.5.7-py3-none-any.whl/myfunx/__init__.py
@@ -0,0 +1,13 @@
+#!/bin/env python3
+
+__all__ = ('Client', 'JsonManager',)
+
+__version__ = '0.5.7'
+VERSION = __version__
+
+from aiotgm.logging import get_logger
+logger = get_logger('myfunx ' + VERSION)
+del get_logger
+
+from .client import Client
+from .json_manager import JsonManager
---
+++ packages/myfunx/myfunx-0.5.7-py3-none-any.whl/myfunx/client.py
@@ -0,0 +1,161 @@
+#!/bin/env python3
+
+__all__ = ()
+
+import time
+import asyncio
+from typing import (Any,
+                    Union,
+                    Literal,
+                    Optional,)
+from aiotgm import Client
+from datetime import datetime
+from . import logger
+from aiotgm.types import *
+from .json_manager import JsonManager
+
+def parse_list(val: list) -> list[list[int]]:
+    '''
+    Used in the method clean_up_chat()
+    to parse the list of mids in a
+    nested of 100 for each sublist.
+    '''
+    n = 100
+    res = []
+    while True:
+        nest = val[n-100:n]
+        res.append(sorted(nest))
+        if len(val) <= n:
+            return res
+        else:
+            n += 100
+
+
+class Client(Client):
+
+    def __init__(
+        self,
+        token: str,
+        *,
+        parse_mode: Optional[str] = None,
+        protect_content: Optional[bool] = None,
+        proxy: Optional[str] = None,
+        debug: Optional[bool] = None,
+        deep_debug: Optional[bool] = None,
+        tracker: Optional[JsonManager] = None
+    ):
+        if not isinstance(tracker, (JsonManager, type(None))):
+            raise TypeError(
+               "'tracker' must be None or JsonManager,"
+               f' got {tracker.__class__.__name__}.'
+            )
+        self._tracker = tracker
+
+        super().__init__(
+            token,
+            parse_mode=parse_mode,
+            protect_content=protect_content,
+            proxy=proxy,
+            debug=debug,
+            deep_debug=deep_debug
+        )
+
+    @property
+    def tracker(self) -> Optional[JsonManager]:
+        return self._tracker
+
+    def track_message(self, msg: Message, /) -> Optional[dict[str, Any]]:
+        # Check if type is Message
+        if not isinstance(msg, Message):
+            raise TypeError(
+                f'Expected Message in track_message(), got {msg.__class__.__name__}.'
+            )
+        data = self.tracker.check(msg.chat.id)
+        data['mids'] += [msg.message_id]
+        if not data['time']:
+            data['time'] = msg.date
+        return data
+
+
+    async def send_message(self, *args, track: bool = True, **kwargs):
+        msg = await super().send_message(*args, **kwargs)
+        if track and self.tracker:
+            self.track_message(msg)
+        return msg
+
+    async def send_document(self, *args, track: bool = True, **kwargs):
+        msg = await super().send_document(*args, **kwargs)
+        if track and self.tracker:
+            self.track_message(msg)
+        return msg
+
+    async def send_video(self, *args, track: bool = True, **kwargs):
+        msg = await super().send_video(*args, **kwargs)
+        if track and self.tracker:
+            self.track_message(msg)
+        return msg
+
+    async def send_photo(self, *args, track: bool = True, **kwargs):
+        msg = await super().send_photo(*args, **kwargs)
+        if track and self.tracker:
+            self.track_message(msg)
+        return msg
+
+    async def check_mids(self, target_id: Union[int, str], /, delay: float = 3600 * 3) -> None:
+        try:
+            while True:
+                start_time = time.time()
+                users = self.tracker.merge()
+                text = str()
+                for chat_id in users:
+                    data = users[chat_id]
+                    if data['time']:
+                        user = data['user']
+                        first_log = data['time']
+                        remaining_hours = 48 - (round((start_time-first_log) / 3600))
+                        if remaining_hours <= 12:
+                            if remaining_hours > 2 or remaining_hours < 0:
+                                text += f'{remaining_hours} hours'
+                            else:
+                                remaining_minutes = 2880 - (round((start_time-first_log) / 60))
+                                text += f'{remaining_minutes} minutes'
+                            text += f" to clean {user}'s chat 📝️\n"
+                if text:
+                    await self.send_message(target_id, text)
+                diff_time = time.time() - start_time
+                await asyncio.sleep(delay - diff_time)
+
+        except:
+            logger.info('Client method check_mids() was interrupted.')
+            raise
+
+    async def clean_up_chat(
+        self,
+        chat_id: Union[int, str]
+    ) -> Literal[True]:
+        data = self.tracker.get(chat_id)
+        messages_history = data['mids']
+        old_time = data['time']
+        old_query = data['query']
+        messages_to_delete = parse_list(messages_history)
+        data.update({'mids': [], 'time': None, 'query': None})
+        try:
+            for messages_list in messages_to_delete:
+                await self.delete_messages(chat_id, messages_list)
+                if len(messages_to_delete) > 1:
+                    messages_history = [x for x in messages_history if x not in messages_list]
+                    await asyncio.sleep(1)
+        except:
+            restored_data = self.tracker.get(chat_id)
+            restored_data['mids'] += messages_history
+            restored_data['time'] = old_time
+            restored_data['query'] = old_query
+            self.tracker.updates[chat_id] = restored_data
+            self.tracker.push_updates()
+            logger.warning(
+                f"{chat_id}'s chat cleanup was"
+                ' interrupted. Json was restored.'
+            )
+            raise
+        else:
+            return True
