fc0a29439253abf976b6e8604ca6bbbc917fb3cc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-19 04:05:08 +0000

    Add package xnxx-dl/0.0.3/xnxx-dl-0.0.3.tar.gz

---
+++ packages/xnxx-dl/xnxx-dl-0.0.3.tar.gz/xnxx-dl-0.0.3/setup.py
@@ -0,0 +1,28 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='xnxx-dl',
+    version='0.0.3',
+    packages=find_packages(),
+    install_requires=[
+        'requests',
+        'beautifulsoup4',
+		'youtube-dl',
+    ],
+    author='Joannes J.A. Wyckmans',
+    author_email='johan.wyckmans@gmail.com',
+    description='A web scraping and URL filtering tool for xnxx.com',
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
+    url='https://github.com/thisisawesome1994/xnxx-dl',
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'Operating System :: OS Independent',
+    ],
+    entry_points={
+        'console_scripts': [
+            'xnxx-dl.py=app:main',
+        ],
+    },
+    python_requires='>=3.6',
+)
