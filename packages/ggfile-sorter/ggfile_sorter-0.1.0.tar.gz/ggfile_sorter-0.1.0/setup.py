8b4c7df6197feded7d4c18b4add8a3955d3caedf
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-19 04:05:00 +0000

    Add package ggfile-sorter/0.1.0/ggfile_sorter-0.1.0.tar.gz

---
+++ packages/ggfile-sorter/ggfile_sorter-0.1.0.tar.gz/ggfile_sorter-0.1.0/setup.py
@@ -0,0 +1,21 @@
+from setuptools import setup
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setup(
+    name='ggfile_sorter',
+    version='0.1.0',
+    author='gthth',
+    author_email='firi8228@gmail.com',
+    description='A tool for sorting files into categories based on their extensions',
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    packages=['file_sorter'],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+)
