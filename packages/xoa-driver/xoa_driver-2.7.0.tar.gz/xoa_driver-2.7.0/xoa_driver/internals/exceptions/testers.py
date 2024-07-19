d1f2aacc8114df5c50056dfdd1b05d8c9adf72e0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-19 03:05:05 +0000

    Add package xoa-driver/2.7.0/xoa_driver-2.7.0.tar.gz

---
+++ packages/xoa-driver/xoa_driver-2.7.0.tar.gz/xoa_driver-2.7.0/xoa_driver/internals/exceptions/testers.py
@@ -0,0 +1,18 @@
+class WrongTesterError(Exception):
+    def __init__(self) -> None:
+        self.msg = "Tester you are trying to connect is of a different type."
+        super().__init__(self.msg)
+
+
+class WrongTesterPasswordError(Exception):
+    def __init__(self, password: str) -> None:
+        self.password = password
+        self.msg = "Can't create session on the tester because the password is incorrect."
+        super().__init__(self.msg)
+
+
+class UnsupportedFirmwareError(Exception):
+    def __init__(self, version: float) -> None:
+        self.version = version
+        self.msg = f"Testers firmware version is to old, current: {self.version} Min required: 446.5"
+        super().__init__(self.msg)
