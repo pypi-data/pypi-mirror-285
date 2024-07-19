e43c690e8966ac80f755ea228bdedd0f4d3b4191
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-19 03:06:21 +0000

    Add package multi-python-logger/2.0.8/multi_python_logger-2.0.8.tar.gz

---
+++ packages/multi-python-logger/multi_python_logger-2.0.8.tar.gz/multi_python_logger-2.0.8/setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="multi_python_logger",
+    version="2.0.8",
+    packages=find_packages(),
+    # Metadata
+    author="Mohamed Naji Aboo",
+    author_email="aboonaji@gmail.com",
+    description="Application to provide different logging mechanisms",
+    keywords="python logger cloud_logger, aws_cloudwatch_logger",
+    url="",
+    install_requires=["watchtower==3.2.0", "boto3==1.34.144","python-dotenv" ]
+
+)
