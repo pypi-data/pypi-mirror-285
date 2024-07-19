bc558f928f9e691c6d528779232b781d90f4c9a0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-19 03:06:14 +0000

    Add package Simba-UW-tf-dev/1.97.3/Simba-UW-tf-dev-1.97.3.tar.gz

---
+++ packages/Simba-UW-tf-dev/Simba-UW-tf-dev-1.97.3.tar.gz/Simba-UW-tf-dev-1.97.3/setup.py
@@ -0,0 +1,68 @@
+"""
+SimBA (Simple Behavioral Analysis)
+https://github.com/sgoldenlab/simba
+Contributors.
+https://github.com/sgoldenlab/simba#contributors-
+Licensed under GNU Lesser General Public License v3.0
+"""
+
+import setuptools
+import platform
+from setuptools.command.build_py import build_py
+import distutils.log
+
+
+M_CHIP = 'arm'
+DARWIN = 'darwin'
+M_CHIP_INCOMPATIBLE_PKG = ['cefpython3 == 66.0']
+
+with open("docs/project_description.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+with open("requirements.txt", "r") as f:
+    requirements = f.read().splitlines()
+
+processor = platform.processor()
+system = platform.system()
+machine = platform.machine()
+python_version = platform.python_version()
+
+pre_install_statement = f'Installing SimBA. \n ' \
+                        f'CPU: {processor} \n ' \
+                        f'System: {system} \n' \
+                        f'Machine: {machine} \n' \
+                        f'Python version: {python_version} \n'
+
+class CustomBuildCommand(build_py):
+    """Customized build_py command - prints a pre-install statement."""
+    def run(self):
+        distutils.log.set_verbosity(distutils.log.INFO)
+        distutils.log.info(pre_install_statement)
+        build_py.run(self)
+
+if M_CHIP in processor.lower() and DARWIN in system.lower():
+    requirements = [x for x in requirements if x not in M_CHIP_INCOMPATIBLE_PKG]
+
+exclusion_patterns = ["pose_configurations_archive"]
+
+setuptools.setup(
+    name="Simba-UW-tf-dev",
+    version="1.97.3",
+    author="Simon Nilsson, Jia Jie Choong, Sophia Hwang",
+    author_email="sronilsson@gmail.com",
+    description="Toolkit for computer classification and analysis of behaviors in experimental animals",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/sgoldenlab/simba",
+    install_requires=requirements,
+    license='GNU General Public License v3 (GPLv3)',
+    packages=setuptools.find_packages(exclude=["*.tests", "*.tests.*", "tests.*", "tests", "__pycache__", "pose_configurations_archive"]),
+    include_package_data=True,
+    classifiers=(
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: GNU Lesser General Public License v3 (LGPLv3)",
+        "Operating System :: OS Independent",
+    ),
+    entry_points={'console_scripts':['simba=simba.SimBA:main'],},
+    cmdclass={'build_py': CustomBuildCommand}
+)
