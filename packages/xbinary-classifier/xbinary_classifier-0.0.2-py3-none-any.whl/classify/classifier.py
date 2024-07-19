d43408599bf29bc4be263d5c982fb928ff5f2546
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-19 15:05:19 +0000

    Add package xbinary-classifier/0.0.2/xbinary_classifier-0.0.2-py3-none-any.whl

---
+++ packages/xbinary-classifier/xbinary_classifier-0.0.2-py3-none-any.whl/classify/classifier.py
@@ -0,0 +1,28 @@
+import seaborn as sns
+from sklearn.model_selection import train_test_split, cross_val_score
+from sklearn.neighbors import KNeighborsClassifier
+import numpy as np
+class classify:
+    
+    def __init__(self,x,y,n_neighbors=4):
+        self.X = X
+        self.y = y
+        self.n_neighbors = n_neighbors
+        self.knn = KNeighborsClassifier(n_neighbors=self.n_neighbors)
+        
+    def predict(self):
+        X_train, X_test, y_train, y_test = train_test_split(self.X, self.y, random_state=0)
+        
+        
+        KNN_fit=self.knn.fit(X_train, y_train)
+        accuracy = self.knn.score(X_test, y_test)
+        
+        print(f'KNN score: {accuracy}\n')
+        
+        cv_scores = cross_val_score(self.knn, self.X, self.y, cv=3)
+        print('Cross-validation scores (3-fold):', cv_scores)
+        print('Mean cross-validation score (3-fold): {:.3f}'.format(np.mean(cv_scores)))
+        preds = KNN_fit.predict(X_test)
+        cm = confusion_matrix(y_test, preds)
+        sns.heatmap(cm, annot=True, cmap='viridis')
+        return preds
