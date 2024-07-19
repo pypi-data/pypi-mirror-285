708b8987a5c9e86cd815deb4793d1244442aef87
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-19 04:05:07 +0000

    Add package pyhOn-drs/0.16.2/pyhOn_drs-0.16.2-py3-none-any.whl

---
+++ packages/pyhOn-drs/pyhOn_drs-0.16.2-py3-none-any.whl/pyhon/const.py
@@ -0,0 +1,14 @@
+AUTH_API = "https://account2.hon-smarthome.com"
+API_URL = "https://api-iot.he.services"
+API_KEY = "GRCqFhC6Gk@ikWXm1RmnSmX1cm,MxY-configuration"
+APP = "hon"
+CLIENT_ID = (
+    "3MVG9QDx8IX8nP5T2Ha8ofvlmjLZl5L_gvfbT9."
+    "HJvpHGKoAS_dcMN8LYpTSYeVFCraUnV.2Ag1Ki7m4znVO6"
+)
+APP_VERSION = "2.6.5"
+OS_VERSION = 999
+OS = "android"
+DEVICE_MODEL = "pyhOn"
+USER_AGENT = "Chrome/999.999.999.999"
+MOBILE_ID = "pyhOn"
