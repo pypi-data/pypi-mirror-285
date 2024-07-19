5204a83966fca40ee049154ddfffe734df371962
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-19 03:05:01 +0000

    Add package recharge-api/1.12.0/recharge_api-1.12.0-py3-none-any.whl

---
+++ packages/recharge-api/recharge_api-1.12.0-py3-none-any.whl/recharge/api/v2/tokens.py
@@ -0,0 +1,31 @@
+from typing import TypedDict
+
+from recharge.api import RechargeResource, RechargeScope, RechargeVersion
+
+
+class TokenClient(TypedDict):
+    name: str
+    email: str
+
+
+class TokenInformation(TypedDict):
+    client: TokenClient
+    contact_email: str
+    name: str
+    scopes: list[RechargeScope]
+
+
+class TokenResource(RechargeResource):
+    """
+    https://developer.rechargepayments.com/2021-11/token_information/token_information_object
+    """
+
+    object_list_key = "token_information"
+    recharge_version: RechargeVersion = "2021-11"
+
+    def get(self) -> TokenInformation:
+        """Get token information.
+        https://developer.rechargepayments.com/2021-11/token_information/token_information_retrieve
+        """
+        data = self._http_get(self._url)
+        return TokenInformation(**data)
