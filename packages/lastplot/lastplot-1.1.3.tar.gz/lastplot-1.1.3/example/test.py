2d9ffcd688670edf8fbd84d3eb8de1dfc6c65f78
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-20 05:05:22 +0000

    Add package lastplot/1.1.3/lastplot-1.1.3.tar.gz

---
+++ packages/lastplot/lastplot-1.1.3.tar.gz/lastplot-1.1.3/example/test.py
@@ -0,0 +1,19 @@
+import lastplot
+
+df = lastplot.data_workflow(
+    file_path="Dementia project.xlsx",
+    data_sheet="Quantification",
+    mice_sheet="Sheet1",
+    output_path=".",
+    control_name="WT",
+    experimental_name=["FTLD"],
+)
+
+lastplot.zscore_graph_class_average(
+    df,
+    control_name="WT",
+    experimental_name=["FTLD"],
+    output_path=".",
+    palette="tab20b_r",
+    show=True,
+)
