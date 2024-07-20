81a14afd09eed7dcff3cb86d906c5eb78f1fa02f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-20 15:05:48 +0000

    Add package asyncapi-container/0.3.1/asyncapi_container-0.3.1.tar.gz

---
+++ packages/asyncapi-container/asyncapi_container-0.3.1.tar.gz/asyncapi_container-0.3.1/asyncapi_container/containers/v3/simple_spec.py
@@ -0,0 +1,19 @@
+from pydantic import BaseModel, Field
+
+from asyncapi_container.asyncapi.spec.v3.info import Info
+from asyncapi_container.custom_types import RoutingMap
+
+
+class SimpleSpecV3(BaseModel):
+    """
+    Simplified version of AsyncAPI specification mapper.
+    Doesn't require to understand and use the whole asyncapi specification
+    allowing focus only on producers and consumers.
+    """
+
+    info: Info = Info(title="My Project", version="0.0.1", description="")
+    sends: RoutingMap = Field(default={}, description="What your service can send. Acting as producer.")
+    receives: RoutingMap = Field(default={}, description="What your service can receive. Acting as consumer.")
+
+    class Config:
+        arbitrary_types_allowed = True
