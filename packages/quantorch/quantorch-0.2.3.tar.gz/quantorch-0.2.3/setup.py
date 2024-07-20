7ec7f88fe95439412dfab58c4758e2e015dedd5f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-20 05:06:03 +0000

    Add package quantorch/0.2.3/quantorch-0.2.3.tar.gz

---
+++ packages/quantorch/quantorch-0.2.3.tar.gz/quantorch-0.2.3/setup.py
@@ -0,0 +1,13 @@
+import setuptools
+
+setuptools.setup(
+    name='quantorch',
+    version='0.2.3',
+    packages=setuptools.find_packages(),
+    
+    url='https://github.com/jialuechen/quantorch',
+    license='MIT',
+    author='Jialue Chen',
+    author_email='jialuechen@outlook.com',
+    description='A PyTorch-Based Python Library for Derivatives and Pricing'
+)
