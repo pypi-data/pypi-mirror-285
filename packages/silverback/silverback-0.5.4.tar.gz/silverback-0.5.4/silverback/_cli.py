859890c96a271cfa75fd30a262856e1852991cff
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-20 06:06:22 +0000

    Add package silverback/0.5.4/silverback-0.5.4.tar.gz

---
+++ packages/silverback/silverback-0.5.4.tar.gz/silverback-0.5.4/silverback/_cli.py
@@ -0,0 +1,144 @@
+import asyncio
+import os
+from concurrent.futures import ThreadPoolExecutor
+
+import click
+from ape.cli import (
+    AccountAliasPromptChoice,
+    ConnectedProviderCommand,
+    ape_cli_context,
+    network_option,
+    verbosity_option,
+)
+from ape.exceptions import Abort
+from taskiq import AsyncBroker
+from taskiq.cli.worker.run import shutdown_broker
+from taskiq.receiver import Receiver
+
+from silverback._importer import import_from_string
+from silverback.runner import PollingRunner, WebsocketRunner
+
+
+@click.group()
+def cli():
+    """Work with Silverback applications in local context (using Ape)."""
+
+
+def _runner_callback(ctx, param, val):
+    if not val:
+        return None
+
+    elif runner := import_from_string(val):
+        return runner
+
+    raise ValueError(f"Failed to import runner '{val}'.")
+
+
+def _recorder_callback(ctx, param, val):
+    if not val:
+        return None
+
+    elif recorder := import_from_string(val):
+        return recorder()
+
+    raise ValueError(f"Failed to import recorder '{val}'.")
+
+
+def _account_callback(ctx, param, val):
+    if val:
+        val = val.alias.replace("dev_", "TEST::")
+        os.environ["SILVERBACK_SIGNER_ALIAS"] = val
+
+    return val
+
+
+def _network_callback(ctx, param, val):
+    # NOTE: Make sure both of these have the same setting
+    if env_network_choice := os.environ.get("SILVERBACK_NETWORK_CHOICE"):
+        if val.network_choice != env_network_choice:
+            raise Abort(
+                f"Network choice '{val.network_choice}' does not "
+                f"match environment variable '{env_network_choice}'."
+            )
+
+        # else it matches, no issue
+
+    else:
+        os.environ["SILVERBACK_NETWORK_CHOICE"] = val.network_choice
+
+    return val
+
+
+async def run_worker(broker: AsyncBroker, worker_count=2, shutdown_timeout=90):
+    try:
+        tasks = []
+        with ThreadPoolExecutor(max_workers=worker_count) as pool:
+            for _ in range(worker_count):
+                receiver = Receiver(
+                    broker=broker,
+                    executor=pool,
+                    validate_params=True,
+                    max_async_tasks=1,
+                    max_prefetch=0,
+                )
+                broker.is_worker_process = True
+                tasks.append(receiver.listen())
+
+            await asyncio.gather(*tasks)
+    finally:
+        await shutdown_broker(broker, shutdown_timeout)
+
+
+@cli.command(cls=ConnectedProviderCommand, help="Run Silverback application client")
+@ape_cli_context()
+@verbosity_option()
+@network_option(
+    default=os.environ.get("SILVERBACK_NETWORK_CHOICE", "auto"),
+    callback=_network_callback,
+)
+@click.option("--account", type=AccountAliasPromptChoice(), callback=_account_callback)
+@click.option(
+    "--runner",
+    "runner_class",
+    help="An import str in format '<module>:<CustomRunner>'",
+    callback=_runner_callback,
+)
+@click.option(
+    "--recorder",
+    help="An import string in format '<module>:<CustomRecorder>'",
+    callback=_recorder_callback,
+)
+@click.option("-x", "--max-exceptions", type=int, default=3)
+@click.argument("path")
+def run(cli_ctx, account, runner_class, recorder, max_exceptions, path):
+    if not runner_class:
+        # NOTE: Automatically select runner class
+        if cli_ctx.provider.ws_uri:
+            runner_class = WebsocketRunner
+        elif cli_ctx.provider.http_uri:
+            runner_class = PollingRunner
+        else:
+            raise click.BadOptionUsage(
+                option_name="network", message="Network choice cannot support running app"
+            )
+
+    app = import_from_string(path)
+    runner = runner_class(app, recorder=recorder, max_exceptions=max_exceptions)
+    asyncio.run(runner.run())
+
+
+@cli.command(cls=ConnectedProviderCommand, help="Run Silverback application task workers")
+@ape_cli_context()
+@verbosity_option()
+@network_option(
+    default=os.environ.get("SILVERBACK_NETWORK_CHOICE", "auto"),
+    callback=_network_callback,
+)
+@click.option("--account", type=AccountAliasPromptChoice(), callback=_account_callback)
+@click.option("-w", "--workers", type=int, default=2)
+@click.option("-x", "--max-exceptions", type=int, default=3)
+@click.option("-s", "--shutdown_timeout", type=int, default=90)
+@click.argument("path")
+def worker(cli_ctx, account, workers, max_exceptions, shutdown_timeout, path):
+    app = import_from_string(path)
+    asyncio.run(run_worker(app.broker, worker_count=workers, shutdown_timeout=shutdown_timeout))
