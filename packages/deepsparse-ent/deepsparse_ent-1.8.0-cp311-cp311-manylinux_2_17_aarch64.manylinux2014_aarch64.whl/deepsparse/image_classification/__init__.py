24f004ac82d869e2d5ebe308220f588f2fd24ed2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-20 05:05:10 +0000

    Add package deepsparse-ent/1.8.0/deepsparse_ent-1.8.0-cp311-cp311-manylinux_2_17_aarch64.manylinux2014_aarch64.whl

---
+++ packages/deepsparse-ent/deepsparse_ent-1.8.0-cp311-cp311-manylinux_2_17_aarch64.manylinux2014_aarch64.whl/deepsparse/image_classification/__init__.py
@@ -0,0 +1,37 @@
+# Copyright (c) 2021 - present / Neuralmagic, Inc. All Rights Reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#    http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing,
+# software distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+# flake8: noqa
+from deepsparse.analytics import deepsparse_analytics as _analytics
+
+
+try:
+    import torchvision as _torchvision
+
+    import cv2 as _cv2
+except ImportError:
+    raise ImportError(
+        "Please install deepsparse[image_classification] to use this pathway"
+    )
+
+from .constants import *
+from .pipeline import *
+
+# flake8: noqa
+from .postprocess_operator import *
+from .preprocess_operator import *
+from .schemas import *
+
+
+_analytics.send_event("python__image_classification__init")
