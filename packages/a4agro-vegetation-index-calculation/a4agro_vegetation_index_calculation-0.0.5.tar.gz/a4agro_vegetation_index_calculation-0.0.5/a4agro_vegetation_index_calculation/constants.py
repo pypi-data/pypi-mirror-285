413a871b7ffdbb8aab32947957989b72685240b5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-20 05:05:07 +0000

    Add package a4agro-vegetation-index-calculation/0.0.5/a4agro_vegetation_index_calculation-0.0.5.tar.gz

---
+++ packages/a4agro-vegetation-index-calculation/a4agro_vegetation_index_calculation-0.0.5.tar.gz/a4agro_vegetation_index_calculation-0.0.5/a4agro_vegetation_index_calculation/constants.py
@@ -0,0 +1,39 @@
+NDVI_CLASS_LABELS = [
+    "Área sin o Débil vegetación",
+    "Vegetación escasa o Crecimiento inicial",
+    "Vegetación moderada y saludable",
+    "Vegetación densa y vigorosa",
+    "Vegetación sobresaturada o de Alta densidad",
+]
+
+NDWI_CLASS_LABELS = [
+    "Ausencia o muy baja presencia de humedad",
+    "Saturación de humedad en el cultivo",
+    "Déficit de humedad moderado en el cultivo",
+    "Estado hídrico óptimo del cultivo",
+    "Saturación de humedad en el cultivo",
+]
+
+GNDVI_CLASS_LABELS = [
+    "Área sin o Débil vegetación",
+    "Vegetación escasa o Crecimiento inicial",
+    "Vegetación moderada y saludable",
+    "Vegetación densa y vigorosa",
+    "Vegetación sobresaturada o de Alta densidad",
+]
+
+CGI_CLASS_LABELS = [
+    "Pobre y muy bajo contenido de clorofila",
+    "Bajo contenido de clorofila",
+    "Moderado contenido de clorofila",
+    "Alto contenido de clorofila",
+    "Muy alto contenido de clorofila",
+]
+
+NDRE_CLASS_LABELS = [
+    "Pobre y muy bajo contenido de clorofila",
+    "Bajo contenido de clorofila",
+    "Moderado contenido de clorofila",
+    "Alto contenido de clorofila",
+    "Muy alto contenido de clorofila",
+]
