013292e035b76a071034ddc26ba531829beeeef4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-20 05:05:27 +0000

    Add package bubbletea/0.0.2/bubbletea-0.0.2.tar.gz

---
+++ packages/bubbletea/bubbletea-0.0.2.tar.gz/bubbletea-0.0.2/setup.py
@@ -0,0 +1,31 @@
+import setuptools
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="bubbletea",
+    version="0.0.2",
+    author="Baihan Lin",
+    author_email="doerlbh@gmail.com",
+    description="Python library for creating history-augmented interactive medical chatbots with tabular embedding alignment (TEA)",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/doerlbh/bubbletea",
+    project_urls={
+        "Bug Tracker": "https://github.com/doerlbh/bubbletea/issues",
+    },
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: GNU General Public License v3 (GPLv3)",
+        "Operating System :: OS Independent",
+    ],
+    license="GPLv3",
+    package_dir={"": "bubbletea"},
+    packages=setuptools.find_packages(where="bubbletea"),
+    python_requires=">=3.6",
+    install_requires=["numpy>=1.16.5", "pandas"],
+    test_suite="nose.collector",
+    tests_require=["nose"],
+    include_package_data=True,
+)
