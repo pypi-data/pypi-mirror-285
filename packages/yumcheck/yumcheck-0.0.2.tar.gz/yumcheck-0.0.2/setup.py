bc33cc69b4683115cb7e3243e6213927488197ff
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-20 06:05:45 +0000

    Add package yumcheck/0.0.2/yumcheck-0.0.2.tar.gz

---
+++ packages/yumcheck/yumcheck-0.0.2.tar.gz/yumcheck-0.0.2/setup.py
@@ -0,0 +1,23 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="yumcheck",                     # This is the name of the package
+    version="0.0.2",                        # The initial release version
+    author="Titas Majumder",                     # Full name of the author
+    description="Validate a yum package is installed",
+    long_description=long_description,      # Long description read from the the readme file
+    long_description_content_type="text/markdown",
+    packages=setuptools.find_packages(),    # List of all python modules to be installed
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],                                      # Information to filter the project on PyPi website
+    python_requires='>=3',                # Minimum version requirement of the package
+    py_modules=["yumcheck"],             # Name of the python package
+    package_dir={'':'yumcheck/src'},     # Directory of the source code of the package
+    install_requires=[]                     # Install other dependencies if any
+)
