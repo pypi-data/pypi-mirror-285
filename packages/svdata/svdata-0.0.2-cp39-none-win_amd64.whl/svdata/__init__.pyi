b09941fa3ded5dfd803875f31793e188913fe6fc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-20 04:05:03 +0000

    Add package svdata/0.0.2/svdata-0.0.2-cp39-none-win_amd64.whl

---
+++ packages/svdata/svdata-0.0.2-cp39-none-win_amd64.whl/svdata/__init__.pyi
@@ -0,0 +1,34 @@
+from enum import Enum
+
+class SvVariable:
+    identifier: str
+
+class SvInstance:
+    module_identifier: str
+    hierarchical_instance: str
+    hierarchy: list[str]
+    connections: list[list[str]]
+
+class SvPortDirection(Enum):
+    Inout = "Inout"
+    Input = "Input"
+    Output = "Output"
+    Ref = "Ref"
+    IMPLICIT = "IMPLICIT"
+
+class SvData:
+    modules: list[SvModule]
+
+class SvPort:
+    identifier: str
+    direction: SvPortDirection
+
+class SvModule:
+    identifier: str
+    filepath: str
+    ports: list[SvPort]
+    variables: list[SvVariable]
+
+    def render(self) -> str: ...
+
+def read_sv_file(file_path: str) -> SvData: ...
