301ded71f9970d4c576d2fc3cb70815e5d9bddd0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-20 04:04:59 +0000

    Add package PyPulseHeatPipe/1.1.12/pypulseheatpipe-1.1.12.tar.gz

---
+++ packages/PyPulseHeatPipe/pypulseheatpipe-1.1.12.tar.gz/pypulseheatpipe-1.1.12/setup.py
@@ -0,0 +1,18 @@
+import setuptools
+
+with open("README.md", "r", encoding = "utf-8") as fh:
+    long_description = fh.read()
+
+setuptools.setup(name='PyPulseHeatPipe',
+        version='1.1.12',
+        license='GNU',
+        url='https://github.com/nirmalparmarphd/PyPulseHeatPipe',
+        description='The data analysis Python package for the Pulsating Heat Pipe experimental data', 
+        long_description=long_description,
+        long_description_content_type='text/markdown',
+        author='Nirmal Parmar, PhD',
+        author_email='nirmalparmarphd@gmail.com',
+        packages=setuptools.find_packages(),
+        include_package_data=True,
+        install_requires=['scikit-learn','numpy', 'gitpython', 'pandas','scipy', 'matplotlib', 'seaborn', 'openpyxl', 'pygwalker', 'streamlit'],
+        zip_safe=False)
