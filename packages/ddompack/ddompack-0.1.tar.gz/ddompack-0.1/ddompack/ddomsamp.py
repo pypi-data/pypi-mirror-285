e0354817db2cefb5d2c6fe3328a89bce59cd5c1c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-20 04:05:11 +0000

    Add package ddompack/0.1/ddompack-0.1.tar.gz

---
+++ packages/ddompack/ddompack-0.1.tar.gz/ddompack-0.1/ddompack/ddomsamp.py
@@ -0,0 +1,39 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Jul 19 14:23:08 2024
+
+@author: Peter
+"""
+#from . import __flatchain__,__rng__
+import numpy as np
+
+from pathlib import Path
+
+
+
+rng = np.random.default_rng()
+
+chainpath=Path(__file__).parent/ 'chain4b.txt'
+flatchain=np.genfromtxt(chainpath.open())
+
+__all__=["get_Rsamp"]
+
+
+#print("haha")
+
+def MRddo(m,rho):
+    a= -0.492
+    mu = np.sqrt(1-m)
+    mu2=1-m
+    c=0.8284271247461903
+    return 1+mu*(a+c*rho) + mu2*(-2.-np.sqrt(2)*a+c*rho)
+
+
+
+def get_Rsamp(M,Nsamp=1):
+     sel=flatchain[:,0]>M
+     chain_sel=rng.choice(flatchain[sel],size=Nsamp)
+
+     Rs=chain_sel[:,1]*MRddo(M/chain_sel[:,0],chain_sel[:,2])
+     return Rs
+ 
