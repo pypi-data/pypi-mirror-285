006b9e6d5ed8b7ced1a9ec41f0b9261ef66cdb51
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-20 05:04:58 +0000

    Add package pytube-local-071724/0.3/pytube-local-071724-0.3.tar.gz

---
+++ packages/pytube-local-071724/pytube-local-071724-0.3.tar.gz/pytube-local-071724-0.3/setup.py
@@ -0,0 +1,54 @@
+#!/usr/bin/env python
+"""This module contains setup instructions for pytube."""
+import codecs
+import os
+
+from setuptools import setup
+
+here = os.path.abspath(os.path.dirname(__file__))
+
+with codecs.open(os.path.join(here, "README.md"), encoding="utf-8") as fh:
+    long_description = "\n" + fh.read()
+
+with open(os.path.join(here, "pytube", "version.py")) as fp:
+    exec(fp.read())
+
+setup(
+    name="pytube-local-071724",
+    version=0.3,  # noqa: F821
+    author="Ronnie Ghose, Taylor Fox Dahlin, Nick Ficano",
+    author_email="hey@pytube.io",
+    packages=["pytube", "pytube.contrib"],
+    package_data={"": ["LICENSE"],},
+    url="https://github.com/burd5/pytube_local_071724",
+    license="The Unlicense (Unlicense)",
+    entry_points={
+        "console_scripts": [
+            "pytube = pytube.cli:main"],},
+    classifiers=[
+        "Development Status :: 5 - Production/Stable",
+        "Environment :: Console",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: The Unlicense (Unlicense)",
+        "Natural Language :: English",
+        "Operating System :: OS Independent",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
+        "Programming Language :: Python :: 3.10",
+        "Programming Language :: Python :: 3.11",
+        "Programming Language :: Python",
+        "Topic :: Internet",
+        "Topic :: Multimedia :: Video",
+        "Topic :: Software Development :: Libraries :: Python Modules",
+        "Topic :: Terminals",
+        "Topic :: Utilities",
+    ],
+    description=("Personal fork of pytube library"),
+    include_package_data=True,
+    long_description_content_type="text/markdown",
+    long_description=long_description,
+    zip_safe=True,
+    python_requires=">=3.7",
+    keywords=["youtube", "download", "video", "stream",],
+)
