456998df227426f15856dceecbb50153c1dcccc8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-20 04:05:54 +0000

    Add package nlp-sdk/1.0.2/nlp_sdk-1.0.2.tar.gz

---
+++ packages/nlp-sdk/nlp_sdk-1.0.2.tar.gz/nlp_sdk-1.0.2/nlp/constants.py
@@ -0,0 +1,31 @@
+"""
+/*******************************************************************************
+ * Copyright (c) cortical.io GmbH. All rights reserved.
+ *
+ * This software is confidential and proprietary information.
+ * You shall use it only in accordance with the terms of the
+ * license agreement you entered into with cortical.io GmbH.
+ ******************************************************************************/
+"""
+
+TEXT = """Cortical.io’s mission is to deliver AI-based solutions that streamline the extraction, classification, review 
+and analysis of information hidden in unstructured text while providing short time to value. We accomplish this 
+through our novel, meaning-based approach to natural language understanding that solves many critical challenges of 
+text processing in a business context. With more than 10 years expertise in implementing intelligent document processing 
+solutions in the enterprise, Cortical.io has demonstrated its ability to solve the challenges of language ambiguity and 
+variability across many use cases and verticals and is trusted by major companies across the globe."""
+
+SEGMENTATION_TEXT = """Tigers mostly feed on large and medium-sized mammals, particularly ungulates weighing 60–250 kg 
+(130–550 lb). Range-wide, the most selected prey are sambar deer, Manchurian wapiti, barasingha and wild boar. Tigers are  
+capable of taking down larger prey like adult gaur and wild water buffalo, but opportunistically eat much smaller prey, such 
+as monkeys, peafowl and other ground-based birds, hares, porcupines and fish. They also prey on other predators, including dogs, 
+leopards, bears, snakes and crocodiles. Tiger attacks on adult Asian elephants and Indian rhinoceros have also been reported. 
+The Middle English tigre and Old English tigras derive from Old French tigre, from Latin tigris. This was a borrowing of 
+Classical Greek 'tigris', a foreign borrowing of unknown origin meaning 'tiger' and the river Tigris. The origin may have 
+been the Persian word tigra ('pointed or sharp') and the Avestan word tigrhi ('arrow'), perhaps referring to the speed of the 
+tiger's leap, although these words are not known to have any meanings associated with tigers. There are three other colour 
+variants – white, golden and nearly stripeless snow white – that are now virtually non-existent in the wild due to the 
+reduction of wild tiger populations, but continue in captive populations. The white tiger has white fur and sepia-brown stripes. 
+The golden tiger has a pale golden pelage with a blond tone and reddish-brown stripes. The snow white tiger is a morph with 
+extremely faint stripes and a pale reddish-brown ringed tail. Both snow white and golden tigers are homozygous for CORIN 
+gene mutations."""
