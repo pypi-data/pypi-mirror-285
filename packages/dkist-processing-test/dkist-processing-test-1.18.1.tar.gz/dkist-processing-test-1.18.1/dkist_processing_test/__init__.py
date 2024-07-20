a0bb886fbb22550c441fb2c85e6e0c346747427b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-20 05:05:09 +0000

    Add package dkist-processing-test/1.18.1/dkist-processing-test-1.18.1.tar.gz

---
+++ packages/dkist-processing-test/dkist-processing-test-1.18.1.tar.gz/dkist-processing-test-1.18.1/dkist_processing_test/__init__.py
@@ -0,0 +1,10 @@
+from dkist_service_configuration.logging import logger  # first import to call logging.BasicConfig
+from pkg_resources import DistributionNotFound
+from pkg_resources import get_distribution
+
+
+try:
+    __version__ = get_distribution(__name__).version
+except DistributionNotFound:
+    # package is not installed
+    __version__ = "unknown"
