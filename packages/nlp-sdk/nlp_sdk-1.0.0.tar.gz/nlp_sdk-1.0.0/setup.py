f9c38e573d57c4d012598e606a382e3be6d33e01
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-20 04:05:07 +0000

    Add package nlp-sdk/1.0.0/nlp_sdk-1.0.0.tar.gz

---
+++ packages/nlp-sdk/nlp_sdk-1.0.0.tar.gz/nlp_sdk-1.0.0/setup.py
@@ -0,0 +1,30 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="nlp_sdk",
+    version="1.0.0",
+    description="A Python SDK for the Cortical.io Natural Language Processing API",
+    author="Cortical.io",
+    author_email="support@cortical.io",
+    url="https://github.com/cortical-io/python-sdk",
+    packages=find_packages(exclude=["tests*"]),
+    install_requires=[
+        "httpx",
+        "pydantic",
+        "python-dotenv"
+    ],
+    extras_require={
+        "dev": [
+            "pytest",
+            "pytest-mock",
+            "unittest",
+        ]
+    },
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.7',
+    license="MIT",
+)
