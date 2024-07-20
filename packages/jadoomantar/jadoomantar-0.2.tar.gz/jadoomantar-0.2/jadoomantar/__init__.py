63da8463a205207b9cfcf72e6c8bfa14d4c00e46
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-20 15:05:54 +0000

    Add package jadoomantar/0.2/jadoomantar-0.2.tar.gz

---
+++ packages/jadoomantar/jadoomantar-0.2.tar.gz/jadoomantar-0.2/jadoomantar/__init__.py
@@ -0,0 +1,4 @@
+from .main import invisibility_cloak
+
+# Optionally, you can define __all__ to specify what is available for import when using 'from jadoomantar import *'
+__all__ = ['invisibility_cloak']
