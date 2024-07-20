5c4cee25e45724e8b2736bb36c326e9511fe9e9e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-20 06:06:12 +0000

    Add package igs-slm/0.1.4b0/igs_slm-0.1.4b0-py3-none-any.whl

---
+++ packages/igs-slm/igs_slm-0.1.4b0-py3-none-any.whl/slm/defines/FlagSeverity.py
@@ -0,0 +1,14 @@
+from django.utils.translation import gettext_lazy as _
+from django_enum import IntegerChoices
+from enum_properties import s
+
+
+class FlagSeverity(IntegerChoices):
+    _symmetric_builtins_ = [s("name", case_fold=True)]
+
+    BLOCK_SAVE = 1, _("Block Save")
+    BLOCK_PUBLISH = 2, _("Block Publish")
+    NOTIFY = 3, _("Notify")
+
+    def __str__(self):
+        return str(self.label)
