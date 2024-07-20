ba8f9a2117866aee6a89875b38c4cd0e9cbd7617
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-20 03:05:34 +0000

    Add package pytest-yaml-sanmu/0.5.1.dev0/pytest_yaml_sanmu-0.5.1.dev0-cp312-cp312-win_amd64.whl

---
+++ packages/pytest-yaml-sanmu/pytest_yaml_sanmu-0.5.1.dev0-cp312-cp312-win_amd64.whl/pytest_yaml/__init__.pyi
@@ -0,0 +1,6 @@
+from .templates import add_filters as add_filters, add_globals as add_globals, add_locals as add_locals, env_globals as env_globals, item_locals as item_locals
+from _typeshed import Incomplete
+
+__all__ = ['schema', 'env_globals', 'item_locals', 'add_filters', 'add_globals', 'add_locals']
+
+schema: Incomplete
