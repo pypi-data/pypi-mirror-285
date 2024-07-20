798e02c03d16c1cf51f5639c6dbcffb11035f9d0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-20 04:05:08 +0000

    Add package fcapacity-management/1.0/fcapacity_management-1.0.tar.gz

---
+++ packages/fcapacity-management/fcapacity_management-1.0.tar.gz/fcapacity_management-1.0/setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="fcapacity_management",
+    version="1.0",
+    packages=find_packages(),
+    include_package_data=True,
+    install_requires=["azure-identity","azure-mgmt-resource"],  # Agrega las dependencias si las tienes
+)
+
+
+# python setup.py sdist
