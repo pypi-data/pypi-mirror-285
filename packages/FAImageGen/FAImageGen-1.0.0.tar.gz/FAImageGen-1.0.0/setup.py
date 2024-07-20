8ce661e08f3f2622855da2138fd01f27d560a0e9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-20 15:05:53 +0000

    Add package FAImageGen/1.0.0/FAImageGen-1.0.0.tar.gz

---
+++ packages/FAImageGen/FAImageGen-1.0.0.tar.gz/FAImageGen-1.0.0/setup.py
@@ -0,0 +1,22 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="FAImageGen",
+    version="1.0.0",
+    description="Python SDK for ImageGen",
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
+    author="Saliou Kane",
+    author_email="saliou@fotographer.ai",
+    url="https://github.com/FotographerAI/ImageGen-sdk/",
+    packages=find_packages(),
+    install_requires=[
+        "requests"
+    ],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+)
