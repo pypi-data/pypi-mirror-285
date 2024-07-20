b11ab6123d76c1ee77ec7d0d9f5a70152e045ddd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-20 15:05:37 +0000

    Add package nonebot-plugin-l4d2-server/1.0.0a1/nonebot_plugin_l4d2_server-1.0.0a1.tar.gz

---
+++ packages/nonebot-plugin-l4d2-server/nonebot_plugin_l4d2_server-1.0.0a1.tar.gz/nonebot_plugin_l4d2_server-1.0.0a1/nonebot_plugin_l4d2_server/__main__.py
@@ -0,0 +1,90 @@
+"""
+* Copyright (c) 2023, Agnes Digital
+*
+* This program is free software: you can redistribute it and/or modify
+* it under the terms of the GNU General Public License as published by
+* the Free Software Foundation, either version 3 of the License, or
+* (at your option) any later version.
+*
+* This program is distributed in the hope that it will be useful,
+* but WITHOUT ANY WARRANTY; without even the implied warranty of
+* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+* GNU General Public License for more details.
+*
+* You should have received a copy of the GNU General Public License
+* along with this program.  If not, see <https://www.gnu.org/licenses/>.
+"""
+
+from typing import Optional
+
+from nonebot import require
+from nonebot.adapters import Message
+from nonebot.log import logger
+from nonebot.matcher import Matcher
+from nonebot.params import CommandArg, CommandStart, RawCommand
+from nonebot.plugin import on_command
+
+require("nonebot_plugin_alconna")
+from nonebot_plugin_alconna import UniMessage
+
+from .l4_help import get_l4d2_core_help
+from .l4_request import COMMAND, get_ip_server, get_server_detail, reload_ip
+
+l4_help = on_command("l4帮助", aliases={"l4help", "l4d2帮助"})
+l4_request = on_command("anne", aliases=COMMAND)
+l4_reload = on_command("l4重载", aliases={"l4刷新"})
+l4_connect = on_command("connect", aliases={"l4连接"})
+
+
+@l4_help.handle()
+async def _(matcher: Matcher):
+    """帮助"""
+    logger.info("开始执行[l4d2帮助]")
+    im = await get_l4d2_core_help()
+    print(type(im))
+    if isinstance(im, str):
+        await matcher.finish(im)
+    await UniMessage.image(raw=im).send()
+
+
+@l4_request.handle()
+async def _(
+    start: str = CommandStart(),
+    command: str = RawCommand(),
+    args: Message = CommandArg(),
+):
+    """例：
+    指令：  /橘5
+    start: /(command开头指令)
+    command: /橘(响应的全部指令)
+    args: 5(响应的指令后的数字)
+    """
+
+    if start:
+        command = command.replace(start, "")
+    if command == "anne":
+        command = "云"
+    _id: Optional[str] = args.extract_plain_text()
+    if not _id:
+        _id = None
+    logger.info(f"组:{command} ;数字:{_id}")
+    msg = await get_server_detail(command, _id)
+    if msg is not None:
+        if isinstance(msg, str):
+            await UniMessage.text(msg).finish()
+        await UniMessage.image(raw=msg).finish()
+    else:
+        await UniMessage.text("没有这个服呢").send()
+
+
+@l4_connect.handle()
+async def _(args: Message = CommandArg()):
+    ip: Optional[str] = args.extract_plain_text()
+    if ip is not None:
+        await UniMessage.text(await get_ip_server(ip)).finish()
+
+
+@l4_reload.handle()
+async def _():
+    reload_ip()
+    logger.success("重载ip完成")
---
+++ packages/nonebot-plugin-l4d2-server/nonebot_plugin_l4d2_server-1.0.0a1.tar.gz/nonebot_plugin_l4d2_server-1.0.0a1/nonebot_plugin_l4d2_server/l4_request/draw_msg.py
@@ -0,0 +1,88 @@
+import asyncio
+from typing import List
+
+import a2s
+
+from ..utils.api.models import NserverOut, OutServer
+from ..utils.api.request import L4API
+
+
+async def draw_one_ip(host: str, port: int):
+    """输出单个ip"""
+    # 先用文字凑合
+    try:
+        one_server = await L4API.a2s_info(host, port)
+        one_player = await L4API.a2s_players(host, port)
+    except asyncio.exceptions.TimeoutError:
+        return "服务器无响应"
+    player_msg = ""
+    if len(one_player):
+        max_duration_len = max([len(str(i.duration)) for i in one_player])
+        max_score_len = max([len(str(i.score)) for i in one_player])
+
+        for player in one_player:
+            soc = "[{:>{}}]".format(player.score, max_score_len)
+            chines_dur = await convert_duration(player.duration)
+            dur = "{:^{}}".format(chines_dur, max_duration_len)
+
+            player_msg += f"{soc} | {dur} | {player.name} \n"
+    else:
+        player_msg = "服务器感觉很安静啊"
+
+    return f""" 【{one_server.server_name}】
+游戏: {one_server.folder}
+地图: {one_server.map_name}
+ping: {one_server.ping*1000:.0f}ms
+{player_msg}
+connect {host}:{port}
+"""
+
+
+async def get_much_server(server_json: List[NserverOut], command):
+    out_server: List[OutServer] = []
+    for i in server_json:
+        try:
+            one_server = await L4API.a2s_info(i["host"], i["port"])
+            one_player = await L4API.a2s_players(i["host"], i["port"])
+            out_server.append(
+                {
+                    "server": one_server,
+                    "player": one_player,
+                    "host": i["host"],
+                    "port": i["port"],
+                    "command": command,
+                    "id_": i["id"],
+                },
+            )
+
+        except (asyncio.exceptions.TimeoutError, ConnectionRefusedError):
+            emtry_server = a2s.SourceInfo()
+            emtry_server.server_name = "服务器无响应"
+            emtry_server.map_name = "无"
+            emtry_server.player_count = 0
+            emtry_server.max_players = 0
+            out_server.append(
+                {
+                    "server": emtry_server,
+                    "player": [],
+                    "host": i["host"],
+                    "port": i["port"],
+                    "command": command,
+                    "id_": i["id"],
+                },
+            )  # type: ignore
+    return out_server
+
+
+async def convert_duration(duration: float) -> str:
+    minutes, seconds = divmod(duration, 60)
+    hours, minutes = divmod(minutes, 60)
+    time_str = ""
+    if hours > 0:
+        time_str += f"{int(hours)}h "
+    if minutes > 0:
+        time_str += f"{int(minutes)}m "
+    time_str += f"{int(seconds)}s"
+    return time_str
+    time_str += f"{int(seconds)}s"
+    return time_str
