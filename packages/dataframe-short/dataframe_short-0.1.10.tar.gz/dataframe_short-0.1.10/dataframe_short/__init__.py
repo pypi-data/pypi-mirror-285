b44c4211558b39054fe8b6e24610823eeffd91f8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-20 14:05:26 +0000

    Add package dataframe-short/0.1.10/dataframe_short-0.1.10.tar.gz

---
+++ packages/dataframe-short/dataframe_short-0.1.10.tar.gz/dataframe_short-0.1.10/dataframe_short/__init__.py
@@ -0,0 +1,9 @@
+from dataframe_short.func_24_Feb import *
+from dataframe_short.utils_ds import *
+from dataframe_short.sandbox1_ds import *
+from dataframe_short.convert_types import *
+import dataframe_short.move_column
+__version__ = "0.1.10"
+
+
+
