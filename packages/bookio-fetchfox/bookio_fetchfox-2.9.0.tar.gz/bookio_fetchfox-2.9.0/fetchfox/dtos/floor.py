33c0873f3eb5b323e53591d07903a55bec4fd7da
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-20 05:05:35 +0000

    Add package bookio-fetchfox/2.9.0/bookio_fetchfox-2.9.0.tar.gz

---
+++ packages/bookio-fetchfox/bookio_fetchfox-2.9.0.tar.gz/bookio_fetchfox-2.9.0/fetchfox/dtos/floor.py
@@ -0,0 +1,14 @@
+import typing
+
+if typing.TYPE_CHECKING:
+    from .listing import ListingDTO
+
+
+class FloorDTO:
+    def __init__(
+        self,
+        listing: "ListingDTO",
+        listing_count: int,
+    ):
+        self.listing: "ListingDTO" = listing
+        self.listing_count: int = listing_count
