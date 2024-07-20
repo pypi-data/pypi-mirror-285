aa23384c31bee3fdec6b11209314e2de1c2c1cc7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-20 05:05:34 +0000

    Add package hyperSel/2.1/hypersel-2.1.tar.gz

---
+++ packages/hyperSel/hypersel-2.1.tar.gz/hypersel-2.1/setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="hyperSel",
+    version=2.10,
+    packages=find_packages(),
+    install_requires=[
+        "selenium==4.21.0",
+        "beautifulsoup4==4.12.3",
+        "webdriver-manager==4.0.1",
+        "lxml==5.2.2",
+        "requests==2.32.3",
+    ],
+    include_package_data=True, 
+)
