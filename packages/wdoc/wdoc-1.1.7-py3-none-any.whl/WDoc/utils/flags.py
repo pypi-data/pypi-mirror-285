3f1f77091bbce87412cc357fb472a464d1007ec5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-20 06:05:40 +0000

    Add package wdoc/1.1.7/wdoc-1.1.7-py3-none-any.whl

---
+++ packages/wdoc/wdoc-1.1.7-py3-none-any.whl/WDoc/utils/flags.py
@@ -0,0 +1,26 @@
+"""
+easy access for all other files wether we are in verbose mode or not
+"""
+
+import fire
+import platform
+
+# parse args again to know globally if we're in verbose mode
+kwargs = fire.Fire(lambda *args, **kwargs: kwargs)
+is_linux = platform.system() == "Linux"
+
+if "verbose" in kwargs and kwargs["verbose"]:
+    is_verbose = True
+else:
+    is_verbose = False
+
+if "debug" in kwargs and kwargs["debug"]:
+    is_debug = True
+    is_verbose = True
+else:
+    is_debug = False
+
+if "disable_md_printing" in kwargs and kwargs["disable_md_printing"]:
+    disable_md_printing = True
+else:
+    disable_md_printing = False
