86b6ed3bf83bfc762b4330005f86cb84f4bb2b39
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-20 15:05:31 +0000

    Add package ceylon/0.14.0/ceylon-0.14.0.tar.gz

---
+++ packages/ceylon/ceylon-0.14.0.tar.gz/ceylon-0.14.0/bindings/ceylon/tests/test_worker/test.py
@@ -0,0 +1,37 @@
+import asyncio
+import pickle
+
+from ceylon.ceylon import enable_log, uniffi_set_event_loop
+from ceylon.workspace.admin import Admin
+from ceylon.workspace.worker import Worker
+
+
+async def main():
+    admin = Admin(
+        name="admin",
+        port=8000
+    )
+    worker1 = Worker(
+        name="worker1",
+        admin_port=8000,
+        admin_peer="admin",
+        workspace_id="admin"
+    )
+    worker2 = Worker(
+        name="worker2",
+        admin_port=8000,
+        admin_peer="admin",
+        workspace_id="admin"
+    )
+
+    await admin.run_admin(pickle.dumps({
+        "title": "How to use AI for Machine Learning",
+    }), [
+        worker1,
+        worker2
+    ])
+
+
+if __name__ == '__main__':
+    enable_log("INFO")
+    asyncio.run(main())
