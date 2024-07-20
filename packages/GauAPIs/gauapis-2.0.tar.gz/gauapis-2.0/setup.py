05aab32d3487672c924e5fa6eee3a1463c732c71
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-20 03:05:33 +0000

    Add package GauAPIs/2.0/gauapis-2.0.tar.gz

---
+++ packages/GauAPIs/gauapis-2.0.tar.gz/gauapis-2.0/setup.py
@@ -0,0 +1,17 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='GauAPIs',
+    version='2.0',
+    packages=find_packages(),
+    install_requires=[
+        
+    ],
+    author='Sam Yunteng Liao',
+    author_email='lmasgne@gmail.com',
+    description='APIs for Gaussian running other calculators through External',
+    license='MIT',
+    platforms="linux",
+    keywords='Gaussian--external python APIs',
+    url='https://github.com/SamYToeL/APIs'
+)
