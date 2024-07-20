c0ab2583b09e0e6cb9398aa28e2711819eb396ca
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-20 03:06:09 +0000

    Add package Stirfax/8.0.5/Stirfax-8.0.5.tar.gz

---
+++ packages/Stirfax/Stirfax-8.0.5.tar.gz/Stirfax-8.0.5/ScftKraft/__init__.py
@@ -0,0 +1,3 @@
+from .Stirfax import GoldenX1, GoldenX2, GoldenX3, GoldenX4, GoldenX5
+
+__all__ = ['GoldenX1', 'GoldenX2', 'GoldenX3', 'GoldenX4', 'GoldenX5']
---
+++ packages/Stirfax/Stirfax-8.0.5.tar.gz/Stirfax-8.0.5/setup.py
@@ -0,0 +1,13 @@
+import setuptools
+setuptools.setup(
+    name='Stirfax',
+    version='8.0.5',
+    author='Programmer RESS ',
+    description='By @A_T_9 In telegram ',
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "Operating System :: OS Independent",
+        "License :: OSI Approved :: MIT License"
+    ]
+)
