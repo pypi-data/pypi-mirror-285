14014f1d8df9cb7126c54b1617f38b5659c68c99
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-20 06:05:35 +0000

    Add package lincbrain-cli/0.10.0/lincbrain_cli-0.10.0.tar.gz

---
+++ packages/lincbrain-cli/lincbrain_cli-0.10.0.tar.gz/lincbrain_cli-0.10.0/lincbrain/conftest.py
@@ -0,0 +1,29 @@
+from __future__ import annotations
+
+from pytest import Config, Item, Parser
+
+from .tests.fixtures import *  # noqa: F401, F403  # lgtm [py/polluting-import]
+
+
+def pytest_addoption(parser: Parser) -> None:
+    parser.addoption(
+        "--dandi-api",
+        action="store_true",
+        default=False,
+        help="Only run tests of the new Django Dandi API",
+    )
+
+
+def pytest_collection_modifyitems(items: list[Item], config: Config) -> None:
+    # Based on <https://pythontesting.net/framework/pytest/pytest-run-tests
+    # -using-particular-fixture/>
+    if config.getoption("--dandi-api"):
+        selected_items = []
+        deselected_items = []
+        for item in items:
+            if "local_dandi_api" in getattr(item, "fixturenames", ()):
+                selected_items.append(item)
+            else:
+                deselected_items.append(item)
+        config.hook.pytest_deselected(items=deselected_items)
+        items[:] = selected_items
