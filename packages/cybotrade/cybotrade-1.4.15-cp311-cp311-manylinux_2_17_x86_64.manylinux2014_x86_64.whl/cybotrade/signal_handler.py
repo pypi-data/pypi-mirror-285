eb4287919c8f8c6bfb1bd122e319cb68a50bad29
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-20 15:06:06 +0000

    Add package cybotrade/1.4.15/cybotrade-1.4.15-cp311-cp311-manylinux_2_17_x86_64.manylinux2014_x86_64.whl

---
+++ packages/cybotrade/cybotrade-1.4.15-cp311-cp311-manylinux_2_17_x86_64.manylinux2014_x86_64.whl/cybotrade/signal_handler.py
@@ -0,0 +1,16 @@
+import logging
+import asyncio
+from cybotrade.server import WebServer
+
+
+class SignalHandler:
+    server: WebServer | None = None
+    def __init__(self):
+        print("initing")
+
+    async def on_init(self):
+        self.server = WebServer(self, logger=logging.Logger(name="signal"))
+        await self.server.start()
+
+    async def on_signal(self):
+        print("haha")
