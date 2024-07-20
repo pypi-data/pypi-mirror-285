f5733559e2bbc09a275ed6e495b9033da20634ea
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-20 15:05:34 +0000

    Add package datamaxi/0.13.0/datamaxi-0.13.0.tar.gz

---
+++ packages/datamaxi/datamaxi-0.13.0.tar.gz/datamaxi-0.13.0/datamaxi/datamaxi/__init__.py
@@ -0,0 +1,23 @@
+from typing import Any
+from datamaxi.lib.constants import BASE_URL
+from datamaxi.datamaxi.candle import Candle
+from datamaxi.datamaxi.funding_rate import FundingRate
+from datamaxi.datamaxi.dex_trade import DexTrade
+
+
+class Datamaxi:
+    """Client to fetch unified data from DataMaxi+ API."""
+
+    def __init__(self, api_key=None, **kwargs: Any):
+        """Initialize the object.
+
+        Args:
+            api_key (str): The DataMaxi+ API key
+            **kwargs: Keyword arguments used by `datamaxi.api.API`.
+        """
+        if "base_url" not in kwargs:
+            kwargs["base_url"] = BASE_URL
+
+        self.candle = Candle(api_key, **kwargs)
+        self.funding_rate = FundingRate(api_key, **kwargs)
+        self.dex_trade = DexTrade(api_key, **kwargs)
