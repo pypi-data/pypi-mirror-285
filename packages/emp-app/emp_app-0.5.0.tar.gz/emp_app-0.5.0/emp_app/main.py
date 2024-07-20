b246b59cdd448a19c0215013e64200c8bb170295
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-20 05:05:35 +0000

    Add package emp-app/0.5.0/emp_app-0.5.0.tar.gz

---
+++ packages/emp-app/emp_app-0.5.0.tar.gz/emp_app-0.5.0/emp_app/main.py
@@ -0,0 +1,16 @@
+from flask import Flask
+
+app = Flask(__name__)
+
+emp = [{"name": "emp1"}, {"name": "emp2"}]
+
+
+@app.route("/")
+def get_emp_data():
+    return emp
+
+
+@app.route("/<id>")
+def get_emp_data_id(id):
+    print(id)
+    return emp[int(id)]
