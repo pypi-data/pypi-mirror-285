603edd443ed33849d0709155e4834aad8e137ee5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-20 15:06:26 +0000

    Add package llm-nexus/0.1.2/llm-nexus-0.1.2.tar.gz

---
+++ packages/llm-nexus/llm-nexus-0.1.2.tar.gz/llm-nexus-0.1.2/src/llm_nexus/__init__.py
@@ -0,0 +1,33 @@
+"""
+This module provides a unified interface for multiple language model providers.
+"""
+
+# Import utility functions and classes
+from ._utils.completions import CompletionParameters
+from ._utils.function_calls import (
+    Argument,
+    Function,
+    FunctionCallParameters,
+    ParamType,
+)
+
+# Import main components
+from .model_provider import ModelProvider
+
+# Import provider classes
+from .providers._anthropic_provider import AnthropicProvider
+from .providers._googleai_provider import GoogleAIProvider
+from .providers._openai_provider import OpenAIProvider
+
+# You can also define __all__ to control what gets imported with "from llm_nexus import *"
+__all__ = [
+    "ModelProvider",
+    "CompletionParameters",
+    "Argument",
+    "Function",
+    "FunctionCallParameters",
+    "ParamType",
+    "AnthropicProvider",
+    "GoogleAIProvider",
+    "OpenAIProvider",
+]
