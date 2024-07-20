5824ebff799d8e6d0204ece48af17fd356749c55
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-20 06:05:42 +0000

    Add package mongomock-stable/0.0.1.dev1120/mongomock_stable-0.0.1.dev1120.tar.gz

---
+++ packages/mongomock-stable/mongomock_stable-0.0.1.dev1120.tar.gz/mongomock_stable-0.0.1.dev1120/mongomock/__version__.py
@@ -0,0 +1,10 @@
+from platform import python_version_tuple
+
+python_version = python_version_tuple()
+
+if (int(python_version[0]), int(python_version[1])) >= (3, 8):
+    from importlib.metadata import version
+    __version__ = version('mongomock')
+else:
+    import pkg_resources
+    __version__ = pkg_resources.get_distribution('mongomock').version
