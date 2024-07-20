95ad0c82c09af07296338d51c5187842dc2e7b5d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-20 14:05:12 +0000

    Add package gtfs2gmns/0.2.1/gtfs2gmns-0.2.1-py3-none-any.whl

---
+++ packages/gtfs2gmns/gtfs2gmns-0.2.1-py3-none-any.whl/gtfs2gmns/pkg_configs.py
@@ -0,0 +1,12 @@
+# -*- coding:utf-8 -*-
+##############################################################
+# Created Date: Sunday, July 9th 2023
+# Contact Info: luoxiangyong01@gmail.com
+# Author/Copyright: Mr. Xiangyong Luo
+##############################################################
+
+# **** Package Info **** #
+pkg_version = "0.2.0"
+pkg_name = "gtfs2gmns"
+pkg_author = "Mr. Xiangyong Luo, Mrs. Fang Tang,  Dr. Xuesong Simon Zhou"
+pkg_email = "luoxiangyong01@gmail.com,fangt@asu.edu, xzhou74@asu.edu"
