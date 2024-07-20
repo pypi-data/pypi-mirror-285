ba5bd24f27be06ea9b6d46e32eb4d5d87eeed75c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-20 05:05:17 +0000

    Add package db-weights/0.0.3/db_weights-0.0.3.tar.gz

---
+++ packages/db-weights/db_weights-0.0.3.tar.gz/db_weights-0.0.3/setup.py
@@ -0,0 +1,29 @@
+from setuptools import setup, find_packages
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setup(
+    name="db_weights",
+    version="0.0.3",
+    author="Aydin Abedinia",
+    author_email="abedinia.aydin@gmail.com",
+    description="Calculating db_weights for test data based on training data.",
+    long_description="Distance based weighting alg for semi-supervised learning",
+    long_description_content_type="text/markdown",
+    url="https://github.com/WeightedBasedAI/db_weights",
+    packages=find_packages(),
+    install_requires=[
+        'numpy',
+        'scikit-learn',
+        'scipy',
+        'joblib'
+    ],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+)
+
