ec208919f6de208bfb8ef1e3a2ed0df2b43b3ebd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-20 05:04:59 +0000

    Add package allotropy/0.1.42/allotropy-0.1.42-py3-none-any.whl

---
+++ packages/allotropy/allotropy-0.1.42-py3-none-any.whl/allotropy/parsers/ctl_immunospot/ctl_immunospot_parser.py
@@ -0,0 +1,27 @@
+from allotropy.allotrope.models.adm.plate_reader.benchling._2023._09.plate_reader import (
+    Model,
+)
+from allotropy.allotrope.schema_mappers.adm.plate_reader.benchling._2023._09.plate_reader import (
+    Mapper,
+)
+from allotropy.named_file_contents import NamedFileContents
+from allotropy.parsers.ctl_immunospot.ctl_immunospot_structure import create_data
+from allotropy.parsers.lines_reader import LinesReader, read_to_lines
+from allotropy.parsers.release_state import ReleaseState
+from allotropy.parsers.vendor_parser import VendorParser
+
+
+class CtlImmunospotParser(VendorParser):
+    @property
+    def display_name(self) -> str:
+        return "CTL ImmunoSpot"
+
+    @property
+    def release_state(self) -> ReleaseState:
+        return ReleaseState.RECOMMENDED
+
+    def to_allotrope(self, named_file_contents: NamedFileContents) -> Model:
+        lines = read_to_lines(named_file_contents)
+        data = create_data(LinesReader(lines))
+        mapper = Mapper(self.get_asm_converter_name(), self._get_date_time)
+        return mapper.map_model(data)
