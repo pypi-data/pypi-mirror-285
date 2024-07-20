5507a8d65065d611be1b1b2c99db07a374384cd2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-20 15:06:03 +0000

    Add package qcanvas-backend/0.1.6/qcanvas_backend-0.1.6.tar.gz

---
+++ packages/qcanvas-backend/qcanvas_backend-0.1.6.tar.gz/qcanvas_backend-0.1.6/qcanvas_backend/util/dev_failed_resource_list.py
@@ -0,0 +1,46 @@
+import asyncio
+import logging
+from typing import Union
+
+from qcanvas_api_clients.util import enable_api_caching
+from stones import LmdbStore, stone
+
+_logger = logging.getLogger(__name__)
+
+
+class _NOPFailList:
+    async def failed(self, id: str) -> bool:
+        return False
+
+    async def record_failure(self, id: str):
+        pass
+
+
+class FailedResourcesList:
+    """
+    Meant to be used as a development convenience only:
+    Remembers if a resource couldn't be retrieved, so we don't bother getting it again. This is saved to disk.
+    Should NOT be used for release builds!
+    """
+
+    @staticmethod
+    def create_if_enabled() -> Union["FailedResourcesList", _NOPFailList]:
+        if enable_api_caching:
+            _logger.warning(
+                'Using development "fail-db". You should not see this message in a release build!'
+            )
+            return FailedResourcesList()
+        else:
+            return _NOPFailList()
+
+    def __init__(self):
+        self.sem = asyncio.Semaphore()
+        self.db = stone("debug_failed_resources", LmdbStore)
+
+    async def failed(self, id: str) -> bool:
+        async with self.sem:
+            return self.db.get(id, False)
+
+    async def record_failure(self, id: str):
+        async with self.sem:
+            self.db[id] = True
