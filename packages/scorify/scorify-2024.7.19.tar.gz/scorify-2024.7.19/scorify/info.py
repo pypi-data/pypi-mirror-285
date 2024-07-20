090c813dee4393a67e04f47651da194fb374a476
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-20 06:05:40 +0000

    Add package scorify/2024.7.19/scorify-2024.7.19.tar.gz

---
+++ packages/scorify/scorify-2024.7.19.tar.gz/scorify-2024.7.19/scorify/info.py
@@ -0,0 +1,7 @@
+# -*- coding: utf-8 -*-
+version = __VERSION__ = "2024.07.19"
+author = "Nathan Vack"
+author_email = "njvack@wisc.edu"
+license = "MIT"
+copyright = "Copyright 2024 Boards of Regent of the University of Wisconsin System"
+url = "https://github.com/uwmadison-chm/scorify"
