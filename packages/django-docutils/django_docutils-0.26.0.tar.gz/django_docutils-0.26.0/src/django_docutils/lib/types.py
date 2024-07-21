8b70200d63442dfa0834d225456f002c8b5c3843
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-21 03:07:05 +0000

    Add package django-docutils/0.26.0/django_docutils-0.26.0.tar.gz

---
+++ packages/django-docutils/django_docutils-0.26.0.tar.gz/django_docutils-0.26.0/src/django_docutils/lib/types.py
@@ -0,0 +1,35 @@
+"""Typings for Django Docutils settings for django."""
+
+import typing as t
+
+from typing_extensions import NotRequired, TypedDict
+
+
+class DjangoDocutilsLibRSTRolesSettings(TypedDict):
+    """Docutils role mappings."""
+
+    local: NotRequired[t.Dict[str, str]]
+
+
+class DjangoDocutilsLibRSTDocutilsSettings(TypedDict):
+    """Docutils document settings."""
+
+    raw_enabled: NotRequired[bool]
+    strip_comments: NotRequired[bool]
+    initial_header_level: NotRequired[int]
+
+
+class DjangoDocutilsLibRSTSettings(TypedDict):
+    """Core settings object for ``DJANGO_DOCUTILS_LIB_RST``."""
+
+    metadata_processors: NotRequired[t.List[str]]
+    transforms: NotRequired[t.List[str]]
+    docutils: NotRequired[DjangoDocutilsLibRSTDocutilsSettings]
+    directives: NotRequired[t.Dict[str, str]]
+    roles: NotRequired[DjangoDocutilsLibRSTRolesSettings]
+
+
+class DjangoDocutilsLibTextSettings(TypedDict):
+    """Core settings object for ``DJANGO_DOCUTILS_TEXT_RST``."""
+
+    uncapitalized_word_filters: t.List[str]
