b22db372932dd7a249a4cda175cc3f8d4048519a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-21 03:06:04 +0000

    Add package pyHana/0.0.24/pyHana-0.0.24-py3-none-any.whl

---
+++ packages/pyHana/pyHana-0.0.24-py3-none-any.whl/pyHana/common/urlProc.py
@@ -0,0 +1,35 @@
+import requests
+import time
+import datetime
+
+def requests_url_call(urlStr, headers='', params='', prtInd = False):
+    errInd = 'N'
+    
+    if prtInd: 
+        print('\r' + datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S'), urlStr, params, end='')
+
+    while(1):
+        try:                       
+            # 2023.10.24 post 방식 추가
+            if len(headers) == 0:      
+                resp = requests.get(urlStr, timeout=(5, 60)) # connect timeout 5초, read timeout 30로 각각 설정             
+            elif len(headers) > 0 and len(params) > 0 :
+                resp = requests.post(urlStr, timeout=(5, 60), headers=headers, params=params)   
+            elif len(headers) > 0:
+                resp = requests.post(urlStr, timeout=(5, 60), headers=headers)   
+            elif len(params) > 0 :
+                resp = requests.post(urlStr, timeout=(5, 60), params=params)   
+
+            if errInd == 'Y':
+                print('>> Requests retry success : ', flush=True)    
+                
+            return resp
+        
+        except Exception as e:
+            print("\n", type(e), ':',  e, flush=True)
+            errInd = 'Y'
+            time.sleep(1)
+    if prtInd: 
+        print(' '*400, '\r', end='')         
+           
+    return resp
