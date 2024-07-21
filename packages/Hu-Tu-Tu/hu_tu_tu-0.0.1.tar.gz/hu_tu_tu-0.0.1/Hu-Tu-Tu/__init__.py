aef9f23a8d1a3c600be1de394b7bde8413839e6a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-21 03:06:16 +0000

    Add package Hu-Tu-Tu/0.0.1/hu_tu_tu-0.0.1.tar.gz

---
+++ packages/Hu-Tu-Tu/hu_tu_tu-0.0.1.tar.gz/hu_tu_tu-0.0.1/Hu-Tu-Tu/__init__.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+# @Time        : 2024/07/20 19:44
+# @File        : __init__.py.py
+# @Description : None
+# ----------------------------------------------
+# ☆ ☆ ☆ ☆ ☆ ☆ ☆ 
+# >>> Author    : Alex
+# >>> Mail      : liu_zhao_feng_alex@163.com
+# >>> Github    : https://github.com/koking0
+# >>> Blog      : https://alex007.blog.csdn.net/
+# ☆ ☆ ☆ ☆ ☆ ☆ ☆
+print("TuTu: 基于视觉基座模型的图片识别与理解功能库")
