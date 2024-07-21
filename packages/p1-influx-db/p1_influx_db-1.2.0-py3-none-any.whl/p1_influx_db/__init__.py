85098c6cf73e7b17bd1b19a9de20777fa45f4ce3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-21 03:07:35 +0000

    Add package p1-influx-db/1.2.0/p1_influx_db-1.2.0-py3-none-any.whl

---
+++ packages/p1-influx-db/p1_influx_db-1.2.0-py3-none-any.whl/p1_influx_db/__init__.py
@@ -0,0 +1,19 @@
+from loguru import logger
+from .httpmain import httpmain
+from .mqttmain import mqttmain
+
+__version__ = "1.0.0"
+
+try:
+    import paho.mqtt.client
+
+    method = "mqtt"
+except ImportError:
+    logger.warning("Not running in mqtt method")
+    try:
+        import influxdb_client
+
+        method = "http"
+    except ImportError:
+        logger.critical("No method is available; fix package installation")
+        raise NotImplementedError("No method available")
