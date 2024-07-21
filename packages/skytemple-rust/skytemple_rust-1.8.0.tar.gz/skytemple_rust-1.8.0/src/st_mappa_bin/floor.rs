f8b8a45a594c28682551e2f0115dff9cfcf7e866
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-21 03:06:00 +0000

    Add package skytemple-rust/1.8.0/skytemple_rust-1.8.0.tar.gz

---
+++ packages/skytemple-rust/skytemple_rust-1.8.0.tar.gz/skytemple_rust-1.8.0/src/st_mappa_bin/floor.rs
@@ -0,0 +1,204 @@
+/*
+ * Copyright 2021-2024 Capypara and the SkyTemple Contributors
+ *
+ * This file is part of SkyTemple.
+ *
+ * SkyTemple is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * SkyTemple is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with SkyTemple.  If not, see <https://www.gnu.org/licenses/>.
+ */
+use crate::st_mappa_bin::{
+    MappaFloorLayout, MappaItemList, MappaMonster, MappaMonsterList, MappaTrapList,
+};
+use crate::util::Lazy;
+use pyo3::prelude::*;
+use std::ops::Deref;
+
+#[pyclass(module = "skytemple_rust.st_mappa_bin")]
+#[derive(Clone, PartialEq)]
+pub struct MappaFloor {
+    pub layout: Lazy<Py<MappaFloorLayout>>,
+    pub monsters: Lazy<Py<MappaMonsterList>>,
+    pub traps: Lazy<Py<MappaTrapList>>,
+    pub floor_items: Lazy<Py<MappaItemList>>,
+    pub shop_items: Lazy<Py<MappaItemList>>,
+    pub monster_house_items: Lazy<Py<MappaItemList>>,
+    pub buried_items: Lazy<Py<MappaItemList>>,
+    pub unk_items1: Lazy<Py<MappaItemList>>,
+    pub unk_items2: Lazy<Py<MappaItemList>>,
+}
+
+#[pymethods]
+impl MappaFloor {
+    #[new]
+    #[allow(clippy::too_many_arguments)]
+    pub fn new(
+        layout: MappaFloorLayout,
+        monsters: Vec<Py<MappaMonster>>,
+        traps: MappaTrapList,
+        floor_items: MappaItemList,
+        shop_items: MappaItemList,
+        monster_house_items: MappaItemList,
+        buried_items: MappaItemList,
+        unk_items1: MappaItemList,
+        unk_items2: MappaItemList,
+        py: Python,
+    ) -> PyResult<Self> {
+        Ok(Self {
+            layout: Lazy::Instance(Py::new(py, layout)?),
+            monsters: Lazy::Instance(Py::new(py, MappaMonsterList(monsters))?),
+            traps: Lazy::Instance(Py::new(py, traps)?),
+            floor_items: Lazy::Instance(Py::new(py, floor_items)?),
+            shop_items: Lazy::Instance(Py::new(py, shop_items)?),
+            monster_house_items: Lazy::Instance(Py::new(py, monster_house_items)?),
+            buried_items: Lazy::Instance(Py::new(py, buried_items)?),
+            unk_items1: Lazy::Instance(Py::new(py, unk_items1)?),
+            unk_items2: Lazy::Instance(Py::new(py, unk_items2)?),
+        })
+    }
+
+    #[getter]
+
+    pub fn layout(&mut self) -> PyResult<Py<MappaFloorLayout>> {
+        Ok(self.layout.instance()?.clone())
+    }
+
+    #[setter]
+
+    pub fn set_layout(&mut self, value: Py<MappaFloorLayout>) -> PyResult<()> {
+        self.layout = Lazy::Instance(value);
+        Ok(())
+    }
+
+    #[getter]
+
+    pub fn monsters(&mut self) -> PyResult<Py<MappaMonsterList>> {
+        Ok(self.monsters.instance()?.clone())
+    }
+
+    #[setter]
+
+    pub fn set_monsters(&mut self, py: Python, value: PyObject) -> PyResult<()> {
+        if let Ok(val) = value.extract::<Py<MappaMonsterList>>(py) {
+            self.monsters = Lazy::Instance(val);
+            Ok(())
+        } else {
+            match value.extract::<Vec<Py<MappaMonster>>>(py) {
+                Ok(v) => {
+                    self.monsters = Lazy::Instance(Py::new(py, MappaMonsterList(v))?);
+                    Ok(())
+                }
+                Err(e) => Err(e),
+            }
+        }
+    }
+
+    #[getter]
+
+    pub fn traps(&mut self) -> PyResult<Py<MappaTrapList>> {
+        Ok(self.traps.instance()?.clone())
+    }
+
+    #[setter]
+
+    pub fn set_traps(&mut self, value: Py<MappaTrapList>) -> PyResult<()> {
+        self.traps = Lazy::Instance(value);
+        Ok(())
+    }
+
+    #[getter]
+
+    pub fn floor_items(&mut self) -> PyResult<Py<MappaItemList>> {
+        Ok(self.floor_items.instance()?.clone())
+    }
+
+    #[setter]
+
+    pub fn set_floor_items(&mut self, value: Py<MappaItemList>) -> PyResult<()> {
+        self.floor_items = Lazy::Instance(value);
+        Ok(())
+    }
+
+    #[getter]
+
+    pub fn shop_items(&mut self) -> PyResult<Py<MappaItemList>> {
+        Ok(self.shop_items.instance()?.clone())
+    }
+
+    #[setter]
+
+    pub fn set_shop_items(&mut self, value: Py<MappaItemList>) -> PyResult<()> {
+        self.shop_items = Lazy::Instance(value);
+        Ok(())
+    }
+
+    #[getter]
+
+    pub fn monster_house_items(&mut self) -> PyResult<Py<MappaItemList>> {
+        Ok(self.monster_house_items.instance()?.clone())
+    }
+
+    #[setter]
+
+    pub fn set_monster_house_items(&mut self, value: Py<MappaItemList>) -> PyResult<()> {
+        self.monster_house_items = Lazy::Instance(value);
+        Ok(())
+    }
+
+    #[getter]
+
+    pub fn buried_items(&mut self) -> PyResult<Py<MappaItemList>> {
+        Ok(self.buried_items.instance()?.clone())
+    }
+
+    #[setter]
+
+    pub fn set_buried_items(&mut self, value: Py<MappaItemList>) -> PyResult<()> {
+        self.buried_items = Lazy::Instance(value);
+        Ok(())
+    }
+
+    #[getter]
+
+    pub fn unk_items1(&mut self) -> PyResult<Py<MappaItemList>> {
+        Ok(self.unk_items1.instance()?.clone())
+    }
+
+    #[setter]
+
+    pub fn set_unk_items1(&mut self, value: Py<MappaItemList>) -> PyResult<()> {
+        self.unk_items1 = Lazy::Instance(value);
+        Ok(())
+    }
+
+    #[getter]
+
+    pub fn unk_items2(&mut self) -> PyResult<Py<MappaItemList>> {
+        Ok(self.unk_items2.instance()?.clone())
+    }
+
+    #[setter]
+
+    pub fn set_unk_items2(&mut self, value: Py<MappaItemList>) -> PyResult<()> {
+        self.unk_items2 = Lazy::Instance(value);
+        Ok(())
+    }
+
+    fn __richcmp__(&self, other: PyRef<Self>, op: pyo3::basic::CompareOp) -> Py<PyAny> {
+        let py = other.py();
+        match op {
+            pyo3::basic::CompareOp::Eq => (self == other.deref()).into_py(py),
+            pyo3::basic::CompareOp::Ne => (self != other.deref()).into_py(py),
+            _ => py.NotImplemented(),
+        }
+    }
+}
---
+++ packages/skytemple-rust/skytemple_rust-1.8.0.tar.gz/skytemple_rust-1.8.0/src/util.rs
@@ -0,0 +1,190 @@
+/*
+ * Copyright 2021-2024 Capypara and the SkyTemple Contributors
+ *
+ * This file is part of SkyTemple.
+ *
+ * SkyTemple is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * SkyTemple is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with SkyTemple.  If not, see <https://www.gnu.org/licenses/>.
+ */
+use crate::bytes::{AsStBytes, StBytes};
+use bytes::BytesMut;
+use pyo3::{PyErr, PyResult};
+use std::cmp::{max, min};
+use std::fmt::{Debug, Formatter};
+use std::hash::{Hash, Hasher};
+use std::hint::unreachable_unchecked;
+use std::iter::repeat;
+
+#[inline]
+#[allow(unused)]
+pub(crate) fn slice_to_array<const N: usize>(slice: &[u8]) -> [u8; N] {
+    let mut arr: [u8; N] = [0; N];
+    arr.copy_from_slice(slice);
+    arr
+}
+
+#[allow(unused)]
+pub(crate) fn init_default_vec<U, T>(size: usize) -> U
+where
+    U: FromIterator<T>,
+    T: Default,
+{
+    (0..size).map(|_| Default::default()).collect()
+}
+
+#[allow(unused)]
+pub(crate) fn gcd(a: usize, b: usize) -> usize {
+    match ((a, b), (a & 1, b & 1)) {
+        ((x, y), _) if x == y => y,
+        ((0, x), _) | ((x, 0), _) => x,
+        ((x, y), (0, 1)) | ((y, x), (1, 0)) => gcd(x >> 1, y),
+        ((x, y), (0, 0)) => gcd(x >> 1, y >> 1) << 1,
+        ((x, y), (1, 1)) => {
+            let (x, y) = (min(x, y), max(x, y));
+            gcd((y - x) >> 1, x)
+        }
+        _ => unreachable!(),
+    }
+}
+
+#[allow(unused)]
+pub(crate) fn lcm(a: usize, b: usize) -> usize {
+    a * b / gcd(a, b)
+}
+
+/// Smart pointer to lazily build data from StBytes.
+/// Can fail converting from StBytes/T and into T.
+pub enum Lazy<T> {
+    Source(StBytes),
+    Instance(T),
+}
+
+impl<T> Clone for Lazy<T>
+where
+    T: AsStBytes + TryFrom<StBytes> + Clone,
+{
+    fn clone(&self) -> Self {
+        match self {
+            Self::Source(v) => Self::Source(v.clone()),
+            Self::Instance(v) => Self::Instance(v.clone()),
+        }
+    }
+}
+
+impl<T> Debug for Lazy<T>
+where
+    T: AsStBytes + TryFrom<StBytes> + Debug,
+{
+    fn fmt(&self, f: &mut Formatter<'_>) -> std::fmt::Result {
+        match self {
+            Self::Source(v) => write!(f, "Lazy::Source({:?})", v),
+            Self::Instance(v) => write!(f, "Lazy::Instance({:?})", v),
+        }
+    }
+}
+
+impl<T, E> PartialEq for Lazy<T>
+where
+    T: AsStBytes + TryFrom<StBytes, Error = E>,
+    E: Into<PyErr>,
+{
+    fn eq(&self, other: &Self) -> bool {
+        self.as_bytes() == other.as_bytes()
+    }
+}
+
+impl<T, E> Eq for Lazy<T>
+where
+    T: AsStBytes + TryFrom<StBytes, Error = E>,
+    E: Into<PyErr>,
+{
+}
+
+impl<T, E> Hash for Lazy<T>
+where
+    T: AsStBytes + TryFrom<StBytes, Error = E> + Hash,
+    E: Into<PyErr>,
+{
+    fn hash<H: Hasher>(&self, state: &mut H) {
+        self.as_bytes().hash(state)
+    }
+}
+
+impl<T, E> Lazy<T>
+where
+    T: AsStBytes + TryFrom<StBytes, Error = E>,
+    E: Into<PyErr>,
+{
+    /// Creates a Lazy wrapper from some byte source but immediately constructs it.
+    /// This is useful, if the source has an unbound length.
+    pub fn instance_from_source(source: StBytes) -> PyResult<Self> {
+        Ok(Self::Instance(source.try_into().map_err(Into::into)?))
+    }
+
+    pub fn instance(&mut self) -> PyResult<&T> {
+        self.instance_mut().map(|v| &*v)
+    }
+
+    pub fn instance_mut(&mut self) -> PyResult<&mut T> {
+        Ok(match self {
+            Lazy::Source(v) => {
+                *self = Self::Instance(T::try_from(v.clone()).map_err(Into::into)?);
+                match self {
+                    Lazy::Instance(v) => v,
+                    Lazy::Source(_) => unsafe { unreachable_unchecked() },
+                }
+            }
+            Lazy::Instance(v) => v,
+        })
+    }
+}
+
+impl<T> From<StBytes> for Lazy<T>
+where
+    T: AsStBytes + TryFrom<StBytes>,
+{
+    fn from(value: StBytes) -> Self {
+        Self::Source(value)
+    }
+}
+
+impl<T, E> From<Lazy<T>> for StBytes
+where
+    T: AsStBytes + TryFrom<StBytes, Error = E>,
+    E: Into<PyErr>,
+{
+    fn from(source: Lazy<T>) -> Self {
+        source.as_bytes()
+    }
+}
+
+impl<T, E> AsStBytes for Lazy<T>
+where
+    T: AsStBytes + TryFrom<StBytes, Error = E>,
+    E: Into<PyErr>,
+{
+    fn as_bytes(&self) -> StBytes {
+        match self {
+            Lazy::Source(s) => s.clone(),
+            Lazy::Instance(i) => i.as_bytes(),
+        }
+    }
+}
+
+#[allow(dead_code)]
+pub fn pad(data: &mut BytesMut, padlen: usize, padwith: u8) {
+    let lenp = data.len() % padlen;
+    if lenp != 0 {
+        data.extend(repeat(padwith).take(padlen - lenp))
+    }
+}
