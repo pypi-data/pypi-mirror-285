ec4b505a5bfcd162fc9d04e9c157aa951989778a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-21 03:06:19 +0000

    Add package opthub_client/0.1.1/opthub_client-0.1.1.tar.gz

---
+++ packages/opthub_client/opthub_client-0.1.1.tar.gz/opthub_client-0.1.1/opthub_client/context/version.py
@@ -0,0 +1,24 @@
+"""Read version from toml file."""
+
+import tomllib
+from pathlib import Path
+
+from opthub_client.errors.cache_io_error import CacheIOError, CacheIOErrorMessage
+
+
+def get_version_from_toml() -> str:
+    """Read version from pyproject.toml file.
+
+    Returns:
+        str: The version of the project as a string.
+
+    Raises:
+        VersionNotFoundError: If the version cannot be found or is in an incorrect format.
+    """
+    path = Path("pyproject.toml")
+    with path.open("rb") as file:
+        data = tomllib.load(file)
+    version = data["project"]["version"]
+    if not isinstance(version, str):
+        raise CacheIOError(CacheIOErrorMessage.VERSION_FILE_READ_FAILED)
+    return version
