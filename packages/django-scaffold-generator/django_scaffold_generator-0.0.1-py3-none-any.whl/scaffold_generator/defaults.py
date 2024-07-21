93f7ec7485457a22bf31d5d86c9b5c37007197fa
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-21 03:06:31 +0000

    Add package django-scaffold-generator/0.0.1/django_scaffold_generator-0.0.1-py3-none-any.whl

---
+++ packages/django-scaffold-generator/django_scaffold_generator-0.0.1-py3-none-any.whl/scaffold_generator/defaults.py
@@ -0,0 +1,191 @@
+DEFAULT_SETTINGS = {
+    'CREATE_HTML_VIEW_RESOURCES': True,
+    'CREATE_REST_VIEW_RESOURCES': True,
+    'DEFAULT_MODEL_IMPORTS': [],
+    'DEFAULT_FORM_IMPORTS': [
+        'django.forms',
+    ],
+    'FIELDS': {},
+    'MODEL_EXTRA_IMPORT_CLASSES': [
+        'django.db.models',
+    ],
+    'MODEL_PARENT_CLASSES': ['django.db.models.Model'],
+    'FORM_EXTRA_IMPORT_CLASSES': ['django.forms'],
+    'FORM_PARENT_CLASSES': ['django.forms.ModelForm'],
+    'VIEW_EXTRA_IMPORT_CLASSES': [],
+    'VIEW_PERMISSION_CLASSES': [
+        'django.contrib.auth.mixins.PermissionRequiredMixin',
+    ],
+    'VIEW_PERMISSION_CODES': [
+        'view',
+    ],
+    'LIST_VIEW_PARENT_CLASSES': ['django.views.generic.list.ListView'],
+    'DETAIL_VIEW_PARENT_CLASSES': ['django.views.generic.detail.DetailView'],
+    'ADD_PERMISSION_CLASSES': [
+        'django.contrib.auth.mixins.PermissionRequiredMixin',
+    ],
+    'ADD_PERMISSION_CODES': [
+        'add',
+    ],
+    'CREATE_VIEW_PARENT_CLASSES': ['django.views.generic.edit.CreateView'],
+    'CREATE_URL_PATH': 'create',
+    'CHANGE_PERMISSION_CLASSES': [
+        'django.contrib.auth.mixins.PermissionRequiredMixin',
+    ],
+    'CHANGE_PERMISSION_CODES': [
+        'change',
+    ],
+    'UPDATE_VIEW_PARENT_CLASSES': ['django.views.generic.edit.UpdateView'],
+    'UPDATE_URL_PATH': 'update',
+    'DELETE_PERMISSION_CLASSES': [
+        'django.contrib.auth.mixins.PermissionRequiredMixin',
+    ],
+    'DELETE_PERMISSION_CODES': [
+        'delete',
+    ],
+    'DELETE_VIEW_PARENT_CLASSES': ['django.views.generic.edit.DeleteView'],
+    'DELETE_URL_PATH': 'delete',
+    'ADMIN_EXTRA_IMPORT_CLASSES': ['django.contrib.admin'],
+    'ADMIN_PARENT_CLASSES': ['django.contrib.admin.ModelAdmin'],
+    'URL_EXTRA_IMPORT_CLASSES': ['django.urls.path'],
+    'SCAFFOLD_HTML_VIEWS': True,
+    'SCAFFOLD_REST_FRAMEWORK': True,
+    'REST_FRAMEWORK_SERIALIZER_EXTRA_IMPORT_CLASSES': ['rest_framework.serializers'],
+    'REST_FRAMEWORK_SERIALIZER_PARENT_CLASSES': ['rest_framework.serializers.ModelSerializer'],
+    'REST_FRAMEWORK_VIEWSET_EXTRA_IMPORT_CLASSES': ['rest_framework.viewsets'],
+    'REST_FRAMEWORK_VIEWSET_PARENT_CLASSES': ['rest_framework.viewsets.ModelViewSet'],
+    'REST_FRAMEWORK_VIEWSET_PERMISSION_CLASSES': ['rest_framework.permissions.DjangoModelPermissions'],
+    'REST_FRAMEWORK_DEFAULT_ROUTER': 'rest_framework.routers.DefaultRouter',
+    'SCAFFOLD_TEMPLATES': False,
+    'FORM_EXTRA': '',
+    'TEMPLATE_VIEW_LIST': 'scaffold_generator/views/model_list.html.template',
+    'TEMPLATE_VIEW_DETAIL': 'scaffold_generator/views/model_detail.html.template',
+    'TEMPLATE_VIEW_FORM': 'scaffold_generator/views/model_form.html.template',
+    'TEMPLATE_VIEW_DELETE': 'scaffold_generator/views/model_delete.html.template',
+    'ADD_LIST_VIEW_TO_NAVBAR_TEMPLATE': '',
+    'NAVBAR_ITEM_TEMPLATE': 'scaffold_generator/navbar_item.html.template',
+    'REST_FRAMEWORK_PATH': 'api',
+}
+
+DEFAULT_FIELDS = {
+    'AutoField': {
+        'class_name': 'models.AutoField',
+    },
+    'BigAutoField': {
+        'class_name': 'models.BigAutoField',
+    },
+    'BigIntegerField': {
+        'class_name': 'models.BigIntegerField',
+    },
+    'BinaryField': {
+        'class_name': 'models.BinaryField',
+    },
+    'BooleanField': {
+        'class_name': 'models.BooleanField',
+    },
+    'CharField': {
+        'class_name': 'models.CharField',
+        'default_kwargs': {
+            'max_length': '128',
+        },
+        'nullable': False,
+    },
+    'CommaSeparatedIntegerField': {
+        'class_name': 'models.CommaSeparatedIntegerField',
+        'nullable': False,
+    },
+    'DateField': {
+        'class_name': 'models.DateField',
+    },
+    'DateTimeField': {
+        'class_name': 'models.DateTimeField',
+    },
+    'DecimalField': {
+        'class_name': 'models.DecimalField',
+    },
+    'DurationField': {
+        'class_name': 'models.DurationField',
+    },
+    'EmailField': {
+        'class_name': 'models.EmailField',
+        'nullable': False,
+    },
+    'FileField': {
+        'class_name': 'models.FileField',
+        'nullable': False,
+    },
+    'FilePathField': {
+        'class_name': 'models.FilePathField',
+    },
+    'FloatField': {
+        'class_name': 'models.FloatField',
+    },
+    'ForeignKey': {
+        'class_name': 'models.ForeignKey',
+        'default_kwargs': {
+            'on_delete': 'models.CASCADE',
+        },
+    },
+    'GenericIPAddressField': {
+        'class_name': 'models.GenericIPAddressField',
+    },
+    'IPAddressField': {
+        'class_name': 'models.IPAddressField',
+    },
+    'ImageField': {
+        'class_name': 'models.ImageField',
+        'nullable': False,
+    },
+    'IntegerField': {
+        'class_name': 'models.IntegerField',
+    },
+    'JSONField': {
+        'class_name': 'models.JSONField',
+    },
+    'ManyToManyField': {
+        'class_name': 'models.ManyToManyField',
+        'nullable': False,
+    },
+    'NullBooleanField': {
+        'class_name': 'models.NullBooleanField',
+    },
+    'OneToOneField': {
+        'class_name': 'models.OneToOneField',
+        'default_kwargs': {
+            'on_delete': 'models.CASCADE',
+        },
+    },
+    'PositiveBigIntegerField': {
+        'class_name': 'models.PositiveBigIntegerField',
+    },
+    'PositiveIntegerField': {
+        'class_name': 'models.PositiveIntegerField',
+    },
+    'PositiveSmallIntegerField': {
+        'class_name': 'models.PositiveSmallIntegerField',
+    },
+    'SlugField': {
+        'class_name': 'models.SlugField',
+        'nullable': False,
+    },
+    'SmallAutoField': {
+        'class_name': 'models.SmallAutoField',
+    },
+    'SmallIntegerField': {
+        'class_name': 'models.SmallIntegerField',
+    },
+    'TextField': {
+        'class_name': 'models.TextField',
+        'nullable': False,
+    },
+    'TimeField': {
+        'class_name': 'models.TimeField',
+    },
+    'URLField': {
+        'class_name': 'models.URLField',
+        'nullable': False,
+    },
+    'UUIDField': {
+        'class_name': 'models.UUIDField',
+    },
+}
---
+++ packages/django-scaffold-generator/django_scaffold_generator-0.0.1-py3-none-any.whl/scaffold_generator/scaffold.py
@@ -0,0 +1,149 @@
+import logging
+import os
+import re
+
+from django.apps import apps
+from django.template import loader
+from django.template.loader import render_to_string
+
+from .settings import ScaffoldGeneratorConfig
+from .utils import FileTransaction
+
+LOGGER = logging.getLogger(__name__)
+
+
+def clean_model_fields(model_fields, scaffold_generator_settings):
+    fields = []
+    for model_field in model_fields:
+        type = model_field['type']
+        if type not in scaffold_generator_settings.FIELDS:
+            raise ValueError('Unknown field type : {}'.format(type))
+        field_config = scaffold_generator_settings.FIELDS[type]
+
+        pos_args, kw_args = model_field['arguments']
+        if field_config.get('default_kwargs'):
+            kw_args = {**field_config['default_kwargs'], **kw_args}
+        if model_field['optional']:
+            kw_args['blank'] = 'True'
+            if field_config.get('nullable', True):
+                kw_args['null'] = 'True'
+        fields.append(
+            {
+                'name': model_field['name'],
+                'class_name': field_config['class_name'],
+                'pos_args': pos_args,
+                'kw_args': kw_args,
+            }
+        )
+    return fields
+
+
+TEMPLATE_REPLACES = {
+    '[[': '{{',
+    ']]': '}}',
+    '[%': '{%',
+    '%]': '%}',
+}
+
+
+def clean_template(template_string):
+    for k, v in TEMPLATE_REPLACES.items():
+        template_string = template_string.replace(k, v)
+    return template_string
+
+
+class ScaffoldGenerator:
+
+    def __init__(self, app_label, model_name, model_fields):
+        config = ScaffoldGeneratorConfig()
+        self.config = config
+        self.app_label = app_label
+        self.model_name = re.sub(r'\W+', '', model_name[0].upper() + model_name[1:])
+        self.model_fields = clean_model_fields(model_fields, config)
+
+    def get_context(self):
+        return {
+            'app_label': self.app_label,
+            'model_name': self.model_name,
+            'model_code': self.model_name.lower(),
+            'model_fields': self.model_fields,
+            'config': self.config,
+        }
+
+    def generate(self):
+        app_path = apps.get_app_config(self.app_label).path
+        context = self.get_context()
+        LOGGER.debug('Using context : %s', context)
+        with FileTransaction() as ft:
+            with ft.open(path=os.path.join(app_path, 'models.py'), mode='a') as fp:
+                fp.write(render_to_string('scaffold_generator/models.py.template', context=context))
+            with ft.open(path=os.path.join(app_path, 'admin.py'), mode='a') as fp:
+                fp.write(render_to_string('scaffold_generator/admin.py.template', context=context))
+            if self.config['SCAFFOLD_HTML_VIEWS']:
+                with ft.open(
+                    path=os.path.join(app_path, 'urls.py'),
+                    mode='a',
+                    default_file_content=render_to_string('scaffold_generator/urls.py.default.template', context=context)
+                ) as fp:
+                    fp.write(render_to_string('scaffold_generator/urls.py.template', context=context))
+                with ft.open(path=os.path.join(app_path, 'forms.py'), mode='a') as fp:
+                    fp.write(render_to_string('scaffold_generator/forms.py.template', context=context))
+                with ft.open(path=os.path.join(app_path, 'views.py'), mode='a') as fp:
+                    fp.write(render_to_string('scaffold_generator/views.py.template', context=context))
+            import pprint
+            pprint.pprint(context)
+            if self.config['SCAFFOLD_REST_FRAMEWORK']:
+                api_path = os.path.join(app_path, self.config['REST_FRAMEWORK_PATH'])
+                if not os.path.exists(api_path):
+                    os.mkdir(api_path)
+                    with ft.open(os.path.join(app_path, '__init__.py'), mode='w') as fp:
+                        fp.write('')
+                with ft.open(
+                    path=os.path.join(api_path, 'urls.py'),
+                    mode='r+',
+                    default_file_content=render_to_string(
+                        'scaffold_generator/api/urls.py.default.template', context=context
+                    )
+                ) as fp:
+                    buf = []
+                    for line in fp.readlines():
+                        if 'router.urls' in line:
+                            buf.append(render_to_string('scaffold_generator/api/urls.py.template', context=context))
+                        buf.append(line)
+                    fp.seek(0)
+                    fp.writelines(buf)
+                with ft.open(path=os.path.join(api_path, 'serializers.py'), mode='a') as fp:
+                    fp.write(render_to_string('scaffold_generator/api/serializers.py.template', context=context))
+                with ft.open(path=os.path.join(api_path, 'views.py'), mode='a') as fp:
+                    fp.write(render_to_string('scaffold_generator/api/views.py.template', context=context))
+            if self.config['SCAFFOLD_TEMPLATES']:
+                template_path = os.path.join(app_path, 'templates')
+                if not os.path.exists(template_path):
+                    os.mkdir(template_path)
+                template_app_path = os.path.join(template_path, os.path.basename(app_path))
+                if not os.path.exists(template_app_path):
+                    os.mkdir(template_app_path)
+                with ft.open(
+                    path=os.path.join(template_app_path, context['model_code'] + '_list.html'), mode='a'
+                ) as fp:
+                    fp.write(clean_template(render_to_string(self.config['TEMPLATE_VIEW_LIST'], context=context)))
+                with ft.open(
+                    path=os.path.join(template_app_path, context['model_code'] + '_detail.html'), mode='a'
+                ) as fp:
+                    fp.write(clean_template(render_to_string(self.config['TEMPLATE_VIEW_DETAIL'], context=context)))
+                with ft.open(
+                    path=os.path.join(template_app_path, context['model_code'] + '_form.html'), mode='a'
+                ) as fp:
+                    fp.write(clean_template(render_to_string(self.config['TEMPLATE_VIEW_FORM'], context=context)))
+                with ft.open(
+                    path=os.path.join(template_app_path, context['model_code'] + '_confirm_delete.html'), mode='a'
+                ) as fp:
+                    fp.write(clean_template(render_to_string(self.config['TEMPLATE_VIEW_DELETE'], context=context)))
+                if self.config['ADD_LIST_VIEW_TO_NAVBAR_TEMPLATE']:
+                    navbar_template_file = loader.get_template(
+                        self.config['ADD_LIST_VIEW_TO_NAVBAR_TEMPLATE']
+                    ).origin.name
+                    with ft.open(path=navbar_template_file, mode='a') as fp:
+                        fp.write(
+                            clean_template(render_to_string(self.config['NAVBAR_ITEM_TEMPLATE'], context=context))
+                        )
