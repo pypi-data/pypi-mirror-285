1e9d82e824daea8b4f567308a5fc896f434635ad
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-21 03:06:25 +0000

    Add package pycountries/1.1.1/pycountries-1.1.1-py3-none-any.whl

---
+++ packages/pycountries/pycountries-1.1.1-py3-none-any.whl/pycountries/__init__.py
@@ -0,0 +1,25 @@
+from pycountries.countries import Country
+from pycountries.currencies import (
+    AmountSpecialValuesNotAllowedError,
+    Currency,
+    NegativeAmountNotAllowedError,
+    WrongAmountDigitsNumberError,
+    WrongAmountTypeError,
+    ZeroAmountNotAllowedError,
+)
+from pycountries.languages import Language
+from pycountries.phones import Phone
+
+__version__ = "1.1.1"
+__author__ = "Ivan Koldakov"
+__all__ = [
+    "AmountSpecialValuesNotAllowedError",
+    "Country",
+    "Currency",
+    "Language",
+    "NegativeAmountNotAllowedError",
+    "Phone",
+    "WrongAmountDigitsNumberError",
+    "WrongAmountTypeError",
+    "ZeroAmountNotAllowedError",
+]
