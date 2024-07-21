96e9549e0d3c4dde17fbb821fc83a9a98fd6381a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-21 03:05:39 +0000

    Add package universal-test-platform/0.0.63/universal_test_platform-0.0.63-py3-none-any.whl

---
+++ packages/universal-test-platform/universal_test_platform-0.0.63-py3-none-any.whl/utp/appVariables.py
@@ -0,0 +1,16 @@
+import os
+from typing import Optional
+
+
+def appVariables(androidApp: str, androidPlatformVersion: Optional[str], remote: Optional[bool]=False, path: Optional[str] = None, noReset: Optional[bool] = None):
+    my_env = os.environ
+    my_env["ANDROID_AUTOMATION_NAME"] = 'UIAutomator2'
+    my_env["ANDROID_APP"] = androidApp or 'http://172.20.74.25:4723/device-farm/apps/file-1717336325068.apk'
+    my_env["ANDROID_PLATFORM_NAME"] = 'Android'
+    my_env["ANDROID_PLATFORM_VERSION"] = androidPlatformVersion or ""
+    my_env['REMOTE'] = str(remote) if remote else ""
+    my_env['TEST_PATH'] = path if path else ""
+    my_env['NO_RESET'] = str(noReset) if noReset else ""
+    
+    return my_env
+    
