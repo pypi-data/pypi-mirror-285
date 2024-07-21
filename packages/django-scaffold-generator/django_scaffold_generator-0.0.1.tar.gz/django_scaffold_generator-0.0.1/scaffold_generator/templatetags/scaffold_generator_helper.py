14499d49aa5c174b4ff8378578784b9a6f6ce6f2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-21 03:06:32 +0000

    Add package django-scaffold-generator/0.0.1/django_scaffold_generator-0.0.1.tar.gz

---
+++ packages/django-scaffold-generator/django_scaffold_generator-0.0.1.tar.gz/django_scaffold_generator-0.0.1/scaffold_generator/templatetags/scaffold_generator_helper.py
@@ -0,0 +1,41 @@
+from django import template
+from django.template import Context
+from django.template import Template
+
+register = template.Library()
+
+KEY_VALUE_TEMPLATE = Template("{{ key }}={{ value }}")
+
+
+@register.filter
+def to_keyvalues(dictionary):
+    return [KEY_VALUE_TEMPLATE.render(Context({'key': key, 'value': value})) for key, value in dictionary.items()]
+
+
+@register.filter
+def to_import(class_name):
+    modules = class_name.split('.')
+    return '.'.join(modules[:-1]), modules[-1]
+
+
+@register.filter
+def to_classname(classname):
+    if classname.split('.'):
+        return classname.split('.')[-1]
+    return classname
+
+
+@register.filter
+def to_classnames(classnames):
+    return [to_classname(cl) for cl in classnames]
+
+
+@register.filter
+def to_permission_codes(permission_codes, **kwargs):
+    return permission_codes
+
+
+
+@register.filter
+def to_single_item_list(item):
+    return [item]
---
+++ packages/django-scaffold-generator/django_scaffold_generator-0.0.1.tar.gz/django_scaffold_generator-0.0.1/setup.py
@@ -0,0 +1,55 @@
+import os
+from pathlib import Path
+
+from setuptools import setup
+from setuptools import setup, find_packages
+
+version = '0.0.1'
+
+description = Path(os.path.join(os.path.abspath(os.path.dirname(__file__)),
+                                'README.md')).read_text(encoding='utf-8').strip()
+
+setup(
+    name='django-scaffold-generator',
+    packages=find_packages('.', include=['scaffold_generator','scaffold_generator.*']),
+    package_data={'scaffold_generator': ['templates/scaffold_generator/*.template','templates/scaffold_generator/components/*.template', 'templates/scaffold_generator/api/*.template']},
+
+    version=version,
+    license='mit',
+    license_files=['LICENSE.txt',],
+    description='Ruby on Rails like Scaffolding Provider for Django',
+    long_description=description,
+    long_description_content_type='text/markdown',
+    author='Swaroop P',
+    author_email='iamswaroopp@gmail.com',
+    url='https://github.com/iamswaroopp/django-scaffold-generator',
+    download_url='https://github.com/iamswaroopp/django-scaffold-generator/archive/' + version + '.zip',
+    keywords=[
+        'django',
+        'developement',
+        'scaffold_generator',
+        'startapp',
+        'rapid','rad','rapid application development'
+        'python',
+    ],
+    install_requires=['django',],
+    python_requires='>=3.6',
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Intended Audience :: Developers',
+        'Topic :: Software Development :: Libraries',
+        'Topic :: Software Development :: Libraries :: Application Frameworks',
+        'License :: OSI Approved :: MIT License',
+        'Framework :: Django',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3.9',
+        'Programming Language :: Python :: 3.10',
+        'Programming Language :: Python :: 3.11',
+        'Programming Language :: Python :: 3.12',
+    ],
+    project_urls={ # Optional
+        'Bug Reports': 'https://github.com/iamswaroopp/django-scaffold-generator/issues',
+    },
+    include_package_data=True,
+)
