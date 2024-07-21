f5869af7a5f0953bf26d2cdae4b5827bf85c45cb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-21 03:06:03 +0000

    Add package PySimpleGUI/4.70.1/PySimpleGUI-4.70.1.tar.gz

---
+++ packages/PySimpleGUI/PySimpleGUI-4.70.1.tar.gz/PySimpleGUI-4.70.1/setup.py
@@ -0,0 +1,48 @@
+import setuptools
+
+def readme():
+    try:
+        with open('README.md') as f:
+            return f.read()
+    except IOError:
+        return ''
+
+
+setuptools.setup(
+    name="PySimpleGUI",
+    version="4.70.1",
+    author="PySimpleGUI",
+    author_email="wroteit@PySimpleGUI.com",
+    description="Python GUIs for Humans. Launched in 2018. It's 2024 & PySimpleGUI is an ACTIVE project. Super-simple to create custom GUI's. 340+ Demo programs & Cookbook for rapid start. Extensive documentation.  Great for beginners & advanced GUI programmers.",
+    long_description=readme(),
+    long_description_content_type="text/markdown",
+    keywords="GUI UI tkinter Qt WxPython Remi wrapper simple easy beginner novice student graphics progressbar progressmeter NotDeadYet",
+    url="https://github.com/PySimpleGUI/PySimpleGUI",
+    packages=setuptools.find_packages(),
+    classifiers=(
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.4",
+        "Programming Language :: Python :: 3.5",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
+        "Programming Language :: Python :: 3.10",
+        "Programming Language :: Python :: 3.11",
+        "Programming Language :: Python :: 3.12",
+        "Programming Language :: Python :: 3.13",
+        "License :: OSI Approved :: GNU Lesser General Public License v3 or later (LGPLv3+)",
+        "Topic :: Multimedia :: Graphics",
+        "Operating System :: OS Independent",
+        "Framework :: PySimpleGUI",
+        "Framework :: PySimpleGUI :: 4",
+    ),
+    entry_points={
+        'gui_scripts': [
+            'psgissue=PySimpleGUI.PySimpleGUI:main_open_github_issue',
+            'psgmain=PySimpleGUI.PySimpleGUI:_main_entry_point',
+            'psghelp=PySimpleGUI.PySimpleGUI:main_sdk_help',
+            'psgver=PySimpleGUI.PySimpleGUI:main_get_debug_data',
+            'psgsettings=PySimpleGUI.PySimpleGUI:main_global_pysimplegui_settings',
+        ],},
+)
