9572358e5481222755a78f371c10f63a76419f4e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-21 03:06:06 +0000

    Add package Red-Web-Dashboard/1.6.7/red_web_dashboard-1.6.7.tar.gz

---
+++ packages/Red-Web-Dashboard/red_web_dashboard-1.6.7.tar.gz/red_web_dashboard-1.6.7/reddash/__init__.py
@@ -0,0 +1,10 @@
+"""
+License: Commercial
+Copyright (c) 2019 - present AppSeed.us
+Copyright (c) 2020 - present Neuro Assassin (https://github.com/Cog-Creators/Red-Dashboard)
+"""
+
+__version__ = "1.6.7"
+__authos__ = ["Neuro Assassin", "AAA3A"]
+
+from .app import FlaskApp
