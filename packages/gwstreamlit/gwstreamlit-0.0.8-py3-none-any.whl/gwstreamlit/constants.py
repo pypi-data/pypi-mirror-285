b4b6e98d91909423b236857c951c81f6ca18ac41
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-21 03:05:45 +0000

    Add package gwstreamlit/0.0.8/gwstreamlit-0.0.8-py3-none-any.whl

---
+++ packages/gwstreamlit/gwstreamlit-0.0.8-py3-none-any.whl/gwstreamlit/constants.py
@@ -0,0 +1,26 @@
+from enum import Enum
+
+
+class KeyType(Enum):
+    INPUT = "input"
+    BUTTON = "button"
+    TAB = "tab"
+    STORAGE = "storage"
+
+
+class ButtonVariantType(Enum):
+    primary = "primary"
+    secondary = "secondary"
+
+
+class ButtonLevel(Enum):
+    application = "application"
+    tab = "tab"
+
+
+DEFAULT_BUTTONS = [
+    {'label': 'Submit', 'on_click': 'gwstreamlit.utils:button_submit', 'type': 'submit', 'variant': 'primary'},
+    {'label': 'Cancel', 'on_click': 'gwstreamlit.utils:button_cancel', 'type': 'cancel', 'variant': 'secondary'}
+]
+
+YAML_UI_LOCATION = "./resources/yaml_ui"
